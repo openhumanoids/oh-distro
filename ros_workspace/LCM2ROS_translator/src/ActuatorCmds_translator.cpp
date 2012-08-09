278fb23446ca15664114662fce6cb0edc63d1683
Sisir Karumanchi <sisir@mit.edu>
2012-08-09 22:42:27 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@147 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/LCM2ROS_translator/src/ActuatorCmds_translator.cpp
+++ ros_workspace/LCM2ROS_translator/src/ActuatorCmds_translator.cpp
@@ -22,56 +22,74 @@ double getTime_now()
 class ActuatorCmdHandler{
 	private:
 		ros::Publisher actuator_cmd_pub;
-	public:
-	ActuatorCmdHandler()
-	{
-		//ros::init(argc,argv,"actuator_cmd_publisher");
 		ros::NodeHandle actuator_cmd_node;
-		actuator_cmd_pub = actuator_cmd_node.advertise<atlas_gazebo_msgs::ActuatorCmd>("actuator_cmd",10);
+	public:
+	ActuatorCmdHandler(ros::NodeHandle &node): actuator_cmd_node(node)	
+	{
+	 actuator_cmd_pub = actuator_cmd_node.advertise<atlas_gazebo_msgs::ActuatorCmd>("actuator_cmd",10);
 	}
+	~ActuatorCmdHandler() {}
+	
 	void actuator_cmd_Callback(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::actuator_cmd_t* msg)//what is rbuf and channel here?
 	{
 		atlas_gazebo_msgs::ActuatorCmd actuator_cmd_msg;
                 long t = msg->timestamp*1000; // from usec to nsec
 		actuator_cmd_msg.header.stamp.fromNSec(t);
 		actuator_cmd_msg.robot_name = msg->robot_name;
-		//actuator_cmd_msg.joint_name = new string[msg.num_joints];
-		//actuator_cmd_msg.joint_effort = new float[msg.num_joints]
 		for(std::vector<int>::size_type i=0;i!=msg->joint_name.size();i++){//So varaibles in lcm message are all in vector type
 			actuator_cmd_msg.joint_name.push_back(msg->joint_name.at(i));
 			actuator_cmd_msg.joint_effort.push_back(msg->joint_effort.at(i));
+			actuator_cmd_msg.duration.push_back(msg->duration.at(i));
 		}
 		if(ros::ok())
 		{
 			actuator_cmd_pub.publish(actuator_cmd_msg);
-			ros::spinOnce();
+			//ros::spinOnce(); // required?
 		}
-		//delete[] actuator_cmd_msg.joint_name;
-		//delete[] actuator_cmd_msg.joint_effort;
+
 	}
 };
 
 int main(int argc,char** argv)
 {
-	ros::init(argc,argv,"actuator_cmd_publisher");
-	lcm::LCM talker;
-	if(!talker.good())
-		return 1;
-	drc::actuator_cmd_t actuator_cmd;
-	actuator_cmd.timestamp = 0;
-	actuator_cmd.robot_name = "atlas";
-	actuator_cmd.num_joints = 2;
-	actuator_cmd.joint_name.push_back("hip");
-	actuator_cmd.joint_name.push_back("neck");
-	actuator_cmd.joint_effort.push_back(0.0);
-	actuator_cmd.joint_effort.push_back(1.0);
+	ros::init(argc,argv,"actuator_cmd_publisher",ros::init_options::NoSigintHandler);
+        ros::NodeHandle actuator_cmd_node;
+
 	lcm::LCM listener;
-	ActuatorCmdHandler handler;
-	listener.subscribe("ACTUATOR_CMDS",&ActuatorCmdHandler::actuator_cmd_Callback,&handler);
-	while(true)
-	{
-	        talker.publish("ACTUATOR_CMDS",&actuator_cmd);
-		listener.handle();
-	}
+	if(!listener.good())
+             return 1;
+	ActuatorCmdHandler handlerObject(actuator_cmd_node);
+	
+	//LCM subscription
+	listener.subscribe("ACTUATOR_CMDS",&ActuatorCmdHandler::actuator_cmd_Callback,&handlerObject);
+
+	while(0 == listener.handle());
 	return 0;
 }
+
+
+
+// 	lcm::LCM talker;
+// 	if(!talker.good())
+// 		return 1;
+// 	drc::actuator_cmd_t actuator_cmd;
+// 	actuator_cmd.timestamp = 0;
+// 	actuator_cmd.robot_name = "wheeled_atlas";
+// 	actuator_cmd.num_joints = 2;
+// 	actuator_cmd.joint_name.push_back("LShoulderRoll");
+// 	actuator_cmd.joint_name.push_back("RShoulderRoll");
+// 	actuator_cmd.joint_effort.push_back(12.0);
+// 	actuator_cmd.joint_effort.push_back(-12.0);
+// 	actuator_cmd.duration.push_back(0.1);
+// 	actuator_cmd.duration.push_back(0.1);
+// // 	lcm::LCM listener;
+// 	if(!listener.good())
+//              return 1;
+// 	ActuatorCmdHandler handler;
+// 	listener.subscribe("ACTUATOR_CMDS",&ActuatorCmdHandler::actuator_cmd_Callback,&handler);
+// 	while(true)
+// 	{
+// 	        talker.publish("ACTUATOR_CMDS",&actuator_cmd); 
+// 		listener.handle();
+// 	}
+// 	return 0;
