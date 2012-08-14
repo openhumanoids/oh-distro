1a8a60884da23734b122d0dd5eeeb3b16c780bef
Cecilia Cantu <ceci91@mit.edu>
2012-08-14 17:14:21 +0000

    Added 3 recorded trajectories and a program that runs them in a loop for swinging arm motion
    git-svn-id: https://svn.csail.mit.edu/drc@193 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runAtlasLCMVisualizer.m
+++ software/control/runAtlasLCMVisualizer.m
@@ -4,4 +4,4 @@ r = RigidBodyManipulator('../../ros_workspace/atlas_description/urdf/atlas_robot
 r = setSimulinkParam(r,'MinStep','0.001');
 v = r.constructVisualizer;
 
-runDRCLCMControl(v,'atlas','true_robot_state');
+runDRCControl(v,'atlas','TRUE_ROBOT_STATE');
---
+++ software/control/runReachingPlayback.m
@@ -0,0 +1,14 @@
+function runReachingPlayback
+
+load test2.mat
+load test.mat
+load test3.mat
+
+v2.playback(xtraj);
+while(1)
+    v.playback(xtraj2);
+    v3.playback(xtraj3);
+end
+
+end
+
