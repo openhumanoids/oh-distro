f244f8010d618e92c2dd620f877f3db7883453de
Russ Tedrake <russt@csail.mit.edu>
2012-08-15 13:53:40 +0000

    turned off cascade warning
    git-svn-id: https://svn.csail.mit.edu/drc@206 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runAtlasDynamics.m
+++ software/control/runAtlasDynamics.m
@@ -13,7 +13,9 @@ x0.RElbowPitch = -.2;
 if (1)
   % Run animation while it is simulating (as fast as possible, but probably
   % slower than realtime)
+  s = warning('off','Drake:DrakeSystem:UnsupportedSampleTime');  % we are knowingly breaking out to a simulink model with the cascade on the following line.
   sys = cascade(r,v);
+  warning(s);
   simulate(sys,[0 10],x0); 
 else
   % Run simulation, then play it back at realtime speed
