c63666e26829ae11df6fce7b2ca4b6ff237496ac
Hongkai Dai <hongkai@csail.mit.edu>
2012-08-20 20:44:01 +0000

    The pendulum swing up and balance works now
    git-svn-id: https://svn.csail.mit.edu/drc@224 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runPendulumControl.m
+++ software/control/runPendulumControl.m
@@ -5,16 +5,21 @@ r = setSimulinkParam(r,'MinStep','0.001');
 v = r.constructVisualizer;
 v.display_dt = 0.05;
 
-x0 = Point(r.getStateFrame());
-xf = x0;
-xf.theta = pi;
-xf.thetadot = 0;
-x0 = double(x0);
-xf = double(xf);
+x0_pt = Point(r.getStateFrame());
+xf_pt = x0_pt;
+xf_pt.theta = pi;
+xf_pt.thetadot = 0;
+x0 = double(x0_pt);
+xf = double(xf_pt);
 con.x0.lb = x0;
 con.x0.ub = x0;
 con.xf.lb = xf;
 con.xf.ub = xf;
+uf_pt = Point(r.getInputFrame());
+uf_pt.theta = 0;
+uf = double(uf_pt);
+con.uf.lb = uf;
+con.uf.ub = uf;
 tf0 = 1;
 con.T.lb = 0.5;
 con.T.ub = 2;
@@ -24,10 +29,30 @@ N = 11;
 utraj0 = PPTrajectory(foh(linspace(0,tf0,N),zeros(r.getNumInputs(),N)));
 options.MajorIterationsLimit = 50;
 [utraj,xtraj,info] = trajectoryOptimization(r,@cost,@finalcost,x0,utraj0,con,options);
-utraj = utraj.setOutputFrame(r.getInputFrame());
-options = struct('TSPAN',[0 5]);
-runDRCControl(utraj,'pendulum','TRUE_ROBOT_STATE','ACTUATOR_CMDS',options);
-keyboard;
+% utraj = utraj.setOutputFrame(r.getInputFrame());
+% utraj = utraj.setInputFrame(r.getOutputFrame());
+[ltvsys,V] = tvlqr(r,xtraj,utraj,eye(2),1,eye(2));
+c_tvlqr_y0 = FunctionHandleTrajectory(@(t) utraj.eval(t)-ltvsys.D.eval(t)*xtraj.eval(t),[1 1],utraj.getBreaks());
+c_tvlqr = AffineSystem([],[],[],[],[],[],[],ltvsys.D,c_tvlqr_y0);
+c_tvlqr = c_tvlqr.setOutputFrame(r.getInputFrame());
+c_tvlqr = c_tvlqr.setInputFrame(r.getOutputFrame());
+[c_tilqr,V_tilqr] = tilqr(r,xf_pt,uf_pt,eye(2),1);
+c_tilqr = AffineSystem([],[],[],[],[],[],[],c_tilqr.D,-c_tilqr.D*xf);
+c_tilqr = c_tilqr.setOutputFrame(r.getInputFrame());
+c_tilqr = c_tilqr.setInputFrame(r.getOutputFrame());
+% c = HybridDrakeSystem(2,1);
+% c = c.setInputFrame(c_tvlqr.getInputFrame());
+% c = c.setOutputFrame(c_tvlqr.getOutputFrame());
+% [c,mode1] = c.addMode(c_tvlqr,'tvlqr');
+% [c,mode2] = c.addMode(c_tilqr,'tilqr');
+% c = c.addTransition(mode1,@(obj,t,x,u)tilqr_guard(obj,t,x,u,V_tilqr),...
+%     @(obj,t,x,u) tilqr_transition(obj,mode1,t,x,u,mode2),false,true,mode2);
+% ltvsys = ltvsys.setInputFrame(r.getOutputFrame());
+% ltvsys = ltvsys.setOutputFrame(r.getInputFrame());
+options1 = struct('tspan',utraj.tspan);
+runDRCControl(c_tvlqr,'Pendulum','TRUE_ROBOT_STATE','ACTUATOR_CMDS',options1);
+options2 = struct('tspan',[utraj.tspan(2) inf]);
+runDRCControl(c_tilqr,'Pendulum','TRUE_ROBOT_STATE','ACTUATOR_CMDS',options2);
 end
 
 function [g,dg] = cost(t,x,u);
@@ -39,4 +64,14 @@ end
 function [h,dh] = finalcost(t,x)
 h = t;
 dh = [1,zeros(1,size(x,1))];
+end
+
+function g = tilqr_guard(obj,t,x,u,V_tilqr)
+g = (u-[pi;0])'*V_tilqr*(u-[pi;0])-1;
+end
+
+function [xp,mode,status] = tilqr_transition(obj,mode,t,xm,u,to_mode)
+xp = xm;
+mode = to_mode;
+status = 0;
 end
--- software/control/util/runDRCControl.m
+++ software/control/util/runDRCControl.m
@@ -11,7 +11,7 @@ if(~isfield(options,'tspan'))
     options.tspan = [0,inf];
 end
 
-if (sys.getNumStates()~=0)
+if (sys.getNumContStates()~=0)
   error('not implemented yet');  % but won't be too hard
 end
 
@@ -20,7 +20,8 @@ joint_name = sys.getInputFrame.coordinates(1:ndof);
 state_listener = RobotStateListener(robot_name,joint_name,state_channel);
 
 if (getNumOutputs(sys)>0)
-    cmd_names=cellfun(@(a)a(1:end-6),sys.getOutputFrame().coordinates,'UniformOutput',false);  % temporary hack: remove the string "Torque" from the end of each string    
+    cmd_names = sys.getOutputFrame().coordinates;
+    cmd_names = regexprep(cmd_names,'Torque',''); % temporary hack: remove the string "Torque" from each string    
     cmd_publisher = ActuatorCmdPublisher(robot_name,cmd_names,cmd_channel);
 end
 
@@ -29,12 +30,16 @@ global g_scope_enable; g_scope_enable = true;
 % just run as fast as possible
 t=options.tspan(1); tic;
 while (t<=options.tspan(2))
-  x = getNextMessage(state_listener,1000);
+  x = getNextMessage(state_listener,100);
   if (~isempty(x))
     u = sys.output(t,[],x);
+    fprintf('time is %f\n',t);
+    fprintf('state is %f\n',x);
+    fprintf('control is %f\n\n',u);
     if (getNumOutputs(sys)>0)
       cmd_publisher.publish(u);
     end
+    t = options.tspan(1)+toc;
   else
     t=options.tspan(1)+toc;
     fprintf(1,'waiting... (t=%f)\n',t);
