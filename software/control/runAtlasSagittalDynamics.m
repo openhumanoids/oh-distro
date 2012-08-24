8eda0940fe6b73ae80eb9e661ff4c522f1adbf6a
Russ Tedrake <russt@csail.mit.edu>
2012-08-24 13:32:22 +0000

    added contact model for planar case.  slow but cool
    git-svn-id: https://svn.csail.mit.edu/drc@237 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runAtlasSagittalDynamics.m
+++ software/control/runAtlasSagittalDynamics.m
@@ -2,16 +2,19 @@ function runAtlasSagittalDynamics
 
 % just runs it as a passive system for now
 
-r = PlanarRigidBodyManipulator('../../ros_workspace/atlas_description/urdf/atlas_robot.urdf');
+options.view = 'right';
+options.floating = true;
+m = PlanarRigidBodyModel('../../ros_workspace/atlas_description/urdf/atlas_robot.urdf',options);
+r = TimeSteppingRigidBodyManipulator(m,.01);
 r = setSimulinkParam(r,'MinStep','0.001');
 v = r.constructVisualizer;
 v.display_dt = .05;
 
-
 x0 = Point(r.getStateFrame);
 x0.RElbowPitch = -.2;
+x0 = resolveConstraints(r.manip,double(x0));
 
-if (1)
+if (0)
   % Run animation while it is simulating (as fast as possible, but probably
   % slower than realtime)
   s = warning('off','Drake:DrakeSystem:UnsupportedSampleTime');  % we are knowingly breaking out to a simulink model with the cascade on the following line.
@@ -20,6 +23,7 @@ if (1)
   simulate(sys,[0 10],x0); 
 else
   % Run simulation, then play it back at realtime speed
-  xtraj = simulate(r,[0 5],x0);
+  xtraj = simulate(r,[0 3],x0);
+  pause;
   v.playback(xtraj);
 end
