768f79dd2ab7f4c8c91302b944dcc80f0701d490
Russ Tedrake <russt@csail.mit.edu>
2012-08-26 15:00:38 +0000

    lcp dynamics for atlas model.  but it doesn't work well yet
    git-svn-id: https://svn.csail.mit.edu/drc@240 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runAtlasDynamics.m
+++ software/control/runAtlasDynamics.m
@@ -1,16 +1,18 @@
 function runAtlasDynamics
 
 % just runs it as a passive system for now
-
-r = RigidBodyManipulator('../../ros_workspace/atlas_description/urdf/atlas_robot.urdf');
-r = setSimulinkParam(r,'MinStep','0.001');
+options.floating = true;
+m = RigidBodyModel('../../ros_workspace/atlas_description/urdf/atlas_robot.urdf',options);
+%r = setSimulinkParam(r,'MinStep','0.001');
+r = TimeSteppingRigidBodyManipulator(m,.001);
 v = r.constructVisualizer;
 v.display_dt = .05;
 
 x0 = Point(r.getStateFrame);
 x0.RElbowPitch = -.2;
+x0 = r.manip.resolveConstraints(double(x0));
 
-if (1)
+if (0)
   % Run animation while it is simulating (as fast as possible, but probably
   % slower than realtime)
   s = warning('off','Drake:DrakeSystem:UnsupportedSampleTime');  % we are knowingly breaking out to a simulink model with the cascade on the following line.
@@ -19,6 +21,7 @@ if (1)
   simulate(sys,[0 10],x0); 
 else
   % Run simulation, then play it back at realtime speed
-  xtraj = simulate(r,[0 5],x0);
+  xtraj = simulate(r,[0 3],x0);
+  save falling.mat v xtraj;
   v.playback(xtraj);
 end
