c5b71fe0a905df0258419f7501fb4090fd766fc1
Hongkai Dai <hongkai@csail.mit.edu>
2012-09-06 17:12:29 +0000

    add LQR controller to runAtlasBalance, but the controller works only for the first second, and then fails when the contact breaks
    git-svn-id: https://svn.csail.mit.edu/drc@296 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/runAtlasBalance.m
+++ software/control/runAtlasBalance.m
@@ -8,31 +8,49 @@ v = r.constructVisualizer;
 v.display_dt = .05;
 
 x0 = Point(r.getStateFrame);
+% x0.RHipPitch = -0.2;
+% x0.LHipPitch = -0.2;
 x0.RKneePitch = 0.1;
 x0.LKneePitch = 0.1;
 x0.LAnklePitch = -0.1;
 x0.RAnklePitch = -0.1;
 x0.base_link_z = 1;
 u0 = Point(r.getInputFrame);
-if(nargin<1)
+x0 = xD;
+u0 = uD;
+if(nargin<3)
     [xD,uD] = computeInitialPosture(double(x0),double(u0),r);
+    xD = Point(r.getStateFrame,xD);
+    uD = Point(r.getInputFrame,uD);
 end
+% [xD,uD] = computeInitialPosture(double(x0),double(u0),r);
+[A,B] = linDynamics(r,double(xD),double(uD));
 num_q = r.getNumStates/2;
 num_u = r.getNumInputs;
-k_D = [-5*[zeros(num_u,num_q-num_u) eye(num_u)] -5*[zeros(num_u,num_q-num_u) eye(num_u)]];
-k_y0 = -k_D*xD+uD;
-control = AffineSystem([],[],[],[],[],[],[],k_D,k_y0);
+% k_D = [zeros(num_u,num_q-num_u) diag([-10 -1 -1 -1 -1 -1 -6 -6 -8 -6 -6 -8]) ...
+%     zeros(num_u,num_q-num_u) diag([-8 -2 -2 -2 -2 -2 -4 -4 -8 -4 -4 -8])];
+% %k_D = [-5*[zeros(num_u,num_q-num_u) eye(num_u)] -5*[zeros(num_u,num_q-num_u) eye(num_u)]];
+% k_y0 = -k_D*xD+uD;
+% control = AffineSystem([],[],[],[],[],[],[],k_D,k_y0);
+Q = diag([1 1 10 100 1 1 1 1 1 100 100 100 100 100 100 1 1 10 100 1 1 1 1 1 100 100 100 100 100 100]);
+R = eye(num_u);
+N = zeros(2*num_q,num_u);
+[K,S] = dlqr(A,B,Q,R,N);
+k_y0 = K*double(xD)+double(uD);
+control = AffineSystem([],[],[],[],[],[],[],-K,k_y0);
 control = control.setInputFrame(r.getOutputFrame);
 control = control.setOutputFrame(r.getInputFrame);
 r_cl = feedback(r,control);
 %r_cl = cascade(feedback(r,control),v);
 %simulate(r_cl,[0 1],x0);
-x0 = Point(r.getStateFrame,xD);
+x0 = xD;
 x0.RShoulderPitch = x0.RShoulderPitch;
 x0.LShoulderPitch = x0.LShoulderPitch;
-x0.BackPitch = x0.BackPitch+0.1;
-xtraj = simulate(r_cl,[0 2],xD);
-save PDcontrol.mat v xtraj;
+x0.BackPitch = x0.BackPitch;
+xtraj = simulate(r_cl,[0 2],x0);
+tBreaks = xtraj.getBreaks();
+utraj = control.output(tBreaks,[],xtraj);
+save LQRcontrol.mat v xtraj utraj;
 v.playback_speed = 0.2;
 v.playback(xtraj);
 
@@ -44,13 +62,39 @@ problem.objective = @(x) 0;
 problem.x0 = [x0;u0];
 problem.nonlcon = @(xu) mycon(xu,obj);
 problem.solver = 'fmincon';
-problem.options=optimset('Algorithm','interior-point','Display','off');
+problem.options=optimset('Algorithm','interior-point','Display','off','TolX',1e-6);
+problem.lb = [obj.manip.joint_limit_min;-inf(obj.manip.num_q,1);obj.manip.umin];
+problem.ub = [obj.manip.joint_limit_max;inf(obj.manip.num_q,1);obj.manip.umax];
+problem.ub(10) = -0.1;
+problem.lb(11) = 0.1;
+problem.ub(13) = -0.1;
+problem.lb(14) = 0.1;
 [xu_sol,~,exitflag] = fmincon(problem);
-success=(exitflag==1);
+%success=(exitflag==1);
 nX = obj.getNumStates();
 nU = obj.getNumInputs();
 xD = xu_sol(1:nX);
 uD = xu_sol(nX+(1:nU));
+% if (~success)
+%     error('Drake:PlanarRigidBodyManipulator:ResolveConstraintsFailed','failed to resolve constraints');
+% end
+% check the solution by myself
+if(exitflag==1)
+    success = 1;
+else
+    if(any(xu_sol-problem.lb<0)||any(xu_sol-problem.ub>0))
+        success = 0;
+    else
+        [c,ceq] = problem.nonlcon(xu_sol);
+        if(any(c>0))
+            success = 0;
+        elseif(max(abs(ceq)>1e-2))
+            success = 0;
+        else
+            success = 1;
+        end
+    end
+end
 if (~success)
     error('Drake:PlanarRigidBodyManipulator:ResolveConstraintsFailed','failed to resolve constraints');
 end
@@ -70,5 +114,26 @@ u = xu(nX+(1:nU));
 q = x(1:nq);
 contactCandidate = contactConstraints(manipObj,q);
 c = -[jointLimits(manipObj,q);contactCandidate(~footContactInd)];
-ceq = [stateConstraints(manipObj,x);x-update(obj,0,x,u);contactCandidate(footContactInd)];
+ceq = [stateConstraints(manipObj,x);...
+    x-update(obj,0,x,u);...
+    contactCandidate(footContactInd)];
+end
+
+function [A,B] = linDynamics(sys,xD,uD)
+nX = length(xD);
+nU = length(uD);
+A = zeros(nX);
+B = zeros(nX,nU);
+for i = 1:nX
+    x_ei = zeros(nX,1);
+    x_err = 1e-5;
+    x_ei(i) = x_err;
+    A(:,i) = (sys.update(0,xD+x_ei,uD)-xD)/x_err;
+end 
+for i = 1:nU
+    u_ei = zeros(nU,1);
+    u_err = 1e-5;
+    u_ei(i) = u_err;
+    B(:,i) = (sys.update(0,xD,uD+u_ei)-xD)/u_err;
+end
 end
