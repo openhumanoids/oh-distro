35c734af87722c8934a363e03b05ec43f91d3d78
Robin Deits <rdeits@csail.mit.edu>
2012-09-25 19:15:15 +0000

    added files for simulating the compass tripod (.urdf is in a temporary location)
    git-svn-id: https://svn.csail.mit.edu/drc@398 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/runCompassTripodSagittalDynamics.m
@@ -0,0 +1,28 @@
+function runCompassTripodSagittalDynamics
+
+% just runs it as a passive system for now
+
+options.view = 'right';
+options.floating = true;
+m = PlanarRigidBodyModel('compass_tripod_pneumo.urdf',options);
+r = TimeSteppingRigidBodyManipulator(m,.01);
+r = setSimulinkParam(r,'MinStep','0.001');
+v = r.constructVisualizer;
+v.display_dt = .05;
+
+x0 = Point(r.getStateFrame);
+x0 = resolveConstraints(r.manip,double(x0));
+
+if (0)
+  % Run animation while it is simulating (as fast as possible, but probably
+  % slower than realtime)
+  s = warning('off','Drake:DrakeSystem:UnsupportedSampleTime');  % we are knowingly breaking out to a simulink model with the cascade on the following line.
+  sys = cascade(r,v);
+  warning(s);
+  simulate(sys,[0 10],x0); 
+else
+  % Run simulation, then play it back at realtime speed
+  xtraj = simulate(r,[0 3],x0);
+  save saggital.mat v xtraj;
+  v.playback(xtraj);
+end
