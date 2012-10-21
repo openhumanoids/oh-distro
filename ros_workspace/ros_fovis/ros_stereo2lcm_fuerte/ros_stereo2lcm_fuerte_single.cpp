060d59c99edd8c8f293a0128cc501a5b3a02b30c
Robin Deits <rdeits@csail.mit.edu>
2012-10-21 01:55:34 +0000

    minor stuff
    git-svn-id: https://svn.csail.mit.edu/drc@501 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/ros_fovis/ros_stereo2lcm_fuerte/ros_stereo2lcm_fuerte_single.cpp
+++ ros_workspace/ros_fovis/ros_stereo2lcm_fuerte/ros_stereo2lcm_fuerte_single.cpp
@@ -25,6 +25,7 @@
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/tfMessage.h>
 #include <rosgraph_msgs/Clock.h>
@@ -46,8 +47,10 @@ class App{
 
     lcm_t* get_lcmref(){return lcmref_;}
 
-    double left_time;
-    double right_time;
+    double left_image_time;
+    double right_image_time;
+    double left_info_time;
+    double right_info_time;
 
   private:
     lcm_t* lcmref_ ;
@@ -59,15 +62,22 @@ class App{
 
     ros::Subscriber tilt_scan_sub_, base_scan_sub_, odom_sub_, tf_sub_;
     ros::Subscriber clock_sub_,left_image_sub_,right_image_sub_,points_sub_;
+    ros::Subscriber left_info_sub_, right_info_sub_;
     void left_image_cb(const sensor_msgs::ImageConstPtr& msg);
     void right_image_cb(const sensor_msgs::ImageConstPtr& msg);
+    void left_info_cb(const sensor_msgs::CameraInfoConstPtr& msg);
+    void right_info_cb(const sensor_msgs::CameraInfoConstPtr& msg);
 };
 
 App::App(){
   lcmref_ = lcm_create(NULL);
 
-  left_image_sub_ = node_.subscribe(string("/wide_stereo/left/image_raw"), 10, &App::left_image_cb,this);
-  right_image_sub_ = node_.subscribe(string("/wide_stereo/right/image_raw"), 10, &App::right_image_cb,this);
+  left_info_sub_ = node_.subscribe(string("/camera/left/camera_info"), 10, &App::left_info_cb,this);
+  right_info_sub_ = node_.subscribe(string("/camera/right/camera_info"), 10, &App::right_info_cb,this);
+
+
+  left_image_sub_ = node_.subscribe(string("/camera/left/image_raw"), 10, &App::left_image_cb,this);
+  right_image_sub_ = node_.subscribe(string("/camera/right/image_raw"), 10, &App::right_image_cb,this);
 };
 
 App::~App()  {
@@ -79,19 +89,26 @@ App::~App()  {
 // points: xyz
 // channels: rgb u v
 void App::left_image_cb(const sensor_msgs::ImageConstPtr& msg){
-  left_time = msg->header.stamp.toSec();
+  left_image_time = msg->header.stamp.toSec();
 //  std::cout << "here right\n";
-  double diff =  right_time - left_time;
+  double diff =  right_image_time - left_image_time;
   std::cout << "L: "<< msg->header.stamp.toSec() << " " << diff  << " [RAW]\n";
-
 }
 void App::right_image_cb(const sensor_msgs::ImageConstPtr& msg){
-  right_time = msg->header.stamp.toSec();
-//  std::cout << "here right\n";
-  double diff =  right_time - left_time;
+  right_image_time = msg->header.stamp.toSec();
+  double diff =  right_image_time - left_image_time;
   std::cout << "R: "<<msg->header.stamp.toSec() << " " << diff  << " [RAW]\n";
+}
 
-
+void App::left_info_cb(const sensor_msgs::CameraInfoConstPtr& msg){
+  left_info_time = msg->header.stamp.toSec();
+  double diff =  right_info_time - left_info_time;
+  std::cout << "L: "<<msg->header.stamp.toSec() << " " << diff  << " [INFO]\n";
+}
+void App::right_info_cb(const sensor_msgs::CameraInfoConstPtr& msg){
+  right_info_time = msg->header.stamp.toSec();
+  double diff =  right_info_time - left_info_time;
+  std::cout << "R: "<<msg->header.stamp.toSec() << " " << diff  << " [INFO]\n";
 }
 
 
