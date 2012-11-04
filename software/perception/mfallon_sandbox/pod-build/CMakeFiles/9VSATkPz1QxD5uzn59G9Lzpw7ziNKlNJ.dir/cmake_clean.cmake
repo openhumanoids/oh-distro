8ea0a7e5e4a60c82310d82a5f97ca3a29152f7e8
Maurice Fallon <mfallon@csail.mit.edu>
2012-11-04 15:20:58 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@555 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/mfallon_sandbox/pod-build/CMakeFiles/9VSATkPz1QxD5uzn59G9Lzpw7ziNKlNJ.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/9VSATkPz1QxD5uzn59G9Lzpw7ziNKlNJ"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/9VSATkPz1QxD5uzn59G9Lzpw7ziNKlNJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- software/perception/mfallon_sandbox/pod-build/CMakeFiles/X9tzliiofOFqqHoatP1zcJxWW7TNE57d.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/X9tzliiofOFqqHoatP1zcJxWW7TNE57d"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/X9tzliiofOFqqHoatP1zcJxWW7TNE57d.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
