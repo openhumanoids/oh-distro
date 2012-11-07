b10d3c064654a3b90d3ae8f990d57e41dd58940b
Maurice Fallon <mfallon@csail.mit.edu>
2012-11-07 00:51:37 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@574 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/gazebo_plugins/gazebo_plugin/lcmtypes/c/lcmtypes/gazebo_plugin.h
+++
@@ -1,6 +0,0 @@
-#ifndef __lcmtypes_gazebo_plugin_h__
-#define __lcmtypes_gazebo_plugin_h__
-
-#include "exlcm_example_t.h"
-
-#endif
--- software/gazebo_plugins/gazebo_plugin/lcmtypes/cpp/lcmtypes/gazebo_plugin.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __lcmtypes_gazebo_plugin_hpp__
-#define __lcmtypes_gazebo_plugin_hpp__
-
-#include "exlcm/example_t.hpp"
-
-#endif
