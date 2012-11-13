31b97a0bc438708951b747f7e34ce8cde028f6ff
Mathew Antone <antone@csail.mit.edu>
2012-11-13 15:14:41 +0000

    added command line switches to delta send/receive apps
    git-svn-id: https://svn.csail.mit.edu/drc@641 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/maps/src/apps/BuilderOperatorMain.cpp
+++ software/perception/world_model/maps/src/apps/BuilderOperatorMain.cpp
@@ -5,12 +5,10 @@
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 #include <lcm/lcm-cpp.hpp>
-#include <lcmtypes/drc/heightmap_t.hpp>
 #include <lcmtypes/drc/local_map_t.hpp>
 #include <bot_core/timestamp.h>
 
-#include <bot_lcmgl_client/lcmgl.h>
-#include <GL/gl.h>
+#include <ConciseArgs>
 
 using namespace std;
 
@@ -19,17 +17,16 @@ public:
   boost::shared_ptr<DeltaReceiver> mDeltaReceiver;
   boost::shared_ptr<MapManager> mManager;
   boost::shared_ptr<lcm::LCM> mLcm;
-  bot_lcmgl_t* mLcmGl;
 
   State() {
     mDeltaReceiver.reset(new DeltaReceiver());
     mManager.reset(new MapManager());
     mLcm.reset(new lcm::LCM());
-    mLcmGl = bot_lcmgl_init(mLcm->getUnderlyingLCM(), "map-debug");
+    mDeltaReceiver->setManager(mManager);
+    mDeltaReceiver->setLcm(mLcm);
   }
 
   ~State() {
-    bot_lcmgl_destroy(mLcmGl);
   }
 };
 
@@ -46,6 +43,7 @@ public:
       timer.expires_from_now(boost::posix_time::seconds(5));
       timer.wait();
 
+      // publish map
       boost::shared_ptr<LocalMap> localMap = mState->mManager->getActiveMap();
       if (localMap == NULL) {
         continue;
@@ -62,59 +60,11 @@ public:
       cout << "Published map (" << bytes.size() << " bytes)" << endl;
 
       // publish as octomap (for debugging)
-      octomap::raw_t raw = mState->mManager->getActiveMap()->getAsRaw();
+      octomap::raw_t raw = localMap->getAsRaw();
       raw.utime = bot_timestamp_now();
       std::cout << "Publishing debug octomap (" << raw.length <<
         " bytes)..." << std::endl;
       mState->mLcm->publish("OCTOMAP", &raw);
-
-      // write pgm height map (for debugging)
-      LocalMap::HeightMap heightMap =
-        mState->mManager->getActiveMap()->getAsHeightMap(3, 3.5);
-      std::ofstream ofs("/home/antone/heightmap.txt");
-      for (int i = 0; i < heightMap.mHeight; ++i) {
-        for (int j = 0; j < heightMap.mWidth; ++j) {
-          ofs << heightMap.mData[i*heightMap.mWidth+j] << " ";
-        }
-        ofs << std::endl;
-      }
-      std::cout << "Wrote debug height map" << std::endl;
-
-      // publish height map via lcm
-      drc::heightmap_t heightMapMsg;
-      heightMapMsg.utime = bot_timestamp_now();
-      heightMapMsg.nx = heightMap.mWidth;
-      heightMapMsg.ny = heightMap.mHeight;
-      heightMapMsg.npix = heightMapMsg.nx * heightMapMsg.ny;
-      Eigen::Vector3d p0 = heightMap.mTransformToLocal*Eigen::Vector3d(0,0,0);
-      Eigen::Vector3d px = heightMap.mTransformToLocal*Eigen::Vector3d(1,0,0);
-      Eigen::Vector3d py = heightMap.mTransformToLocal*Eigen::Vector3d(0,1,0);
-      heightMapMsg.scale_x = (px-p0).norm();
-      heightMapMsg.scale_y = (py-p0).norm();
-      for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-          heightMapMsg.transform_to_local[i][j] =
-            heightMap.mTransformToLocal(i,j);
-        }
-      }
-      heightMapMsg.heights = heightMap.mData;
-      mState->mLcm->publish("HEIGHT_MAP", &heightMapMsg);
-
-      if (false) {
-        bot_lcmgl_t* lcmgl = mState->mLcmGl;
-        bot_lcmgl_color3f(lcmgl, 1.0f, 0.5f, 0.0f);
-        bot_lcmgl_point_size(lcmgl, 3.0f);
-        bot_lcmgl_begin(lcmgl, GL_POINTS);
-        LocalMap::PointCloud::Ptr cloud =
-          mState->mManager->getActiveMap()->getAsPointCloud();
-        cout << "lcmgl'ing " << cloud->size() << " points..." << endl;
-        for (int k = 0; k < cloud->size(); ++k) {
-          bot_lcmgl_vertex3f(lcmgl, cloud->points[k].x, cloud->points[k].y,
-                             cloud->points[k].z);
-        }
-        bot_lcmgl_end(lcmgl);
-        bot_lcmgl_switch_buffer(lcmgl);
-      }
     }
   }
 
@@ -123,23 +73,32 @@ protected:
 };
 
 int main(const int iArgc, const char** iArgv) {
+  // create state instance
   State state;
 
-  if (!state.mLcm->good()) {
-    cerr << "Cannot create lcm instance." << endl;
-    return -1;
-  }
+  // parse command line options
+  string paramsChannel = "MAP_PARAMS";
+  string updateChannel = "MAP_UPDATE";
+  string ackChannel = "MAP_ACK";
+  ConciseArgs opt(iArgc, (char**)iArgv);
+  opt.add(paramsChannel, "c", "create_channel",
+          "channel for receiving map create messages");
+  opt.add(paramsChannel, "u", "update_channel",
+          "channel for receiving map update messages");
+  opt.add(paramsChannel, "a", "ack_channel",
+          "channel for sending ack messages");
+  state.mDeltaReceiver->setParamsChannel(paramsChannel);
+  state.mDeltaReceiver->setUpdateChannel(updateChannel);
+  state.mDeltaReceiver->setAckChannel(ackChannel);
 
-  state.mManager->setMapResolution(0.01);
-  state.mManager->setMapDimensions(Eigen::Vector3d(1e20, 1e20, 1e20));
-
-  state.mDeltaReceiver->setLcm(state.mLcm);
-  state.mDeltaReceiver->setManager(state.mManager);
+  // start up receiver
   state.mDeltaReceiver->start();
 
+  // start up publisher thread
   MapPublisher mapPublisher(&state);
   boost::thread thread(boost::ref(mapPublisher));
 
+  // main lcm loop
   while (0 == state.mLcm->handle());
 
   state.mDeltaReceiver->stop();
--- software/perception/world_model/maps/src/apps/BuilderRobotMain.cpp
+++ software/perception/world_model/maps/src/apps/BuilderRobotMain.cpp
@@ -6,7 +6,11 @@
 #include <lcm/lcm-cpp.hpp>
 #include <boost/thread.hpp>
 
+#include <lcmtypes/drc/map_params_t.hpp>
+
 #include <pcl/common/transforms.h>
+
+#include <ConciseArgs>
 
 using namespace std;
 
@@ -15,11 +19,51 @@ public:
   boost::shared_ptr<SensorDataReceiver> mSensorDataReceiver;
   boost::shared_ptr<DeltaPublisher> mDeltaPublisher;
   boost::shared_ptr<MapManager> mManager;
+  boost::shared_ptr<lcm::LCM> mLcm;
+  bool mTraceRays;
+  string mMapParamsChannel;
+  lcm::Subscription* mParamsSubscription;
 
   State() {
     mSensorDataReceiver.reset(new SensorDataReceiver());
     mDeltaPublisher.reset(new DeltaPublisher());
     mManager.reset(new MapManager());
+    mLcm.reset(new lcm::LCM());
+    mSensorDataReceiver->setLcm(mLcm);
+    mDeltaPublisher->setManager(mManager);
+    mDeltaPublisher->setLcm(mLcm);
+
+    // defaults; should be set by command line args in main()
+    mTraceRays = false;
+    mMapParamsChannel = "MAP_CREATE";
+    mParamsSubscription = NULL;
+  }
+
+  ~State() {
+    mLcm->unsubscribe(mParamsSubscription);
+  }
+
+  void onMapParams(const lcm::ReceiveBuffer* iBuf,
+                   const std::string& iChannel,
+                   const drc::map_params_t* iMessage) {
+    std::cout << "Received map parameters; creating new map" << std::endl;
+    mManager->setMapResolution(iMessage->resolution);
+    mManager->setMapDimensions(Eigen::Vector3d(iMessage->dimensions[0],
+                                               iMessage->dimensions[1],
+                                               iMessage->dimensions[2]));
+    Eigen::Isometry3d xform = Eigen::Isometry3d::Identity();
+    Eigen::Quaterniond quat;
+    quat.x() = iMessage->transform_to_local.rotation.x;
+    quat.y() = iMessage->transform_to_local.rotation.y;
+    quat.z() = iMessage->transform_to_local.rotation.z;
+    quat.w() = iMessage->transform_to_local.rotation.w;
+    Eigen::Vector3d trans;
+    trans[0] = iMessage->transform_to_local.translation.x;
+    trans[1] = iMessage->transform_to_local.translation.y;
+    trans[2] = iMessage->transform_to_local.translation.z;
+    xform.rotate(quat);
+    xform.translate(trans);
+    mManager->createMap(xform, iMessage->map_id);
   }
 };
 
@@ -27,7 +71,6 @@ class DataConsumer {
 public:
   DataConsumer(State* iState) {
     mState = iState;
-    mCounter = 0;
   }
 
   void operator()() {
@@ -36,57 +79,81 @@ public:
       if (mState->mSensorDataReceiver->waitForData(data)) {
         mState->mManager->addToBuffer(data.mTimestamp, data.mPointCloud,
                                       data.mPose);
-        // TODO: should be true (ray trace) but very inefficient
-        mState->mManager->getActiveMap()->add(data.mPointCloud,
-                                              data.mPose, false);
+        mState->mManager->getActiveMap()->add(data.mPointCloud, data.mPose,
+                                              mState->mTraceRays);
       }
     }
   }
 
 protected:
   State* mState;
-  int mCounter;
 };
 
 int main(const int iArgc, const char** iArgv) {
+  // instantiate state object
   State state;
 
-  boost::shared_ptr<lcm::LCM> theLcm(new lcm::LCM());
-  if (!theLcm->good()) {
-    cerr << "Cannot create lcm instance." << endl;
-    return -1;
-  }
-
-  BotParam* theParam = bot_param_new_from_server(theLcm->getUnderlyingLCM(), 0);
-
-  state.mSensorDataReceiver->setLcm(theLcm);
-  state.mSensorDataReceiver->setBotParam(theParam);
-  state.mSensorDataReceiver->setMaxBufferSize(100);
+  // parse arguments
+  double mapResolution = 0.02;
+  string laserChannel = "ROTATING_SCAN";
+  double xDim(10), yDim(10), zDim(10);
+  int publishPeriod(3000);
+  string paramsChannel = "MAP_PARAMS";
+  string updateChannel = "MAP_UPDATE";
+  string ackChannel = "MAP_ACK";
+  ConciseArgs opt(iArgc, (char**)iArgv);
+  opt.add(laserChannel, "l", "laser_channel",
+          "laser channel to use in map creation");
+  opt.add(xDim, "x", "xsize", "size of map in x direction");
+  opt.add(yDim, "y", "ysize", "size of map in y direction");
+  opt.add(zDim, "z", "zsize", "size of map in z direction");
+  opt.add(state.mTraceRays, "t", "raytrace",
+          "use raytracing when creating map");
+  opt.add(mapResolution, "r", "resolution",
+          "size of smallest (leaf) octree nodes");
+  opt.add(publishPeriod, "p", "publish_period",
+          "interval between delta publications, in ms");
+  opt.add(paramsChannel, "c", "create_channel",
+          "channel for publishing map create messages");
+  opt.add(updateChannel, "u", "update_channel",
+          "channel for publishing map update messages");
+  opt.add(ackChannel, "a", "ack_channel",
+          "channel for receiving ack messages");
+  opt.parse();
   state.mSensorDataReceiver->
-    addChannel("ROTATING_SCAN",
+    addChannel(laserChannel,
                SensorDataReceiver::SensorTypePlanarLidar,
-               "ROTATING_SCAN", "local");
-  /*
-    addChannel("WIDE_STEREO_POINTS",
-               SensorDataReceiver::SensorTypePointCloud,
-               "CAMERA", "local");
-  */
-  state.mSensorDataReceiver->start();
+               laserChannel, "local");
+  state.mManager->setMapResolution(mapResolution);
+  state.mManager->setMapDimensions(Eigen::Vector3d(xDim, yDim, zDim));
+  state.mDeltaPublisher->setPublishInterval(publishPeriod);
+  state.mDeltaPublisher->setParamsChannel(paramsChannel);
+  state.mDeltaPublisher->setUpdateChannel(updateChannel);
+  state.mDeltaPublisher->setAckChannel(ackChannel);
 
-  state.mManager->setMapResolution(0.01);
-  state.mManager->setMapDimensions(Eigen::Vector3d(10,10,10));
+  // set up remaining parameters
+  state.mSensorDataReceiver->setMaxBufferSize(100);
   state.mManager->setDataBufferLength(1000);
   state.mManager->createMap(Eigen::Isometry3d::Identity());
+  state.mParamsSubscription =
+    state.mLcm->subscribe(state.mMapParamsChannel,
+                          &State::onMapParams, &state);
 
-  state.mDeltaPublisher->setPublishInterval(5000);
-  state.mDeltaPublisher->setManager(state.mManager);
-  state.mDeltaPublisher->setLcm(theLcm);
+  // start running data receiver
+  BotParam* theParam =
+    bot_param_new_from_server(state.mLcm->getUnderlyingLCM(), 0);
+  state.mSensorDataReceiver->setBotParam(theParam);
+  state.mSensorDataReceiver->start();
+
+  // start running the delta publisher
   state.mDeltaPublisher->start();
 
+  // start consuming data
   DataConsumer consumer(&state);
   boost::thread thread(consumer);
-                                       
-  while (0 == theLcm->handle());
+
+  // main lcm loop
+  while (0 == state.mLcm->handle());
 
   state.mDeltaPublisher->stop();
   thread.join();
