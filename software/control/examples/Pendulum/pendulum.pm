ede66c49be943285554098df4186ff0115eddf43
Russ Tedrake <russt@csail.mit.edu>
2012-12-16 02:32:19 +0000

    adding drc-relevant examplesdir
    git-svn-id: https://svn.csail.mit.edu/drc@829 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/examples/Pendulum/pendulum.pm
@@ -0,0 +1,24 @@
+group "Drake-only" {
+  cmd "PendulumEnergyControl" {
+      exec = "matlab -nosplash -nodesktop -r \"cd([getenv('HOME'),'/locomotion/drake/examples/Pendulum']); c=PendulumEnergyControl(setInputLimits(PendulumPlant,-inf,inf)); runLCM(c);\"";
+      host = "localhost";
+  }
+  cmd "PendulumVisualizer" {
+      exec = "matlab -nosplash -nodesktop -r \"cd([getenv('HOME'),'/locomotion/drake/examples/Pendulum']); v=PendulumVisualizer(); runLCM(v);\"";
+      host = "localhost";
+  }
+  cmd "PendulumPlant" {
+      exec = "matlab -nosplash -nodesktop -r \"cd([getenv('HOME'),'/locomotion/drake/examples/Pendulum']); p=PendulumPlant(); runLCM(p);\" ";
+      host = "localhost";
+  }
+}
+group "DRC viewer" {
+    cmd "viewer" {
+	exec = "drc-viewer -c drc_robot.cfg";
+	host = "localhost";
+    }
+    cmd "urdf publisher" {
+	exec = "robot_model_publisher Pendulum.urdf";
+	host = "localhost";
+    }
+}
