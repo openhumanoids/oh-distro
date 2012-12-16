055b26bd08cbba3bf861d994ded36bfeb4dd43bc
Russ Tedrake <russt@csail.mit.edu>
2012-12-16 08:47:05 +0000

    drake-only plant / visualizer work with drc state messages
    git-svn-id: https://svn.csail.mit.edu/drc@833 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/examples/Pendulum/pendulum.pm
+++ software/control/examples/Pendulum/pendulum.pm
@@ -1,24 +1,27 @@
 group "Drake-only" {
-  cmd "PendulumEnergyControl" {
-    exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/examples/Pendulum']); c=PendulumEnergyControl(setInputLimits(PendulumPlant,-inf,inf)); runLCM(c);\"";
-    host = "localhost";
-  }
-  cmd "PendulumVisualizer" {
-    exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/locomotion/drake/examples/Pendulum']); v=PendulumVisualizer(); runLCM(v);\"";
-    host = "localhost";
-  }
-  cmd "PendulumPlant" {
-    exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/examples/Pendulum']); p=PendulumPlant(); runLCM(p);\" ";
-    host = "localhost";
-  }
+    cmd "PendulumEnergyControl" {
+        exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/examples/Pendulum']); c=PendulumEnergyControl(setInputLimits(PendulumPlant,-inf,inf)); runLCM(c);\"";
+        host = "localhost";
+    }
+    cmd "PendulumPlant" {
+        exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/examples/Pendulum']); p=PendulumPlant(); runLCM(p);\" ";
+        host = "localhost";
+    }
+    cmd "PendulumVisualizer" {
+        exec = "matlab -nosplash -nodesktop -r \"addpath([getDrakePath(),'/examples/Pendulum']); v=PendulumVisualizer(); runLCM(v);\"";
+        host = "localhost";
+    }
 }
+
 group "DRC viewer" {
-  cmd "viewer" {
-	  exec = "drc-viewer -c drc_robot.cfg";
-	  host = "localhost";
-  }
-  cmd "urdf publisher" {
-	  exec = "robot_model_publisher Pendulum.urdf";
-	  host = "localhost";
-  }
+    cmd "viewer" {
+        exec = "drc-viewer -c drc_robot.cfg";
+        host = "localhost";
+    }
+    cmd "urdf publisher" {
+        exec = "robot_model_publisher ../../drake/examples/Pendulum/Pendulum.urdf";
+        host = "localhost";
+    }
 }
+
+
--- software/control/util/RobotStateListener.java
+++ software/control/util/RobotStateListener.java
@@ -32,97 +32,116 @@ public class RobotStateListener implements LCMSubscriber
     
     public synchronized void messageReceived(LCM lcm, String channel, LCMDataInputStream dins) 
     {
-        int index;
-        try { 
-            drc.robot_state_t msg = new drc.robot_state_t(dins);
-            if (msg.robot_name.equals(m_robot_name)) {
-                m_timestamp = (double)msg.utime / 100000.0;
-                for (int i=0; i<msg.num_joints; i++) {
-                    Integer j = m_joint_map.get(msg.joint_name[i]);
-                    if (j!=null) {
-                        index = j.intValue();
-                        m_x[index] = msg.joint_position[i];
-                        m_x[index + m_num_joints] = msg.joint_velocity[i];
-                    }
-                }
-                
-                // get body position and orientation
-                index = m_joint_map.get("base_x").intValue();
-                m_x[index] = msg.origin_position.translation.x;
-                m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.x;
-                index = m_joint_map.get("base_y").intValue();
-                m_x[index] = msg.origin_position.translation.y;
-                m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.y;
-                index = m_joint_map.get("base_z").intValue();
-                m_x[index] = msg.origin_position.translation.z;
-                m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.z;
-                
-                // convert quaternion to euler
-                double x = msg.origin_position.rotation.x;
-                double y = msg.origin_position.rotation.y;
-                double z = msg.origin_position.rotation.z;
-                double w = msg.origin_position.rotation.w;
-
-                index = m_joint_map.get("base_roll").intValue();
-                m_x[index] = Math.atan2(2*(x*y + z*w),1-2*(y*y+z*z));
-                m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.x;
-
-                index = m_joint_map.get("base_pitch").intValue();
-                m_x[index] = Math.asin(2*(x*z - w*y));
-                m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.y;
-
-                index = m_joint_map.get("base_yaw").intValue();
-                m_x[index] = Math.atan2(2*(x*w + y*z),1-2*(z*z+w*w))+Math.PI;
-                if (m_x[index] > Math.PI)
-                   m_x[index] -= 2*Math.PI;
-                m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.z;
-                
-                m_has_new_message = true;
+      Integer j;
+      int index;
+      try {
+        drc.robot_state_t msg = new drc.robot_state_t(dins);
+        if (msg.robot_name.equals(m_robot_name)) {
+          m_timestamp = (double)msg.utime / 100000.0;
+          for (int i=0; i<msg.num_joints; i++) {
+            j = m_joint_map.get(msg.joint_name[i]);
+            if (j!=null) {
+              index = j.intValue();
+              m_x[index] = msg.joint_position[i];
+              m_x[index + m_num_joints] = msg.joint_velocity[i];
             }
-        } catch (IOException ex) {
-            System.out.println("Exception: " + ex);
+          }
+          
+          // get body position and orientation
+          j = m_joint_map.get("base_x");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = msg.origin_position.translation.x;
+            m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.x;
+          }
+          j = m_joint_map.get("base_y");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = msg.origin_position.translation.y;
+            m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.y;
+          }
+          j = m_joint_map.get("base_z");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = msg.origin_position.translation.z;
+            m_x[index + m_num_joints] = msg.origin_twist.linear_velocity.z;
+          }
+                
+          // convert quaternion to euler
+          double x = msg.origin_position.rotation.x;
+          double y = msg.origin_position.rotation.y;
+          double z = msg.origin_position.rotation.z;
+          double w = msg.origin_position.rotation.w;
+          
+          j = m_joint_map.get("base_roll");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = Math.atan2(2*(x*y + z*w),1-2*(y*y+z*z));
+            m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.x;
+          }
+          
+          j = m_joint_map.get("base_pitch");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = Math.asin(2*(x*z - w*y));
+            m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.y;
+          }
+          
+          j = m_joint_map.get("base_yaw");
+          if (j!=null) {
+            index = j.intValue();
+            m_x[index] = Math.atan2(2*(x*w + y*z),1-2*(z*z+w*w))+Math.PI;
+            if (m_x[index] > Math.PI)
+              m_x[index] -= 2*Math.PI;
+            m_x[index + m_num_joints] = msg.origin_twist.angular_velocity.z;
+          }
+          
+          m_has_new_message = true;
         }
+      } catch (IOException ex) {
+        System.out.println("Exception: " + ex);
+      }
     }
-
+    
     public synchronized double[] getNextMessage(long timeout_ms)
     {
-        if (m_has_new_message) {
-            m_has_new_message = false;
-            return m_x;
-        }
-
-        if(timeout_ms == 0)
-            return null;
-
-        try {
-            if(timeout_ms > 0)
-                wait(timeout_ms);
-            else
-                wait();
-
-            if (m_has_new_message) {
-                m_has_new_message = false;
-                return m_x;
-            }
-        } catch (InterruptedException xcp) { }
-
+      if (m_has_new_message) {
+        m_has_new_message = false;
+        return m_x;
+      }
+      
+      if(timeout_ms == 0)
         return null;
+      
+      try {
+        if(timeout_ms > 0)
+          wait(timeout_ms);
+        else
+          wait();
+        
+        if (m_has_new_message) {
+          m_has_new_message = false;
+          return m_x;
+        }
+      } catch (InterruptedException xcp) { }
+      
+      return null;
     }
-
+    
     public synchronized double[] getNextMessage()
     {
-        return getNextMessage(-1);
+      return getNextMessage(-1);
     }
-
+    
     public synchronized double getLastTimestamp()
     {
-        return m_timestamp;
+      return m_timestamp;
     }
     
     public synchronized double[] getState()
     {
-        m_has_new_message = false;
-        return m_x;
+      m_has_new_message = false;
+      return m_x;
     }
 
 }
