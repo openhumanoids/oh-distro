58b63080c2f4c620de0c69f2f766f2c3a09ba338
Scott Kuindersma <kuindersma@gmail.com>
2013-01-10 02:41:50 +0000

    adding joint angles coder class
    git-svn-id: https://svn.csail.mit.edu/drc@964 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/util/JointAnglesCoder.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.lang.*;
+import lcm.lcm.*;
+
+public class JointAnglesCoder implements drake.util.LCMCoder
+{    
+    String m_robot_name;
+    java.util.TreeMap<String,Integer> m_joint_map;
+    int m_num_joints;
+
+    drc.joint_angles_t msg;   
+    
+    public JointAnglesCoder(String robot_name, String[] joint_name)
+    {
+      m_robot_name = robot_name;
+      m_num_joints = joint_name.length;
+      m_joint_map = new java.util.TreeMap<String,Integer>();
+      
+      for (int i=0; i<m_num_joints; i++) {
+        m_joint_map.put(joint_name[i],i);
+      }
+      
+      msg = new drc.joint_angles_t();
+      msg.robot_name = robot_name;
+      msg.num_joints = joint_name.length;
+      msg.joint_name = joint_name;
+      msg.joint_position = new double[m_num_joints];
+    }
+
+    public drake.util.CoordinateFrameData decode(byte[] data)
+    {
+      try {
+        drc.joint_angles_t msg = new drc.joint_angles_t(data);
+        if (msg.robot_name.equals(m_robot_name)) {
+          Integer j;
+          int index;
+          
+          drake.util.CoordinateFrameData fdata = new drake.util.CoordinateFrameData();
+          fdata.val = new double[m_num_joints];
+          fdata.t = (double)msg.utime / 100000.0;
+          for (int i=0; i<msg.num_joints; i++) {
+            j = m_joint_map.get(msg.joint_name[i]);
+            if (j!=null) {
+              index = j.intValue();
+              fdata.val[index] = msg.joint_position[i];
+              if (fdata.val[index] > Math.PI)
+                fdata.val[index] -= 2*Math.PI;
+            }
+          }
+
+          return fdata;
+        }
+      } catch (IOException ex) {
+        System.out.println("Exception: " + ex);
+      }
+      return null;
+    }
+    
+    public LCMEncodable encode(drake.util.CoordinateFrameData d)
+    {
+      msg.utime = (long)(d.t*100000);
+      for (int i=0; i<m_num_joints; i++) {
+        msg.joint_position[i] = (float) d.val[i];
+      }
+      return msg;
+    }
+       
+    public String timestampName()
+    {
+      return "utime";
+    }
+}
