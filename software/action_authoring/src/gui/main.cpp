3885edac33265f34f154518ea0236b6c2e8b026b
Michael S Fleder <mfleder@mit.edu>
2013-01-17 00:14:34 +0000

    added lcm loop
    git-svn-id: https://svn.csail.mit.edu/drc@1067 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/action_authoring/src/gui/main.cpp
+++ software/action_authoring/src/gui/main.cpp
@@ -16,14 +16,38 @@
 
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <lcm/lcm-cpp.hpp>
+#include <boost/thread.hpp>
 
 using namespace action_authoring;
+using namespace boost;
+
+void runLcmHandleLoop(const shared_ptr<lcm::LCM> theLcm)
+{
+	//lcm loop
+	cout << "\nstarting lcm loop" << endl;
+	while (0 == theLcm->handle());
+
+}
 
 int main(int argc, char *argv[])
 {
+	//construct an lcm object
+	shared_ptr<lcm::LCM> theLcm(new lcm::LCM());
+	if (!theLcm->good())
+	{
+		cerr << "Cannot create lcm object" << endl;
+	    return -1;
+	}
+
+	boost::thread testThread = boost::thread(runLcmHandleLoop, theLcm);
+
+	//=======GUI stuff
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     
     return a.exec();
 }
+
+
