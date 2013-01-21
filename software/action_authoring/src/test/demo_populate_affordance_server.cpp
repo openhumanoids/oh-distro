1d4d1024a8fe0de7cd6e956fee2f9a943919e8d6
Michael S Fleder <mfleder@mit.edu>
2013-01-21 23:16:04 +0000

    changed box to ground place
    git-svn-id: https://svn.csail.mit.edu/drc@1159 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/action_authoring/src/test/demo_populate_affordance_server.cpp
+++ software/action_authoring/src/test/demo_populate_affordance_server.cpp
@@ -35,12 +35,12 @@ void runPopulate(const shared_ptr<lcm::LCM> lcm)
 
 	//box
 	AffordanceState box("Box", uniqueObjId++, mapId,
-			    KDL::Frame(KDL::Vector( 1.0, 0.0, 0.0 )),
+			    KDL::Frame(KDL::Vector( 0.0, 0.0, -1.6)),
 			    Eigen::Vector3f( 1.0, 1.0, 0.0 ));
 	box._otdf_id = AffordanceState::BOX;
-	box._params[AffordanceState::LENGTH_NAME] = 0.25;
-	box._params[AffordanceState::WIDTH_NAME]  = 0.25;
-	box._params[AffordanceState::HEIGHT_NAME] = 0.25;
+	box._params[AffordanceState::LENGTH_NAME] = 100;
+	box._params[AffordanceState::WIDTH_NAME]  = 100;
+	box._params[AffordanceState::HEIGHT_NAME] = 1.0;
 	wrapper.addOrReplace(box);
 
 	//cylinder
