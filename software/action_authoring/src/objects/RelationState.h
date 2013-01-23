e7377711d6267624335cac64d3444f20044f2caf
drc <drc@csail.mit.edu>
2013-01-23 19:33:59 +0000

    fixed Isaac's mess by moving objects to object folder and added RelationState.h
    git-svn-id: https://svn.csail.mit.edu/drc@1230 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/action_authoring/src/objects/RelationState.h
@@ -0,0 +1,44 @@
+#ifndef RELATION_STATE_H
+#define RELATION_STATE_H
+
+#include "boost/shared_ptr.hpp"
+
+namespace action_authoring
+{
+
+/**todo: add comment*/
+class RelationState
+{
+  
+  //----------Enumerations
+ public:
+    typedef enum {
+      UNDEFINED,
+      GRASP,
+      FORCE_CLOSURE,
+      OFFSET
+    } RelationType;
+
+
+    //------------fields
+ private:
+    RelationType _relationType;
+
+    //------------Constructor--------
+ public:
+    AtomicConstraint(const RelationState::RelationType &relationType);
+
+  //---------------Accessors
+  RelationType getRelationType() const { return _relationType; };
+
+  //mutators
+  
+}; //class RelationState
+ 
+ typedef boost::shared_ptr<RelationState> RelationStatePtr;
+ typedef boost::shared_ptr<const RelationState> RelationStateConstPtr;
+
+} //namespace action_authoring
+
+
+#endif //AFFORDANCE_RELATION_H
