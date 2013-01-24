e7e2030c5af3a78123406bce031429f8028c0e84
Michael S Fleder <mfleder@mit.edu>
2013-01-24 07:07:21 +0000

    added stubs
    git-svn-id: https://svn.csail.mit.edu/drc@1275 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/affordance/src/ManipulatorState.cpp
+++ software/perception/world_model/affordance/src/ManipulatorState.cpp
@@ -1,7 +1,71 @@
 #include "ManipulatorState.h"
 
 using namespace affordance;
+using namespace std;
+using namespace boost;
+using namespace Eigen;
 
-ManipulatorState::ManipulatorState(std::string name) {
+ManipulatorState::ManipulatorState(std::string name) 
+{
     _name = name;
 }
+
+GlobalUID ManipulatorState::getGlobalUniqueId() const
+{
+ throw NotImplementedException("manip state");
+}
+
+string ManipulatorState::getName() const
+{
+	return _name;
+}
+
+Vector3f ManipulatorState::getColor() const
+{
+ throw NotImplementedException("manip state");
+}
+ 
+
+Vector3f ManipulatorState::getXYZ() const
+{
+ throw NotImplementedException("manip state");
+}
+ 
+Vector3f ManipulatorState::getRPY() const
+{
+ throw NotImplementedException("manip state");
+} 
+bool ManipulatorState::isAffordance() const
+{
+	return false;
+}
+ 
+bool ManipulatorState::isManipulator() const
+{
+	return true;
+}
+
+bool ManipulatorState::hasChildren() const
+{
+ throw NotImplementedException("manip state");
+}
+ 
+bool ManipulatorState::hasParent() const
+{
+ throw NotImplementedException("manip state");
+}
+ 
+void ManipulatorState::getChildren(vector<shared_ptr<const ManipulatorState> > &children) const 
+{
+ throw NotImplementedException("manip state");
+}
+ 
+void ManipulatorState::getParents(vector<shared_ptr<const ManipulatorState> > &children) const 
+{
+ throw NotImplementedException("manip state");
+}
+
+ void ManipulatorState::getCopy(ManipulatorState &copy) const 
+ {
+ throw NotImplementedException("manip state");
+ }
