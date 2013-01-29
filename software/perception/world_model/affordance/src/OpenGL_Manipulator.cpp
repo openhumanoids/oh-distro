5ebe8c4a23a519f8ef03def43cd74fa9438c963c
Michael S Fleder <mfleder@mit.edu>
2013-01-29 20:43:50 +0000

    added some useful << operators that KDL was lacking
    git-svn-id: https://svn.csail.mit.edu/drc@1392 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/affordance/src/OpenGL_Manipulator.cpp
+++ software/perception/world_model/affordance/src/OpenGL_Manipulator.cpp
@@ -28,6 +28,57 @@ OpenGL_Manipulator::~OpenGL_Manipulator()
 {
 }
 
+//=====
+string toStr(const KDL::Vector &v)
+{
+  return string("(") + ModelState::toStr(v.x()) + string(", ") 
+    + ModelState::toStr(v.y()) + string(", ") 
+    + ModelState::toStr(v.z()) + string(")");
+}
+
+
+string toStr(const KDL::Rotation &r)
+{
+  double q1,q2,q3,q4;
+  r.GetQuaternion(q1,q2,q3,q4);
+  return string("(") + ModelState::toStr(q1) + string(", ") 
+    + ModelState::toStr(q2) + string(", ") 
+    + ModelState::toStr(q3)
+    + string(", ")
+    + ModelState::toStr(q4) + string(")");
+}
+
+
+string toStr(const KDL::Frame &frame)
+{
+  return string("Position = ") + toStr(frame.p)
+    + string("\t Rotation = ") + toStr(frame.M);
+}
+
+
+std::ostream& operator << (std::ostream &out, const KDL::Rotation &r)
+{
+  out << toStr(r);
+  return out;
+}
+
+std::ostream& operator << (std::ostream &out, const KDL::Vector &v)
+{
+  out << toStr(v);
+  return out;
+}
+
+std::ostream& operator <<(std::ostream& out, const KDL::Frame &frame)
+{
+  out << "Position = " << frame.p << "\t Rotation = " << frame.M << endl;
+  return out;
+}
+
+//=========
+
+
+
+
 /**sets the state of the object we want to draw and then draws*/
 void OpenGL_Manipulator::draw()
 {
@@ -49,11 +100,13 @@ void OpenGL_Manipulator::draw()
 	  KDL::Frame f = cPtAsFrame * _manipulator->getLinkFrame(); //manipulator is in robot-oriented frame?
 	  s.set(f, 0.05); //todo : pick a radius
 
-
-	  /*	  cout << "\n cPtAsFrame = " << cPtAsFrame << endl;
-	  cout << "\n link frame in world = " << _manipulator->getLinkFrame() << endl;
-	  cout << "cPtAsFrame * manipulator->getLinkFrame() = " << f << endl;
-	  cout << "manipulator->getLinkFrame() * cPtAsFrame = " << _manipulator->getLinkFrame() * cPtAsFrame << endl;*/
+	  /*
+	  cout << "\n\n=========================================" << endl;
+	  cout << "\n cPtAsFrame = " << toStr(cPtAsFrame) << endl;
+	  cout << "\n link frame in world = " << toStr(_manipulator->getLinkFrame()) << endl;
+	  cout << "cPtAsFrame * manipulator->getLinkFrame() = " << toStr(f) << endl;
+	  cout << "manipulator->getLinkFrame() * cPtAsFrame = " << toStr(_manipulator->getLinkFrame() * cPtAsFrame) << endl;
+	  */
 
 	  if (_isHighlighted) 
 	    {
