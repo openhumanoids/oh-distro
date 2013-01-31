acbf1f170063d8bb795842affd9added30465d1f
Robin Deits <rdeits@csail.mit.edu>
2013-01-31 16:17:23 +0000

    Update plan publisher for robots w/o floating base; deprecate unused foostep planner
    git-svn-id: https://svn.csail.mit.edu/drc@1441 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/traj_opt/dev/dircol_test.m
+++ software/control/traj_opt/dev/dircol_test.m
@@ -1,11 +1,10 @@
 function dircol_test()
 
-options.floating = true;
+options.floating = false;
 r = RigidBodyManipulator('../../../../ros_workspace/mit_drcsim_scripts/models/mit_wheeled_stick_torso/model.sdf', options);
+% r = TimeSteppingRigidBodyManipulator('../../../models/mit_gazebo_models/mit_robot_drake/model_minimal_contact.urdf', 0.01, options);
 
 nx = r.getNumStates();
-B = r.B;
-idx = B'*(1:nx/2)';
 
 robot_name = 'wheeled_atlas';
 options = struct('num_knot_points', 11);
@@ -19,33 +18,43 @@ joint_names
 state_listener = LCMCoordinateFrameWCoder(robot_name, nx, r.getStateFrame().prefix, lcmcoder);
 state_listener.subscribe('EST_ROBOT_STATE');
 
-plan_publisher = RobotPlanPublisher(robot_name, joint_names, r.has_floating_base, 'CANDIDATE_ROBOT_PLAN', options.num_knot_points);
+plan_publisher = RobotPlanPublisher(robot_name, joint_names, r.hrunas_floating_base, 'CANDIDATE_ROBOT_PLAN', options.num_knot_points);
 
 
 
+% traj_options.method='implicitdirtran';
 traj_options.method='dircol';
 traj_options.warning=0;
 traj_options.snopt_major_iter_lim = 3;
 
-
-x0 = []
+x0 = [];
 while isempty(x0)
   [x0, t0] = state_listener.getNextMessage(1);
 end
+% t0 = 0;
+% x0 = r.getInitialState();
 traj_options.trajectory_cost_fun = @(t, x, u) publish_dircol_traj(t, x, u, t0, plan_publisher);
-goal_ndx = 7;
+goal_ndx = 5;
 xf = x0 + [zeros(goal_ndx - 1, 1); goal_dist; zeros(length(x0) - goal_ndx, 1)];
-xf_tol = [0; inf(length(x0) - 1, 1)];
+xf_tol = [inf(goal_ndx - 1, 1); 0; inf(length(x0) - goal_ndx, 1)];
+vel_lim = cell2mat({r.body(2:end).velocity_limit})';
+pos_lim_max = cell2mat({r.body(2:end).joint_limit_max})';
+pos_lim_min = cell2mat({r.body(2:end).joint_limit_min})';
+con.x.lb = [pos_lim_min; -vel_lim];
+con.x.ub = [pos_lim_max; vel_lim];
 con.x0.lb = x0;
 con.x0.ub = x0;
 con.xf.lb = xf - xf_tol;
 con.xf.ub = xf + xf_tol;
+con.xf.c = @(x) end_eff_goal(r, x(1:end/2), r.body(8), [0;0;0],[-0.5478;0.2685;0.6122]);
 con.T.lb = .8 * time_to_goal;   
 con.T.ub = 1.2 * time_to_goal;
 current_utraj = PPTrajectory(foh(linspace(0, time_to_goal, options.num_knot_points),...
   randn(r.getNumInputs(), options.num_knot_points)));
 disp('starting trajopt')
 [current_utraj, current_xtraj, info] = trajectoryOptimization(r, @cost, @finalcost, x0, current_utraj, con, traj_options);
+% v = r.constructVisualizer();
+% v.playback(current_xtraj, struct('slider', 'true'));
 disp('finished')
 while (1)
   [x0, t0] = state_listener.getNextMessage(1);
@@ -57,17 +66,20 @@ while (1)
 %     planned_states = repmat(x0, 1, options.num_knot_points);
 %     planned_states(goal_ndx,:) = linspace(x0(goal_ndx), x0(goal_ndx) + goal_dist, options.num_knot_points);
 %     plan_publisher.publish(linspace(t0, t0 + time_to_goal, options.num_knot_points), planned_states);
+%     x = current_xtraj.eval(current_xtraj.tspan(end));
+%     x(5)
+%     g = end_eff_goal(r, x(1:end/2), r.body(14), [0;0;0], [-0.5478;0.2685;0.6122])
   end
 end
 
 end
 
-function [g,dg] = cost(t,x,u)
-  R = 0;
+function g = cost(t,x,u)
+  R = 1;
   g = u'*R*u;
   %g = sum((R*u).*u,1);
-  %dg = [zeros(1,1+size(x,1)),2*u'*R];
-  dg = zeros(1, 1 + size(x,1) + size(u,1));
+  dg = [zeros(1,1+size(x,1)),2*u'*R];
+%   dg = zeros(1, 1 + size(x,1) + size(u,1));
 end
 
 function [h,dh] = finalcost(t,x)
@@ -84,5 +96,11 @@ function [J, dJ] = publish_dircol_traj(t, x, u, t0, plan_publisher)
   plan_publisher.publish(t + t0, x);
   J=0;
   dJ=[0*t(:);0*x(:);0*u(:)]';
+end
+
+function [g] = end_eff_goal(r, q, link, link_pt, goal)
+  kindsol = r.doKinematics(q, true);
+  [pt1, J1, dJ1] = r.forwardKin(kindsol, link, link_pt);
+  g = sum((goal - pt1) .^ 2);
 end
   
--- software/control/util/RobotPlanPublisher.java
+++ software/control/util/RobotPlanPublisher.java
@@ -7,26 +7,34 @@ public class RobotPlanPublisher
     drc.robot_plan_t msg;
     String channel_name;
     String[] nonfloating_joint_name;
+    boolean has_floating_base;
+    int nonfloating_joint_start_ndx;
 
-    public RobotPlanPublisher(String robot_name,String[] joint_name,String channel)
+    public RobotPlanPublisher(String robot_name,String[] joint_name,boolean floating_base, String channel)
     {
       msg = new drc.robot_plan_t();
       msg.robot_name = robot_name;
       channel_name = channel;
+      has_floating_base = floating_base;
       
-      int num_nonfloating_joints = 0;
-      for (int i=0; i<joint_name.length; i++)
-        if (!(joint_name[i].startsWith("base_"))) 
-          num_nonfloating_joints+=1;
+      nonfloating_joint_start_ndx = 0;
+      System.out.println(has_floating_base);
+      if (has_floating_base)
+        nonfloating_joint_start_ndx = 6;
+      int num_nonfloating_joints = joint_name.length - nonfloating_joint_start_ndx;
       nonfloating_joint_name = new String[num_nonfloating_joints];
-      for (int i=0,j=0; i<joint_name.length; i++)
-        if (!(joint_name[i].startsWith("base_"))) 
+      for (int i=nonfloating_joint_start_ndx,j=0; i<joint_name.length; i++)
           nonfloating_joint_name[j++] = joint_name[i];
+        // if (!(joint_name[i].startsWith("pelvis_"))) 
+          // num_nonfloating_joints+=1;
+
+      // for (int i=0,j=0; i<joint_name.length; i++)
+      //   if (!(joint_name[i].startsWith("pelvis_"))) 
     }
     
-    public RobotPlanPublisher(String robot_name, String[] joint_name,String channel, int num_states)
+    public RobotPlanPublisher(String robot_name, String[] joint_name,boolean has_floating_base, String channel, int num_states)
     {
-      this(robot_name,joint_name,channel);
+      this(robot_name,joint_name,has_floating_base,channel);
       allocate(num_states);
     }
 
@@ -77,37 +85,38 @@ public class RobotPlanPublisher
       }
       if (x[0].length != t.length) {
         // todo: throw an exception here instead
-        System.out.format("Error: x[0].length=%d and t.length=%d\n",x.length,t.length);
+        System.out.format("Error: x[0].length=%d and t.length=%d\n",x[0].length,t.length);
         return;
       }
-        
       for(int i = 0;i<msg.num_states;i++)
       {
-        msg.plan[i].utime = (long) (t[i]*1000000+msg.utime);
-/*        
-        msg.plan[i].origin_position.translation.x = (float) x[xind];
-        msg.plan[i].origin_position.translation.y = (float) x[xind+1];
-        msg.plan[i].origin_position.translation.z = (float) x[xind+2];
-        double roll = x[xind+3];
-        double pitch = x[xind+4];
-        double yaw = x[xind+5];
-        msg.plan[i].origin_position.rotation.x = Math.sin(roll/2)*Math.cos(pitch/2)*Math.cos(yaw/2)-Math.cos(roll/2)*Math.sin(pitch/2)*Math.sin(yaw/2);
-        msg.plan[i].origin_position.rotation.y = Math.cos(roll/2)*Math.sin(pitch/2)*Math.cos(yaw/2)+Math.sin(roll/2)*Math.cos(pitch/2)*Math.sin(yaw/2);
-        msg.plan[i].origin_position.rotation.z = Math.cos(roll/2)*Math.cos(pitch/2)*Math.sin(yaw/2)-Math.sin(roll/2)*Math.sin(pitch/2)*Math.cos(yaw/2);
-        msg.plan[i].origin_position.rotation.w = Math.cos(roll/2)*Math.cos(pitch/2)*Math.cos(yaw/2)+Math.sin(roll/2)*Math.sin(pitch/2)*Math.sin(yaw/2);
-        msg.plan[i].origin_twist.linear_velocity.x = x[xind+6+msg.plan[i].num_joints];
-        msg.plan[i].origin_twist.linear_velocity.y = x[xind+6+msg.plan[i].num_joints+1];
-        msg.plan[i].origin_twist.linear_velocity.z = x[xind+6+msg.plan[i].num_joints+2];
-        msg.plan[i].origin_twist.angular_velocity.x = x[xind+6+msg.plan[i].num_joints+3];
-        msg.plan[i].origin_twist.angular_velocity.y = x[xind+6+msg.plan[i].num_joints+4];
-        msg.plan[i].origin_twist.angular_velocity.z = x[xind+6+msg.plan[i].num_joints+5];
-*/        
+        if (has_floating_base) {
+          msg.plan[i].utime = (long) (t[i]*1000000+msg.utime);        
+          msg.plan[i].origin_position.translation.x = (float) x[0][i];
+          msg.plan[i].origin_position.translation.y = (float) x[1][i];
+          msg.plan[i].origin_position.translation.z = (float) x[2][i];
+          
+          double roll = x[3][i];
+          double pitch = x[4][i];
+          double yaw = x[5][i];
+          msg.plan[i].origin_position.rotation.x = Math.sin(roll/2)*Math.cos(pitch/2)*Math.cos(yaw/2)-Math.cos(roll/2)*Math.sin(pitch/2)*Math.sin(yaw/2);
+          msg.plan[i].origin_position.rotation.y = Math.cos(roll/2)*Math.sin(pitch/2)*Math.cos(yaw/2)+Math.sin(roll/2)*Math.cos(pitch/2)*Math.sin(yaw/2);
+          msg.plan[i].origin_position.rotation.z = Math.cos(roll/2)*Math.cos(pitch/2)*Math.sin(yaw/2)-Math.sin(roll/2)*Math.sin(pitch/2)*Math.cos(yaw/2);
+          msg.plan[i].origin_position.rotation.w = Math.cos(roll/2)*Math.cos(pitch/2)*Math.cos(yaw/2)+Math.sin(roll/2)*Math.sin(pitch/2)*Math.sin(yaw/2);
+          msg.plan[i].origin_twist.linear_velocity.x = x[6+msg.plan[i].num_joints][i];
+          msg.plan[i].origin_twist.linear_velocity.y = x[6+msg.plan[i].num_joints+1][i];
+          msg.plan[i].origin_twist.linear_velocity.z = x[6+msg.plan[i].num_joints+2][i];
+          msg.plan[i].origin_twist.angular_velocity.x = x[6+msg.plan[i].num_joints+3][i];
+          msg.plan[i].origin_twist.angular_velocity.y = x[6+msg.plan[i].num_joints+4][i];
+          msg.plan[i].origin_twist.angular_velocity.z = x[6+msg.plan[i].num_joints+5][i];
+        }
         
         //System.out.format("The %d's state is at time %d, origin's translation is %f, %f, %f, angles are %f,%f,%f; the velocity of origin is %f, %f, %f, %f, %f, %f\n",i+1,msg.plan[i].utime,msg.plan[i].origin_position.translation.x,msg.plan[i].origin_position.translation.y,msg.plan[i].origin_position.translation.z,msg.plan[i].origin_position.rotation.x,msg.plan[i].origin_position.rotation.y,msg.plan[i].origin_position.rotation.z,msg.plan[i].origin_twist.linear_velocity.x,msg.plan[i].origin_twist.linear_velocity.y,msg.plan[i].origin_twist.linear_velocity.z,msg.plan[i].origin_twist.angular_velocity.x,msg.plan[i].origin_twist.angular_velocity.y,msg.plan[i].origin_twist.angular_velocity.z);
+        
         for(int j = 0;j<msg.plan[i].num_joints;j++)
         {
-          msg.plan[i].joint_position[j] = (float) x[j][i];
-          msg.plan[i].joint_velocity[j] = (float) x[msg.plan[i].num_joints+j][i];
+          msg.plan[i].joint_position[j] = (float) x[j+nonfloating_joint_start_ndx][i];
+          msg.plan[i].joint_velocity[j] = (float) x[msg.plan[i].num_joints+j+nonfloating_joint_start_ndx][i];
           //System.out.format("The %d's joint of %d's state has joint angle %f, joint velocity %f\n",j+1,i+1,msg.plan[i].joint_position[j],msg.plan[i].joint_velocity[j]);
         }
       }
