705eec04ad7814e5ab56b26f288b330b3f9bc49a
Riad Hammoud <riad.hammoud@baesystems.com>
2013-02-05 18:44:26 +0000

    my standalone PCL-based cylinder fitting code
    git-svn-id: https://svn.csail.mit.edu/drc@1511 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/perception/rhammoud_sandbox/cylinder_segmentation/CMakeFiles/cylinder_segmentation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cylinder_segmentation.dir/cylinder_segmentation.cpp.o"
+  "cylinder_segmentation.pdb"
+  "cylinder_segmentation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cylinder_segmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
