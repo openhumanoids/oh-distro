d5fabc3b0c7aa67ce00e5b4ad0dfb9dcb23ed912
Scott Kuindersma <kuindersma@gmail.com>
2013-02-05 18:24:58 +0000

    grasp synthesis pool server now takes number of workers as an argument
    git-svn-id: https://svn.csail.mit.edu/drc@1505 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/sticky_hands/runGraspSynthesis1PoolServer.m
+++
@@ -1,5 +0,0 @@
-function runGraspSynthesis1PoolServer()
-%profile on;
-no_of_workers =  1;
-runGraspOptSinglePoolService(no_of_workers);    
-
---
+++ software/control/sticky_hands/runGraspSynthesisPoolServer.m
@@ -0,0 +1,26 @@
+function runGraspSynthesisPoolServer(n_workers)
+%NOTEST
+mode = 1; % 0 = robot, 1 = base
+if mode ==1
+  lcm_url = 'udpm://239.255.12.68:1268?ttl=1';
+else
+  lcm_url = 'udpm://239.255.76.67:7667?ttl=1';
+end
+lcm.lcm.LCM.getSingletonTemp(lcm_url); % only works on mfallons machine
+
+%profile on;
+no_of_workers = n_workers;
+if(matlabpool('size')==0)
+matlabpool(no_of_workers)
+end
+%
+spmd
+    runGraspOptSinglePoolService(no_of_workers);    
+end % end spmd
+
+% end
+matlabpool close
+%profile viewer;
+end
+
+% How to get rid SPMD overhead at startup?
