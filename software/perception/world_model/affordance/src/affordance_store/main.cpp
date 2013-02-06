f71ca471e12f2d95cdfbd7e847edff0b3e0b3054
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-06 14:29:26 +0000

    added roles to aff server
    git-svn-id: https://svn.csail.mit.edu/drc@1539 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/affordance/src/affordance_store/main.cpp
+++ software/perception/world_model/affordance/src/affordance_store/main.cpp
@@ -4,24 +4,45 @@
 #include "AffordanceServer.h"
 #include <iostream>
 
+#include <ConciseArgs>
+
 using namespace boost;
 using namespace std;
 using namespace affordance;
 int main(int argc, char ** argv)
 {
-	shared_ptr<lcm::LCM> theLcm(new lcm::LCM());
-	 if (!theLcm->good())
-	 {
-	    cerr << "Cannot create lcm object" << endl;
-	    return -1;
-	 }
+  string role = "robot";
+  ConciseArgs opt(argc, (char**)argv);
+  opt.add(role, "r", "role","Role - robot or base");
+  opt.parse();
+  std::cout << "role: " << role << "\n";
+  
 
-    //create the server
-    AffordanceServer s(theLcm);
+  
+  string lcm_url ="";
+  if(role.compare("robot") == 0){
+     lcm_url = ""; // put robot url if needed
+  }else if(role.compare("base") == 0){  
+     lcm_url = "udpm://239.255.12.68:1268?ttl=1";
+  }else{
+    std::cout << "DRC Viewer role not understood, choose: robot or base\n";
+    return 1;
+  }
 
-    //lcm loop
-    cout << "\nstarting lcm loop" << endl;
-    while (0 == theLcm->handle());
+  
+  shared_ptr<lcm::LCM> theLcm(new lcm::LCM( lcm_url ));
+  if (!theLcm->good())
+  {
+    cerr << "Cannot create lcm object" << endl;
+    return -1;
+  }
 
-    return 0;
+  //create the server
+  AffordanceServer s(theLcm);
+
+  //lcm loop
+  cout << "\nstarting lcm loop" << endl;
+  while (0 == theLcm->handle());
+
+  return 0;
 }
