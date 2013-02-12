ed086c95c21f1f873b6b6da476e06e74aefd20f8
Scott Kuindersma <kuindersma@gmail.com>
2013-02-12 23:49:56 +0000

    lcm to ros translator uses drcsim joint command API
    git-svn-id: https://svn.csail.mit.edu/drc@1597 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/translators/src/lcm2ros.cpp
+++ ros_workspace/translators/src/lcm2ros.cpp
@@ -1,17 +1,14 @@
 #include <cstdlib>
-#include <map>
 #include <string>
 #include <ros/ros.h>
 #include <lcm/lcm-cpp.hpp>
 #include <lcmtypes/drc_lcmtypes.hpp>
-
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/Float64.h>
 #include <std_srvs/Empty.h>
-// MIT Homebrew messages
-#include <atlas_gazebo_msgs/RobotState.h>
-#include <atlas_gazebo_msgs/ActuatorCmd.h>
+#include <osrf_msgs/JointCommands.h>
 
 #include <ConciseArgs>
 
@@ -19,17 +16,17 @@ using namespace std;
 
 class LCM2ROS{
   public:
-    LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_,
-      bool reconfig_, bool pause_physics_then_reconfig_);
+    LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_,bool reconfig_, bool pause_physics_then_reconfig_);
     ~LCM2ROS() {}
 
   private:
     boost::shared_ptr<lcm::LCM> lcm_;
     ros::NodeHandle nh_;
-    std::map<std::string, ros::Publisher> pub_map_;
     
-    
-    void positionCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::joint_angles_t* msg);
+    // DRCSIM 2.0 joint command API
+    void jointCommandHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::joint_command_t* msg);
+    ros::Publisher joint_cmd_pub_;
+        
     ros::Publisher rot_scan_cmd_pub_;
     void rot_scan_rate_cmd_Callback(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::twist_timed_t* msg);
     
@@ -42,7 +39,7 @@ class LCM2ROS{
     void reconfigCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::robot_state_t* msg);
 
     // Non-api translations:
-    ros::Publisher actuator_cmd_pub_, body_twist_cmd_pub_;
+    ros::Publisher body_twist_cmd_pub_;
     void actuatorCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::actuator_cmd_t* msg);
     void bodyTwistCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::twist_t* msg);
     ros::Publisher gas_pedal_pub_, brake_pedal_pub_;
@@ -50,36 +47,12 @@ class LCM2ROS{
 };
 
 
-LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_, 
-           bool reconfig_, bool pause_physics_then_reconfig_): 
-           lcm_(lcm_),nh_(nh_), 
-           reconfig_(reconfig_), pause_physics_then_reconfig_(pause_physics_then_reconfig_) {
-             
-             
-  // Typical Position Control: /////////////////////////////////////////
-  vector<string> joints = {"l_arm_elx", "l_arm_ely", "l_arm_mwx", "l_arm_shx", "l_arm_usy", "l_arm_uwy", 
-                           "r_arm_elx", "r_arm_ely", "r_arm_mwx", "r_arm_shx", "r_arm_usy", "r_arm_uwy", 
-                           "l_leg_kny", "l_leg_lax", "l_leg_lhy", "l_leg_mhx", "l_leg_uay", "l_leg_uhz", 
-                           "r_leg_kny", "r_leg_lax", "r_leg_lhy", "r_leg_mhx", "r_leg_uay", "r_leg_uhz", 
-                           "neck_ay", "back_lbz", "back_mby", "back_ubx"};
-  // sandiaPositionCommandTranslator used to send right_* ... switched to r_*
-  vector<string> finger_joints = {"r_f0_j0", "r_f0_j1", "r_f0_j2", 
-                           "r_f1_j0", "r_f1_j1", "r_f1_j2", 
-                           "r_f2_j0", "r_f2_j1", "r_f2_j2", 
-                           "r_f3_j0", "r_f3_j1", "r_f3_j2", 
-                           "l_f0_j0", "l_f0_j1", "l_f0_j2", 
-                           "l_f1_j0", "l_f1_j1", "l_f1_j2", 
-                           "l_f2_j0", "l_f2_j1", "l_f2_j2", 
-                           "l_f3_j0", "l_f3_j1", "l_f3_j2",
-                           "real_base_x", "real_base_y", "real_base_z",
-                           "real_base_roll", "real_base_pitch", "real_base_yaw"};
-  joints.insert( joints.end(), finger_joints.begin(), finger_joints.end() );
-  cout << joints.size() << " joints available for command\n";
-            
-  for (size_t i=0; i < joints.size() ; i++){
-      pub_map_.insert(std::make_pair( joints[i] ,nh_.advertise<std_msgs::Float64>( string( joints[i] + "_position_controller/command") ,10)));
-  }
-  lcm_->subscribe("JOINT_POSITION_CMDS",&LCM2ROS::positionCmdHandler,this);  
+LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_, bool reconfig_, bool pause_physics_then_reconfig_): 
+           lcm_(lcm_),nh_(nh_), reconfig_(reconfig_), pause_physics_then_reconfig_(pause_physics_then_reconfig_) {
+
+  // DRCSIM 2.0 joint command API
+  lcm_->subscribe("JOINT_CMDS",&LCM2ROS::jointCommandHandler,this);  
+  joint_cmd_pub_ = nh_.advertise<osrf_msgs::JointCommands>("/atlas/joint_commands",10);
 
   /// Spinning Laser control:
   lcm_->subscribe("ROTATING_SCAN_RATE_CMD",&LCM2ROS::rot_scan_rate_cmd_Callback,this);
@@ -93,11 +66,7 @@ LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_,
     lcm_->subscribe("SET_ROBOT_CONFIG",&LCM2ROS::reconfigCmdHandler,this);
   }
   
-  
-  /// Older style control of robot via controller_manager:
-  lcm_->subscribe("ACTUATOR_CMDS",&LCM2ROS::actuatorCmdHandler,this);
   lcm_->subscribe("NAV_CMDS",&LCM2ROS::bodyTwistCmdHandler,this);
-  actuator_cmd_pub_ = nh_.advertise<atlas_gazebo_msgs::ActuatorCmd>("actuator_cmd",10);
   body_twist_cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel",10);
 
   /// Experiments with car control:
@@ -107,15 +76,25 @@ LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_,
 }
 
 
-void LCM2ROS::positionCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::joint_angles_t* msg){
-  std_msgs::Float64 position_command_msg;
+void LCM2ROS::jointCommandHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::joint_command_t* msg){
+  osrf_msgs::JointCommands joint_command_msg;
   for (int i=0; i<msg->num_joints; i++) {
-      // should we instead publish all joints at the same time?
-      position_command_msg.data = msg->joint_position[i];
-    if(ros::ok()) {
-      pub_map_[msg->joint_name[i]].publish(position_command_msg);
-    }
+    joint_command_msg.name.push_back(msg->name[i]);
+    joint_command_msg.position.push_back(msg->position[i]);
+    joint_command_msg.velocity.push_back(msg->velocity[i]);
+    joint_command_msg.effort.push_back(msg->effort[i]);
+
+    joint_command_msg.kp_position.push_back(msg->kp_position[i]);
+    joint_command_msg.ki_position.push_back(msg->ki_position[i]);
+    joint_command_msg.kd_position.push_back(msg->kd_position[i]);
+    joint_command_msg.kp_velocity.push_back(msg->kp_velocity[i]);
+
+    joint_command_msg.i_effort_min.push_back(msg->i_effort_min[i]);
+    joint_command_msg.i_effort_max.push_back(msg->i_effort_max[i]);
   }
+  if(ros::ok()) {
+    joint_cmd_pub_.publish(joint_command_msg);
+  } 
 }  
 
 void LCM2ROS::rot_scan_rate_cmd_Callback(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::twist_timed_t* msg){
@@ -162,33 +141,20 @@ void LCM2ROS::reconfigCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::strin
   }      
   
   // set PID goals
-  std_msgs::Float64 position_command_msg;
+  osrf_msgs::JointCommands joint_command_msg;
   for (int i=0; i<msg->num_joints; i++) {
-    position_command_msg.data = msg->joint_position[i];
-    if(ros::ok()) {
-      pub_map_[msg->joint_name[i]].publish(position_command_msg);
-    }
+    joint_command_msg.name.push_back(msg->joint_name[i]);
+    joint_command_msg.position.push_back(msg->joint_position[i]);
+    joint_command_msg.velocity.push_back(0);
+    joint_command_msg.effort.push_back(0);
   }
+  if(ros::ok()) {
+    joint_cmd_pub_.publish(joint_command_msg);
+  } 
+  
   ros::spinOnce();
   cout << "... Reconfiguration complete\n";
 }
-
-
-
-void LCM2ROS::actuatorCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::actuator_cmd_t* msg){
-  atlas_gazebo_msgs::ActuatorCmd actuator_cmd_msg;
-  actuator_cmd_msg.header.stamp.fromNSec( msg->utime*1000 ); // from usec to nsec
-  actuator_cmd_msg.robot_name = msg->robot_name;
-  for(std::vector<int>::size_type i=0;i!=msg->actuator_name.size();i++){//So varaibles in lcm message are all in vector type
-    actuator_cmd_msg.actuator_name.push_back(msg->actuator_name.at(i));
-    actuator_cmd_msg.actuator_effort.push_back(msg->actuator_effort.at(i));
-    actuator_cmd_msg.effort_duration.push_back(msg->effort_duration.at(i));
-  }
-  if(ros::ok()){
-    actuator_cmd_pub_.publish(actuator_cmd_msg);
-  }
-}
-
 
 void LCM2ROS::bodyTwistCmdHandler(const lcm::ReceiveBuffer* rbuf,const std::string &channel,const drc::twist_t* msg){
   geometry_msgs::Twist body_twist_cmd_msg;
@@ -216,7 +182,6 @@ void LCM2ROS::estopHandler(const lcm::ReceiveBuffer* rbuf,const std::string &cha
   }
 }
 
- 
 
 int main(int argc,char** argv) {
   ConciseArgs parser(argc, argv, "lcm2ros");
