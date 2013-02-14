4064770505beaf8481c41e57d9517c0d48e8f189
Scott Kuindersma <kuindersma@gmail.com>
2013-02-14 15:31:40 +0000

    updated joint command coder to use respect fixed joint ordering in the DRCSIM AtlasPlugin.cpp
    git-svn-id: https://svn.csail.mit.edu/drc@1613 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/util/JointCommandCoder.java
+++ software/control/util/JointCommandCoder.java
@@ -2,40 +2,88 @@ import java.io.*;
 import java.lang.*;
 import lcm.lcm.*;
 
-public class JointCommandCoder implements drake.util.LCMCoder
+public class JointCommandCoder implements drake.util.LCMCoder 
 {
     String m_robot_name;
-    java.util.TreeMap<String,Integer> m_joint_map;
-    int m_num_joints;
+    java.util.TreeMap<String,Integer> m_drake_joint_map;
+    java.util.TreeMap<Integer,String> m_rev_drake_joint_map; // should replace with a BiMap
+    java.util.TreeMap<String,Integer> m_atlas_joint_map;
+    java.util.TreeMap<Integer,String> m_rev_atlas_joint_map; // should replace with a BiMap
+    
+    // Atlas plugin has a fixed order for joints, 
+    //        so number of joints here is fixed
+    int m_num_joints = 28;
+
     int mode = 1; // mode==1: torque-only, mode==2: position-only, fixed gains
     // TODO: add additional modes (e.g., position w/variable gains, mixed torque-position control
     
     drc.joint_command_t msg;
 
-    public JointCommandCoder(String robot_name, String[] joint_name, double[] Kp, double[] Kd) {
-    
+    public JointCommandCoder(String robot_name, String[] joint_name, double[] Kp, double[] Kd) throws Exception
+    {
       this(robot_name, joint_name);
+      
       mode = 2;
+      int j;
       for (int i=0; i<msg.num_joints; i++) {
-        msg.kp_position[i] = Kp[i];
-        msg.kd_position[i] = Kd[i];
+        j = m_atlas_joint_map.get(m_rev_drake_joint_map.get(i)).intValue();
+        msg.kp_position[j] = Kp[i];
+        msg.kd_position[j] = Kd[i];
       }
     }
     
-    public JointCommandCoder(String robot_name, String[] joint_name)
+    public JointCommandCoder(String robot_name, String[] joint_name) throws Exception
     {
-      m_num_joints = joint_name.length;
+      if (joint_name.length != m_num_joints)
+        throw new Exception("Length of joint_name must be " + m_num_joints);
+      
       m_robot_name = robot_name;
-      m_joint_map = new java.util.TreeMap<String,Integer>();
+      m_drake_joint_map = new java.util.TreeMap<String,Integer>();
+      m_rev_drake_joint_map = new java.util.TreeMap<Integer,String>();
+      m_atlas_joint_map = new java.util.TreeMap<String,Integer>();
+      m_rev_atlas_joint_map = new java.util.TreeMap<Integer,String>();
+      
+      String[] atlas_joint_name = new String[28];
+      atlas_joint_name[0] = "back_lbz";
+      atlas_joint_name[1] = "back_mby";
+      atlas_joint_name[2] = "back_ubx";
+      atlas_joint_name[3] = "neck_ay";
+      atlas_joint_name[4] = "l_leg_uhz";
+      atlas_joint_name[5] = "l_leg_mhx";
+      atlas_joint_name[6] = "l_leg_lhy";
+      atlas_joint_name[7] = "l_leg_kny";
+      atlas_joint_name[8] = "l_leg_uay";
+      atlas_joint_name[9] = "l_leg_lax";
+      atlas_joint_name[10] = "r_leg_uhz";
+      atlas_joint_name[11] = "r_leg_mhx";
+      atlas_joint_name[12] = "r_leg_lhy";
+      atlas_joint_name[13] = "r_leg_kny";
+      atlas_joint_name[14] = "r_leg_uay";
+      atlas_joint_name[15] = "r_leg_lax";
+      atlas_joint_name[16] = "l_arm_usy";
+      atlas_joint_name[17] = "l_arm_shx";
+      atlas_joint_name[18] = "l_arm_ely";
+      atlas_joint_name[19] = "l_arm_elx";
+      atlas_joint_name[20] = "l_arm_uwy";
+      atlas_joint_name[21] = "l_arm_mwx";
+      atlas_joint_name[22] = "r_arm_usy";
+      atlas_joint_name[23] = "r_arm_shx";
+      atlas_joint_name[24] = "r_arm_ely";
+      atlas_joint_name[25] = "r_arm_elx";
+      atlas_joint_name[26] = "r_arm_uwy";
+      atlas_joint_name[27] = "r_arm_mwx";
       
       for (int i=0; i<m_num_joints; i++) {
-        m_joint_map.put(joint_name[i],i);
+        m_drake_joint_map.put(joint_name[i],i);
+        m_rev_drake_joint_map.put(i,joint_name[i]);
+        m_atlas_joint_map.put(atlas_joint_name[i],i);
+        m_rev_atlas_joint_map.put(i,atlas_joint_name[i]);
       }
-  
+      
       msg = new drc.joint_command_t();
       msg.robot_name = robot_name;
       msg.num_joints = m_num_joints;
-      msg.name = joint_name;
+      msg.name = atlas_joint_name; // name matches order in AtlasPlugin.cpp
       
       msg.position = new double[msg.num_joints];
     	msg.velocity = new double[msg.num_joints];
@@ -75,7 +123,7 @@ public class JointCommandCoder implements drake.util.LCMCoder
           fdata.val = new double[m_num_joints];
           fdata.t = (double)msg.utime / 1000000.0;
           for (int i=0; i<m_num_joints; i++) {
-            j = m_joint_map.get(msg.name[i]);
+            j = m_drake_joint_map.get(m_rev_atlas_joint_map.get(i));
             if (j!=null) {
               index = j.intValue();
               if (mode==1)
@@ -95,11 +143,13 @@ public class JointCommandCoder implements drake.util.LCMCoder
     public LCMEncodable encode(drake.util.CoordinateFrameData d)
     {
       msg.utime = (long)(d.t*1000000);
+      int j;
       for (int i=0; i<m_num_joints; i++) {
+        j = m_atlas_joint_map.get(m_rev_drake_joint_map.get(i)).intValue();
         if (mode==1)
-          msg.effort[i] = d.val[i];
+          msg.effort[j] = d.val[i];
         else if (mode==2)
-          msg.position[i] = d.val[i];
+          msg.position[j] = d.val[i];
       }
       return msg;
     }
