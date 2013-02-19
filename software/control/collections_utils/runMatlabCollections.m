0d4a7197ff3ba44090fa98cdb98af6765a299137
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-19 21:21:59 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@1716 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/collections_utils/runMatlabCollections.m
+++ software/control/collections_utils/runMatlabCollections.m
@@ -4,7 +4,7 @@
 pos= rand(100,3) 
 pos(:,1) = pos(:,1) - 2
 ypr= rand(size(pos))/5
-plot_lcm(pos, ypr, 9, 'Triads', 5, 1)
+plot_lcm_poses(pos, ypr, 9, 'Triads', 5, 1)
 
 % 2. Plot a set of points and lines
 pos= rand(12,3)
@@ -39,4 +39,19 @@ pos = [0.5  , 0 ,0;
 pos(:,1)= pos(:,1) + 2;
 ypr= zeros(size(pos))
 ypr(:,1) = ( 90-10 + 20*rand( size(pos,1) ,1) )*pi/180
-plot_lcm(pos, ypr, 14, 'Foot Steps', 4, 1)     
+plot_lcm_poses(pos, ypr, 14, 'Foot Steps', 4, 1)
+
+
+
+% 5. A polygon:
+pos = [0.5  , 0 ,0;
+       0  , 1.05 ,0;
+       0.6, 2.05 ,0;
+       0.1, 2.9,0;
+       0.66 3.8 , 0 ;
+       0  , 4.7 ,0;
+       0.7, 5.9 ,0;
+       0.15, 6.3,0       ];
+pos(:,1)= pos(:,1) + 3;
+cols=repmat( [0,1,0], size(pos)) % red
+plot_lcm_points(pos, cols, 15, 'Polygon', 10, 1)
