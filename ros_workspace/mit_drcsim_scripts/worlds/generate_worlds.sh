49f73a1fbc9038dc42cda68efefd1d6994b8e47d
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-21 19:47:00 +0000

    cleaning up worlds
    git-svn-id: https://svn.csail.mit.edu/drc@1755 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/mit_drcsim_scripts/worlds/generate_worlds.sh
+++ ros_workspace/mit_drcsim_scripts/worlds/generate_worlds.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cat world_segments/common_top.world world_segments/objects_driving.world world_segments/objects_walking.world  world_segments/objects_manipulation.world  world_segments/common_bottom.world > test_combined.world
 
-
+# control - nothing in world:
 cat world_segments/common_top.world world_segments/common_bottom.world > test_empty.world
+# driving:
 cat world_segments/common_top.world world_segments/objects_driving.world world_segments/common_bottom.world > test_driving.world
+# walking:
 cat world_segments/common_top.world world_segments/objects_walking.world world_segments/common_bottom.world > test_walking.world
+# manipulation:
 cat world_segments/common_top.world world_segments/objects_manipulation.world world_segments/common_bottom.world > test_manipulation.world
 
 # for sisir:
