16564542c4b41c5d0332397c7b16331045ac276b
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-21 19:46:02 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@1754 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/mit_drcsim_scripts/worlds/generate_worlds.sh
+++ ros_workspace/mit_drcsim_scripts/worlds/generate_worlds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cat world_segments/common_top.world world_segments/objects_driving.world world_segments/objects_walking.world  world_segments/objects_manipulation.world  world_segments/common_bottom.world > test_combined.world
 
+
+cat world_segments/common_top.world world_segments/common_bottom.world > test_empty.world
 cat world_segments/common_top.world world_segments/objects_driving.world world_segments/common_bottom.world > test_driving.world
 cat world_segments/common_top.world world_segments/objects_walking.world world_segments/common_bottom.world > test_walking.world
 cat world_segments/common_top.world world_segments/objects_manipulation.world world_segments/common_bottom.world > test_manipulation.world
