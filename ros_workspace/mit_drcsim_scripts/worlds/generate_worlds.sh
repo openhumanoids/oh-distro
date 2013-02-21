36f6dcc45c26c12dade0fa8e51208235ac55fb80
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-21 19:44:33 +0000

    adding world segments heirarchy
    git-svn-id: https://svn.csail.mit.edu/drc@1752 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ ros_workspace/mit_drcsim_scripts/worlds/generate_worlds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cat world_segments/common_top.world world_segments/objects_driving.world world_segments/objects_walking.world  world_segments/objects_manipulation.world  world_segments/common_bottom.world > test_combined.world
+
+cat world_segments/common_top.world world_segments/objects_driving.world world_segments/common_bottom.world > test_driving.world
+cat world_segments/common_top.world world_segments/objects_walking.world world_segments/common_bottom.world > test_walking.world
+cat world_segments/common_top.world world_segments/objects_manipulation.world world_segments/common_bottom.world > test_manipulation.world
+
+# for sisir:
+cat world_segments/common_top.world world_segments/objects_manipulation.world world_segments/floatinghands_bottom.world > test_manipulation_floating_hands.world
+
