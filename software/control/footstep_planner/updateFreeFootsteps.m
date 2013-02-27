a96bd39b6d154069cfd1d861f247300358b0ae43
Robin Deits <rdeits@csail.mit.edu>
2013-02-27 22:48:52 +0000

    Restrict maximum distance between alternate foot placements (prevents unnaturally large leaps from one foot to another)
    git-svn-id: https://svn.csail.mit.edu/drc@1900 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/footstep_planner/updateFreeFootsteps.m
+++ software/control/footstep_planner/updateFreeFootsteps.m
@@ -1,20 +1,6 @@
 function [X, outputflag] = updateFreeFootsteps(X, biped, fixed_steps)
 
-function c = cost(x_flat)
-  X = locate_step_centers(x_flat);
-%   [Xright, Xleft] = biped.footPositions(X);
-%   [d1, r1] = stepDistance(Xright(:,1:(end-1)), Xright(:,2:end));
-%   [d2, r2] = stepDistance(Xleft(:,1:(end-1)), Xleft(:,2:end));
-%   c1 = sum(d1.^2 + (r1 .* (biped.max_step_length / biped.max_step_rot)).^2 + (d1 .* r1 .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
-%   c2 = sum(d2.^2 + (r2 .* (biped.max_step_length / biped.max_step_rot)).^2 + (d2 .* r2 .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
-%   c = c1 + c2;
-  [d, r] = stepDistance(X(:,1:(end-1)), X(:,2:end),1);
-  c = sum(d.^2 + (r .* (biped.max_step_length / biped.max_step_rot)).^2 + (d .* r .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
-%   plot_lcm_poses(Xright(1:3,ndx_r)', Xright(6:-1:4,ndx_r)', 1, 'Foot Steps (right)', 4, 1, 0, -1);
-%   plot_lcm_poses(Xleft(1:3,ndx_l)', Xleft(6:-1:4,ndx_l)', 2, 'Foot Steps (left)', 4, 1, 0, -1);
-
-end
-
+max_diag_dist = sqrt(biped.max_step_length^2 + biped.step_width^2);
 
 x_flat = reshape(X([1,2,6],:), 1, []);
 X = locate_step_centers(x_flat);
@@ -51,11 +37,30 @@ function [c, ceq] = nonlcon(x_flat)
   [Xright, Xleft] = biped.footPositions(X);
   [d_r, r_r] = stepDistance(Xright(:,1:(end-1)), Xright(:,2:end), 0);
   [d_l, r_l] = stepDistance(Xleft(:,1:(end-1)), Xleft(:,2:end), 0);
+  [d_alt_r, r_alt_r] = stepDistance(Xright(:,1:end-1), Xleft(:,2:end), 0);
+  [d_alt_l, r_alt_l] = stepDistance(Xleft(:,1:end-1), Xright(:,2:end), 0);
   c = [d_r - biped.max_step_length;
        d_l - biped.max_step_length;
+       d_alt_r - max_diag_dist;
+       d_alt_l - max_diag_dist;
        r_r - biped.max_step_rot;
        r_l - biped.max_step_rot];
   ceq = 0;
+end
+
+function c = cost(x_flat)
+  X = locate_step_centers(x_flat);
+%   [Xright, Xleft] = biped.footPositions(X);
+%   [d1, r1] = stepDistance(Xright(:,1:(end-1)), Xright(:,2:end));
+%   [d2, r2] = stepDistance(Xleft(:,1:(end-1)), Xleft(:,2:end));
+%   c1 = sum(d1.^2 + (r1 .* (biped.max_step_length / biped.max_step_rot)).^2 + (d1 .* r1 .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
+%   c2 = sum(d2.^2 + (r2 .* (biped.max_step_length / biped.max_step_rot)).^2 + (d2 .* r2 .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
+%   c = c1 + c2;
+  [d, r] = stepDistance(X(:,1:(end-1)), X(:,2:end),1);
+  c = sum(d.^2 + (r .* (biped.max_step_length / biped.max_step_rot)).^2 + (d .* r .* (10 * biped.max_step_length / biped.max_step_rot)).^2);
+%   plot_lcm_poses(Xright(1:3,ndx_r)', Xright(6:-1:4,ndx_r)', 1, 'Foot Steps (right)', 4, 1, 0, -1);
+%   plot_lcm_poses(Xleft(1:3,ndx_l)', Xleft(6:-1:4,ndx_l)', 2, 'Foot Steps (left)', 4, 1, 0, -1);
+
 end
 
 function X = locate_step_centers(x_flat)
