85a109b592e3656c12287a3500a9e325e2d4648e
Maurice Fallon <mfallon@csail.mit.edu>
2013-02-27 03:31:01 +0000

    adding compression by factor of 4
    git-svn-id: https://svn.csail.mit.edu/drc@1893 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/image-compress/src/image-compress/image-compress.cpp
+++ software/perception/image-compress/src/image-compress/image-compress.cpp
@@ -1,12 +1,14 @@
+// 1024x544 from Gazebo
+// reduced by 4 to 256x136 - 5KB per image @ 50% quality 
+// (1024x544 per image @ 50% quality is 40KB for same image)
 //
-// TODO:
-// Add resizing as an option
-//
-// 320x240 images from Gazebo:
+// Older Calculations:
+// 320x240 image:
 // 1.8MB per sec @ 8Hz uncompressed [230kB per image = 320w*240h*3stride]
 // 145KB per sec @ 8Hz 94% quality jpeg [18KB per image]
 // 50KB per sec @ 8Hz 50% quality jpeg [6.25KB per image]
 // 21kB per sec @ 8Hz 10% quality jpeg [1.6KB per image = about 13kbits]
+
 
 #include <iostream>
 #include <stdio.h>
@@ -38,6 +40,7 @@ typedef struct _Comp {
   int width;
   int height;
   int counter;
+  int resize;
 }Comp;
 
 
@@ -45,77 +48,56 @@ void on_image_frame(const lcm_recv_buf_t *rbuf, const char *channel,
     const bot_core_image_t *msg, void *user_data)
 {
   Comp *self = (Comp*) user_data;
-  
-  int compressed_size =  msg->width*msg->height*3;//image_buf_size;
-  //cout << msg->width << " and " << msg->height << "\n";
-  //cout << msg->size << "\n";
-  int compression_status = jpeg_compress_8u_rgb (msg->data, msg->width, msg->height, msg->width*3,
-                                                     self->image_buf, &compressed_size, self->jpeg_quality);
-  if (0 != compression_status) {
-    fprintf(stderr, "JPEG compression failed. Not compressing...\n");
-    return;
-  }
-  
-  bot_core_image_t msgout;
-  msgout.utime = msg->utime;
-  msgout.width = msg->width;
-  msgout.height = msg->height;
-  msgout.row_stride = msg->row_stride;
-  msgout.size = compressed_size;
-  msgout.pixelformat = BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG;
-  msgout.data = self->image_buf;
-  msgout.nmetadata =0;
-  msgout.metadata = NULL;
-  
-  string channel_out = string(channel) + "_COMPRESSED";
-  bot_core_image_t_publish(self->publish_lcm, channel_out.c_str(), &msgout);
-  self->counter++;
-  if (self->counter%30 ==0){
-     cout << self->counter << " | " << msg->utime << "\n"; 
-  }
-  
-  /// 1024x544 --> 256x136
-  Mat src= Mat::zeros( msg->width , msg->height ,CV_8UC3);
+    
+  /// factor of 4: 1024x544 --> 256x136
+  Mat src= Mat::zeros( msg->height,msg->width  ,CV_8UC3);
   src.data = msg->data;
-  
-  int resize_height = msg->height/4;
-  int resize_width  = msg->width/4;
+  int resize_height = msg->height/self->resize;
+  int resize_width  = msg->width/self->resize;
   Mat img = Mat::zeros( resize_height , resize_width ,CV_8UC3);
-  // Resize src to img size
-  cv::resize(src, img, img.size());  
+  cv::resize(src, img, img.size());  // Resize src to img size
+  int compressed_size =  resize_width*resize_height*3;//image_buf_size;
+  int compression_status = jpeg_compress_8u_rgb (img.data, resize_width, resize_height, resize_width*3,
+                                                     self->image_buf, &compressed_size, self->jpeg_quality);
 
   bot_core_image_t msgout_small;
   msgout_small.utime = msg->utime;
   msgout_small.width = resize_width;
   msgout_small.height = resize_height;
   msgout_small.row_stride = 3*resize_width;
-  msgout_small.size = resize_height*resize_width*3;
-  msgout_small.pixelformat = BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB;
-  msgout_small.data = img.data;
+  msgout_small.size = compressed_size;
+  msgout_small.pixelformat = BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG;
+  msgout_small.data = self->image_buf;
   msgout_small.nmetadata =0;
   msgout_small.metadata = NULL;
-  
-  string channel_out_small = string(channel) + "_COMPRESSED_SMALL";
+  string channel_out_small = string(channel) + "_COMPRESSED";
   bot_core_image_t_publish(self->publish_lcm, channel_out_small.c_str(), &msgout_small);
   
-  
+  self->counter++;
+  if (self->counter%30 ==0){
+     cout << self->counter << " | " << msg->utime << "\n"; 
+  }  
 }
 
 int main(int argc, char ** argv) {
   int jpeg_quality = 50;
   string channel = "CAMERALEFT";
+  int resize = 4;
   ConciseArgs opt(argc, (char**)argv);
   opt.add(jpeg_quality, "j", "jpeg_quality","jpeg_quality");
   opt.add(channel, "c", "channel","channel");
+  opt.add(resize, "r", "resize","resize image by this factor");
   opt.parse();
   std::cout << "jpeg_quality: " << jpeg_quality << "\n";  
   std::cout << "channel: " << channel << "\n";  
+  std::cout << "resize: " << resize << "\n";  
   
   
   //
   Comp *self = (Comp*) calloc (1, sizeof (Comp));
   self->width = 1024;
   self->height = 544;
+  self->resize = resize;
   self->jpeg_quality = jpeg_quality;
   self->image_buf_size = self->width * self->height * 10;
   if (0 != posix_memalign((void**) &self->image_buf, 16, self->image_buf_size)) {
