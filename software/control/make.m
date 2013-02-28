75a7faa647a72cdc2ec14462615af08dbab5b944
Russ Tedrake <russt@csail.mit.edu>
2013-02-28 12:40:44 +0000

    calling drake configure/make/etc from pod makefile
    git-svn-id: https://svn.csail.mit.edu/drc@1912 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/make.m
@@ -0,0 +1,36 @@
+function make
+
+% This file is intended to be run from the Makefile.  It assumes that the 
+% environment variable 'BUILD_PREFIX' has been set.
+
+p = cd('drake');
+
+%% configure drake
+configure;
+
+%% build drake
+make;
+
+cd(p);
+
+%% write config/drc_control_setup.m
+
+BUILD_PREFIX=getenv('BUILD_PREFIX');
+
+fptr = fopen([BUILD_PREFIX,'/config/drc_control_setup.m'],'w');
+
+% setup javaclasspath
+fprintf(fptr,'\n\n% Setup javaclasspath\n');
+fprintf(fptr,'javaaddpath(''%s/share/java/lcmtypes_drc_lcmtypes.jar'');\n',BUILD_PREFIX);
+fprintf(fptr,'javaaddpath(''%s/drake/drake.jar'');\n',pwd);
+fprintf(fptr,'javaaddpath(''%s/share/java/drc_control_util.jar'');\n',BUILD_PREFIX);
+
+% tell drake about ROS
+fprintf(fptr,'\n\n% Tell drake about ROS\n');
+fprintf(fptr,'setenv(''ROS_ROOT'',''%s'');\n',getenv('ROS_ROOT'));
+fprintf(fptr,'setenv(''ROS_PACKAGE_PATH'',''%s'');\n',getenv('ROS_PACKAGE_PATH'));
+
+fclose(fptr);
+
+
+
