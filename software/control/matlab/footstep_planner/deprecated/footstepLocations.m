29b137d4417dc444a0f6f889e4793c27beb24cec
Robin Deits <rdeits@csail.mit.edu>
2013-03-01 15:23:09 +0000

    Remove extra footstep planner folder
    git-svn-id: https://svn.csail.mit.edu/drc@1922 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/deprecated/footstepLocations.m
+++
@@ -1,7 +0,0 @@
-function [X] = footstepLocations(traj, lambda, foot_angle, step_width)
-  X = traj.eval(lambda);
-	yaw = X(6,:);
-	foot_angle = yaw + foot_angle;
-	step_offset = [cos(foot_angle); sin(foot_angle); zeros(4, length(X(1,:)))] .* (step_width / 2);
-	X = X + step_offset;
-end
