dfcb1e852a8627d6557b95e9d14c1ae5b2cea908
Maurice Fallon <mfallon@csail.mit.edu>
2013-03-06 21:03:58 +0000

    adding env variables for mode
    git-svn-id: https://svn.csail.mit.edu/drc@1995 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/viewer/src/viewer/main.cpp
+++ software/viewer/src/viewer/main.cpp
@@ -113,28 +113,39 @@ int main(int argc, char *argv[])
   opt.parse();
   std::cout << "config_file: " << config_file << "\n";
   std::cout << "role: " << role << "\n";
+  string viewer_title = "(" + role + ") MIT DRC Viewer";
+  string vis_config_file = ".bot-plugin-"+ role +"-drc-viewer";
   
   //todo: comment this section
   gtk_init(&argc, &argv);
   glutInit(&argc, argv);
   g_thread_init(NULL);
+
+  
+  string lcm_url="";
+  std::string role_upper;
+  for(short i = 0; i < role.size(); ++i)
+     role_upper+= (std::toupper(role[i]));
+  if((role.compare("robot") == 0) || (role.compare("base") == 0) ){
+    for(short i = 0; i < role_upper.size(); ++i)
+       role_upper[i] = (std::toupper(role_upper[i]));
+    string env_variable_name = string("LCM_URL_DRC_" + role_upper); 
+    char* env_variable;
+    env_variable = getenv (env_variable_name.c_str());
+    if (env_variable!=NULL){
+      //printf ("The env_variable is: %s\n",env_variable);      
+      lcm_url = string(env_variable);
+    }else{
+      std::cout << env_variable_name << " environment variable has not been set ["<< lcm_url <<"]\n";     
+      exit(-1);
+    }
+  }else{
+    std::cout << "Role not understood, choose: robot or base\n";
+    return 1;
+  }   
   
   lcm_t * lcm;
-  string viewer_title="";
-  string vis_config_file="";
-  if(role.compare("robot") == 0){
-     lcm= bot_lcm_get_global(NULL);
-     viewer_title = "(Robot) MIT DRC Viewer";
-     vis_config_file = ".bot-plugin-robot-drc-viewer";
-  }else if(role.compare("base") == 0){
-     string lcm_url = "udpm://239.255.12.68:1268?ttl=1";
-     lcm= lcm_create(lcm_url.c_str());// bot_lcm_get_global(lcm_url.c_str());
-     viewer_title = "(Base) MIT DRC Viewer";
-     vis_config_file = ".bot-plugin-base-drc-viewer";
-  }else{
-    std::cout << "DRC Viewer role not understood, choose: robot or base\n";
-    return 1;
-  }
+  lcm= lcm_create(lcm_url.c_str());// bot_lcm_get_global(lcm_url.c_str());
   
   bot_glib_mainloop_attach_lcm(lcm);
   BotParam * bot_param;
