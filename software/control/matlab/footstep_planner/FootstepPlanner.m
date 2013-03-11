2917a0f192625b65fed594ee58a43258a6687110
Scott Kuindersma <kuindersma@gmail.com>
2013-03-11 21:21:43 +0000

    fixed left/right swap bug
    git-svn-id: https://svn.csail.mit.edu/drc@2082 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/FootstepPlanner.m
+++ software/control/matlab/footstep_planner/FootstepPlanner.m
@@ -20,7 +20,7 @@ classdef FootstepPlanner < DRCPlanner
       obj.plan_publisher = FootstepPlanPublisher(robot_name, 'r_foot','l_foot', 'CANDIDATE_FOOTSTEP_PLAN');
       
       obj = addInput(obj,'x0','TRUE_ROBOT_STATE',obj.biped.getStateFrame.lcmcoder,true,true);
-      obj = addInput(obj,'con', 'TRAJ_OPT_CONSTRAINT', drc.traj_opt_constraint_t, false, true);
+%       obj = addInput(obj,'con', 'TRAJ_OPT_CONSTRAINT', drc.traj_opt_constraint_t, false, true);
     end
     
     function plan(obj,navgoal,data)
@@ -36,7 +36,7 @@ classdef FootstepPlanner < DRCPlanner
 %       tright = [0,((2:size(Xright,2))-1.5)*obj.biped.step_time];
 %       tleft = ((1:size(Xleft,2))-1)*obj.biped.step_time;
       
-      obj.plan_publisher.publish(tright,Xright(1:6,:),tleft,Xleft(1:6,:));
+      obj.plan_publisher.publish(tleft,Xleft(1:6,:),tright,Xright(1:6,:));
     end
   end
 end
