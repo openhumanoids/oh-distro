9ce75ead83bfc760f9a468685d080038d15bf577
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-03-21 19:32:40 +0000

    Adds RobotStateCoder.decode method that takes a robot_state_t object as input.
    The original RobotStateCoder.decode method that takes a byte[] as input should
    work exactly as it did before. This change allows us to 
    generate drake.util.CoordinateFrameData from existing robot_state_t objects,
    such as the initial-state field of an action_sequence_t object.
    
    
    git-svn-id: https://svn.csail.mit.edu/drc@2304 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/src/RobotStateCoder.java
+++ software/control/src/RobotStateCoder.java
@@ -76,94 +76,99 @@ public class RobotStateCoder implements drake.util.LCMCoder
     {
       try {
         drc.robot_state_t msg = new drc.robot_state_t(data);
-        if (msg.robot_name.equals(m_robot_name)) {
-          Integer j;
-          int index;
-          
-          drake.util.CoordinateFrameData fdata = new drake.util.CoordinateFrameData();
-          fdata.val = new double[2*(m_num_joints+m_num_floating_joints)];
-          fdata.t = (double)msg.utime / 1000000.0;
-          for (int i=0; i<msg.num_joints; i++) {
-            j = m_joint_map.get(msg.joint_name[i]);
-            if (j!=null) {
-              index = j.intValue();
-              fdata.val[index] = msg.joint_position[i];
-              if (fdata.val[index] > Math.PI)
-                fdata.val[index] -= 2*Math.PI;
-              fdata.val[index+m_num_joints+m_num_floating_joints] = msg.joint_velocity[i];
-            }
-          }
-          
-          // get floating joint body position and orientation
-          // TODO: should be generalized eventually
-          j = m_floating_joint_map.get("base_x");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = msg.origin_position.translation.x;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.x;
-          }
-          j = m_floating_joint_map.get("base_y");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = msg.origin_position.translation.y;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.y;
-          }
-          j = m_floating_joint_map.get("base_z");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = msg.origin_position.translation.z;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.z;
-          }
-                        
-          // convert quaternion to euler
-          // note: drake uses XYZ convention
-          double[] q = new double[4];
-          q[0] = msg.origin_position.rotation.w;
-          q[1] = msg.origin_position.rotation.x;
-          q[2] = msg.origin_position.rotation.y;
-          q[3] = msg.origin_position.rotation.z;
-
-          q = quatnormalize(q);
-          double[] rpy = threeaxisrot(-2*(q[2]*q[3] - q[0]*q[1]), 
-                                    q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3], 
-                                    2*(q[1]*q[3] + q[0]*q[2]), -2.*(q[1]*q[2] - q[0]*q[3]),
-                                    q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3]);
-          
-          j = m_floating_joint_map.get("base_roll");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = rpy[0];
-            if (fdata.val[index] > Math.PI)
-              fdata.val[index] -= 2*Math.PI;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.x;
-          }
-          
-          j = m_floating_joint_map.get("base_pitch");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = rpy[1];
-            if (fdata.val[index] > Math.PI)
-              fdata.val[index] -= 2*Math.PI;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.y;
-          }
-          
-          j = m_floating_joint_map.get("base_yaw");
-          if (j!=null) {
-            index = j.intValue();
-            fdata.val[index] = rpy[2];
-            if (fdata.val[index] > Math.PI)
-              fdata.val[index] -= 2*Math.PI;
-            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.z;
-          }
-          
-          return fdata;
-        }
+        return decode(msg);
       } catch (IOException ex) {
         System.out.println("Exception: " + ex);
       }
       return null;
     }
  
+    public drake.util.CoordinateFrameData decode(drc.robot_state_t msg)
+    {
+      if (msg.robot_name.equals(m_robot_name)) {
+        Integer j;
+        int index;
+
+        drake.util.CoordinateFrameData fdata = new drake.util.CoordinateFrameData();
+        fdata.val = new double[2*(m_num_joints+m_num_floating_joints)];
+        fdata.t = (double)msg.utime / 1000000.0;
+        for (int i=0; i<msg.num_joints; i++) {
+          j = m_joint_map.get(msg.joint_name[i]);
+          if (j!=null) {
+            index = j.intValue();
+            fdata.val[index] = msg.joint_position[i];
+            if (fdata.val[index] > Math.PI)
+              fdata.val[index] -= 2*Math.PI;
+            fdata.val[index+m_num_joints+m_num_floating_joints] = msg.joint_velocity[i];
+          }
+        }
+
+        // get floating joint body position and orientation
+        // TODO: should be generalized eventually
+        j = m_floating_joint_map.get("base_x");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = msg.origin_position.translation.x;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.x;
+        }
+        j = m_floating_joint_map.get("base_y");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = msg.origin_position.translation.y;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.y;
+        }
+        j = m_floating_joint_map.get("base_z");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = msg.origin_position.translation.z;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.linear_velocity.z;
+        }
+
+        // convert quaternion to euler
+        // note: drake uses XYZ convention
+        double[] q = new double[4];
+        q[0] = msg.origin_position.rotation.w;
+        q[1] = msg.origin_position.rotation.x;
+        q[2] = msg.origin_position.rotation.y;
+        q[3] = msg.origin_position.rotation.z;
+
+        q = quatnormalize(q);
+        double[] rpy = threeaxisrot(-2*(q[2]*q[3] - q[0]*q[1]), 
+            q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3], 
+            2*(q[1]*q[3] + q[0]*q[2]), -2.*(q[1]*q[2] - q[0]*q[3]),
+            q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3]);
+
+        j = m_floating_joint_map.get("base_roll");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = rpy[0];
+          if (fdata.val[index] > Math.PI)
+            fdata.val[index] -= 2*Math.PI;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.x;
+        }
+
+        j = m_floating_joint_map.get("base_pitch");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = rpy[1];
+          if (fdata.val[index] > Math.PI)
+            fdata.val[index] -= 2*Math.PI;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.y;
+        }
+
+        j = m_floating_joint_map.get("base_yaw");
+        if (j!=null) {
+          index = j.intValue();
+          fdata.val[index] = rpy[2];
+          if (fdata.val[index] > Math.PI)
+            fdata.val[index] -= 2*Math.PI;
+          fdata.val[index+m_num_joints+m_num_floating_joints] = msg.origin_twist.angular_velocity.z;
+        }
+
+        return fdata;
+      }
+    }
+
     public LCMEncodable encode(drake.util.CoordinateFrameData d)
     {
       msg.utime = (long)(d.t*1000000);
