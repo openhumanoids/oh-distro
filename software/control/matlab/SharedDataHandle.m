3478ecf48e29a5737001560cdeaed1e117a528f0
Scott Kuindersma <kuindersma@gmail.com>
2013-03-25 20:40:13 +0000

    adding local copy of SharedDataHandle class for now
    git-svn-id: https://svn.csail.mit.edu/drc@2368 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/SharedDataHandle.m
@@ -0,0 +1,44 @@
+classdef SharedDataHandle < handle
+% a utility class to make it convenient to make a piece of data behave like
+% a handle class
+%  example usage:
+%      mex_model_ptr = SharedDataHandle(mex_model_ptr);
+%    now mex_model_ptr continues to act like it did before, but
+%    will internally be accessing the shared data structure. 
+
+  properties (SetAccess=private,GetAccess=private)
+    data
+    deleteFcn=[];
+  end
+  
+  methods 
+    function obj = SharedDataHandle(data, deleteFcn)
+      obj.data = data;
+      if (nargin>1)
+        typecheck(deleteFcn,'function_handle');
+%        valuecheck(nargin(deleteFcn),1);
+        obj.deleteFcn = deleteFcn;
+      end
+    end
+    
+    function data = getData(obj)
+      data = obj.data;
+    end
+    
+    function delete(obj)
+      if ~isempty(obj.deleteFcn)
+        feval(obj.deleteFcn,obj.data);
+      end
+    end
+    
+    % somewhat scary functions added by sk
+    function setData(obj,data)
+      obj.data = data;
+    end
+    
+    function setField(obj,fieldname,val)
+      typecheck(obj.data,'struct');
+      obj.data = setfield(obj.data,fieldname,val);
+    end
+end
+end
