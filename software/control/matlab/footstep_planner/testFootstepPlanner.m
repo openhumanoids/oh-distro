ea422dc0ad9c02841db43f6993cfc605effca83b
Robin Deits <rdeits@csail.mit.edu>
2013-03-28 19:39:31 +0000

    more detailed footstep plan tests
    git-svn-id: https://svn.csail.mit.edu/drc@2506 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/testFootstepPlanner.m
+++ software/control/matlab/footstep_planner/testFootstepPlanner.m
@@ -12,12 +12,23 @@ qstar = xstar(1:nq);
 % poses = [[0;1;0;0;0;pi/2], [1;1;0;0;0;0], [1;0;0;0;0;0]];
 % poses = [[0;1;0;0;0;pi/2], [1;1;0;0;0;0]];
 % poses = [3;3;0;0;0;-pi/4];
-% poses = [1;0;0;0;0;0];
 % poses = [0; -.4; 0;0;0;0];
-poses = [0.1;1;0;0;0;pi/2];
-% poses = [1;1;0;0;0;0];
 % poses = [0;1;0;0;0;0];
-[xtraj, qtraj] = r.walkingPlan(x0, qstar, [poses(:,end); 30]);
+
+% Sidestep
+% poses = [0; 0.4; 0;0;0;0];
+
+
+% Walk straight forward
+% poses = [1;0;0;0;0;0];
+
+% Diagonal
+% poses = [1;1;0;0;0;0];
+
+% Left turn, then straight
+poses = [0;1;0;0;0;pi/2];
+
+[xtraj, qtraj] = r.walkingPlan(x0, qstar, [poses(:,end); 10]);
 
 visualizer = r.constructVisualizer();
 visualizer.playback(xtraj, struct('slider', true));
