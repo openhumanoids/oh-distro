1f1ef3a8b2ab9e14dbbc3c875009fc21b84e2b49
Robin Deits <rdeits@csail.mit.edu>
2013-04-04 19:15:15 +0000

    fix bug when leading with left foot
    git-svn-id: https://svn.csail.mit.edu/drc@2641 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/@Biped/createInitialSteps.m
+++ software/control/matlab/footstep_planner/@Biped/createInitialSteps.m
@@ -8,9 +8,14 @@ function [X, foot_goals] = createInitialSteps(biped, x0, poses, options)
   poses(6, poses(6,:) > pi) = poses(6, poses(6,:) > pi) - 2 * pi;
   foot_goals = struct('right', biped.stepCenter2FootCenter(poses(1:6,end), 1), 'left', biped.stepCenter2FootCenter(poses(1:6,end), 0));
 
+  if options.right_foot_lead
+    X(1) = struct('pos', biped.footOrig2Contact(foot_orig.right, 'center', 1), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', true);
+    X(2) = struct('pos', biped.footOrig2Contact(foot_orig.left, 'center', 0), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', false);
+  else
+    X(1) = struct('pos', biped.footOrig2Contact(foot_orig.left, 'center', 1), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', false);
+    X(2) = struct('pos', biped.footOrig2Contact(foot_orig.right, 'center', 0), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', true);
+  end
 
-  X(1) = struct('pos', biped.footOrig2Contact(foot_orig.right, 'center', 1), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', options.right_foot_lead);
-  X(2) = struct('pos', biped.footOrig2Contact(foot_orig.left, 'center', 0), 'time', 0, 'id', biped.getNextStepID(), 'pos_fixed', ones(6,1), 'is_right_foot', ~X(1).is_right_foot);
 
   p0 = mean([X(1).pos, X(2).pos], 2);
   if options.yaw_fixed 
