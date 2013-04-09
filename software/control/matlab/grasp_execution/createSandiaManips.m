2d04b0e5cab2a12b6d50883616e88c1b0db43561
Sisir Karumanchi <sisir@mit.edu>
2013-04-09 17:05:22 +0000

    removed support for floating hands to just hands
    git-svn-id: https://svn.csail.mit.edu/drc@2709 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/grasp_execution/createSandiaManips.m
+++ software/control/matlab/grasp_execution/createSandiaManips.m
@@ -3,10 +3,12 @@ function [r_left,r_right] = createSandiaManips()
   options.floating=false; % If this is set to true there are issues, need to fix them later
   drc_path=getenv('DRC_PATH');
 
-    filelocation=[drc_path 'models/mit_gazebo_models/mit_robot_hands/floatingbase_urdfs/sandia_hand_drake_left_floating.urdf'];
+    %filelocation=[drc_path 'models/mit_gazebo_models/mit_robot_hands/floatingbase_urdfs/sandia_hand_drake_left_floating.urdf'];
+    filelocation=[drc_path '/models/mit_gazebo_models/mit_robot_hands/drake_urdfs/sandia_hand_drake_left.urdf'];
     r_left=TimeSteppingRigidBodyManipulator(filelocation,dt,options);
 
-    filelocation=[drc_path 'models/mit_gazebo_models/mit_robot_hands/floatingbase_urdfs/sandia_hand_drake_right_floating.urdf'];
+    %filelocation=[drc_path 'models/mit_gazebo_models/mit_robot_hands/floatingbase_urdfs/sandia_hand_drake_right_floating.urdf'];
+    filelocation=[drc_path '/models/mit_gazebo_models/mit_robot_hands/drake_urdfs/sandia_hand_drake_right.urdf'];
     r_right=TimeSteppingRigidBodyManipulator(filelocation,dt,options);
 
 end
--- software/control/matlab/grasp_execution/runSimpleGraspExecutionScript.m
+++ software/control/matlab/grasp_execution/runSimpleGraspExecutionScript.m
@@ -3,16 +3,12 @@ megaclear
 [r_left,r_right] = createSandiaManips();
 
 nq_l = r_left.getNumStates/2;
-l_jointNames = r_left.getStateFrame.coordinates(7:nq_l);
+l_jointNames = r_left.getStateFrame.coordinates(1:nq_l);
 nq_r = r_right.getNumStates/2;
-r_jointNames = r_right.getStateFrame.coordinates(7:nq_r);
-
-% Temporary fix as URDFs for sticky hands are not up to date.
-% l_jointNames = regexprep(l_jointNames,'left_','l_');
-% r_jointNames = regexprep(r_jointNames,'right_','r_');
+r_jointNames = r_right.getStateFrame.coordinates(1:nq_r);
 
 lcmcoder = JLCMCoder(GraspStateCoder('atlas',l_jointNames,r_jointNames));
-nx=18+nq_l-6+nq_r-6; % this changes?
+nx=18+nq_l+nq_r;
 
 channel = ['COMMITTED_GRASP_SEED'];
 disp(channel);
@@ -22,19 +18,21 @@ grasp_state_listener.subscribe(channel);
 %defaultChannel(grasp_opt_listener)
 
 
-floating =true;
-l_jointNames_w_floating = r_left.getStateFrame.coordinates(1:nq_l);
-r_jointNames_w_floating = r_right.getStateFrame.coordinates(1:nq_r);
-Kp = [80 80 1000   40 40   5  15  10  10   15  10  10    15  10  10 15  10  10]';
-Kd = [34 34 80   14 14 2.5  1.5 0.9 0.6 1.5 0.9 0.6   1.5 0.9 0.6 1.5 0.9 0.6]';
+floating =false;
+l_jointNames = r_left.getStateFrame.coordinates(1:nq_l);
+r_jointNames = r_right.getStateFrame.coordinates(1:nq_r);
 
-l_coder = JLCMCoder(SandiaJointCommandCoder('atlas',floating,'left', l_jointNames_w_floating,Kp,Kd));
+Kp = [15  10  10   15  10  10    15  10  10 15  10  10]';
+Kd = [1.5 0.9 0.6 1.5 0.9 0.6   1.5 0.9 0.6 1.5 0.9 0.6]';
+
+l_coder = JLCMCoder(SandiaJointCommandCoder('atlas',floating,'left', l_jointNames,Kp,Kd));
 l_hand_joint_cmd_publisher=LCMCoordinateFrameWCoder('sandia_left',4*nq_l,'q',l_coder);
-r_coder = JLCMCoder(SandiaJointCommandCoder('atlas',floating,'right', r_jointNames_w_floating,Kp,Kd));
+r_coder = JLCMCoder(SandiaJointCommandCoder('atlas',floating,'right', r_jointNames,Kp,Kd));
 r_hand_joint_cmd_publisher=LCMCoordinateFrameWCoder('sandia_right',4*nq_r,'q',r_coder);
-Kp2 = [  80 80 800    40 40 5      15  0 0    15  0 0    15 0 0    15  10  10]';
-Kd2 = [   34 34 40    14 14 2.5    1.5 0 0    1.5 0 0   1.5 0 0   1.5 0.9 0.6]';
-pos_control_flag = [1.0 1.0 1.0    1.0 1.0 1.0  1.0 0 0    1.0 1.0 1.0   1.0 0 0   1.0 1.0 1.0]';
+
+Kp2 = [ 15  0 0    15  0 0    15 0 0    15  10  10]';
+Kd2 = [1.5 0 0    1.5 0 0   1.5 0 0   1.5 0.9 0.6]';
+pos_control_flag = [1.0 0 0    1.0 1.0 1.0   1.0 0 0   1.0 1.0 1.0]';
 %Kd(find(pos_control_flag==0))= 0;
 %Kp(find(pos_control_flag==0))= 10;
 while(1)
@@ -44,18 +42,22 @@ while(1)
         %fprintf('state is %f\n',x);
         
         msg = grasp_state_listener.lcmcoder.encode(ts,x);
-        %   n_l_joints =nq_l-6;
-        %   n_r_joints =nq_r-6;
-        %   l_joint_positions = x(19:19+n_l_joints-1);
-        %   r_joint_positions = x(19+n_l_joints:19+n_l_joints+n_r_joints-1);
-  
 
-        [r,p,y] = quat2angle([x(9);x(6:8)]','XYZ');   
-        q_l = [x(3:5);r;p;y;msg.l_joint_position];   
-        [r,p,y] = quat2angle([x(16);x(13:15)]','XYZ');
-        q_r = [x(10:12);r;p;y;msg.r_joint_position];%-sign(msg.r_joint_position)*0.01
-        q_l(1) =q_l(1)+0.1;
-        q_r(1) =q_r(1)-0.1;
+%         rpy = quat2rpy([x(9);x(6:8)]);   
+%         q_l = [x(3:5);rpy(1);rpy(2);rpy(3);msg.l_joint_position];   
+%         [r,p,y] = quat2angle([x(16);x(13:15)]','XYZ');
+%         rpy = quat2rpy([x(16);x(13:15)]); 
+%         q_r = [x(10:12);rpy(1);rpy(2);rpy(3);msg.r_joint_position];%-sign(msg.r_joint_position)*0.01
+%         q_l(1) =q_l(1)+0.1;
+%         q_r(1) =q_r(1)-0.1;
+
+        rpy = quat2rpy([x(9);x(6:8)]);
+        l_hand_pose = [x(3:5);rpy(1);rpy(2);rpy(3)];
+        rpy = quat2rpy([x(16);x(13:15)]); 
+        r_hand_pose = [x(10:12);rpy(1);rpy(2);rpy(3)];
+        
+        q_l = [msg.l_joint_position];   
+        q_r = [msg.r_joint_position];  
         
         K_pos=Kp;
         K_vel=Kd;
