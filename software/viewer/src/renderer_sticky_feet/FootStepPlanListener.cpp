303918fd5eab47e72440d4ccc805e1a9af408c66
Steven Jorgensen <stevenjj@mit.edu>
2013-04-09 22:00:10 +0000

    changed it to 2.5D
    git-svn-id: https://svn.csail.mit.edu/drc@2747 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/viewer/src/renderer_sticky_feet/FootStepPlanListener.cpp
+++ software/viewer/src/renderer_sticky_feet/FootStepPlanListener.cpp
@@ -187,7 +187,7 @@ void FootStepPlanListener::handleFootStepPlanMsg(const lcm::ReceiveBuffer* rbuf,
          jointpos_in =  _gl_planned_stickyfeet_list[i]->_current_jointpos;
 
         _gl_planned_stickyfeet_list[i]->set_state(T_worldframe_footframe,jointpos_in);       
-        _gl_planned_stickyfeet_list[i]->set_bodypose_adjustment_type((int)InteractableGlKinematicBody::TWO_D);
+        _gl_planned_stickyfeet_list[i]->set_bodypose_adjustment_type((int)InteractableGlKinematicBody::TWO_HALF_D);
       }
       else
       {
@@ -206,7 +206,7 @@ void FootStepPlanListener::handleFootStepPlanMsg(const lcm::ReceiveBuffer* rbuf,
          jointpos_in =  _gl_planned_stickyfeet_list[i]->_current_jointpos;
         
         _gl_planned_stickyfeet_list[i]->set_state(T_worldframe_footframe,jointpos_in);
-        _gl_planned_stickyfeet_list[i]->set_bodypose_adjustment_type((int)InteractableGlKinematicBody::TWO_D);
+        _gl_planned_stickyfeet_list[i]->set_bodypose_adjustment_type((int)InteractableGlKinematicBody::TWO_HALF_D);
       }  
       
       
