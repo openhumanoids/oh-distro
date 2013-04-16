abff77a6ca961eea442315e7ad9d76039ca65766
Maurice Fallon <mfallon@csail.mit.edu>
2013-04-16 20:08:21 +0000

    minor change to sys test
    git-svn-id: https://svn.csail.mit.edu/drc@2855 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/config/systemtests/runSystemTest.sh
+++ software/config/systemtests/runSystemTest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in `seq 1 10`;
 do
-  echo "starting run $i of 2"
+  echo "starting run $i of 10"
 	bot-procman-sheriff -l --on-script-complete exit ~/drc/software/config/drc_robot.pmd systemtest 
+  sleep 5  
 done
