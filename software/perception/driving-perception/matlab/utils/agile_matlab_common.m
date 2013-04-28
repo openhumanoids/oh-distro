4cd16fb8a683555d8414d29cc14d90589e5bac48
Sachi Hemachandra <sachih@csail.mit.edu>
2013-04-28 16:43:48 +0000

    moved driving out
    git-svn-id: https://svn.csail.mit.edu/drc@3141 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/driving-perception/matlab/utils/agile_matlab_common.m
+++
@@ -1,7 +0,0 @@
-function [x] = agile_matlab_common()
-%
-% AGILE_MATLAB_COMMON Empty function used to test whether Agile's common
-% directory is in Matlab's path.
-%   [X] = AGILE_MATLAB_COMMON() Return value X is always true(1,1).
-
-x = true(1,1);
--- software/perception/driving-perception/matlab/utils/setup_lcm.m
+++
@@ -1,4 +0,0 @@
-javaaddpath /usr/local/share/java/lcm.jar
-javaaddpath /home/spillai/envoy_latest/software/build/share/java/lcmtypes_bot2-core.jar
-javaaddpath /home/spillai/envoy_latest/software/build/share/java/lcmtypes_lcmtypes.jar
-javaaddpath /home/spillai/envoy_latest/software/build/share/java/lcmtypes_velodyne.jar
