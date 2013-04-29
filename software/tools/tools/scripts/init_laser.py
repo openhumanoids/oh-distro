f24773cec79705a9f2a2b223a2e4fbf166c10bf3
Maurice Fallon <mfallon@csail.mit.edu>
2013-04-29 15:07:07 +0000

    minor change to make it work better ;)
    git-svn-id: https://svn.csail.mit.edu/drc@3162 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/tools/tools/scripts/init_laser.py
+++ software/tools/tools/scripts/init_laser.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import os,sys
 import lcm
 import time
@@ -7,18 +6,15 @@ from lcm import LCM
 import math
 
 home_dir =os.getenv("HOME")
-#print home_dir
 sys.path.append(home_dir + "/drc/software/build/lib/python2.7/site-packages")
 sys.path.append(home_dir + "/drc/software/build/lib/python2.7/dist-packages")
 
 from drc.utime_t import utime_t
 from drc.twist_timed_t import twist_timed_t
 from drc.vector_3d_t import vector_3d_t
-
 from drc.data_request_t import data_request_t
 from drc.data_request_list_t import data_request_list_t
 ########################################################################################
-def timestamp_now (): return int (time.time () * 1000000)
 
 def on_utime(channel, data):
   m = utime_t.decode(data)
@@ -31,32 +27,33 @@ def on_utime(channel, data):
     l = vector_3d_t()
     a = vector_3d_t()
     a.x = 0.733038
-    o.linear_velocity =a
+    o.linear_velocity =l
     o.angular_velocity =a
     lc.publish("SCAN_RATE_CMD", o.encode())    
   if (abs(t - 3000000) < 5000): # after 4secs send the data request
     print "gimmie the data or else - %d" %(t)
     o = data_request_list_t()
     o.utime = t
-    r = data_request_t()
-    r.type = 1
-    r.period = 10
+    r1 = data_request_t()
+    r1.type = 1
+    r1.period = 10
     o.num_requests= 3
-    o.requests.append(r)
-    r.type =2
-    o.requests.append(r)
-    r.type =6
-    o.requests.append(r)
+    o.requests.append(r1)
+    r2 = data_request_t()
+    r2.period = 10
+    r2.type =2
+    o.requests.append(r2)
+    r3 = data_request_t()
+    r3.period = 10
+    r3.type =6
+    o.requests.append(r3)
     lc.publish("DATA_REQUEST", o.encode())    
 
 ####################################################################
 lc = lcm.LCM()
 print "started"
-
 sub1 = lc.subscribe("ROBOT_UTIME", on_utime)
-
 while True:
-  ## Handle LCM if new messages have arrived.
   lc.handle()
 
 lc.unsubscribe(sub)
