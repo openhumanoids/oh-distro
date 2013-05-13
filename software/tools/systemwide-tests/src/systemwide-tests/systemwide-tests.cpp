719a67c526fcfc09ec7baf959882050c3168dd26
Maurice Fallon <mfallon@csail.mit.edu>
2013-05-13 20:14:28 +0000

    system testing works again
    git-svn-id: https://svn.csail.mit.edu/drc@3615 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/tools/systemwide-tests/src/systemwide-tests/systemwide-tests.cpp
+++ software/tools/systemwide-tests/src/systemwide-tests/systemwide-tests.cpp
@@ -10,6 +10,10 @@
 #include <lcmtypes/bot_core.hpp>
 #include <lcmtypes/drc_lcmtypes.hpp>
 
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+#include <pointcloud_tools/pointcloud_math.hpp>
+
 #include <ConciseArgs>
 
 using namespace std;
@@ -17,7 +21,7 @@ using namespace std;
 
 class Pass{
   public:
-    Pass(boost::shared_ptr<lcm::LCM> &lcm_, int stage_);
+    Pass(boost::shared_ptr<lcm::LCM> &lcm_, int stage_, int mode_);
     
     ~Pass(){
     }    
@@ -29,11 +33,14 @@ class Pass{
     void candidateFootstepPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::footstep_plan_t* msg);    
     void candidateRobotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::robot_plan_t* msg);    
     void controllerStatusHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::controller_status_t* msg);    
+    
+    Eigen::Isometry3d getRandomGoal();
     int stage_;
+    int mode_;
 };
 
-Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, int stage_): 
-    lcm_(lcm_), stage_(stage_){
+Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, int stage_, int mode_): 
+    lcm_(lcm_), stage_(stage_), mode_(mode_){
   
 
   lcm_->subscribe( "EST_ROBOT_STATE" ,&Pass::robotStateHandler,this);
@@ -44,35 +51,72 @@ Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, int stage_):
 }
 
 
+// get the robots position and rotation in rpy
+Eigen::Isometry3d Pass::getRandomGoal(){
+    float dx=0.0;
+    float dy=0.0;
+    float dyaw=0.0;
+    
+    float nx=2.0;
+    float ny=2.0;
+    float nyaw = 2.0;
+    if (mode_==0){
+      dx = 3.0 + (float)  nx*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dy = 0.0 + (float)  ny*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dyaw = 0.0 + (float) nyaw*( ((float) rand ()/(RAND_MAX)) -0.5); // -/+28degrees
+    }else if(mode_==1){
+      dx = 3.0 + (float)  nx*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dy = 3.0 + (float)  ny*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dyaw = 0.75 + (float)  nyaw*( ((float) rand ()/(RAND_MAX)) -0.5);
+    }else if(mode_==2){
+      dx =  3.0 + (float)  nx*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dy = -3.0 + (float)  ny*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dyaw = -0.75 + (float)  nyaw*( ((float) rand ()/(RAND_MAX)) -0.5);
+    }else if(mode_==3){
+      dx =   100.0*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dy =   100.0*( ((float) rand ()/(RAND_MAX)) -0.5);
+      dyaw = 100.0 + (float)  nyaw*( ((float) rand ()/(RAND_MAX)) -0.5);
+    }
+
+    Eigen::Isometry3d delta_pose;
+    delta_pose.setIdentity();
+    delta_pose.translation()  << dx, dy, 0.0;
+    delta_pose.rotate( euler_to_quat(dyaw, 0,0) );    
+    
+    std::cout << "Stage 0: DX: " << dx << " DY " << dy << " DYAW: " << 180.0*dyaw/M_PI <<"\n";   
+    return delta_pose;    
+}
+
+
+
 void Pass::robotStateHandler(const lcm::ReceiveBuffer* rbuf, 
                         const std::string& channel, const  drc::robot_state_t* msg){
   if (stage_==0){
-    // q= bot_roll_pitch_yaw_to_quat (rpy)
-    float dx = 3.0 + (float)  1.0*( ((float) rand ()/(RAND_MAX)) -0.5);
-    float dy = 0.0 + (float)  0.5*( ((float) rand ()/(RAND_MAX)) -0.5);
-    float dyaw = 0.0 + (float) 0.5*( ((float) rand ()/(RAND_MAX)) -0.5); // -/+28degrees
 
-    double quat_in[] = {msg->origin_position.rotation.w, msg->origin_position.rotation.x,
-                     msg->origin_position.rotation.y, msg->origin_position.rotation.z};
-    double rpy[3];
-    bot_quat_to_roll_pitch_yaw (quat_in, rpy); 
-    rpy[2] = rpy[2] + dyaw;
-    bot_roll_pitch_yaw_to_quat (rpy, quat_in);
+    Eigen::Isometry3d current_pose;
+    current_pose.setIdentity();
+    current_pose.translation()  << msg->origin_position.translation.x, msg->origin_position.translation.y, msg->origin_position.translation.z;
+    Eigen::Quaterniond quat_in = Eigen::Quaterniond( msg->origin_position.rotation.w, msg->origin_position.rotation.x,
+                     msg->origin_position.rotation.y, msg->origin_position.rotation.z);
+    current_pose.rotate(quat_in);    
+    Eigen::Isometry3d goal_pose = current_pose*getRandomGoal();
     
+    ////////////////////////////////
     drc::walking_goal_t goal;
     goal.utime  = msg->utime;
     goal.robot_name = "atlas";
     drc::position_3d_t goal_pos;
     drc::vector_3d_t trans;// = drc_vector_3d_t();
-    trans.x =msg->origin_position.translation.x +dx;
-    trans.y =msg->origin_position.translation.y +dy; 
+    trans.x =  goal_pose.translation().x();
+    trans.y =  goal_pose.translation().y();
     trans.z = 0.0;
     goal_pos.translation = trans;
     drc::quaternion_t quat;
-    quat.w = quat_in[0];
-    quat.x = quat_in[1];
-    quat.y = quat_in[2];
-    quat.z = quat_in[3];
+    Eigen::Quaterniond quat_out = Eigen::Quaterniond(goal_pose.rotation());
+    quat.w = quat_out.w();
+    quat.x = quat_out.x();
+    quat.y = quat_out.y();
+    quat.z = quat_out.z();
     goal_pos.rotation = quat;
     goal.goal_pos = goal_pos;
     goal.max_num_steps = 10;
@@ -88,7 +132,6 @@ void Pass::robotStateHandler(const lcm::ReceiveBuffer* rbuf,
    
     sleep(5); // added to make sure that the footstep plane is new before accepting it, could be smaller?
     stage_=1;
-    std::cout << "Stage 0: DX: " << dx << " DY " << dy << " DYAW: " << 180*dyaw/M_PI <<"\n";
     std::cout << "Stage 0: got EST_ROBOT_STATE, publishing WALKING_GOAL. Moving to Stage 1\n";
     
   }
@@ -130,19 +173,23 @@ void Pass::controllerStatusHandler(const lcm::ReceiveBuffer* rbuf,
 int main(int argc, char ** argv) {
   int verbose = 0;
   int stage = 3;
+  int mode = 0;
   ConciseArgs opt(argc, (char**)argv);
   opt.add(verbose, "v", "verbosity","0 none, 1 little, 2 debug");
   opt.add(stage, "s", "stage","Starting Stage");
+  opt.add(mode, "m", "mode","Mode 0forward 1left 2right 3random");
   opt.parse();
   std::cout << "verbose: " << verbose << "\n";    
   std::cout << "stage: " << stage << "\n";    
+  std::cout << "mode: " << mode << " [where 0forward 1left 2right 3random]\n";    
+  
 
   boost::shared_ptr<lcm::LCM> lcm(new lcm::LCM);
   if(!lcm->good()){
     std::cerr <<"ERROR: lcm is not good()" <<std::endl;
   }
   
-  Pass app(lcm, stage);
+  Pass app(lcm, stage, mode);
   cout << "System Test Ready" << endl << "============================" << endl;
   while(0 == lcm->handle());
   return 0;
