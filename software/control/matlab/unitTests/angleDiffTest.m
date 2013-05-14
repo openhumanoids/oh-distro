90077dbfc6dffabf1101e72e7603498830266166
Robin Deits <rdeits@csail.mit.edu>
2013-05-14 15:41:06 +0000

    demonstrate that angleDiff is equivalent to what the unwrap() builtin does
    git-svn-id: https://svn.csail.mit.edu/drc@3657 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/unitTests/angleDiffTest.m
+++ software/control/matlab/unitTests/angleDiffTest.m
@@ -5,11 +5,13 @@ function angleDiffTest()
     theta = random('unif', -pi, pi);
     phi2 = phi1 + theta;
     d = angleDiff(phi1, phi2);
-    if abs(d - theta) > 1e-3
+    if abs(d - theta) > 1e-3 || abs(d - diff(unwrap([phi1, phi2]))) > 1e-3
       phi1
       phi2
       theta
       d
+      unwrap(phi1, phi2)
+      diff(unwrap(phi1, phi2))
       error('angle diff unit test failed')
     end
   end
