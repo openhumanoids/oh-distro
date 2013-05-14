19faaa93d5c5cde0b3d11a2275e2c5a6a5ceb0ec
Scott Kuindersma <kuindersma@gmail.com>
2013-05-14 15:37:52 +0000

    modified zmp hold time parameter
    git-svn-id: https://svn.csail.mit.edu/drc@3655 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/planSwing.m
+++ software/control/matlab/footstep_planner/planSwing.m
@@ -9,7 +9,7 @@ debug = false;
 
 planar_clearance = 0.05;
 nom_z_clearance = 0.02;
-hold_frac = 1/7.5;
+hold_frac = 0.2;
 ramp_distance = 0.03; % m
 
 % Let lambda be a variable which indicates cartesian distance along the line from last_pos to next_pos. 
@@ -73,8 +73,8 @@ end
 traj_ts = [0, cumsum(sqrt(sum(diff(traj_pts_xyz, 1, 2).^2, 1)))] ./ options.foot_speed;
 traj_pts_xyz = [last_pos(1:3), traj_pts_xyz, next_pos(1:3)];
 
-%hold_time = traj_ts(end) * hold_frac;
-hold_time = 0.13;
+hold_time = traj_ts(end) * hold_frac;
+%hold_time = 0.13;
 traj_ts = [0, traj_ts + hold_time, traj_ts(end) + 2.5*hold_time];
 landing_time = traj_ts(end-1);
 takeoff_time = traj_ts(2);
