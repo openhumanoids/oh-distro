041d6150c0c96c19c3109dc3ae06d8b07c9186e1
Scott Kuindersma <kuindersma@gmail.com>
2013-05-14 20:42:32 +0000

    removing raw terrain map class---drc terrain map takes a raw option for now while we still need it. can now also pass in a name to display in the viewer status messages
    git-svn-id: https://svn.csail.mit.edu/drc@3667 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/util/DRCTerrainMap.m
+++ software/control/matlab/util/DRCTerrainMap.m
@@ -1,10 +1,28 @@
 classdef DRCTerrainMap < RigidBodyTerrain
 
   methods
-    function obj = DRCTerrainMap(mapWrapperFunc)
+    function obj = DRCTerrainMap(mapWrapperFunc,options)
       if (~exist('mapWrapperFunc','var'))
           mapWrapperFunc = @mapAPIwrapper;
       end
+      if nargin < 2
+        options = struct();
+      else
+        typecheck(options,'struct');
+      end
+
+      if isfield(options,'name');
+        typecheck(options.name,'char');
+      else
+        options.name = '';
+      end
+      
+      if isfield(options,'raw');
+        typecheck(options.raw,'logical');
+      else
+        options.raw = false;
+      end
+      obj.raw = options.raw;
       
       obj.map_handle = MapHandle(mapWrapperFunc);
 
@@ -14,7 +32,7 @@ classdef DRCTerrainMap < RigidBodyTerrain
       lc = lcm.lcm.LCM.getSingleton();
       lc.subscribe('MAP_DEPTH',map_mon);
       
-      msg = ['????????? : Waiting for a non-empty terrain map message... [DRCTerrainMap.m]'];
+      msg = [options.name,' : Waiting for a non-empty terrain map message... [DRCTerrainMap.m]'];
       send_status(3, 0, 0, msg );
       fprintf(1,msg);
       obj.minval=[];
@@ -41,11 +59,13 @@ classdef DRCTerrainMap < RigidBodyTerrain
       z = ones(1,size(xy,2));  normal=repmat([0;0;1],1,size(xy,2));
       [p,normal] = obj.map_handle.getClosest([xy;0*xy(1,:)]);
       z=p(3,:);
-      if any(isnan(z))  % temporary hack because the robot is initialized without knowing the ground under it's feet
-        nn=sum(isnan(z)); 
-        normal(:,isnan(z)) = [zeros(2,nn);ones(1,nn)];
-        z(isnan(z))=obj.minval;
-      end      
+      if ~obj.raw
+        if any(isnan(z))  % temporary hack because the robot is initialized without knowing the ground under it's feet
+          nn=sum(isnan(z)); 
+          normal(:,isnan(z)) = [zeros(2,nn);ones(1,nn)];
+          z(isnan(z))=obj.minval;
+        end     
+      end
     end
     
     function writeWRL(obj,fptr)
@@ -56,5 +76,6 @@ classdef DRCTerrainMap < RigidBodyTerrain
   properties
     map_handle = [];
     minval = 0;  % only used for temporary hack
+    raw = false; % hackish for footstep planner---probably going away
   end
 end
