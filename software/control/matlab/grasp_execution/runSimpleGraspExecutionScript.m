924012c8ef861b8d081b2f451a4c96ef9c42afe7
Sisir Karumanchi <sisir@mit.edu>
2013-05-19 00:02:56 +0000

    handles power grasps
    git-svn-id: https://svn.csail.mit.edu/drc@3793 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/grasp_execution/runSimpleGraspExecutionScript.m
+++ software/control/matlab/grasp_execution/runSimpleGraspExecutionScript.m
@@ -8,7 +8,7 @@ nq_r = r_right.getNumStates/2;
 r_jointNames = r_right.getStateFrame.coordinates(1:nq_r);
 
 lcmcoder = JLCMCoder(GraspStateCoder('atlas',l_jointNames,r_jointNames));
-nx=18+nq_l+nq_r;
+nx=19+nq_l+nq_r;
 
 channel = ['COMMITTED_GRASP_SEED'];
 disp(channel);
@@ -22,8 +22,8 @@ floating =false;
 l_jointNames = r_left.getStateFrame.coordinates(1:nq_l);
 r_jointNames = r_right.getStateFrame.coordinates(1:nq_r);
 
-Kp = 5*[15  10  10   15  10  10    15  10  10 15  10  10]';
-Kd = 0.1*[0.75 0.45 0.45 0.75 0.45 0.45   0.75 0.45 0.45 0.75 0.45 0.45]';
+Kp = [15  10  10   15  10  10    15  10  10 15  10  10]';
+Kd = [0.75 0.45 0.45 0.75 0.45 0.45   0.75 0.45 0.45 0.75 0.45 0.45]';
 
 l_coder = JLCMCoder(SandiaJointCommandCoder('atlas',floating,'left', l_jointNames,Kp,Kd));
 l_hand_joint_cmd_publisher=LCMCoordinateFrameWCoder('sandia_left',4*nq_l,'q',l_coder);
@@ -44,6 +44,9 @@ while(1)
         %fprintf('state is %f\n',x);
         
         msg = grasp_state_listener.lcmcoder.encode(ts,x);
+        if(msg.power_grasp==1.0)
+         pos_control_flag = [1.0 0 0    1.0 0.0 0.0   1.0 0 0   1.0 0 0]';
+        end
 
 %         rpy = quat2rpy([x(9);x(6:8)]);   
 %         q_l = [x(3:5);rpy(1);rpy(2);rpy(3);msg.l_joint_position];   
@@ -67,7 +70,13 @@ while(1)
         e_r =q_r*0;
         e_l(find(pos_control_flag>0)) = 0;
         e_r(find(pos_control_flag>0)) = 0;
-        torque =2;
+        if(msg.power_grasp==1.0)
+          torque = 20;
+          K_pos(find(pos_control_flag==0))= 0;
+          K_vel(find(pos_control_flag==0))= 0;
+        else
+          torque =1;
+        end
         if(sum(msg.l_joint_position)>0)
            e_l(find(pos_control_flag==0)) = torque;   
            %K_pos(find(pos_control_flag==0))= 0;
