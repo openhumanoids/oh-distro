eaa9c5cfcae44eb1ae3083761374abc0a88cb9ab
Scott Kuindersma <kuindersma@gmail.com>
2013-05-21 22:24:28 +0000

    small changes to default step length and zmp instep shift
    git-svn-id: https://svn.csail.mit.edu/drc@3883 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/@Biped/Biped.m
+++ software/control/matlab/footstep_planner/@Biped/Biped.m
@@ -31,8 +31,8 @@ classdef Biped < TimeSteppingRigidBodyManipulator
       obj = obj@TimeSteppingRigidBodyManipulator(urdf,dt,options);
       defaults = struct('short_step_time', 1.4,... % s
         'long_step_time', 2,...
-        'nom_forward_step', 0.3,... %m
-        'max_forward_step', 0.5,...%m
+        'nom_forward_step', 0.25,... %m
+        'max_forward_step', 0.4,...%m
         'max_backward_step', 0.20,...%m
         'max_step_width', 0.35,...%m
         'max_step_dz', 0.3,...%m
