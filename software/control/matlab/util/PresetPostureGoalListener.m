e0bbd5a2d8b0acece1a3991eb3146ab5af07650b
Sisir Karumanchi <sisir@mit.edu>
2013-05-22 17:57:31 +0000

    adding a posture goal listener
    git-svn-id: https://svn.csail.mit.edu/drc@3909 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/util/PresetPostureGoalListener.m
@@ -0,0 +1,33 @@
+classdef PresetPostureGoalListener
+    properties
+        lc
+        aggregator
+    end
+    
+    methods
+        function obj = PresetPostureGoalListener(channel)
+            obj.lc = lcm.lcm.LCM.getSingleton();
+            obj.aggregator = lcm.lcm.MessageAggregator();
+            obj.lc.subscribe(channel, obj.aggregator);
+        end
+        
+        function [X] = getNextMessage(obj, t_ms)
+            goal_msg = obj.aggregator.getNextMessage(t_ms);
+            if isempty(goal_msg)
+                X = [];
+            else
+                [X] = PresetPostureGoalListener.decodeAffGoal(drc.robot_posture_preset_t(goal_msg.data));
+            end
+        end
+        
+    end
+
+    methods(Static)
+        function X = decodeAffGoal(msg)
+                X.time = double(msg.utime)/1000000; 
+                X.preset =msg.preset;
+         end
+            
+    end
+
+end
