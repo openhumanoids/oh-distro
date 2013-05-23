72c265b85656e93efb8544ba77a8ca9612a14b6a
Maurice Fallon <mfallon@csail.mit.edu>
2013-05-23 04:25:01 +0000

    added jpeg decompress to stereo bm cloud
    git-svn-id: https://svn.csail.mit.edu/drc@3958 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/stereo-bm/src/stereo-bm/stereo-bm-cloud.cpp
+++ software/perception/stereo-bm/src/stereo-bm/stereo-bm-cloud.cpp
@@ -28,13 +28,21 @@ class Pass{
   private:
     boost::shared_ptr<lcm::LCM> lcm_;
     void imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg);   
+    void decodeImage(const  bot_core::image_t* msg);
 
+    
     std::string image_channel_;
     StereoB*  stereob_;
 
     pointcloud_lcm* pc_lcm_;      
 
     cv::Mat_<double> Q_;
+    
+    int width_;
+    int height_;
+    uint8_t* left_buf_;
+    uint8_t* right_buf_;
+    uint8_t* images_buf_;    
 };
 
 Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, std::string image_channel_, float scale):
@@ -43,6 +51,13 @@ Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, std::string image_channel_, float 
   lcm_->subscribe( image_channel_ ,&Pass::imageHandler,this);
   stereob_ = new StereoB(lcm_);
   stereob_->setScale(scale);
+  
+  height_=800;
+  width_=800;
+  int image_size = 800*800;
+  left_buf_ = (uint8_t*) malloc(3*image_size);
+  right_buf_ = (uint8_t*) malloc(3*image_size);
+  images_buf_ =(uint8_t*) malloc(6*image_size);
 
   if (1==0){
     /////// From original simulation
@@ -70,6 +85,50 @@ Pass::Pass(boost::shared_ptr<lcm::LCM> &lcm_, std::string image_channel_, float 
   pc_lcm_->set_decimate( decimate_ );  
 }
 
+/// Added for RGB-to-Gray:
+int pixel_convert_8u_rgb_to_8u_gray (uint8_t *dest, int dstride, int width,
+        int height, const uint8_t *src, int sstride)
+{
+  int i, j;
+  for (i=0; i<height; i++) {
+    uint8_t *drow = dest + i * dstride;
+    const uint8_t *srow = src + i * sstride;
+    for (j=0; j<width; j++) {
+      drow[j] = 0.2125 * srow[j*3+0] +
+        0.7154 * srow[j*3+1] +
+        0.0721 * srow[j*3+2];
+    }
+  }
+  return 0;
+}
+
+void Pass::decodeImage(const  bot_core::image_t* msg){
+   int buf_size = width_*height_;
+  
+  switch (msg->pixelformat) {
+    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_GRAY:
+      memcpy(left_buf_,  msg->data.data() , msg->size/2);
+      memcpy(right_buf_,  msg->data.data() + msg->size/2 , msg->size/2);
+      break;
+    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB:
+      pixel_convert_8u_rgb_to_8u_gray(  left_buf_, width_, width_, height_, msg->data.data(),  width_*3);
+      pixel_convert_8u_rgb_to_8u_gray(  right_buf_, width_,  width_, height_, msg->data.data() + msg->size/2,  width_*3);
+      break;
+    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG:
+      // for some reason msg->row_stride is 0, so we use msg->width instead.
+      // Assumes Gray Scale:
+      jpeg_decompress_8u_gray(msg->data.data(), msg->size, images_buf_,
+                              msg->width, msg->height, msg->width);
+      std::copy(images_buf_          , images_buf_+buf_size   , left_buf_);
+      std::copy(images_buf_+buf_size , images_buf_+2*buf_size , right_buf_);
+      
+      break;
+    default:
+      fprintf(stderr, "Unrecognized image format\n");
+      break;
+  }  
+}
+
 void Pass::imageHandler(const lcm::ReceiveBuffer* rbuf, 
                         const std::string& channel, const  bot_core::image_t* msg){
   cv::Mat left_img, right_img;
@@ -81,8 +140,12 @@ void Pass::imageHandler(const lcm::ReceiveBuffer* rbuf,
   if (right_img.empty() || right_img.rows != h || right_img.cols != w)
         right_img.create(h, w, CV_8UC1);
 
-  memcpy(left_img.data,  msg->data.data() , msg->size/2);
-  memcpy(right_img.data,  msg->data.data() + msg->size/2 , msg->size/2);
+  decodeImage(msg);
+  left_img.data = left_buf_;
+  right_img.data = right_buf_;
+  
+//  memcpy(left_img.data,  msg->data.data() , msg->size/2);
+//  memcpy(right_img.data,  msg->data.data() + msg->size/2 , msg->size/2);
   stereob_->doStereoB(left_img, right_img);
   stereob_->sendRangeImage(msg->utime);
 
