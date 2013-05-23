88a5e8d513135d8b1e24763cfe17a3771b68f53b
Robin Deits <rdeits@csail.mit.edu>
2013-05-23 19:52:56 +0000

    Walking planner is now more transactional. Details:
    Walking planner listens on APPROVED_FOOTSTEP_PLAN and COMMITTED_FOOTSTEP_PLAN. Messages to either result in robot plans being generated and sent to the viewer for review. Messages to COMMITTED_FOOTSTEP_PLAN additionally result in a walking_plan being sent over the WALKING_PLAN channel, to which the controller is listening.
    
    
    git-svn-id: https://svn.csail.mit.edu/drc@3994 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/runWalkingPlanner.m
+++ software/control/matlab/runWalkingPlanner.m
@@ -35,9 +35,11 @@ while true
   if ~lcm_plan
     footsteps = planFootsteps(r, x0, navgoal, struct('plotting', true, 'interactive', false));
   else
-    footstep_plan_listener = FootstepPlanListener('COMMITTED_FOOTSTEP_PLAN');
+    approved_footstep_plan_listener = FootstepPlanListener('APPROVED_FOOTSTEP_PLAN');
+    committed_footstep_plan_listener = FootstepPlanListener('COMMITTED_FOOTSTEP_PLAN');
     msg ='Walk Plan : Listening for plans'; disp(msg); send_status(6,0,0,msg);
     waiting = true;
+    committed = false;
     foottraj = [];
 
     while waiting
@@ -46,22 +48,39 @@ while true
         x0=x;
       end
        
-      footsteps = footstep_plan_listener.getNextMessage(10);
+      footsteps = committed_footstep_plan_listener.getNextMessage(10);
       if (~isempty(footsteps))
-        msg ='Walk Plan : plan received'; disp(msg); send_status(6,0,0,msg);
+        msg ='Walk Plan : committed plan received'; disp(msg); send_status(6,0,0,msg);
         waiting = false;
+        committed = true;
+      else
+        footsteps = approved_footstep_plan_listener.getNextMessage(10);
+        if (~isempty(footsteps))
+          msg ='Walk Plan : plan received'; disp(msg); send_status(6,0,0,msg);
+          waiting = false;
+          committed = false;
+        end
       end
     end
   end
   [xtraj, qtraj, htraj, supptraj, comtraj, lfoottraj,rfoottraj, V, ts,zmptraj] = walkingPlanFromSteps(r, x0, qstar, footsteps);
+  hddot = fnder(htraj,2);
   
   % publish robot plan
   msg ='Walk Plan : Publishing robot plan...'; disp(msg); send_status(6,0,0,msg);
   joint_names = r.getStateFrame.coordinates(1:getNumDOF(r));
   joint_names = regexprep(joint_names, 'pelvis', 'base', 'preservecase'); % change 'pelvis' to 'base'
   plan_pub = RobotPlanPublisher('atlas',joint_names,true,'CANDIDATE_ROBOT_PLAN');
-
   plan_pub.publish(ts,xtraj);
+
+  if committed
+    msg ='Walk Plan : Publishing committed plan...'; disp(msg); send_status(6,0,0,msg);
+    walking_pub = WalkingPlanPublisher('WALKING_PLAN');
+    walking_pub.publish(0,struct('S',V.S,'s1',V.s1,'htraj',htraj,'hddtraj', ...
+      hddot,'supptraj',supptraj,'comtraj',comtraj,'qtraj',[],...
+      'lfoottraj',lfoottraj,'rfoottraj',rfoottraj,'zmptraj',zmptraj));
+  end
+
 
   % if 0 % do proper time-varying linear system approach
   %   disp('Computing ZMP controller...');
@@ -69,7 +88,6 @@ while true
   %   [~,V] = ZMPtracker(limp,zmptraj); 
   % end
 
-  hddot = fnder(htraj,2);
 
   if debug
     tt = 0:0.04:ts(end);
@@ -81,36 +99,36 @@ while true
     plot_lcm_points(compoints',[zeros(length(tt),1), ones(length(tt),1), zeros(length(tt),1)],555,'Desired COM',1,true);
   end  
 
-  msg ='Walk Plan : Waiting for confirmation...'; disp(msg); send_status(6,0,0,msg);
-  plan_listener = RobotPlanListener('COMMITTED_ROBOT_PLAN',true);
-  reject_listener = RobotPlanListener('REJECTED_ROBOT_PLAN',true);
-  waiting = true;
-  execute = true;
-  while waiting
-    rplan = plan_listener.getNextMessage(100);
-    if (~isempty(rplan))
-      % for now don't do anything with it, just use it as a flag
-      msg ='Walk Plan : Confirmed. Executing...'; disp(msg); send_status(6,0,0,msg);
-      waiting = false;
-    end
-    rplan = reject_listener.getNextMessage(100);
-    if (~isempty(rplan))
-      % for now don't do anything with it, just use it as a flag
-      disp('Walk Plan : Plan rejected.');
-      waiting = false;
-      execute = false;
-    else 
-    %   plan_pub.publish(ts,xtraj);
-      pause(0.5);
-    end
-  end
+  % msg ='Walk Plan : Waiting for confirmation...'; disp(msg); send_status(6,0,0,msg);
+  % plan_listener = RobotPlanListener('COMMITTED_ROBOT_PLAN',true);
+  % reject_listener = RobotPlanListener('REJECTED_ROBOT_PLAN',true);
+  % waiting = true;
+  % execute = true;
+  % while waiting
+  %   rplan = plan_listener.getNextMessage(100);
+  %   if (~isempty(rplan))
+  %     % for now don't do anything with it, just use it as a flag
+  %     msg ='Walk Plan : Confirmed. Executing...'; disp(msg); send_status(6,0,0,msg);
+  %     waiting = false;
+  %   end
+  %   rplan = reject_listener.getNextMessage(100);
+  %   if (~isempty(rplan))
+  %     % for now don't do anything with it, just use it as a flag
+  %     disp('Walk Plan : Plan rejected.');
+  %     waiting = false;
+  %     execute = false;
+  %   else 
+  %   %   plan_pub.publish(ts,xtraj);
+  %     pause(0.5);
+  %   end
+  % end
 
-  if execute
-    walking_pub = WalkingPlanPublisher('COMMITTED_WALKING_PLAN');
-    walking_pub.publish(0,struct('S',V.S,'s1',V.s1,'htraj',htraj,'hddtraj', ...
-      hddot,'supptraj',supptraj,'comtraj',comtraj,'qtraj',[],...
-      'lfoottraj',lfoottraj,'rfoottraj',rfoottraj,'zmptraj',zmptraj));
-  end
+  % if execute
+  %   walking_pub = WalkingPlanPublisher('WALKING_PLAN');
+  %   walking_pub.publish(0,struct('S',V.S,'s1',V.s1,'htraj',htraj,'hddtraj', ...
+  %     hddot,'supptraj',supptraj,'comtraj',comtraj,'qtraj',[],...
+  %     'lfoottraj',lfoottraj,'rfoottraj',rfoottraj,'zmptraj',zmptraj));
+  % end
 end
 
 end
