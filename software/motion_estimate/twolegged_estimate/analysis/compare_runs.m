92083a40730461bb318e68dea92b0de2f6941cc3
Mathew Antone <antone@csail.mit.edu>
2013-05-26 19:13:01 +0000

    sort csv files by A,B,C
    git-svn-id: https://svn.csail.mit.edu/drc@4101 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/motion_estimate/twolegged_estimate/analysis/compare_runs.m
+++ software/motion_estimate/twolegged_estimate/analysis/compare_runs.m
@@ -7,6 +7,17 @@ for i = 1:numel(d)
     files{end+1} = filename;
 end
 
+% sort by A,B,C
+filelist = cell(size(files));
+for i = 1:numel(files)
+    [~,b,c] = fileparts(files{i});
+    filelist{i} = [b,c];
+end
+a_matches = ~cellfun(@isempty,strfind(filelist,'_A_'));
+b_matches = ~cellfun(@isempty,strfind(filelist,'_B_'));
+c_matches = ~cellfun(@isempty,strfind(filelist,'_C_'));
+files = [sort(files(a_matches)),sort(files(b_matches)),sort(files(c_matches))];
+
 results = struct('filename',{},'name',{},'vars',{},'stats',{});
 for i = 1:numel(files)
     result.filename = files{i};
