e4635af25aaad75f0284db1ab53d05820b8c567d
Maurice Fallon <mfallon@csail.mit.edu>
2013-05-27 14:00:30 +0000

    tool
    git-svn-id: https://svn.csail.mit.edu/drc@4162 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/tools/tools/scripts/bandwidth_scope.m
+++ software/tools/tools/scripts/bandwidth_scope.m
@@ -1,6 +1,11 @@
 function read_shaper_usage()
+% point after the initial startup
+% where the bandwidth will be typical
+% set to 0.0 to see all the bandwidth
+end_of_init_time = 1.0
+
 close all
-d=getFile();
+d=getFile( end_of_init_time );
 
 if (1==0)
     bands_rx =[256,128,64,32]
@@ -15,8 +20,8 @@ end
 figure
 subplot(2,3,1)
 plot( d.sec,   d.data_rx )
-legend( d.names_rx,'Location','NorthWest')
-title('Rx [KB]')
+legend( d.names_rx,'Location','NorthWest'); 
+title('Rx [KB]'); axis([0 inf -inf inf])
 
 
 subplot(2,3,2)
@@ -25,14 +30,14 @@ area(d.sec, d.data_rx)
 grid on
 legend( d.names_rx)
 set(gca,'Layer','top')
-title ('Cum. Rx [KB]')
+title ('Cum. Rx [KB]') ; axis([0 inf -inf inf])
 for i=1:size(bands_rx,2)
     plot(d.sec,  cumsum(d.sec,2)*bands_rx(i) ,['--' color_list(i)],'LineWidth',2)
 end
 
 subplot(2,3,3)
 plot( d.sec(2:end),   diff (d.data_rx) )
-title('Rate Rx [KB]')
+title('Rate Rx [KB]'); axis([0 inf -inf inf])
 
 
 
@@ -41,7 +46,7 @@ hold on
 subplot(2,3,4)
 plot( d.sec,   d.data_tx )
 legend( d.names_tx,'Location','NorthWest')
-title('Tx [KB]')
+title('Tx [KB]') ;axis([0 inf -inf inf])
 
 subplot(2,3,5)
 hold on
@@ -49,19 +54,19 @@ area(d.sec, d.data_tx)
 grid on
 legend( d.names_tx)
 set(gca,'Layer','top')
-title ('Cum. Tx [KB]')
+title ('Cum. Tx [KB]'); axis([0 inf -inf inf])
 for i=1:size(bands_rx,2)
     plot(d.sec,  cumsum(d.sec,2)*bands_tx(i) ,['--' color_list(i)],'LineWidth',2)
 end
 
 subplot(2,3,6)
 plot( d.sec(2:end),   diff (d.data_tx) )
-title('Rate Tx [KB]')
+title('Rate Tx [KB]'); axis([0 inf -inf inf])
 
 
 
 
-function d=getFile()
+function d=getFile(end_of_init_time )
 path = '~/drc/software/config/'
 files = dir([path 'drc-network-shaper-data-usage-base-*']);
 filename = files(end).name 
@@ -75,6 +80,11 @@ data= dlmread([path filename],',',1,0);
 d.sec = data(:,1)*1E-6;
 data = data(:,[2:end]);
 
+
+
+
+
+
 for i=1:size(names,2)
    direction(i) = str2num(names{i}(end));
    temp = names{i}(1:end-1);
@@ -84,6 +94,7 @@ for i=1:size(names,2)
    %d.names(i)(1:end-1)
 end
 
+% scale from bytes to kbytes
 scale = 1024
 
 d.names_rx = names( direction==0 );
@@ -91,9 +102,18 @@ d.data_rx = data(: , (direction ==0) );
 d.names_rx  = d.names_rx (  d.data_rx(end,:) ~=0 );
 d.data_rx  = d.data_rx (:,  d.data_rx(end,:) ~=0 )/ scale;
 
-
-
 d.names_tx = names( direction==1 );
 d.data_tx = data(:, (direction ==1) );
 d.names_tx  = d.names_tx (  d.data_tx(end,:) ~=0 );
 d.data_tx  = d.data_tx (:,  d.data_tx(end,:) ~=0 )/ scale;
+
+
+
+% remove the burp at the start of our operation:
+start_idx = find ( d.sec  >= end_of_init_time , 1 , 'first');
+idx_keep = [start_idx : size(d.sec,1)];
+data_tx_blurp = d.data_tx(start_idx, :);
+d.data_tx = d.data_tx(idx_keep, :) - repmat ( data_tx_blurp,  size(d.data_tx(idx_keep, :),1 ),1 ) ;
+data_rx_blurp = d.data_rx(start_idx, :);
+d.data_rx = d.data_rx(idx_keep, :) - repmat ( data_rx_blurp,  size(d.data_rx(idx_keep, :),1 ),1 ) ;
+d.sec = d.sec(idx_keep);
