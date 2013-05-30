e59ef771938ead44c758dddeea7272f19e761edc
Scott Kuindersma <kuindersma@gmail.com>
2013-05-30 01:09:25 +0000

    adding support state class
    git-svn-id: https://svn.csail.mit.edu/drc@4294 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/util/SupportState.m
@@ -0,0 +1,37 @@
+classdef SupportState
+  %SUPPORTSTATE defines a set of supporting bodies and contacts for a rigid
+  %  body manipulator
+  
+  properties (SetAccess=protected)
+    bodies; % array of supporting body indices
+    contact_pts; % cell array of supporting contact point indices
+  end
+  
+  methods
+    function obj = SupportState(r,bodies,contact_pts)
+      typecheck(r,'Atlas');
+      typecheck(bodies,'double');
+      obj.bodies = bodies(bodies~=0);
+      
+      if nargin>2
+        typecheck(contact_pts,'cell');
+        sizecheck(contact_pts,length(obj.bodies));
+        for i=1:length(obj.bodies)
+          % verify that contact point indices are valid
+          if any(contact_pts{i}>size(getBodyContacts(r,obj.bodies(i)),2))
+            error('SupportState: contact_pts indices exceed number of contact points');
+          end
+        end
+        obj.contact_pts = contact_pts;
+      else
+        % use all points on body
+        obj.contact_pts = cell(length(obj.bodies),1);
+        for i=1:length(obj.bodies)
+          obj.contact_pts{i} = 1:size(getBodyContacts(r,obj.bodies(i)),2);
+        end
+      end
+    end
+  end
+  
+end
+
