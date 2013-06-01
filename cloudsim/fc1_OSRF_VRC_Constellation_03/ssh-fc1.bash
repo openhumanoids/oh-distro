ea305976d3c4ebce17a23946d1276f2983a5cac2
Toby Schneider <tes@mit.edu>
2013-06-01 12:04:47 +0000

    Added cloudsim constellation (03) keys
    git-svn-id: https://svn.csail.mit.edu/drc@4410 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ cloudsim/fc1_OSRF_VRC_Constellation_03/ssh-fc1.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# This command connects you to the simulation
+# It passes a keyfile to the ssh command and logs you in as the 
+# default user on the cloud machine
+
+# ssh -i key-fc1.pem ubuntu@10.0.0.52
+    
+#
+# This commands is similar, but it suppresses prompts and can be invoked 
+# from any directory
+#    
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@10.0.0.52
+    
