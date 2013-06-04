3a37d7b19b9180d59abfbdd485f6a67331954ff5
Paul Ilardi <paul.ilardi@baesystems.com>
2013-06-04 22:28:18 +0000

    - added interpolation to ply2pcd
    git-svn-id: https://svn.csail.mit.edu/drc@4576 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/affordance/src/demo/ply2pcd.cpp
+++ software/perception/world_model/affordance/src/demo/ply2pcd.cpp
@@ -15,6 +15,8 @@
 #include "pcl/PolygonMesh.h"
 #include <pcl/common/transforms.h>
 
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/mls.h>
 
 #include <pointcloud_tools/pointcloud_vis.hpp> // visualize pt clds and write mesh
 #include <ConciseArgs>
@@ -24,6 +26,7 @@
 using namespace std;
 using namespace pcl;
 using namespace pcl::io;
+using namespace Eigen;
 
 int main(int argc, char*argv[]){
   if(argc!=3){
@@ -35,10 +38,41 @@ int main(int argc, char*argv[]){
   std::vector< std::vector< float > > points;
   std::vector< std::vector< int > > triangles;
   affutils.getModelAsLists(argv[1], points, triangles);
-  cout << points.size() << endl;
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud=affutils.getCloudFromAffordance(points);
+
+  float incr=0.01;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  for(int i=0;i<triangles.size();i++){
+    for(int j=0;j<3;j++){
+      vector<float>& p1v = points[triangles[i][j]]; 
+      vector<float>& p2v = points[triangles[i][(j+1)%3]]; 
+      Vector3f p1(p1v[0],p1v[1],p1v[2]);
+      Vector3f p2(p2v[0],p2v[1],p2v[2]);
+      float size = (p2-p1).norm();
+      Vector3f d = (p2-p1)/size;
+      for(float f=0;;f+=incr){
+        if(f>size) f=size;
+        Vector3f p = p1+d*f;
+        //cout << f << " " << p.transpose() << endl;
+        pcl::PointXYZRGB pt;
+        pt.x = p[0];
+        pt.y = p[1];
+        pt.z = p[2];
+        cloud->points.push_back(pt);      
+        if(f==size) break;
+      }
+    }
+  }
+
   cloud->width = 1; cloud->height = cloud->size();
   cout << cloud->size() << endl;
+
+  // Create the filtering object
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::VoxelGrid<pcl::PointXYZRGB> sor;
+  sor.setInputCloud (cloud);
+  sor.setLeafSize (0.01f, 0.01f, 0.01f);
+  sor.filter (*cloud_filtered);
+
   pcl::PCDWriter writer;
-  writer.write (argv[2], *cloud, false);  
+  writer.write (argv[2], *cloud_filtered, false);  
 }
