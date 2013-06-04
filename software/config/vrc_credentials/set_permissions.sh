46b4661076fe248ecbb3daa9f2cd2657601fc894
Maurice Fallon <mfallon@csail.mit.edu>
2013-06-04 13:05:34 +0000

    adding set premis
    git-svn-id: https://svn.csail.mit.edu/drc@4542 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/config/vrc_credentials/set_permissions.sh
+++ software/config/vrc_credentials/set_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd ~/drc/cloudsim >& /dev/null&
+pushd ~/drc/software/config/vrc_credentials >& /dev/null&
 for i in `find -name "*.pem"`; do chmod 400 $i; done
 popd >& /dev/null
 
