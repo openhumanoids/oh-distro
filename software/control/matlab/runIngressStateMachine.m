9ea185c5a0a58c42bb68b212a0f440ca2b30ad10
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-06-07 17:29:27 +0000

    Fixes late initialization of options struct in runIngressStateMachine.m
    git-svn-id: https://svn.csail.mit.edu/drc@4704 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/runIngressStateMachine.m
+++ software/control/matlab/runIngressStateMachine.m
@@ -5,7 +5,8 @@ addpath(fullfile(getDrakePath,'examples','ZMP'));
 
 robot_options.floating = true;
 r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),robot_options);
-r = setTerrain(r,DRCTerrainMap(true,struct('name','IngressStateMachine','fill',true))); 
+%r = setTerrain(r,DRCTerrainMap(true,struct('name','IngressStateMachine','fill',true))); 
+r = setTerrain(r,DRCFlatTerrainMap()); 
 r = compile(r);
 
 robot_with_car = RigidBodyManipulator();
@@ -14,11 +15,12 @@ robot_with_car = addRobotFromURDF(robot_with_car,strcat(getenv('DRC_PATH'),'/mod
 robot_with_car = TimeSteppingRigidBodyManipulator(robot_with_car,0.001,robot_options);
 robot_with_car = compile(robot_with_car);
 
-options.multi_robot = robot_with_car;
+
 
 if(nargin<1) options = struct(); end
+options.multi_robot = robot_with_car;
 if(~isfield(options,'use_mex')) options.use_mex = false; end
-if(~isfield(options,'debug')) options.debug = false; end
+if(~isfield(options,'debug')) options.debug = true; end
 
 standing_controller = StandingController('standing',r,options);
 quasistatic_controller = QuasistaticMotionController('qs_motion',r,options);
