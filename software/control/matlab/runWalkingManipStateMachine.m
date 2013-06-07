820b0a8cf5f99ee8f6ba6d4094e59258d59e7727
Scott Kuindersma <kuindersma@gmail.com>
2013-06-07 19:33:04 +0000

    adding options to walking manip state machine. sorry, maurice, I thought I committed this 4 hours ago...
    git-svn-id: https://svn.csail.mit.edu/drc@4712 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/runWalkingManipStateMachine.m
+++ software/control/matlab/runWalkingManipStateMachine.m
@@ -5,15 +5,27 @@ addpath(fullfile(getDrakePath,'examples','ZMP'));
 
 options.namesuffix = '';
 options.floating = true;
-r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
+
+if(~isfield(options,'use_hand_ft')) options.use_hand_ft = false; end
+if(~isfield(options,'use_mex')) options.use_mex = true; end
+if(~isfield(options,'debug')) options.debug = false; end
+
+
+if (options.use_hand_ft)
+  urdf = strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact.urdf');
+else
+  urdf = strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf');
+end
+
+r = Atlas(urdf,options);
 r = removeCollisionGroupsExcept(r,{'heel','toe'});
 %r = setTerrain(r,DRCTerrainMap(true,struct('name','WalkingManipStateMachine','fill',true)));
 r = setTerrain(r,DRCFlatTerrainMap());
 r = compile(r);
 
-standing_controller = StandingManipController('standing',r);
-walking_controller = WalkingController('walking',r);
-bracing_controller = BracingController('bracing',r);
+standing_controller = StandingManipController('standing',r,options);
+walking_controller = WalkingController('walking',r,options);
+bracing_controller = BracingController('bracing',r,options);
 
 controllers = struct(standing_controller.name,standing_controller, ...
                       walking_controller.name,walking_controller,...     
@@ -22,4 +34,5 @@ controllers = struct(standing_controller.name,standing_controller, ...
 state_machine = DRCStateMachine(controllers,standing_controller.name);
 
 state_machine.run();
+
 end
