479ca215d076ae8d023ad3d27226b0cff785bb37
Maurice Fallon <mfallon@csail.mit.edu>
2013-06-07 22:21:01 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@4726 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/tutorial_atlas_control/scripts/traj_yaml.py
+++ ros_workspace/tutorial_atlas_control/scripts/traj_yaml.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 import roslib; roslib.load_manifest('tutorial_atlas_control')
 import rospy, yaml, sys
+import csv_parse 
+
 from osrf_msgs.msg import JointCommands
 from sensor_msgs.msg import JointState
 from numpy import zeros, array, linspace
 from math import ceil
-
-atlasJointNames = [
-  'atlas::back_lbz', 'atlas::back_mby', 'atlas::back_ubx', 'atlas::neck_ay',
-  'atlas::l_leg_uhz', 'atlas::l_leg_mhx', 'atlas::l_leg_lhy', 'atlas::l_leg_kny', 'atlas::l_leg_uay', 'atlas::l_leg_lax',
-  'atlas::r_leg_uhz', 'atlas::r_leg_mhx', 'atlas::r_leg_lhy', 'atlas::r_leg_kny', 'atlas::r_leg_uay', 'atlas::r_leg_lax',
-  'atlas::l_arm_usy', 'atlas::l_arm_shx', 'atlas::l_arm_ely', 'atlas::l_arm_elx', 'atlas::l_arm_uwy', 'atlas::l_arm_mwx',
-  'atlas::r_arm_usy', 'atlas::r_arm_shx', 'atlas::r_arm_ely', 'atlas::r_arm_elx', 'atlas::r_arm_uwy', 'atlas::r_arm_mwx']
 
 currentJointState = JointState()
 def jointStatesCallback(msg):
@@ -19,25 +14,29 @@ def jointStatesCallback(msg):
   currentJointState = msg
 
 if __name__ == '__main__':
-  # first make sure the input arguments are correct
-  if len(sys.argv) != 3:
-    print "usage: traj_yaml.py YAML_FILE TRAJECTORY_NAME"
-    print "  where TRAJECTORY is a dictionary defined in YAML_FILE"
+  if len(sys.argv) < 2:
+    print "usage: traj_yaml.py crawling_gains.csv"
+    print "  where crawling_gains.csv is the gains and positions file"
     sys.exit(1)
-  traj_yaml = yaml.load(file(sys.argv[1], 'r'))
-  traj_name = sys.argv[2]
-  if not traj_name in traj_yaml:
-    print "unable to find trajectory %s in %s" % (traj_name, sys.argv[1])
-    sys.exit(1)
-  traj_len = len(traj_yaml[traj_name])
+
+    
 
   # Setup subscriber to atlas states
   rospy.Subscriber("/atlas/joint_states", JointState, jointStatesCallback)
 
+  d=csv_parse.csv_parse(sys.argv[1])
+
+  if len(sys.argv) == 2:
+    traj_steps = xrange(0, d.traj_len)
+    print "doing all %d steps" %( len(traj_steps) ) 
+  elif len(sys.argv) == 3:
+    traj_steps = int(sys.argv[2]) 
+    traj_steps =xrange(traj_steps-1,traj_steps)
+    print "just using step %s" % (sys.argv[2]) 
+
   # initialize JointCommands message
   command = JointCommands()
-  command.name = list(atlasJointNames)
-  n = len(command.name)
+  n = len(d.joints)
   command.position     = zeros(n)
   command.velocity     = zeros(n)
   command.effort       = zeros(n)
@@ -50,27 +49,33 @@ if __name__ == '__main__':
 
   # now get gains from parameter server
   rospy.init_node('tutorial_atlas_control')
-  for i in xrange(len(command.name)):
-    name = command.name[i]
-    command.kp_position[i]  = rospy.get_param('atlas_controller/gains/' + name[7::] + '/p')
-    command.ki_position[i]  = rospy.get_param('atlas_controller/gains/' + name[7::] + '/i')
-    command.kd_position[i]  = rospy.get_param('atlas_controller/gains/' + name[7::] + '/d')
-    command.i_effort_max[i] = rospy.get_param('atlas_controller/gains/' + name[7::] + '/i_clamp')
+  for i in xrange(len(d.joints)):
+    name = d.joints[i].name
+    command.name.append(d.joints[i].name)
+    command.kp_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/p')
+    command.ki_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/i')
+    command.kd_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/d')
+    command.i_effort_max[i] = rospy.get_param('atlas_controller/gains/' + name + '/i_clamp')
     command.i_effort_min[i] = -command.i_effort_max[i]
 
   # set up the publisher
   pub = rospy.Publisher('/atlas/joint_commands', JointCommands)
 
   # for each trajectory
-  for i in xrange(0, traj_len):
+  print traj_steps
+  for i in traj_steps:
+    print i
     # get initial joint positions
     initialPosition = array(currentJointState.position)
-    # get joint commands from yaml
-    y = traj_yaml[traj_name][i]
-    # first value is time duration
-    dt = float(y[0])
-    # subsequent values are desired joint positions
-    commandPosition = array([ float(x) for x in y[1].split() ])
+    # time duration
+    dt =1
+
+    # desired joint positions
+    commandPosition2 =[]
+    for j in xrange(len(d.joints)):
+      commandPosition2.append( d.joints[j].points[i] )
+    commandPosition = array(commandPosition2)
+
     # desired publish interval
     dtPublish = 0.02
     n = ceil(dt / dtPublish)
