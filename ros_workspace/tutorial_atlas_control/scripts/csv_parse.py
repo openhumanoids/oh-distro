bcfaa87e8a5b9309a74e3c9fb8c7c37bb5fdc16f
Maurice Fallon <mfallon@csail.mit.edu>
2013-06-07 22:21:31 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@4728 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ ros_workspace/tutorial_atlas_control/scripts/csv_parse.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# Filename: csv_parse.py
+import csv
+
+
+class Joint(object):
+    def __init__(self):
+        self.name=''
+        self.kp=0
+        self.kd=0
+        self.points=[]
+
+class Data(object):
+    def __init__(self):
+        self.joints=[] # joint names
+        self.traj_len = 0
+
+def csv_parse(fname):
+  d = Data()
+
+  with open(fname, 'rb') as csvfile:
+    greader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    init=False
+    for row in greader:
+      if(init==False):
+        init=True
+      else:
+        j=Joint()
+        j.name = row[0]
+        j.kp = float(row[1])
+        j.kd = float(row[2])
+        points=[]
+        for k in range(3,len(row) ):
+          points.append( float(row[k]) )
+        j.points=points
+        d.joints.append(j)
+        d.traj_len = len(points)
+
+  #for j in d.joints:
+  #    print "%s - %f %f %d points" %(j.name, j.kp, j.kd, len(j.points))
+
+  return d
