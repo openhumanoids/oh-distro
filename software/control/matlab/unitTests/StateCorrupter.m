1335303f93b893ac7ba0065d69192c2ddede42d9
Dehann Fourie <dehann@csail.mit.edu>
2013-06-08 17:31:15 +0000

    fixed internal noise corruption functions -- ready for integration test with Drake.
    git-svn-id: https://svn.csail.mit.edu/drc@4752 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/unitTests/StateCorrupter.m
+++ software/control/matlab/unitTests/StateCorrupter.m
@@ -3,78 +3,116 @@ classdef StateCorrupter < DrakeSystem
   methods
     function obj = StateCorrupter(robot,params)
       typecheck(robot,{'RigidBodyManipulator','TimeSteppingRigidBodyManipulator'});
-      obj = obj@DrakeSystem(0,1,getNumStates(robot),getNumStates(robot),true,true);
+      obj = obj@DrakeSystem(0,42,getNumStates(robot),getNumStates(robot),true,true);
       obj = setInputFrame(obj,getStateFrame(robot));
       obj = setOutputFrame(obj,getStateFrame(robot));
       obj.robot = robot;
       obj.params = params;
     end
     
-    function state = ramp(terms,state)
-      state.data = terms.rate*state.t;
+    function state = ramp(state)
+      state.data = state.terms.rate*state.t;
     end
-    function state = gaussmarkov(terms,state)
-      state.data = state.prev + terms.sigma * (terms.dt);
+    function state = gaussmarkov(state)
+      prev = state.mem.data(state.mem.pointer);
+      state.data = prev + state.terms.sigma*randn()*0.005; % assuming 200Hz
+      state.mem.data(state.mem.pointer) = state.data;
+      state.mem.pointer = state.mem.pointer+1;
     end
-    function state = whitenoise(terms,state)
-      state.data = terms.sigma*randn();
+    function state = whitenoise(state)
+      state.data = state.terms.sigma*randn();
     end
-    
-    function ret = addcorruption(type, terms, prev)
-      
+
+    function [ret,mem] = addcorruption(t, type, terms, prev, mem)
+
+      % own type -- starts empty
       state = struct();
-      state.prev = prev;
-      state = eval([type '(terms,state)']);
+
+      % MUX
+      state.mem = mem;
+      state.t = t;
+      state.terms = terms;
+
+      % eval
+      state = eval([type '(state);']);
+
+      % DEMUX
       ret = state.data;
+      mem = state.mem;
     end
-    
-    function ret = eval_per_group(t,types,terms,prev)
-      if (length(types) ~= length(terms))
+
+    function [ret,mem] = eval_per_group(t,types,terms,prev,mem)
+
+      if (length(types.types) ~= length(terms))
         disp('ERROR: StateCorrupter.eval_per_group -- length of types and terms do not match');
       end
-        
-      for n=1:numel(types)
-        state.data = vals;
-        state.t = t;
-          
-        ret = addcorruption(types{n},terms{n},prev);
-      end
-      
+
+      ret = zeros(3,1);
+
+      for n=1:numel(types.types)
+
+        % here we check the individual channels
+        chanoptions = 'xyz';
+        for k=1:numel(chanoptions)
+            if (isempty(strfind(types.channels,chanoptions(k)))~=1)
+             addvalue = 0;
+             [addvalue,mem] = addcorruption(t, types.types{n},terms{n},prev(k),mem);
+             ret(k) = ret(k) + addvalue;
+            end
+        end
+
+      end % for numel(types)
+
     end
-    
-    function ret = eval_groups(t,chk,var,prev)
+
+    function [ret,mem] = parse_groups(t,chk,var,prev,mem)
+
+      ret = zeros(3,1);
+
       if (isfield(chk,var))
         if (eval(['isfield(chk.' var ',''types'')']))
-          ret = eval_per_group(t, eval(['chk.',var,'''types''']),  eval(['chk.' var '.terms']), vals);
+
+
+          if (eval(['isfield(chk.',var,',''channels'')'])==0)
+            eval(['chk.',var,'.channels = ''xyz'';']);
+          end
+
+          eval(['chk.',var,'.int_types.types = chk.',var,'.types;']);
+          eval(['chk.',var,'.int_types.channels = chk.',var,'.channels;']);
+
+          [ret,mem] = eval_per_group(t, eval(['chk.',var,'.int_types']),  eval(['chk.' var '.terms']), prev, mem);
         end
       end
     end
     
-    
     function xn = update(obj,t,x,u)
       %use x as state memory
-      % x must be double vector type
-      %
-      % values on x have a fixed ordering:
-      %  [pos, angles, joints, vel, rates, jointrates, deltaT]'
-      %        6         28        6           28         1
-        
-      P = zeros(3,1);
-      E = zeros(3,1);
-      V = zeros(3,1);
-      R = zeros(3,1);
-      
-      memP = x(1:3);
-      memE = x(4:6);
-      memV = x(7:9);
-      memR = x(10:12);
-      
-      allvals = {'P','V','E','R'};
-      
+
+      %The added noise at the previous time step is available as 'prev'
+      prev.P = x(1:3);
+      prev.E = x(4:6);
+      prev.V = x(7:9);
+      prev.R = x(10:12);
+
+      % grab prevoius memory elements
+      mem.data = x(13:end);
+      mem.pointer = 1;
+
+      allvals = {'P','E','V','R'};
+
       for n=1:numel(allvals)
-        %eval_groups(t,obj.params,'P',P);
-        eval([allvals{n},'=eval_groups(t,obj.params,''',allvals{n},''',',allvals{n},',mem',allvals{n},')']);
+         eval(['[',allvals{n},',mem]=parse_groups(t,obj.params,''',allvals{n},''',','prev.',allvals{n},',mem);']);
       end
+
+      xn = zeros(length(x),1);
+      xn(1:3) = P;
+      xn(4:6) = E;
+      xn(7:9) = V;
+      xn(10:12) = R;
+
+      % store user mempry elements for the next iteration
+      xn(13:end) = mem.data;
+
     end
     
     function y = output(obj,t,x,u)
