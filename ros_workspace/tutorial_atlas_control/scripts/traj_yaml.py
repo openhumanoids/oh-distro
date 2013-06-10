ce3e537f769f0bdc39afec234683a8aca03c4f07
Maurice Fallon <mfallon@csail.mit.edu>
2013-06-10 00:18:34 +0000

    finished null to andres crawl
    git-svn-id: https://svn.csail.mit.edu/drc@4849 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/tutorial_atlas_control/scripts/traj_yaml.py
+++ ros_workspace/tutorial_atlas_control/scripts/traj_yaml.py
@@ -4,7 +4,6 @@ import rospy, yaml, sys
 import csv_parse 
 
 from atlas_msgs.msg import AtlasCommand
-from osrf_msgs.msg import JointCommands
 from sensor_msgs.msg import JointState
 from numpy import zeros, array, linspace
 from math import ceil
@@ -29,55 +28,37 @@ if __name__ == '__main__':
 
   d=csv_parse.csv_parse(sys.argv[1])
 
+  direction =1
   if len(sys.argv) == 2:
-    traj_steps = xrange(0, d.traj_len)
+    traj_steps = range(0, d.traj_len)
     print "doing all %d steps" %( len(traj_steps) ) 
   elif len(sys.argv) == 3:
     traj_steps = int(sys.argv[2]) 
-    traj_steps =xrange(traj_steps-1,traj_steps)
-    print "just using step %s" % (sys.argv[2]) 
-
-  # initialize JointCommands message
-  command = JointCommands()
-  n = len(d.joints)
-  command.position     = zeros(n)
-  command.velocity     = zeros(n)
-  command.effort       = zeros(n)
-  command.kp_position  = zeros(n)
-  command.ki_position  = zeros(n)
-  command.kd_position  = zeros(n)
-  command.kp_velocity  = zeros(n)
-  command.i_effort_min = zeros(n)
-  command.i_effort_max = zeros(n)
+    if (traj_steps == -1):
+      direction = -1 # reverses the sequence.
+      traj_steps = reversed(range(0,d.traj_len))
+    else:
+      traj_steps =xrange(traj_steps-1,traj_steps)
+      print "just using step %s" % (sys.argv[2]) 
 
   # now get gains from parameter server
   rospy.init_node('tutorial_atlas_control')
-  for i in xrange(len(d.joints)):
-    name = d.joints[i].name
-    command.name.append(d.joints[i].name)
-    command.kp_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/p')
-    command.ki_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/i')
-    command.kd_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/d')
-    command.i_effort_max[i] = rospy.get_param('atlas_controller/gains/' + name + '/i_clamp')
-    command.i_effort_min[i] = -command.i_effort_max[i]
 
-  # set up the publisher
-  pub = rospy.Publisher('/atlas/joint_commands', JointCommands)
 
   # initialize AtlasCommand message
-  command2 = AtlasCommand()
+  command = AtlasCommand()
   n = len(d.joints)
-  command2.position     = zeros(n)
-  command2.velocity     = tuple(zeros(n))
-  command2.effort       = tuple(zeros(n))
-  command2.kp_position  = tuple(zeros(n))
-  command2.ki_position  = tuple(zeros(n))
-  command2.kd_position  = tuple(zeros(n))
-  command2.kp_velocity  = tuple(zeros(n))
-  command2.k_effort  = tuple(zeros(n) +255) 
-  command2.i_effort_min = tuple(zeros(n))
-  command2.i_effort_max = tuple(zeros(n))
-  command2.desired_controller_period_ms = 5
+  command.position     = zeros(n)
+  command.velocity     = tuple(zeros(n))
+  command.effort       = tuple(zeros(n))
+  command.kp_position  = tuple(zeros(n))
+  command.ki_position  = tuple(zeros(n))
+  command.kd_position  = tuple(zeros(n))
+  command.kp_velocity  = tuple(zeros(n))
+  command.k_effort  = tuple(zeros(n) +255) 
+  command.i_effort_min = tuple(zeros(n))
+  command.i_effort_max = tuple(zeros(n))
+  command.desired_controller_period_ms = 5
 
   kp_position = [None]*n
   ki_position = [None]*n
@@ -86,17 +67,17 @@ if __name__ == '__main__':
   i_effort_min = [None]*n
   for i in xrange(len(d.joints)):
     name = d.joints[i].name
-    kp_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/p')
-    ki_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/i')
-    kd_position[i]  = rospy.get_param('atlas_controller/gains/' + name + '/d')
-    i_effort_max[i] = rospy.get_param('atlas_controller/gains/' + name + '/i_clamp')
-    i_effort_min[i] = -command2.i_effort_max[i]
+    kp_position[i]  = d.joints[i].kp
+    kd_position[i]  = d.joints[i].kd
+    ki_position[i]  = 0
+    i_effort_max[i] = 0
+    i_effort_min[i] = 0
 
-  command2.kp_position = tuple(kp_position)
-  command2.ki_position = tuple(ki_position)
-  command2.kd_position = tuple(kd_position)
-  command2.i_effort_max = tuple(i_effort_max)
-  command2.i_effort_min = tuple(i_effort_min)
+  command.kp_position = tuple(kp_position)
+  command.ki_position = tuple(ki_position)
+  command.kd_position = tuple(kd_position)
+  command.i_effort_max = tuple(i_effort_max)
+  command.i_effort_min = tuple(i_effort_min)
   pub2 = rospy.Publisher('/atlas/atlas_command', AtlasCommand)
 
 
@@ -121,8 +102,5 @@ if __name__ == '__main__':
     for ratio in linspace(0, 1, n):
       interpCommand = (1-ratio)*initialPosition + ratio * commandPosition
       command.position = [ float(x) for x in interpCommand ]
-
-      command2.position = [ float(x) for x in interpCommand ]
-      pub.publish(command)
-      pub2.publish(command2)
+      pub2.publish(command)
       rospy.sleep(dt / float(n))
