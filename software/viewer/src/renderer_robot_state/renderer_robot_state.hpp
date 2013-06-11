884fcf0994b9551c2aca933879cd07c425a2c4f7
Sisir Karumanchi <sisir@mit.edu>
2013-06-11 17:31:26 +0000

    git-svn-id: https://svn.csail.mit.edu/drc@4943 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/viewer/src/renderer_robot_state/renderer_robot_state.hpp
+++ software/viewer/src/renderer_robot_state/renderer_robot_state.hpp
@@ -330,6 +330,7 @@ inline static double get_shortest_distance_between_robot_links_and_jointdof_mark
     msg.goal_pos.rotation.x = quat_out[1];
     msg.goal_pos.rotation.y = quat_out[2];
     msg.goal_pos.rotation.z = quat_out[3];
+    msg.crawling = false;
     
     msg.nom_step_width = 0.26;
     msg.nom_forward_step = 0.25;
