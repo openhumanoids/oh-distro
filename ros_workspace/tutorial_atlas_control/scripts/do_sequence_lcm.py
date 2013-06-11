e60b6bae6ce4a9b0c1ef4ed5d6478ee2ffa286cc
Maurice Fallon <mfallon@csail.mit.edu>
2013-06-11 22:16:26 +0000

    adding rest of recovery stuff
    git-svn-id: https://svn.csail.mit.edu/drc@4973 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/tutorial_atlas_control/scripts/do_sequence_lcm.py
+++ ros_workspace/tutorial_atlas_control/scripts/do_sequence_lcm.py
@@ -17,19 +17,38 @@ home_dir =os.getenv("HOME")
 sys.path.append(home_dir + "/drc/software/build/lib/python2.7/site-packages")
 sys.path.append(home_dir + "/drc/software/build/lib/python2.7/dist-packages")
 
-from drc.utime_t import utime_t
+from drc.recovery_t import recovery_t
+from drc.system_status_t import system_status_t
 
+filenames = ['projectial_ingress','faceup_to_facedown_wriggle','flat_out', 
+             'knee_rise_set','knee_rise','knee_rise_finish']
 
-def on_utime(channel, data):
-  print "... a giant leap for robotkind"
-  filename = home_dir + "/drc/ros_workspace/tutorial_atlas_control/scripts/projectial_ingress.csv"
-  #print filename
-  carry_out_sequence.carry_out_sequence(filename, 0)
+def on_recovery(channel, data):
+  msg = system_status_t() 
+  msg.system=3
+  msg.importance=0
+  msg.frequency=0
+
+  m = recovery_t.decode(data)  
+  mode = int(m.mode)
+  filename = filenames[mode] 
+  print "Doing [%d: %s]" %(mode, filename)
+  msg.value = "start: " + filename
+  lc.publish("SYSTEM_STATUS", msg.encode())
+  full_filename = home_dir + "/drc/ros_workspace/tutorial_atlas_control/scripts/" + filename + ".csv"
+  #print full_filename
+  carry_out_sequence.carry_out_sequence(full_filename, 0)
+  print "done with sequence\n\n"
+  msg.value = "finish: " + filename
+  lc.publish("SYSTEM_STATUS", msg.encode())
+
+  
+
 
 ####################################################################
 lc = lcm.LCM()
 print "started projectile process"
-sub1 = lc.subscribe("PROJECTILE_LEAP", on_utime)
+sub1 = lc.subscribe("RECOVERY_CMD", on_recovery)
 while True:
   lc.handle()
 
