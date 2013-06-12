ae098122e2f580932fd72cbcc694bc1ce8666380
Russ Tedrake <russt@csail.mit.edu>
2013-06-12 23:25:34 +0000

    call inverseDynamics on crawlingplan
    git-svn-id: https://svn.csail.mit.edu/drc@5048 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/testCrawling.m
+++ software/control/matlab/testCrawling.m
@@ -3,11 +3,8 @@ function testCrawling()
   options.dt = 0.001;
   r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
   d = load(strcat(getenv('DRC_PATH'),'/control/matlab/data/suppine_crawl2.mat'));
-
-  actuated = getActuatedJoints(r);
-  fr = AtlasPositionRef(r,'crawling');
-  publish(fr,0,d.x0(actuated),defaultChannel(fr));
-  
+  nq = getNumDOF(r);
+  nu = getNumInputs(r);
   
   body_spec.body_ind = findLinkInd(r,'pelvis');
   body_spec.pt = zeros(3,1);
@@ -22,23 +19,39 @@ function testCrawling()
   [~,foot_spec(3).contact_pt_ind] = getContactPoints(findLink(r,'r_foot'),'heel_mid');
   [~,foot_spec(4).contact_pt_ind] = getContactPoints(findLink(r,'l_foot'),'heel_mid');
   
+  q0 = d.x0(1:nq); 
+  q0(3)=-10; % artificially put the robot below the ground so that all supports are active
+  u0 = inverseDynamics(r,q0,0*q0,0*q0,SupportState(r,[foot_spec.body_ind],{foot_spec.contact_pt_ind}));  
+  actuated = getActuatedJoints(r);
+  fr = AtlasPositionRef(r,'crawling',4);
+  publish(fr,0,[q0(actuated);zeros(nu,1);u0],defaultChannel(fr));
+  
   options.direction = 0;
   options.step_length = -.2;
   options.gait = 2;
   options.draw = false;
   
 %  [support_times,supports,V,comtraj,zmptraj,qdtraj] = 
-  q_traj = crawlingPlan(r,d.x0,body_spec,foot_spec,options)
+  [q_traj,support_times,supports] = crawlingPlan(r,d.x0,body_spec,foot_spec,options)
   qdot_traj = fnder(q_traj);
   qddot_traj = fnder(qdot_traj);
   
-  keyboard;
-  
+  breaks = getBreaks(q_traj);
+  u = zeros(getNumInputs(r),length(breaks));
+  for i=1:length(breaks)
+    t = breaks(i);
+    supp_idx = find(support_times<=t,1,'last');
+    active_supports = supports(supp_idx);
+    u(:,i) = inverseDynamics(r,eval(q_traj,t),eval(qdot_traj,t),eval(qddot_traj,t),active_supports);
+  end
   q_actuated_traj = q_traj(actuated);
-  q_actuated_traj = setOutputFrame(qd_actuated_traj,fr);
+  qdot_actuated_traj = qdot_traj(actuated);
+  u_traj = PPTrajectory(spline(breaks,u));
+  
+  command_traj = setOutputFrame([q_actuated_traj;qdot_actuated_traj;u_traj],fr);
   
   options.realtime_factor = .12;
   options.tspan = qd_actuated_traj.tspan;
-  runLCM(qd_actuated_traj,[],options);
+  runLCM(command_traj,[],options);
   
 end
