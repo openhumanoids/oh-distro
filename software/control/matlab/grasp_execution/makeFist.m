932688245aca5c99f28c5faa639fd32bda1a5a6b
Russ Tedrake <russt@csail.mit.edu>
2013-06-12 21:38:46 +0000

    script for squeezing the hands shut before crawling.
    git-svn-id: https://svn.csail.mit.edu/drc@5034 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/grasp_execution/makeFist.m
@@ -0,0 +1,27 @@
+function makeFist
+
+% sends a command to the sandia hands to make a strong fist (e.g., useful
+% for crawling, getting up from prone, etc)
+
+[r_left,r_right] = createSandiaManips();
+lc = lcm.lcm.LCM.getSingleton();
+
+Kp = repmat([80 80 80],1,4);
+Kd = repmat([10 10 10],1,4);
+
+t = 0;
+q = [0 pi/2 pi/2   0 pi/2 pi/2  0 pi/2 pi/2  0 1.3 0];
+ff = zeros(1,12);
+data = [Kp,Kd,q,ff];
+
+nq = getNumDOF(r_left);
+joint_names = r_left.getStateFrame.coordinates(1:nq);
+coder_left = JLCMCoder(SandiaJointCommandCoder('atlas',false,'left',joint_names));
+msg = coder_left.encode(t,data);
+lc.publish('L_HAND_JOINT_COMMANDS',msg);
+
+nq = getNumDOF(r_right);
+joint_names = r_right.getStateFrame.coordinates(1:nq);
+coder_right = JLCMCoder(SandiaJointCommandCoder('atlas',false,'right',joint_names));
+msg = coder_right.encode(t,data);
+lc.publish('R_HAND_JOINT_COMMANDS',msg);
