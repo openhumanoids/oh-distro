9bc88df56e131020386eb0099d92bfdc8731320e
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-06-13 18:40:50 +0000

    crawlingPlan: Fills in traj field of link_constraints. runCrawlingPlanner: puts link_constraints in controller data struct. Makes fist before publishing walking_plan.
    git-svn-id: https://svn.csail.mit.edu/drc@5097 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/runCrawlingPlanner.m
+++ software/control/matlab/runCrawlingPlanner.m
@@ -16,6 +16,7 @@ end
 
 %addpath(fullfile(pwd,'frames'));
 addpath(fullfile(getDrakePath,'examples','ZMP'));
+addpath(strcat(getenv('DRC_PATH'),'/control/matlab/grasp_execution');
 
 
 options.floating = true;
@@ -168,8 +169,9 @@ while true
     mu = goal.mu;
     crawling_plan = struct('S',V{1}.S,'s1',s1_full,'s2',s2_full,...
       'support_times',support_times_full,'supports',{supports_full},'comtraj',comtraj_full(1:2),'qtraj',qtraj_full(actuated),'mu',mu,...
-      'link_constraints',[],'zmptraj',zmptraj_full,'qnom',qstar,'ignore_terrain',goal.ignore_terrain)
+      'link_constraints',link_constraints,'zmptraj',zmptraj_full,'qnom',qstar,'ignore_terrain',goal.ignore_terrain)
     msg =['Crawl Plan (', location, '): Publishing committed plan...']; disp(msg); send_status(status_code,0,0,msg);
+    makeFist;
     walking_pub = WalkingPlanPublisher('WALKING_PLAN');
     walking_pub.publish(0,crawling_plan);
   else
