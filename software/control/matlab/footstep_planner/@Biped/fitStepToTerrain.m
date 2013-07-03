de2bb827fe3151e74532577d000a6904e88c3ee7
Robin Deits <rdeits@csail.mit.edu>
2013-07-03 22:39:53 +0000

    Vectorize
    git-svn-id: https://svn.csail.mit.edu/drc@5411 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/footstep_planner/@Biped/fitStepToTerrain.m
+++ software/control/matlab/footstep_planner/@Biped/fitStepToTerrain.m
@@ -1,6 +1,7 @@
 function ground_pos = fitStepToTerrain(biped, pos, contact_grp)
 % Take the pose of the center of the foot as pos and return an adjusted pose which matches the height and normal of the terrain under that point
 
+sizecheck(pos(:,1), [6,1]);
 if nargin < 3
   contact_grp = 'center';
 end
@@ -10,20 +11,14 @@ if needs_xform
   pos = biped.footOrig2Contact(biped.footContact2Orig(pos, contact_grp, 1), 'center', 1);
 end
 
-sizecheck(pos, [6,1]);
-
-closest_terrain_pos = pos(1:3);
-[closest_terrain_pos(3), normal] = biped.getTerrainHeight(pos(1:3));
-if normal(3) < 0
-  normal = -normal;
-end
-
+closest_terrain_pos = pos(1:3,:);
+[closest_terrain_pos(3,:), normal] = biped.getTerrainHeight(pos(1:3,:));
+normal(3,normal(3,:) < 0) = -normal(3,normal(3,:) < 0);
 
 ground_pos = pos;
-if ~any(isnan(closest_terrain_pos))
-  ground_pos(1:3) = closest_terrain_pos;
-  ground_pos = fitPoseToNormal(ground_pos, normal);
-end
+non_nan_mask = ~any(isnan(closest_terrain_pos));
+ground_pos(1:3,non_nan_mask) = closest_terrain_pos(1:3,non_nan_mask);
+ground_pos(:,non_nan_mask) = fitPoseToNormal(ground_pos(:,non_nan_mask), normal(:,non_nan_mask));
 
 if needs_xform
   ground_pos = biped.footOrig2Contact(biped.footContact2Orig(ground_pos, 'center', 1), contact_grp, 1);
