753ef1f95b417b809f17e466a1c06562c67b9b69
Maurice Fallon <mfallon@csail.mit.edu>
2013-07-13 19:11:04 +0000

    added output of laser, joint and camera but not yet depth
    git-svn-id: https://svn.csail.mit.edu/drc@5448 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/multisense/multisense/multisense_ros/src/multisense_ros/msg/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._JointDiagnostics import *
-from ._RawCamConfig import *
-from ._SensorStatus import *
-from ._RawLidarData import *
-from ._SensorDiagnostics import *
-from ._RawCamData import *
