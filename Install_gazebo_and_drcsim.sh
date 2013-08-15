77d1e59368cf4e036bc2ffe8bc5aca396c48daf2
Pat Marion <james.patrick.marion@gmail.com>
2013-08-15 17:12:01 +0000

    update Install_gazebo_and_drcsim.sh
    - don't assume source dir is located at ~/drc
    - setup required ROS environment before building sandia-hand
    
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@5769 c7283977-0100-402a-a91a-fa70b306dbfe

--- Install_gazebo_and_drcsim.sh
+++ Install_gazebo_and_drcsim.sh
@@ -4,78 +4,103 @@ GAZEBO_VERSION=1.8
 SIM_REV=2840
 MODELS_REV=310
 
-install_sandia_from_src(){
+make_parallel=-j6
+
+# get the path to DRC svn repo (where this script is located)
+cd `dirname $0`
+drc_dir=`pwd`
+work_dir=$drc_dir/software/build/drcsim-and-gazebo-builds
+
+
+clean_build_dir()
+{
+  rm -rf $work_dir
+  mkdir -p $work_dir
+}
+
+build_sandia_hand()
+{
   echo "Installing sandia-hand from src ===================="
-  CURRENT_DIR=$PWD
+
+  source /opt/ros/fuerte/setup.bash
+  export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOME/source/drc-trunk/ros_workspace:/usr/share/osrf-common-1.0/ros
   rosmake sandia_hand_driver --pre-clean
-  cd $DRC_BASE/ros_workspace/sandia-hand/  
+
+  cd $drc_dir/ros_workspace/sandia-hand
   ./install.sh
-  cd $CURRENT_DIR
-  echo " 'find_package(sandia-hand)' should now work in cmake "
+
   echo "Finished Installing sandia-hand ===================="
 }
 
-cd ~/
-mkdir ~/gazebo_versions
-mkdir ~/gazebo_versions/gazebo_$GAZEBO_REV
 
-echo "Run this script to install the 3 Gazebo Repositories"
-echo "You should run it three times in order: gazebo, drcsim, models"
-read -p "gazebo, drcsim, models? (g/d/m) " RESP
-if [ "$RESP" = "g" ]; then
-
-  make clean -C ~/drc/software/externals/bullet
+build_gazebo()
+{
   echo "CHECKING OUT GAZEBO ====================================="
-  hg clone https://bitbucket.org/osrf/gazebo ~/gazebo_versions/gazebo_$GAZEBO_REV/gazebo 
-  cd ~/gazebo_versions/gazebo_$GAZEBO_REV/gazebo 
+  hg clone https://bitbucket.org/osrf/gazebo $work_dir/gazebo
+
   echo "Applying Specific Revision of Gazebo ===================="
-  sleep 2
+  cd $work_dir/gazebo
   hg update -r$GAZEBO_REV
-  pwd
-  mkdir build
-  cd build
-  cmake -DPKG_CONFIG_PATH=/opt/ros/fuerte/lib/pkgconfig/:/opt/ros/fuerte/stacks/visualization_common/ogre/ogre/lib/pkgconfig/ .. 
-  echo "Config done on Gazebo, now to build ====================="
-  sleep 2
-  make -j6
-  sudo make install 
-  pwd
-  cd ~/
-  make -C ~/drc/software/externals/bullet
+
+  echo "Configure and build Gazebo ====================="
+  cd ..
+  rm -rf gazebo-build
+  mkdir gazebo-build
+  cd gazebo-build
+  cmake -DPKG_CONFIG_PATH=/opt/ros/fuerte/lib/pkgconfig/:/opt/ros/fuerte/stacks/visualization_common/ogre/ogre/lib/pkgconfig/ ../gazebo
+  make $make_parallel
+  sudo make install
+
+  echo "Performing a clean build of bullet ====================="
+  cd $drc_dir/software/externals/bullet
+  make clean
+  make $make_parallel
+
   echo "Finished Installing Gazebo==================="
+}
 
-elif [ "$RESP" = "d" ]; then
 
-  install_sandia_from_src
+build_drcsim()
+{
   source /opt/ros/fuerte/setup.bash
-  source /usr/local/share/gazebo-$GAZEBO_VERSION/setup.sh 
+  source /usr/local/share/gazebo-$GAZEBO_VERSION/setup.sh
+
   echo "CHECKING OUT DRCSIM ======================="
-  sleep 2
-  hg clone https://bitbucket.org/osrf/drcsim ~/gazebo_versions/gazebo_$GAZEBO_REV/drcsim 
-  cd ~/gazebo_versions/gazebo_$GAZEBO_REV/drcsim 
+  hg clone https://bitbucket.org/osrf/drcsim $work_dir/drcsim
+
+
   echo "Applying Specific Revision of DRCSIM =================="
-  sleep 2
+  cd $work_dir/drcsim
   hg update -r$SIM_REV
-  mkdir build
-  cd build
-  cmake -Dsandia-hand_DIR=$DRC_BASE/ros_workspace/sandia-hand/build ..
-  echo "cmake done on DRCSIM, now to build ====================="
-  sleep 2
-  make -j6
-  sudo make install 
-  cd ../..
+
+  echo "Configure and build DRCSIM ====================="
+  cd ..
+  rm -rf drcsim-build
+  mkdir drcsim-build
+  cd drcsim-build
+  cmake  -Dsandia-hand_DIR=$drc_dir/ros_workspace/sandia-hand/build ../drcsim
+  make $make_parallel
+  sudo make install
+
   echo "Finished Installing DRCSIM==================="
-  
-else
+}
+
+
+download_models()
+{
   echo "GETTING A NEW VERSION OF ~/gazebo/.models ====================================="
   rm ~/.gazebo/models -Rf
-  hg clone https://bitbucket.org/osrf/gazebo_models ~/.gazebo/models 
+  hg clone https://bitbucket.org/osrf/gazebo_models ~/.gazebo/models
   cd ~/.gazebo/models
   hg pull
   hg update -r$MODELS_REV
   echo "Finished Installing ~/gazebo/.models ==================="
+}
 
-fi
-
+clean_build_dir
+build_sandia_hand
+build_gazebo
+build_drcsim
+download_models
 
 
