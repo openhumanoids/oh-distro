a9c201180154bf092dd2fab451a64619850c5dd9
Maurice Fallon <mfallon@csail.mit.edu>
2013-08-19 15:57:55 +0000

    git-svn-id: https://svn.csail.mit.edu/drc/trunk@5853 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -1,4 +1,4 @@
-rosrun xacro xacro.py xacro/atlas_sandia_hands.urdf.xacro > model.urdf
+rosrun xacro xacro.py xacro/atlas_sandia_hands.urdf.xacro > model_LS_RS.urdf
 rosrun xacro xacro.py xacro/atlas_sandia_hands.urdf_sim.xacro > model_sim.urdf
 rosrun xacro xacro.py xacro/atlas_sandia_hands.urdf_sim.xacro > model_sim_gazebo.urdf
 ./renameSimGazeboURDF.sh
