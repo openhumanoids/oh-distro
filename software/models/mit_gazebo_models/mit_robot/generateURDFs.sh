7827de0bd4ff4200d63c62fca5ad7c403c586ec7
Maurice Fallon <mfallon@csail.mit.edu>
2013-08-19 17:07:31 +0000

    completing xacro scripts, seems to be finished
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@5859 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -3,15 +3,15 @@
 # 5 common (real) robot configurations:
 echo "Generating: 1/7 stumps"
 rosrun xacro xacro.py xacro/atlas_stumps.urdf.xacro > model_stumps.urdf
-echo "Generating: 2/7 LS_RS"
+echo "Generating: 2/7 Left Sandia Right Sandia"
 rosrun xacro xacro.py xacro/atlas_sandia_hands.urdf.xacro > model_LS_RS.urdf
-echo "Generating: 3/7 LI_RI"
+echo "Generating: 3/7 Left iRobot Right iRobot"
 rosrun xacro xacro.py xacro/atlas_irobot_hands.urdf.xacro > model_LI_RI.urdf
 
 
-echo "Generating: 4/7 LI_RS"
+echo "Generating: 4/7 Left iRobot Right Sandia"
 rosrun xacro xacro.py xacro/atlas_irobot_left_sandia_right.urdf.xacro > model_LI_RS.urdf
-echo "Generating: 5/7 LS_RI"
+echo "Generating: 5/7 Left Sandia Right iRobot"
 rosrun xacro xacro.py xacro/atlas_sandia_left_irobot_right.urdf.xacro > model_LS_RI.urdf
 
 # the simulated robot configuration - LCM facing and ROS/gazbo facing versions
