ec89f2ebaabe7d290158fcf60ed15bf853b2ba7f
Robin Deits <rdeits@csail.mit.edu>
2013-08-20 20:26:59 +0000

    Fix contact offset detection when a nonexistent contact group is requested
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@5896 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/systems/@Biped/findContactOffsets.m
+++ software/control/matlab/systems/@Biped/findContactOffsets.m
@@ -4,12 +4,16 @@ function offset = findContactOffsets(biped)
 
   for f = {'right', 'left'}
     foot = f{1};
-	  for g = {'toe', 'heel', 'inner'}
-	    grp = g{1};
-      gc = biped.foot_bodies.(foot).contact_pts(:,...
+	  for g = {'toe', 'heel'}
+      grp = g{1};
+      if ~any(cellfun(@(x) strcmp(x, grp), ...
+             biped.foot_bodies.(foot).collision_group_name))
+        error('There is no collision group by the name: %s', grp);
+      end
+      gc = biped.foot_bodies.(foot).contact_pts(:,[...
         biped.foot_bodies.(foot).collision_group{...
             cellfun(@(x) strcmp(x, grp), ...
-             biped.foot_bodies.(foot).collision_group_name)});
+             biped.foot_bodies.(foot).collision_group_name)}]);
       offset.(foot).(grp) = mean(gc(1:3,:), 2);
     end
     offset.(foot).center = mean([offset.(foot).toe, offset.(foot).heel], 2);
