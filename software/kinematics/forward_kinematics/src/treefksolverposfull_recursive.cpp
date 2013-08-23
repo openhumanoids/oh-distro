2ae088836ec0e2d90acf15e513a6b8f61adaedba
Sisir Karumanchi <sisir@mit.edu>
2013-08-23 02:46:08 +0000

    minor modification to error print out
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@5954 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/kinematics/forward_kinematics/src/treefksolverposfull_recursive.cpp
+++ software/kinematics/forward_kinematics/src/treefksolverposfull_recursive.cpp
@@ -58,13 +58,13 @@ void TreeFkSolverPosFull_recursive::addFrameToMap(const map<string, double>& q_i
 {
   // get pose of this segment
   KDL::Frame this_frame;
-  
+
   double jnt_p = 0;
   if (this_segment->second.segment.getJoint().getType() != Joint::None){
     map<string, double>::const_iterator jnt_pos = q_in.find(this_segment->second.segment.getJoint().getName());
     if (jnt_pos == q_in.end()){
      // ROS_DEBUG("Warning: TreeFKSolverPosFull Could not find value for joint '%s'. Skipping this tree branch", this_segment->first.c_str());
-      std::cout<<"Warning: TreeFKSolverPosFull Could not find value for joint"<< this_segment->first.c_str() << " Skipping this tree branch" << std::endl;
+      std::cout<<"Warning: TreeFKSolverPosFull Could not find value for joint "<< this_segment->second.segment.getJoint().getName() << " Skipping this tree branch" << std::endl;
       return;
     }
     jnt_p = jnt_pos->second;
@@ -111,7 +111,7 @@ void TreeFkSolverPosFull_recursive::addFrameToMap(const map<string, double>& q_i
     map<string, double>::const_iterator jnt_pos = q_in.find(this_segment->second.segment.getJoint().getName());
     if (jnt_pos == q_in.end()){
      // ROS_DEBUG("Warning: TreeFKSolverPosFull Could not find value for joint '%s'. Skipping this tree branch", this_segment->first.c_str());
-      std::cout<<"Warning: TreeFKSolverPosFull Could not find value for joint"<< this_segment->first.c_str() << " Skipping this tree branch" << std::endl;
+      std::cout<<"Warning: TreeFKSolverPosFull Could not find value for joint "<< this_segment->second.segment.getJoint().getName() <<" "<< " Skipping this tree branch" << std::endl;
       return;
     }
     jnt_p = jnt_pos->second;
