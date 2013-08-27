e8913a990625abddcc0d469d7cfee29c8a5cd06c
Maurice Fallon <mfallon@csail.mit.edu>
2013-08-27 15:21:58 +0000

    changed log format
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6014 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/tools/tools/scripts/drc-lcm-logger.sh
+++ software/tools/tools/scripts/drc-lcm-logger.sh
@@ -3,6 +3,6 @@
 # Author: Maurice Fallon
 # Writes LCM logs to a specific directory, but adds time as extension
 
-date_str=$(date +"lcmlog-%Y-%m-%d.%H:%M")
+date_str=$(date +"lcmlog-%Y-%m-%d-%H-%M")
 date_str="$DRC_BASE/logs/$date_str"
 lcm-logger $date_str
