64cbf404e66b212253c951ad0b0f5362b57409b3
Maurice Fallon <mfallon@csail.mit.edu>
2013-08-27 15:06:36 +0000

    added a tool to convert lcm log to avi
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6013 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/image_io_utils/src/tools/log_to_avi.cpp
+++ software/perception/image_io_utils/src/tools/log_to_avi.cpp
@@ -1,10 +1,7 @@
-// DRC-tags
-// - detects tags (using april tags)
-// - pairs detections with affordances in a library
-// - updates the affordances in the affordances in the store
-//
-// TODO: read library from file
-// TODO: add rate limiting (process is at about 15Hz)
+// Simple tool to parse a log file and dump out an avi file
+
+#include <signal.h>
+#include <stdlib.h>
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -17,18 +14,28 @@
 
 #include <opencv2/opencv.hpp>
 #include <lcmtypes/bot_core.hpp>
+#include <lcmtypes/multisense.hpp>
+#include <image_io_utils/image_io_utils.hpp> // to simplify jpeg/zlib compression and decompression
 
 #include <ConciseArgs>
 
 using namespace std;
 
+cv::VideoWriter output_video;
 
-cv::VideoWriter outputVideo;                    // Open the output
+struct CommandLineConfig
+{
+    string codec;
+    std::string camera_chan;
+    int frames;
+    string logfile;
+    string outputfile;
+};
 
 
 class Tags{
   public:
-    Tags(boost::shared_ptr<lcm::LCM> &lcm_, string camera_chan_);
+    Tags(boost::shared_ptr<lcm::LCM> &lcm_, const CommandLineConfig& cl_cfg_);
     
     ~Tags(){
     }    
@@ -37,64 +44,145 @@ class Tags{
     string camera_chan_;
     std::string camera_frame_;
     int width_, height_;
-    double fx_, fy_, cx_, cy_;
+    const CommandLineConfig& cl_cfg_;
+    int codec_val_;
     
+    void multisenseHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  multisense::images_t* msg);   
     void imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg);   
+    void doEncode();
+    
+    bot_core::image_t img_;  
+    image_io_utils*  imgutils_; 
+    uint8_t* img_buf_; 
+    
+    int64_t utime_init_;
 };
 
-Tags::Tags(boost::shared_ptr<lcm::LCM> &lcm_, string camera_chan_):
-    lcm_(lcm_), camera_chan_(camera_chan_){
+Tags::Tags(boost::shared_ptr<lcm::LCM> &lcm_, const CommandLineConfig& cl_cfg_):
+    lcm_(lcm_), cl_cfg_(cl_cfg_){
  
-  lcm_->subscribe( camera_chan_ ,&Tags::imageHandler,this);
+  if ((cl_cfg_.camera_chan == "CAMERA") ||  (cl_cfg_.camera_chan == "CAMERA_LEFT") ){
+    lcm_->subscribe( cl_cfg_.camera_chan ,&Tags::multisenseHandler,this);
+  }else{
+    lcm_->subscribe( cl_cfg_.camera_chan ,&Tags::imageHandler,this); 
+  }
   
-   cv::Size size2 = cv::Size(640,480);  
-
-   int codec = CV_FOURCC('M', 'J', 'P', 'G');
-  cv::VideoWriter aoutputVideo = cv::VideoWriter("video.avi",codec,15.0,size2,true);  
-  
-  if (!aoutputVideo.isOpened())
-  {
-    cout  << "Could not open the output video for write" << endl;
-  //  exit(-1);
-  }    
-  
+  if (cl_cfg_.codec  == "flv1"){
+    codec_val_ = CV_FOURCC('F','L','V','1');
+  }else if (cl_cfg_.codec == "divx"){
+    codec_val_ = CV_FOURCC('D','I','V','X');
+  }else if (cl_cfg_.codec == "h263"){
+    codec_val_ = CV_FOURCC('U','2','6','3');
+  }else { // uncompressed
+    codec_val_ =0; 
+  }
+   
+  // left these numbers very large:
+  img_buf_= (uint8_t*) malloc(3* 1524  * 1544);
+  imgutils_ = new image_io_utils( lcm_->getUnderlyingLCM(), 
+                                  1524, 
+                                  3*1544 );  
 }
     
 
 int counter =0;
+
+void Tags::multisenseHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  multisense::images_t* msg){
+  img_= msg->images[0];    
+  doEncode();
+}
+
 void Tags::imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg){
-  std::cout << msg->utime << " chans\n";
+  img_= *msg;
+  doEncode();
+}
   
-  cv::Mat img(cv::Size(640,480),CV_8UC3);
-  //img.data = msg->data;
+void Tags::doEncode(){  
+  if (counter ==0){
+    utime_init_ = img_.utime;
+    
+    cv::Size size2 = cv::Size( img_.width, img_.height);  
+    output_video = cv::VideoWriter( cl_cfg_.outputfile ,codec_val_,15.0,size2,true);  
+    
+    if (!output_video.isOpened()){
+      cout  << "Could not open the output 'video.avi' for write" << endl;
+      exit(-1);
+    }    
+  }
   
-  outputVideo << img;
+  if (counter%30 == 0){
+    char buff[100];
+    sprintf(buff, "%f", (img_.utime - utime_init_) /1E6);
+    string temp = buff;
+    
+    std::cout << (counter+1) << " frames | " << cl_cfg_.camera_chan
+            << " " << cl_cfg_.codec << " | " << temp  << " log time "
+            << img_.width << " width " << img_.height << " height\n";
+  }
+
+  imgutils_->decodeImageToRGB( &img_,  img_buf_ );
+  cv::Mat img(cv::Size( img_.width,img_.height),CV_8UC3);
+  img.data = img_buf_;
+  cv::cvtColor(img, img, CV_BGR2RGB);
+  output_video << img;
 
   counter++;
-  if (counter > 100){
-    outputVideo.release();
-
+  
+  if (cl_cfg_.frames == -1){
+  }else if (counter > cl_cfg_.frames){
+    output_video.release();
     exit(-1);
   }
 }
 
 
+void my_handler(int s){
+  printf("Caught signal %d | finishing video file\n",s);
+  output_video.release();
+
+  exit(1); 
+}
+
 
 int main( int argc, char** argv ){
-  ConciseArgs parser(argc, argv, "drc-tags");
-  string camera_chan="WEBCAM";
-  parser.add(camera_chan, "v", "camera_chan", "Camera Channel");
-  parser.parse();
-  cout << camera_chan << " is camera_chan\n";
+
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = my_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);  
+
+  CommandLineConfig cl_cfg;
+  cl_cfg.codec ="divx";
+  cl_cfg.camera_chan = "WEBCAM";  
+  cl_cfg.frames = -1;
+  cl_cfg.outputfile = "video.avi";
   
-  std::string filename = "/home/mfallon/data/atlas/2013-08-20-atlas-walking/lcmlog-2013-08-20.07_log03";
-  std::string filename_url = "file://" + filename;
+  cout << "Convert images within LCM log to avi\n";
+  cout << "Example Usage:\n";
+  cout << "  lcm-log-to-avi -m WEBCAM -l logfile\n";
+  cout << "  lcm-log-to-avi -m CAMERA -l logfile\n";
+  cout << "  lcm-log-to-avi -m CAMERACHEST_LEFT -l logfile\n";
+  ConciseArgs parser(argc, argv, "");
+  parser.add(cl_cfg.camera_chan, "m", "camera_chan", "Camera Channel e.g. WEBCAM, CAMERA, CAMERACHEST_LEFT");
+  parser.add(cl_cfg.codec, "c", "codec", "Codec: none, divx, flv1, h264");
+  parser.add(cl_cfg.frames, "f", "frames", "No. of Frames to process (-1 = all)");
+  parser.add(cl_cfg.logfile, "l", "logfile", "Path to the LCM log");
+  parser.add(cl_cfg.outputfile, "o", "outputfile", "Output AVI file");
+  parser.parse();
+  cout << cl_cfg.codec << " is codec\n";
+  cout << cl_cfg.camera_chan << " is camera_chan\n";
+  cout << cl_cfg.frames << " is frames\n";
+  cout << cl_cfg.logfile << " is logfile\n";
+  cout << cl_cfg.outputfile << " is outputfile\n";
+  
+  std::string filename_url = "file://" + cl_cfg.logfile;
   boost::shared_ptr<lcm::LCM> lcm(new lcm::LCM ( filename_url    )  );
   if(!lcm->good()){
     std::cerr <<"ERROR: lcm is not good()" <<std::endl;
   }
   
-  Tags app(lcm,camera_chan);
+  Tags app(lcm,cl_cfg);
   cout << "Ready to find tags" << endl << "============================" << endl;
   while(0 == lcm->handle());
   return 0;
