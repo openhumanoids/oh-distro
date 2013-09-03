e08e410908a0c7377a33068d472966646ccae859
Scott Kuindersma <kuindersma@gmail.com>
2013-09-03 21:38:28 +0000

    updated force control gains
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6094 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/test/atlasGainTuning.m
+++ software/control/matlab/test/atlasGainTuning.m
@@ -24,20 +24,20 @@ function atlasGainTuning
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SET JOINT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-joint = 'r_arm_shx';% <---- 
-control_mode = 'position';% <----  force, position
+joint = 'l_arm_mwx';% <---- 
+control_mode = 'force';% <----  force, position
 signal = 'chirp';% <----  zoh, foh, chirp
 
 % GAINS %%%%%%%%%%%%%%%%%%%%%
-ff_const = 0.0;% <----
+ff_const = -0.125;% <----
 if strcmp(control_mode,'force')
   % force gains: only have an effect if control_mode==force
-  k_f_p = 0.0;% <----
+  k_f_p = 0.1;% <----
   ff_f_d = 0.0;% <----
   ff_qd = 0.0;% <----
 elseif strcmp(control_mode,'position')  
   % position gains: only have an effect if control_mode==position
-  k_q_p =  4.0;% <----
+  k_q_p =  0.0;% <----
   k_q_i = 0.0;% <----
   k_qd_p = 0.0;% <----
 else
@@ -46,13 +46,13 @@ end
 
 % SIGNAL PARAMS %%%%%%%%%%%%%
 if strcmp( signal, 'chirp' )
-  zero_crossing = false;
-  ts = linspace(0,20,400);% <----
-  amp = pi/4;% <----  Nm or radians
-  freq = linspace(0.05,0.25,400);% <----  cycles per second
+  zero_crossing = true;
+  ts = linspace(0,25,400);% <----
+  amp = 10;% <----  Nm or radians
+  freq = linspace(0.05,0.3,400);% <----  cycles per second
 else
   ts = linspace(0,10,5);% <----
-  vals = [0 1.0 1.6 1.0 0];% <----  Nm or radians
+  vals = [0 5 -5 10 0];% <----  Nm or radians
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -115,6 +115,7 @@ joint_offset_map.l_arm_shx = -1.45;
 joint_offset_map.l_arm_ely = 1.57;
 joint_offset_map.r_arm_shx = 1.45;
 joint_offset_map.r_arm_ely = 1.57;
+
 % set negative joints
 joint_sign_map.l_arm_ely = -1;
 joint_sign_map.l_arm_usy = -1;
@@ -140,8 +141,6 @@ ref_frame.updateGains(gains);
 qdes = zeros(nq,1);
 if strcmp(joint,'l_arm_shx') || strcmp(joint,'r_arm_shx') || ...
     strcmp(joint,'l_arm_elx') || strcmp(joint,'r_arm_elx') || ...
-    strcmp(joint,'l_leg_hpy') || strcmp(joint,'r_arm_hpy') || ... 
-    strcmp(joint,'l_leg_kny') || strcmp(joint,'r_arm_kny') || ...
     strcmp(joint,'neck_ay')
 
   qdes(joint_index_map.r_arm_shx) = 1.45;
@@ -149,6 +148,8 @@ if strcmp(joint,'l_arm_shx') || strcmp(joint,'r_arm_shx') || ...
   
 elseif strcmp(joint,'l_arm_usy') || strcmp(joint,'r_arm_usy') || ...
     strcmp(joint,'l_arm_uwy') || strcmp(joint,'r_arm_uwy') || ...
+    strcmp(joint,'l_leg_hpy') || strcmp(joint,'r_arm_hpy') || ... 
+    strcmp(joint,'l_leg_kny') || strcmp(joint,'r_arm_kny') || ...
     strcmp(joint,'l_arm_mwx') || strcmp(joint,'r_arm_mwx')
   
   qdes(joint_index_map.r_arm_shx) = 1.3;
@@ -178,7 +179,7 @@ act_idx = getActuatedJoints(r);
 atlasLinearMoveToPos(qdes,state_frame,ref_frame,act_idx,3);
 
 disp('Ready to send input signal.');
-keyboard;
+% keyboard;
 
 % set gains to user specified values
 gains.ff_const(act_idx==joint_index_map.(joint)) = ff_const;
@@ -207,16 +208,23 @@ end
 ref_frame.updateGains(gains);
 udes = zeros(nu,1);
 
-vals = joint_offset_map.(joint) + joint_sign_map.(joint) * vals;
+vals = joint_sign_map.(joint) * vals;
+if strcmp(control_mode,'position')
+  vals = joint_offset_map.(joint) + vals;
+end
 if strcmp(signal,'zoh')
   input_traj = PPTrajectory(zoh(ts,vals));
 elseif strcmp(signal,'foh')
   input_traj = PPTrajectory(foh(ts,vals));
 elseif strcmp(signal,'chirp')
+  offset = 0;
+  if strcmp(control_mode,'position')
+    offset=joint_offset_map.(joint);
+  end
   if zero_crossing
-  	input_traj = PPTrajectory(foh(ts,joint_offset_map.(joint) + amp*sin(ts.*freq*2*pi)));
+  	input_traj = PPTrajectory(foh(ts, offset + amp*sin(ts.*freq*2*pi)));
   else
-    input_traj = PPTrajectory(foh(ts, joint_offset_map.(joint) + joint_sign_map.(joint)*(0.5*amp - 0.5*amp*cos(ts.*freq*2*pi))));
+    input_traj = PPTrajectory(foh(ts, offset + joint_sign_map.(joint)*(0.5*amp - 0.5*amp*cos(ts.*freq*2*pi))));
   end
 else
   error('unknown signal');
