f203e6299e36d957657d2b0fc6724158f52fbcb1
Sisir Karumanchi <sisir@mit.edu>
2013-09-07 04:01:54 +0000

    new listener, removing utime listener.
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6162 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/util/PlanAdjustModeListener.m
+++ software/control/matlab/util/PlanAdjustModeListener.m
@@ -1,22 +1,22 @@
-classdef UtimeListener
+classdef PlanAdjustModeListener
     properties
         lc
         aggregator
     end
     
     methods
-        function obj = UtimeListener(channel)
+        function obj = PlanAdjustModeListener(channel)
             obj.lc = lcm.lcm.LCM.getSingleton();
             obj.aggregator = lcm.lcm.MessageAggregator();
             obj.lc.subscribe(channel, obj.aggregator);
         end
         
         function [X] = getNextMessage(obj, t_ms)
-            msg = obj.aggregator.getNextMessage(t_ms);
-            if isempty(msg)
+            modeset_msg = obj.aggregator.getNextMessage(t_ms);
+            if isempty(modeset_msg)
                 X = [];
             else
-                [X] = UtimeListener.decode(drc.utime_t(msg.data));
+                [X] = PlanAdjustModeListener.decode(drc.plan_adjust_mode_t(modeset_msg.data));
             end
         end
         
@@ -25,6 +25,7 @@ classdef UtimeListener
     methods(Static)
         function X = decode(msg)
                 X.time = double(msg.utime)/1000000; 
+                X.mode =msg.mode;
          end
             
     end
