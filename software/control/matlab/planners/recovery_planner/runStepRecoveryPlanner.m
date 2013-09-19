4f1657cbc708439b0dc1b7eda2572c2d3431e1c6
Robin Deits <rdeits@csail.mit.edu>
2013-09-19 15:52:16 +0000

    Run recovery planner as robot
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6279 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/recovery_planner/runStepRecoveryPlanner.m
+++ software/control/matlab/planners/recovery_planner/runStepRecoveryPlanner.m
@@ -1,11 +1,11 @@
-function planRecoverySteps()
+function runStepRecoveryPlanner()
 %NOTEST
 
 options.floating = true;
 options.dt = 0.001;
 r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
 r = removeCollisionGroupsExcept(r,{'heel','toe'});
-r = setTerrain(r,DRCTerrainMap(false,struct('name','Recovery Plan','status_code',6,'fill', true,'normal_radius',2)));
+r = setTerrain(r,DRCTerrainMap(true,struct('name','Recovery Plan','status_code',6,'fill', true,'normal_radius',2)));
 r = compile(r);
 p = StepRecoveryPlanner(r);
 p.run(0.1)
