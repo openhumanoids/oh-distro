8f720bfbdc7085d1a7cbbe0a33af80a91ed8f686
Twan Koolen <tkoolen@csail.mit.edu>
2013-10-02 21:23:39 +0000

    updated atlasGeometricCalibrationEvalution to use findKinematicPath (in patch sent to Scott). Turns out this also fixes a bug caused by using the wrong indices into the q vector.
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6426 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/test/atlasGeometricCalibrationEvaluation.m
+++ software/control/matlab/test/atlasGeometricCalibrationEvaluation.m
@@ -3,25 +3,35 @@ robot = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_dra
 rand('twister', 1253);
 
 nq = getNumDOF(robot);
-link_name = 'l_hand+l_hand_point_mass';
-link_index = robot.findLinkInd(link_name);
-point = [0; 0; 0];
+
+base_name = 'utorso';
+base_index = robot.findLinkInd(base_name);
+
+end_effector_name = 'l_hand+l_hand_point_mass';
+end_effector_index = robot.findLinkInd(end_effector_name);
+
+[~, joint_path, ~] = robot.findKinematicPath(base_index, end_effector_index);
+joint_names = robot.getJointNames();
+
+joint_indices = cell2mat({robot.getBody(joint_path).dofnum})';
+nq_path = length(joint_indices);
+
+disp([joint_names(joint_path) num2cell(joint_indices)])
+
+point = [0.05; 0.05; 0]; % doesn't work for [0; 0; 0]
 rotation_type = 0; % no rotation included
 
 n_measurements = 50;
 calibration_index = 3; % number of equations per measurement
-n_joints = 6; % number of joints on the path
-W = zeros(n_measurements * calibration_index, n_joints);
+W = zeros(n_measurements * calibration_index, nq_path);
 condition_numbers = zeros(n_measurements, 1);
-
-arm_joint_indices = 6 : 11;
 
 for i = 0 : n_measurements
     q = rand(nq, 1) * pi - pi / 2;
     kinsol = robot.doKinematics(q);
-    [~,J] = robot.forwardKin(kinsol, link_index, point, rotation_type);
+    [~,J] = robot.forwardKin(kinsol, end_effector_index, point, rotation_type);
     start_row = calibration_index * i + 1;
-    W(start_row : start_row + calibration_index - 1, :) = J(:, arm_joint_indices);
+    W(start_row : start_row + calibration_index - 1, :) = J(:, joint_indices);
     condition_numbers(i + 1) = cond(W);
 end
 
