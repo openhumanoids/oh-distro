c09a8cd1c4f163b085300af34e731aff5aa74e3a
Twan Koolen <tkoolen@csail.mit.edu>
2013-10-02 13:58:28 +0000

    adding simple evaluation script for putting Atlas in random configurations and computing condition number of resulting observation matrix for computing arm joint offsets
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6408 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/test/atlasGainTuning.m
+++ software/control/matlab/test/atlasGainTuning.m
@@ -4,7 +4,7 @@ function atlasGainTuning
 % simple function for tuning position and torque control gains
 % joint-by-joint
 
-% gain spec: 
+% gain spec:
 % q, qd, f are sensed position, velocity, torque, from AtlasJointState
 %
 % q_d, qd_d, f_d are desired position, velocity, torque, from
@@ -21,138 +21,75 @@ function atlasGainTuning
 %  ff_f_d  * f_d +
 %  ff_const
 
-
-
-% load robot model
-options.floating = true;
-r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
-
-% setup frames
-state_frame = getStateFrame(r);
-state_frame.subscribe('EST_ROBOT_STATE');
-input_frame = getInputFrame(r);
-ref_frame = AtlasPosTorqueRef(r);
-
-nu = getNumInputs(r);
-nq = getNumDOF(r);
-
-joint_index_map = struct(); % maps joint names to indices
-joint_offset_map = struct(); % maps joint names to nominal angle offsets
-joint_sign_map = struct(); % maps joint names to signs in the direction of desired motion
-for i=1:nq
-  joint_index_map.(state_frame.coordinates{i}) = i;
-  joint_offset_map.(state_frame.coordinates{i}) = 0;
-  joint_sign_map.(state_frame.coordinates{i}) = 1;
-end
-
-act_idx = getActuatedJoints(r);
-
-gains = getAtlasGains(input_frame); 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SET JOINT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-joint = 'r_leg_kny';% <---- 
-control_mode = 'force';% <----  force, position
+joint = 'l_arm_elx';% <----
+control_mode = AtlasJointControlMode.FORCE;
 signal = 'chirp';% <----  zoh, foh, chirp
 
 % GAINS %%%%%%%%%%%%%%%%%%%%%
-ff_const = gains.ff_const(act_idx==joint_index_map.(joint));% -0.16;% <----
-if strcmp(control_mode,'force')
-  % force gains: only have an effect if control_mode==force
-  k_f_p = gains.k_f_p(act_idx==joint_index_map.(joint));%0.125;% <----
-  ff_f_d = gains.ff_f_d(act_idx==joint_index_map.(joint));%0.01;% <----
-  ff_qd = gains.ff_qd(act_idx==joint_index_map.(joint));% <----
-elseif strcmp(control_mode,'position')  
-  % position gains: only have an effect if control_mode==position
-  k_q_p =  0.0;% <----
-  k_q_i = 0.0;% <----
-  k_qd_p = 0.0;% <----
-else
-  error('unknown control mode');
-end
+gains_for_joint_to_tune = getGainsForJointToTune(control_mode);
 
 % SIGNAL PARAMS %%%%%%%%%%%%%
 if strcmp( signal, 'chirp' )
-  zero_crossing = true;
-  ts = linspace(0,40,800);% <----
-  amp = 15.0;% <----  Nm or radians
-  freq = linspace(0.025,0.4,800);% <----  cycles per second
+    zero_crossing = false;
+    ts = linspace(0,25,400);% <----
+    amp = 25;% <----  Nm or radians
+    freq = linspace(0.05,0.3,400);% <----  cycles per second
 else
-  vals = 10*[0 0 1 0 0];% <----  Nm or radians
-  ts = linspace(0,60,length(vals));% <----
+    ts = linspace(0,20,7);% <----
+    vals = [0 0 20 0 -20 0 0];% <----  Nm or radians
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-T=ts(end);
 
-% check gain ranges --- TODO: make this more conservative
-rangecheck(ff_const,-10,10);
-if strcmp(control_mode,'force')
-%   rangecheck(k_f_p,0,1);
-%   rangecheck(ff_f_d,0,1);
-%   rangecheck(ff_qd,0,1.5);
-elseif strcmp(control_mode,'position')  
-  rangecheck(k_q_p,0,50);
-  rangecheck(k_q_i,0,0.5);
-  rangecheck(k_qd_p,0,50);
-end
+checkGainRanges(gains_for_joint_to_tune, control_mode);
+
 
 % check value ranges --- TODO: should be joint specific
 if ~exist('vals','var')
-  vals=amp;
-end
-if strcmp(control_mode,'force')
-%   rangecheck(vals,-70,70);
-%   if ~rangecheck(vals,-50,50)
-%     disp('Warning: about to command relatively high torque. Shift+F5 to cancel.');
-%     keyboard;
-%   end
-elseif strcmp(control_mode,'position')  
-  rangecheck(vals,-pi,pi);
-  if ~rangecheck(vals,-1,1)
-    disp('Warning: about to command relatively large position change. Shift+F5 to cancel.');
-    keyboard;
-  end
+    vals=amp;
 end
 
+checkReferenceRanges(vals, control_mode);
 
+% load robot model
+options.floating = true;
+robot = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
+
+% setup frames
+state_frame = getStateFrame(robot);
+state_frame.subscribe('EST_ROBOT_STATE');
+input_frame = getInputFrame(robot);
+ref_frame = AtlasPosTorqueRef(robot);
+
+nu = getNumInputs(robot);
+nq = getNumDOF(robot);
+
+joint_index_map = struct(); % maps joint names to indices
+joint_offset_map = struct(); % maps joint names to nominal angle offsets
+for i=1:nq
+    joint_index_map.(state_frame.coordinates{i}) = i;
+    joint_offset_map.(state_frame.coordinates{i}) = 0;
+end
 
 
 % set nonzero offsets
 joint_offset_map.l_arm_shx = -1.45;
 joint_offset_map.l_arm_ely = 1.57;
+joint_offset_map.r_arm_shx = 1.45;
 joint_offset_map.r_arm_ely = 1.57;
 joint_offset_map.l_leg_kny = 1.57;
-joint_offset_map.r_leg_kny = 1.57;
 
-
-joint_offset_map.r_arm_uwy = 1.57;
-joint_offset_map.r_arm_elx = -1.57;
-joint_offset_map.r_arm_shx = 1.45;
-
-% set negative joints
-joint_sign_map.l_arm_ely = -1;
-joint_sign_map.l_arm_usy = -1;
-joint_sign_map.r_arm_usy = -1;
-joint_sign_map.r_arm_shx = -1;
-joint_sign_map.r_arm_ely = -1;
-joint_sign_map.r_arm_elx = -1;
-joint_sign_map.r_arm_mwx = -1;
-
-joint_sign_map.l_leg_hpy = -1;
-joint_sign_map.r_leg_hpy = -1;
-
-joint_sign_map.r_leg_hpx = -1;
-joint_sign_map.r_leg_hpz = -1;
+joint_sign_map = getAtlasJointSignMap(state_frame, nq);
 
 if ~isfield(joint_index_map,joint)
-  error ('unknown joint name');
+    error ('unknown joint name');
 end
 
-gains = getAtlasGains(input_frame); 
+gains = getAtlasGains(input_frame);
 
 % zero out force gains to start --- move to nominal joint position
 gains.k_f_p = zeros(nu,1);
@@ -160,203 +97,148 @@ gains.ff_f_d = zeros(nu,1);
 gains.ff_qd = zeros(nu,1);
 ref_frame.updateGains(gains);
 
-% setup desired pose based on joint being tuned
-qdes = zeros(nq,1);
-% if strcmp(joint,'l_arm_shx') || strcmp(joint,'r_arm_shx') || ...
-%     strcmp(joint,'l_arm_elx') || strcmp(joint,'r_arm_elx') || ...
-%     strcmp(joint,'neck_ay')
-% 
-%   qdes(joint_index_map.r_arm_shx) = 1.45;
-%   qdes(joint_index_map.l_arm_shx) = -1.45;
-%   
-% elseif strcmp(joint,'l_arm_usy') || strcmp(joint,'r_arm_usy') || ...
-%     strcmp(joint,'l_arm_uwy') || strcmp(joint,'r_arm_uwy') || ...
-%     strcmp(joint,'l_leg_hpy') || strcmp(joint,'r_arm_hpy') || ... 
-%     strcmp(joint,'l_leg_kny') || strcmp(joint,'r_arm_kny') || ...
-%     strcmp(joint,'l_arm_mwx') || strcmp(joint,'r_arm_mwx')
-%   
-%   qdes(joint_index_map.r_arm_shx) = 1.3;
-%   qdes(joint_index_map.l_arm_shx) = -1.3;
-% 
-% %  qdes(joint_index_map.l_leg_hpy) = -pi/2;
-% %  qdes(joint_index_map.l_leg_kny) = pi/2;
-% 
-% elseif strcmp(joint,'l_arm_ely')
-%   
-%   qdes(joint_index_map.r_arm_shx) = 1.45;
-%   qdes(joint_index_map.l_arm_elx) = 1.57;
-%   qdes(joint_index_map.l_arm_ely) = joint_offset_map.l_arm_ely;
-%   
-% elseif strcmp(joint,'r_arm_ely')
-%   
-%   qdes(joint_index_map.l_arm_shx) = -1.45;
-%   qdes(joint_index_map.r_arm_elx) = -1.57;
-%   qdes(joint_index_map.r_arm_ely) = joint_offset_map.r_arm_ely;
-% 
-% else
-%   error ('that joint isnt supported yet');
-% end
-
-if strcmp(joint,'l_leg_hpy') 
-  
-  qdes(joint_index_map.r_arm_shx) = 1.25;
-  qdes(joint_index_map.l_arm_shx) = -1.25;
-  qdes(joint_index_map.r_leg_hpx) = -0.25;
-
-elseif strcmp(joint,'r_leg_hpy') 
-
-  qdes(joint_index_map.r_arm_shx) = 1.25;
-  qdes(joint_index_map.l_arm_shx) = -1.25;
-  qdes(joint_index_map.l_leg_hpx) = 0.25;
-
-elseif strcmp(joint,'l_leg_hpz') 
-
-  qdes(joint_index_map.r_arm_shx) = 1.0;
-  qdes(joint_index_map.l_arm_shx) = -1.0;
-  qdes(joint_index_map.r_leg_hpx) = -0.4;
-  qdes(joint_index_map.l_leg_kny) = 1.57;
-
-elseif strcmp(joint,'r_leg_hpz') 
-
-  qdes(joint_index_map.r_arm_shx) = 1.0;
-  qdes(joint_index_map.l_arm_shx) = -1.0;
-  qdes(joint_index_map.l_leg_hpx) = 0.4;
-  qdes(joint_index_map.r_leg_kny) = 1.57;
-
-elseif strcmp(joint,'l_leg_hpx') 
-
-  qdes(joint_index_map.r_leg_hpx) = -0.5;
-
-elseif strcmp(joint,'r_leg_hpx') 
-
-  qdes(joint_index_map.l_leg_hpx) = 0.5;
-
-elseif strcmp(joint,'r_leg_kny') 
-
-  qdes(joint_index_map.r_arm_shx) = 1.0;
-  qdes(joint_index_map.l_arm_shx) = -1.0;
-  qdes(joint_index_map.r_leg_hpy) = -pi/2;
-  qdes(joint_index_map.r_leg_kny) = pi/2;  
-
-elseif strcmp(joint,'l_arm_usy') || strcmp(joint,'r_arm_usy') || ...
-    strcmp(joint,'l_arm_shx') || strcmp(joint,'r_arm_shx') 
-  
-  qdes(joint_index_map.r_arm_shx) = 1.45;
-  qdes(joint_index_map.l_arm_shx) = -1.45;
-
-  qdes(joint_index_map.l_arm_uwy) = 1.57;
-  qdes(joint_index_map.r_arm_uwy) = 1.57;
-
-  qdes(joint_index_map.r_arm_ely) = joint_offset_map.r_arm_ely;
-  qdes(joint_index_map.l_arm_ely) = joint_offset_map.l_arm_ely;
-
-elseif strcmp(joint,'l_arm_ely') || strcmp(joint,'l_arm_mwx') || strcmp(joint,'l_arm_elx')
-  
-  qdes(joint_index_map.r_arm_shx) = 1.45;
-  qdes(joint_index_map.l_arm_elx) = 1.57;
-  qdes(joint_index_map.l_arm_ely) = 3.14;
-
-elseif strcmp(joint,'r_arm_ely') || strcmp(joint,'r_arm_mwx') || strcmp(joint,'r_arm_elx')
-  
-  qdes(joint_index_map.l_arm_shx) = -1.45;
-  qdes(joint_index_map.r_arm_elx) = -1.57;
-  qdes(joint_index_map.r_arm_uwy) = 1.57;
-  qdes(joint_index_map.r_arm_ely) = 3.14;
-
-elseif strcmp(joint,'r_arm_uwy')
-  
-  qdes(joint_index_map.l_arm_shx) = -1.45;
-  qdes(joint_index_map.r_arm_ely) = 1.57;
-  qdes(joint_index_map.r_arm_elx) = -1.57;
-  qdes(joint_index_map.r_arm_uwy) = 1.57;
-  qdes(joint_index_map.r_arm_mwx) = 1.15;
-  
-elseif strcmp(joint,'l_arm_uwy')
-  
-  qdes(joint_index_map.r_arm_shx) = 1.45;
-  qdes(joint_index_map.l_arm_ely) = 1.57;
-  qdes(joint_index_map.l_arm_elx) = 1.57;
-  qdes(joint_index_map.l_arm_uwy) = 1.57;
-  qdes(joint_index_map.l_arm_mwx) = -1.15;
-  
-else
-  error ('that joint isnt supported yet');
-end
-
+qdes = getQDesiredToMoveSingleJoint(joint, joint_index_map);
 qdes(joint_index_map.(joint)) = joint_offset_map.(joint);
 
+actuator_indices = getActuatedJoints(robot);
+
 % move to desired pos
-atlasLinearMoveToPos(qdes,state_frame,ref_frame,act_idx,3);
+% atlasLinearMoveToPos(qdes,state_frame,ref_frame,actuator_indices,3);
 
 disp('Ready to send input signal.');
 % keyboard;
 
 % set gains to user specified values
-gains.ff_const(act_idx==joint_index_map.(joint)) = ff_const;
-if strcmp(control_mode,'force')
-  % set force gains
-  gains.k_f_p(act_idx==joint_index_map.(joint)) = k_f_p; 
-  gains.ff_f_d(act_idx==joint_index_map.(joint)) = ff_f_d;
-  gains.ff_qd(act_idx==joint_index_map.(joint)) = ff_qd;
-  % set joint position gains to 0
-  gains.k_q_p(act_idx==joint_index_map.(joint)) = 0;
-  gains.k_q_i(act_idx==joint_index_map.(joint)) = 0;
-  gains.k_qd_p(act_idx==joint_index_map.(joint)) = 0;
-elseif strcmp(control_mode,'position')  
-  % set force gains to 0
-  gains.k_f_p(act_idx==joint_index_map.(joint)) = 0; 
-  gains.ff_f_d(act_idx==joint_index_map.(joint)) = 0;
-  gains.ff_qd(act_idx==joint_index_map.(joint)) = 0;
-  % set joint position gains 
-  gains.k_q_p(act_idx==joint_index_map.(joint)) = k_q_p;
-  gains.k_q_i(act_idx==joint_index_map.(joint)) = k_q_i;
-  gains.k_qd_p(act_idx==joint_index_map.(joint)) = k_qd_p;
-else
-  error('unknown control mode');
-end 
+actuator_index = find(actuator_indices == joint_index_map.(joint));
+gains.ff_const(actuator_index) = gains_for_joint_to_tune.ff_const;
+
+switch control_mode
+    case AtlasJointControlMode.FORCE
+        % set force gains
+        gains.k_f_p(actuator_index) = gains_for_joint_to_tune.k_f_p;
+        gains.ff_f_d(actuator_index) = gains_for_joint_to_tune.ff_f_d;
+        gains.ff_qd(actuator_index) = gains_for_joint_to_tune.ff_qd;
+        
+        % set joint position gains to 0
+        gains.k_q_p(actuator_index) = 0;
+        gains.k_q_i(actuator_index) = 0;
+        gains.k_qd_p(actuator_index) = 0;
+    case AtlasJointControlMode.POSITION
+        % set force gains to 0
+        gains.k_f_p(actuator_index) = 0;
+        gains.ff_f_d(actuator_index) = 0;
+        gains.ff_qd(actuator_index) = 0;
+        
+        % set joint position gains
+        gains.k_q_p(actuator_index) = gains_for_joint_to_tune.k_q_p;
+        gains.k_q_i(actuator_index) = gains_for_joint_to_tune.k_q_i;
+        gains.k_qd_p(actuator_index) = gains_for_joint_to_tune.k_qd_p;
+    otherwise
+        error(['controlMode not recognized: ' control_mode]);
+end
 
 ref_frame.updateGains(gains);
 udes = zeros(nu,1);
 
 vals = joint_sign_map.(joint) * vals;
-if strcmp(control_mode,'position')
-  vals = joint_offset_map.(joint) + vals;
-end
-if strcmp(signal,'zoh')
-  input_traj = PPTrajectory(zoh(ts,vals));
-elseif strcmp(signal,'foh')
-  input_traj = PPTrajectory(foh(ts,vals));
-elseif strcmp(signal,'chirp')
-  offset = 0;
-  if strcmp(control_mode,'position')
-    offset=joint_offset_map.(joint);
-  end
-  if zero_crossing
-  	input_traj = PPTrajectory(foh(ts, offset + amp*sin(ts.*freq*2*pi)));
-  else
-    input_traj = PPTrajectory(foh(ts, offset + joint_sign_map.(joint)*(0.5*amp - 0.5*amp*cos(ts.*freq*2*pi))));
-  end
-else
-  error('unknown signal');
+if control_mode == AtlasJointControlMode.POSITION
+    vals = joint_offset_map.(joint) + vals;
 end
 
-qdes=qdes(act_idx);
+if strcmp(signal,'zoh')
+    input_traj = PPTrajectory(zoh(ts,vals));
+elseif strcmp(signal,'foh')
+    input_traj = PPTrajectory(foh(ts,vals));
+elseif strcmp(signal,'chirp')
+    offset = 0;
+    if control_mode == AtlasJointControlMode.POSITION
+        offset=joint_offset_map.(joint);
+    end
+    if zero_crossing
+        input_traj = PPTrajectory(foh(ts, offset + amp*sin(ts.*freq*2*pi)));
+    else
+        input_traj = PPTrajectory(foh(ts, offset + joint_sign_map.(joint)*(0.5*amp - 0.5*amp*cos(ts.*freq*2*pi))));
+    end
+else
+    error('unknown signal');
+end
+
+qdes=qdes(actuator_indices);
 toffset = -1;
 tt=-1;
+T = ts(end);
 while tt<T
-  [x,t] = getNextMessage(state_frame,1);
-  if ~isempty(x)
-    if toffset==-1
-      toffset=t;
+    [x,t] = getNextMessage(state_frame,1);
+    if ~isempty(x)
+        if toffset==-1
+            toffset=t;
+        end
+        tt=t-toffset;
+        
+        switch control_mode
+            case AtlasJointControlMode.FORCE
+                udes(actuator_index) = input_traj.eval(tt);
+            case AtlasJointControlMode.POSITION
+                qdes(actuator_index) = input_traj.eval(tt);
+            otherwise
+                error(['controlMode not recognized: ' control_mode]);
+        end
+        ref_frame.publish(t,[qdes;udes],'ATLAS_COMMAND');
     end
-    tt=t-toffset;
-    if strcmp(control_mode,'force')
-      udes(act_idx==joint_index_map.(joint)) = input_traj.eval(tt);
-    elseif strcmp(control_mode,'position')
-      qdes(act_idx==joint_index_map.(joint)) = input_traj.eval(tt);
-    end
-    ref_frame.publish(t,[qdes;udes],'ATLAS_COMMAND');
-  end
 end
 
+end
+
+function gains = getGainsForJointToTune(control_mode)
+gains.ff_const = 0.1;% <----
+switch (control_mode)
+    case AtlasJointControlMode.FORCE
+        % force gains: only have an effect if controlMode==AtlasJointControlMode.TORQUE
+        gains.k_f_p = 0.125;% <----
+        gains.ff_f_d = 0.009;% <----
+        gains.ff_qd = 0.0;% <----
+    case AtlasJointControlMode.POSITION
+        % position gains: only have an effect if controlMode==AtlasJointControlMode.POSITION
+        gains.k_q_p = 0.0;% <----
+        gains.k_q_i = 0.0;% <----
+        gains.k_qd_p = 0.0;% <----
+    otherwise
+        error(['controlMode not recognized: ' control_mode]);
+end
+end
+
+function checkGainRanges(gains, control_mode)
+% check gain ranges --- TODO: make this more conservative
+rangecheck(gains.ff_const,-10,10);
+switch (control_mode)
+    case AtlasJointControlMode.FORCE
+        rangecheck(gains.k_f_p,0,1);
+        rangecheck(gains.ff_f_d,0,1);
+        rangecheck(gains.ff_qd,0,1);
+    case AtlasJointControlMode.POSITION
+        rangecheck(gains.k_q_p,0,50);
+        rangecheck(gains.k_q_i,0,0.5);
+        rangecheck(gains.k_qd_p,0,50);
+    otherwise
+        error(['controlMode not recognized: ' control_mode]);
+end
+end
+
+function checkReferenceRanges(vals, control_mode)
+switch control_mode
+    case AtlasJointControlMode.FORCE
+        %   rangecheck(vals,-70,70);
+        %   if ~rangecheck(vals,-50,50)
+        %     disp('Warning: about to command relatively high torque. Shift+F5 to cancel.');
+        %     keyboard;
+        %   end
+    case AtlasJointControlMode.POSITION
+        rangecheck(vals,-pi,pi);
+        if ~rangecheck(vals,-1,1)
+            disp('Warning: about to command relatively large position change. Shift+F5 to cancel.');
+            keyboard;
+        end
+    otherwise
+        error(['controlMode not recognized: ' control_mode]);
+end
 end
