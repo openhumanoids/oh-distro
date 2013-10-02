122e419ed0e6c2e9718f906950f2de2737d823a0
Scott Kuindersma <kuindersma@gmail.com>
2013-10-02 19:25:09 +0000

    working commit
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6418 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/test/atlasStandingLegTuning.m
+++ software/control/matlab/test/atlasStandingLegTuning.m
@@ -22,11 +22,37 @@ function atlasStandingLegTuning
 %  ff_const
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SET JOINT/MOVEMENT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+joint = 'r_leg_hpy';% <---- 
+control_mode = 'force';% <----  force, position
+signal = 'chirp';% <----  zoh, foh, chirp
+
+% SIGNAL PARAMS %%%%%%%%%%%%%
+dim = 3; % what spatial dimension to move COM: x/y/z (1/2/3)
+if strcmp( signal, 'chirp' )
+  zero_crossing = false;
+  ts = linspace(0,40,500);% <----
+  amp = -0.1;% <---- meters, COM DELTA
+  freq = linspace(0.025,0.1,500);% <----  cycles per second
+else
+  vals = 0.02*[0 0 1 0 0];% <---- meters, COM DELTA
+  ts = linspace(0,30,length(vals));% <----
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+T=ts(end);
+
+
 % load robot model
 r = Atlas();
 load(strcat(getenv('DRC_PATH'),'/control/matlab/data/atlas_fp.mat'));
+r = removeCollisionGroupsExcept(r,{'toe','heel'});
+r = compile(r);
 r = r.setInitialState(xstar);
-v = r.constructVisualizer;
+
 % setup frames
 state_frame = getStateFrame(r);
 state_frame.subscribe('EST_ROBOT_STATE');
@@ -41,31 +67,11 @@ for i=1:nq
   joint_index_map.(state_frame.coordinates{i}) = i;
 end
 
-act_idx = getActuatedJoints(r);
+act_idx_map = getActuatedJoints(r);
 gains = getAtlasGains(input_frame); % change gains in this file
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SET JOINT PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-joint = 'r_leg_hpz';% <---- 
-control_mode = 'force';% <----  force, position
-signal = 'chirp';% <----  zoh, foh, chirp
-
-% SIGNAL PARAMS %%%%%%%%%%%%%
-dim = 3; % what spatial dimension to move COM: x/y/z (1/2/3)
-if strcmp( signal, 'chirp' )
-  zero_crossing = false;
-  ts = linspace(0,30,500);% <----
-  amp = -0.15;% <---- meters, COM DELTA
-  freq = linspace(0.025,0.1,500);% <----  cycles per second
-else
-  vals = 0.02*[0 0 1 0 0];% <---- meters, COM DELTA
-  ts = linspace(0,30,length(vals));% <----
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-T=ts(end);
+joint_index = joint_index_map.(joint);
+joint_input_index = find(act_idx_map==joint_index_map.(joint));
 
 % zero out force gains to start --- move to nominal joint position
 gains.k_f_p = zeros(nu,1);
@@ -75,17 +81,17 @@ ref_frame.updateGains(gains);
 
 % move to fixed point configuration 
 qdes = xstar(1:nq);
-atlasLinearMoveToPos(qdes,state_frame,ref_frame,act_idx,3);
+atlasLinearMoveToPos(qdes,state_frame,ref_frame,act_idx_map,3);
 
 gains2 = getAtlasGains(input_frame); 
 % reset force gains for joint being tuned
-gains.k_f_p(act_idx==joint_index_map.(joint)) = gains2.k_f_p(act_idx==joint_index_map.(joint)); 
-gains.ff_f_d(act_idx==joint_index_map.(joint)) = gains2.ff_f_d(act_idx==joint_index_map.(joint));
-gains.ff_qd(act_idx==joint_index_map.(joint)) = gains2.ff_qd(act_idx==joint_index_map.(joint));
+gains.k_f_p(joint_input_index) = gains2.k_f_p(joint_input_index); 
+gains.ff_f_d(joint_input_index) = gains2.ff_f_d(joint_input_index);
+gains.ff_qd(joint_input_index) = gains2.ff_qd(joint_input_index);
 % set joint position gains to 0 for joint being tuned
-gains.k_q_p(act_idx==joint_index_map.(joint)) = 0;
-gains.k_q_i(act_idx==joint_index_map.(joint)) = 0;
-gains.k_qd_p(act_idx==joint_index_map.(joint)) = 0;
+gains.k_q_p(joint_input_index) = 0;
+gains.k_q_i(joint_input_index) = 0;
+gains.k_qd_p(joint_input_index) = 0;
 
 ref_frame.updateGains(gains);
 
@@ -176,10 +182,22 @@ qddtraj = fnder(qtraj,2);
 keyboard;
 
 % kalman filter params
-H = [eye(nq) zeros(nq)];
+Hk = [eye(nq) zeros(nq)];
 R = 5e-4*eye(nq);
 P = eye(2*nq);
 x_est = zeros(2*nq,1);
+
+solver_options.outputflag = 0; % not verbose
+solver_options.method = 2; % -1=automatic, 0=primal simplex, 1=dual simplex, 2=barrier
+solver_options.presolve = 0;
+solver_options.bariterlimit = 20; % iteration limit
+solver_options.barhomogeneous = 0; % 0 off, 1 on
+solver_options.barconvtol = 5e-4;
+
+qp_active_set = [];
+
+float_idx = 1:6;
+act_idx = 7:nq;
 
 udes = zeros(nu,1);
 toffset = -1;
@@ -198,28 +216,95 @@ while tt<T
     F = [eye(nq) dt*eye(nq); zeros(nq) eye(nq)];
     Q = 0.3*[dt*eye(nq) zeros(nq); zeros(nq) eye(nq)];
     
-    % compute filtered velocity
+    % estimate state
     jprior = F*x_est;
     Pprior = F*P*F' + Q;
-    meas_resid = x(1:nq) - H*jprior;
-    S = H*Pprior*H' + R;
-    K = (P*H')/S;
+    meas_resid = x(1:nq) - Hk*jprior;
+    S = Hk*Pprior*Hk' + R;
+    K = (P*Hk')/S;
     x_est = jprior + K*meas_resid;
-    P = (eye(2*nq) - K*H)*Pprior;
+    P = (eye(2*nq) - K*Hk)*Pprior;
     
     q = x_est(1:nq);
     qd = x_est(nq+(1:nq));
     
+    % get desired configuration
     qt = qtraj.eval(tt);
-    qdes = qt(act_idx);
-    
-    qdddes = qddtraj.eval(tt) + 20.0*(qt-q);
-    [Hd,C,B] = manipulatorDynamics(r,q,qd);
+    qdes = qt(act_idx_map);
 
-    % .... need to solve a QP to get udes+contact forces s.t. feet do not
-    % move
+    % get desired acceleration
+    qdddes = qddtraj.eval(tt) + 20.0*(qt-q);
     
-    ref_frame.publish(t,[qdes;0*udes],'ATLAS_COMMAND');
+    % solve QP to compute inputs 
+    kinsol = doKinematics(r,q);
+    [H,C,B] = manipulatorDynamics(r,q,qd);
+
+    H_float = H(float_idx,:);
+    C_float = C(float_idx);
+
+    H_act = H(act_idx,:);
+    C_act = C(act_idx);
+    B_act = B(act_idx,:);
+    
+    [phi,~,JB] = contactConstraintsBV(r,kinsol);
+    Dbar = [JB{:}];
+    Dbar_float = Dbar(float_idx,:);
+    Dbar_act = Dbar(act_idx,:);
+
+    nc=8; nd=4;
+    nf = nc*nd; % number of contact force variables
+    nparams = nq+nf;
+    Iqdd = zeros(nq,nparams); Iqdd(:,1:nq) = eye(nq);
+    Ibeta = zeros(nf,nparams); Ibeta(:,nq+(1:nf)) = eye(nf);
+
+    lb = [-1e3*ones(nq,1); zeros(nf,1)]; % qddot/contact forces
+    ub = [ 1e3*ones(nq,1); 500*ones(nf,1)];
+      
+    % constrained dynamics
+    Aeq = H_float*Iqdd - Dbar_float*Ibeta;
+    beq = -C_float;
+
+%    Ain_{1} = B_act'*(H_act*Iqdd1e-8 - Dbar_act*Ibeta);
+%    bin_{1} = -B_act'*C_act + r.umax;
+%    Ain_{2} = -Ain_{1};
+%    bin_{2} = B_act'*C_act - r.umin;
+% 
+%    Ain = sparse(vertcat(Ain_{:}));
+%    bin = vertcat(bin_{:});
+    
+    Hqp = 1e-6*eye(nparams);
+    Hqp(1:nq,1:nq) = Hqp(1:nq,1:nq)+ eye(nq);
+    fqp = -qdddes'*Iqdd;
+    
+    if 0
+      model.Q = sparse(Hqp);
+      model.obj = 2*fqp;
+      model.A = sparse(Aeq);
+      model.rhs = beq;
+      model.sense = repmat('=',length(beq),1);
+      model.lb = lb;
+      model.ub = ub;
+      result = gurobi(model,solver_options);
+      alpha = result.x;
+    else  
+      IR = eye(nparams);  
+      lbind = lb>-999;  ubind = ub<999;  % 1e3 was used like inf above... right?
+      Ain_fqp = full([-IR(lbind,:); IR(ubind,:)]);
+      bin_fqp = [-lb(lbind); ub(ubind)];
+      QblkDiag = Hqp;
+      Aeq_fqp = full(Aeq);
+      % NOTE: model.obj is 2* f for fastQP!!!
+      [alpha,info_fqp] = fastQPmex(QblkDiag,fqp,Aeq_fqp,beq,Ain_fqp,bin_fqp,qp_active_set);
+      qp_active_set = find(abs(Ain_fqp*alpha - bin_fqp)<1e-6);
+    end     
+    
+    qdd = alpha(1:nq);
+    beta = alpha(nq+(1:nf));
+    u = B_act'*(H_act*qdd + C_act - Dbar_act*beta)
+    
+    udes(joint_input_index) = u(joint_input_index);
+    
+    ref_frame.publish(t,[qdes;udes],'ATLAS_COMMAND');
   end
 end
 
