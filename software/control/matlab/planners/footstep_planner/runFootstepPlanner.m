225b42210b1227f7587741acf95d128ae22c4138
Robin Deits <rdeits@csail.mit.edu>
2013-10-08 22:56:39 +0000

    Automatically restart footstep planner after an error and save a log of the crash
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6517 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/footstep_planner/runFootstepPlanner.m
+++ software/control/matlab/planners/footstep_planner/runFootstepPlanner.m
@@ -1,15 +1,20 @@
 function runFootstepPlanner
 %NOTEST
 
-options.floating = true;
-options.dt = 0.001;
-% r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact.urdf'),options);
-r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
-r = removeCollisionGroupsExcept(r,{'heel','toe'});
-r = setTerrain(r,DRCTerrainMap(false,struct('name','Foot Plan','status_code',6,'fill', true,'normal_radius',2)));
-r = compile(r);
+while 1
+  options.floating = true;
+  options.dt = 0.001;
+  r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
+  r = removeCollisionGroupsExcept(r,{'heel','toe'});
+  r = setTerrain(r,DRCTerrainMap(false,struct('name','Foot Plan','status_code',6,'fill', true,'normal_radius',2)));
+  r = compile(r);
 
-p = FootstepPlanner(r);
-p.run(0.25)
-
+  p = FootstepPlanner(r);
+  try
+    p.run(0.25)
+  catch exception
+    disp(exception)
+    save(['footstep-planner-crash-',datestr(now(),'YYYY-mm-DDTHH-MM'),'.mat'], 'exception');
+    msg ='Foot Plan : Crashed, restarting...'; disp(msg); send_status(6,0,0,msg);
+  end
 end
