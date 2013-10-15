bdba4167a64987cb458bcc116fd2f20f4b9520bf
Michael Posa <mposa@csail.mit.edu>
2013-10-15 16:13:45 +0000

    Added a coder for the extra state messages.
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6607 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/src/AtlasStateExtraCoder.java
@@ -0,0 +1,57 @@
+package drc.control;
+
+import java.io.*;
+import java.lang.*;
+import lcm.lcm.*;
+
+public class AtlasStateExtraCoder implements drake.util.LCMCoder
+{
+    short m_num_joints;
+   
+    boolean include_torques;
+    
+    public AtlasStateExtraCoder(short num_joints) {
+      m_num_joints = num_joints;
+    }
+    
+    public int dim()
+    {
+      return 2*m_num_joints;
+    }
+
+    public drake.util.CoordinateFrameData decode(byte[] data)
+    {
+      try {
+        drc.atlas_state_extra_t msg = new drc.atlas_state_extra_t(data);
+        return decode(msg);
+      } catch (IOException ex) {
+        System.out.println("Exception: " + ex);
+      }
+      return null;
+    }
+ 
+    public drake.util.CoordinateFrameData decode(drc.atlas_state_extra_t msg)
+    {
+      Integer j;
+      int index;
+
+      drake.util.CoordinateFrameData fdata = new drake.util.CoordinateFrameData();
+      fdata.val = new double[dim()];
+      fdata.t = (double)msg.utime / 1000000.0;
+      for (int i=0; i<msg.num_joints; i++) {
+        fdata.val[i] = msg.joint_position_out[i];
+        fdata.val[m_num_joints + i] = msg.joint_velocity_out[i];
+      }
+      return fdata;
+    }
+
+    public LCMEncodable encode(drake.util.CoordinateFrameData d)
+    {
+      return null;
+    }
+
+    public String timestampName()
+    {
+      return "utime";
+    }
+}
