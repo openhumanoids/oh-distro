92e183e076cd74d07ba913169e9b14ab01e22bd4
Hongkai Dai <hongkai@csail.mit.edu>
2013-10-28 19:07:51 +0000

    change inverseKinTrajWcollision to check if the solution is at the  joint limits
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6856 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/atlas_keyframe_planners/inverseKinTrajWcollision.m
+++ software/control/matlab/planners/atlas_keyframe_planners/inverseKinTrajWcollision.m
@@ -1,9 +1,19 @@
-function [xtraj,info,infeasible_constraint] = inverseKinTrajWcollision(obj,collision_status,t,q_seed_traj,q_nom_traj,varargin)
+function [xtraj,info,infeasible_constraint,joint_limit_flag] = inverseKinTrajWcollision(obj,collision_status,t_verify, ...
+  t_breaks,q_seed_traj,q_nom_traj,varargin)
 % The interface is the same as inverseKinTraj
 % @param collision_status             - 0, no validation, no optimizatoin with collision
 %                                     - 1, validation only, no optimization
 %                                     - 2, optimize without collision constraint first, then validate. 
 %                                          if collision happens, optimization with collision constraint
+% @param t_verify                     - The additional time samples to verify if the constraints
+%                                       (collision avoidance) are satisfied
+% @param joint_limit_flag             - The flag of whether the joint are at the joint limit in the
+%                                       xtraj
+t_verify = sort([t_verify t_breaks]);
+nq = obj.getNumDOF();
+[joint_lb,joint_ub] = obj.getJointLimits();
+joint_max = bsxfun(@times,joint_ub,ones(1,length(t_breaks)));
+joint_min = bsxfun(@times,joint_lb,ones(1,length(t_breaks)));
 if(isa(varargin{end},'IKoptions'))
   ikoptions = varargin{end};
   varargin = varargin(1:end-1); 
@@ -12,31 +22,53 @@ else
 end
 collision_constraint_cell = {};
 other_constraint_cell = {};
+
 for i = 1:length(varargin)
   if(isa(varargin{i},'AllBodiesClosestDistanceConstraint'))
     collision_constraint_cell = [collision_constraint_cell varargin(i)];
   elseif(isa(varargin{i},'DrakeMexPointer'))
     if strcmp(varargin{i}.name,'AllBodiesClosestDistanceConstraint')
       error('Please construct MATLAB AllBodiesClosestDistanceConstraint object');
-    end
+%     elseif strcmp(varargin{i}.name,'PostureConstraint')
+%       [joint_lb,joint_ub] = testPostureConstraintmex(varargin{i},t_breaks);
+%       joint_min = reshape(max([joint_min(:) joint_lb(:)],[],2),nq,[]);
+%       joint_max = reshape(min([joint_max(:) joint_ub(:)],[],2),nq,[]);
+%     end
   else
     other_constraint_cell = [other_constraint_cell,varargin(i)];
+%     if(isa(varargin{i},'PostureConstraint'))
+%       [joint_lb,joint_ub] = varargin{i}.bounds(t_breaks);
+%       joint_min = reshape(max([joint_min(:) joint_lb(:)],[],2),nq,[]);
+%       joint_max = reshape(min([joint_max(:) joint_ub(:)],[],2),nq,[]);
+%     end
   end
 end
-[xtraj,info,infeasible_constraint] = inverseKinTraj(obj,t,q_seed_traj,q_nom_traj,other_constraint_cell{:},ikoptions);
-nq = obj.getNumDOF();
-collisionAvoidFlag = false(length(t),length(collision_constraint_cell));
+[xtraj,info,infeasible_constraint] = inverseKinTraj(obj,t_breaks,q_seed_traj,q_nom_traj,other_constraint_cell{:},ikoptions);
+
+x_sol = xtraj.eval(t_breaks);
+q_sol = x_sol(1:nq,:);
+joint_limit_flag = abs(q_sol-joint_max)<1e-5 || abs(q_sol_joint_min)<1e-5;
+coords = obj.gestStateFrame.coordinates;
+for i = 1:length(t_breaks)
+  for j = 1:nq
+    if(joint_limit_flag(j,i))
+      display(sprintf('%s is at joint limit at time %4.2f',coords{joint_limit_flag(j,i)},t));
+    end
+  end
+end
+
+collisionAvoidFlag = false(length(t_verify),length(collision_constraint_cell));
 if(collision_status == 1 || collision_status == 2)
-  for i = 1:length(t)
-    xi = xtraj.eval(t(i));
+  for i = 1:length(t_verify)
+    xi = xtraj.eval(t_verify(i));
     qi = xi(1:nq);
     for j = 1:length(collision_constraint_cell)
-      if(collision_constraint_cell{j}.isTimeValid(t(i)))
+      if(collision_constraint_cell{j}.isTimeValid(t_verify(i)))
         [collisionAvoidFlag(i,j), dist,ptsA,ptsB,idxA,idxB] = collision_constraint_cell{j}.checkConstraint(qi);
         if(~collisionAvoidFlag(i,j))
           for k = 1:length(dist)
             send_status(4,0,0,sprintf('t=%4.2f,Dist from %s to %s is %f\n',...
-              t(i),...
+              t_verify(i),...
               sendNameString(collision_constraint_cell{j},idxA),...
               sendNameString(collision_constraint_cell{j},idxB),...
               dist(k)));
@@ -47,7 +79,7 @@ if(collision_status == 1 || collision_status == 2)
   end
 end
 if(collision_status == 2 && ~all(collisionAvoidFlag))
-  [xtraj,info,infeasible_constraint] = inverseKinTraj(obj,t,q_seed_traj,q_nom_traj,collision_constraint_cell{:},other_constraint_cell{:},ikoptions);
+  [xtraj,info,infeasible_constraint] = inverseKinTraj(obj,t_breaks,q_seed_traj,q_nom_traj,collision_constraint_cell{:},other_constraint_cell{:},ikoptions);
 end
 end
 
