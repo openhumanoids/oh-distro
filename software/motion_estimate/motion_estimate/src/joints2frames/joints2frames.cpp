756b8b28eda0c4cadaf402aed7682297c9229255
Maurice Fallon <mfallon@csail.mit.edu>
2013-10-30 17:41:14 +0000

    adding joints2frames rate limiting
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6924 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/motion_estimate/motion_estimate/src/joints2frames/joints2frames.cpp
+++ software/motion_estimate/motion_estimate/src/joints2frames/joints2frames.cpp
@@ -11,15 +11,23 @@ using namespace std;
 using namespace boost;
 using namespace boost::assign;
 
-#define DO_TIMING_PROFILE FALSE
+// false usually, set true to disable the limiting:
+#define DONT_LIMIT_FREQUENCY FALSE
 
 /////////////////////////////////////
+
+
+
 
 joints2frames::joints2frames(boost::shared_ptr<lcm::LCM> &lcm_, bool show_labels_, bool show_triads_,
   bool standalone_head_, bool ground_height_, bool bdi_motion_estimate_):
           lcm_(lcm_), show_labels_(show_labels_), show_triads_(show_triads_),
           standalone_head_(standalone_head_), ground_height_(ground_height_),
           bdi_motion_estimate_(bdi_motion_estimate_){
+            
+  botparam_ = bot_param_new_from_server(lcm_->getUnderlyingLCM(), 0);
+            
+            
   model_ = boost::shared_ptr<ModelClient>(new ModelClient(lcm_->getUnderlyingLCM(), 0));
   KDL::Tree tree;
   if (!kdl_parser::treeFromString( model_->getURDFString() ,tree)){
@@ -34,12 +42,48 @@ joints2frames::joints2frames(boost::shared_ptr<lcm::LCM> &lcm_, bool show_labels
   pc_vis_->obj_cfg_list.push_back( obj_cfg(6001,"Frames",5,1) );
   lcm_->subscribe("EST_ROBOT_STATE",&joints2frames::robot_state_handler,this);  
 
-  // Draw the Foot positions, as understood by BDI (Disabled)
+  // Draw the Foot positions, as understood by BDI (Disabled Oct 2013)
   // these points are just below the footpad, in the middle of the foot 
   //pc_vis_->obj_cfg_list.push_back( obj_cfg(6003,"BDI Feet",5,1) );
   //lcm_->subscribe("ATLAS_FOOT_POS_EST",&joints2frames::foot_pos_est_handler,this);  
   
-  last_ground_publish_utime_ =0;
+  
+  /// Limit the output frequency of the following messages:
+  // Currently 100Hz, actually ~80Hz:
+  
+  #if DONT_LIMIT_FREQUENCY
+    std::cout << "Output signals will not limited in rate\n";  
+  #else
+    std::cout << "Output signals will be limited to these rates:\n";
+    pub_frequency_["POSE_HEAD"] = FrequencyLimit(0, 1E6/getMaxFrequency( "head") );
+    pub_frequency_["HEAD_TO_BODY"] = FrequencyLimit(0, 1E6/getMaxFrequency( "body") );
+    pub_frequency_["POSE_BODY"] = FrequencyLimit(0, 1E6/ getMaxFrequency( "body_floating" ) );
+
+    // Is this only used for simulation:
+    pub_frequency_["HEAD_TO_HOKUYO_LINK"] = FrequencyLimit(0, 1E6/getMaxFrequency( "hokuyo_link") ); 
+
+    pub_frequency_["BODY_TO_UTORSO"] = FrequencyLimit(0, 1E6/getMaxFrequency( "utorso") ); 
+    pub_frequency_["BODY_TO_CAMERARHAND_LEFT"] = FrequencyLimit(0, 1E6/getMaxFrequency( "CAMERARHAND_LEFT") ); 
+    pub_frequency_["BODY_TO_CAMERALHAND_LEFT"] = FrequencyLimit(0, 1E6/getMaxFrequency( "CAMERALHAND_LEFT")   );
+    pub_frequency_["POSE_GROUND"] = FrequencyLimit(0, 1E6/ getMaxFrequency( "ground") );
+    pub_frequency_["POSE_LEFT_FOOT"] = FrequencyLimit(0, 1E6/ getMaxFrequency( "left_foot") );
+    pub_frequency_["POSE_RIGHT_FOOT"] = FrequencyLimit(0, 1E6/ getMaxFrequency( "right_foot") );    
+  #endif  
+}
+
+
+double joints2frames::getMaxFrequency(std::string query_root){
+  double max_frequency=1.0;  
+  
+  string query = "coordinate_frames." + query_root+ ".max_frequency";
+  
+  if ( bot_param_get_double(botparam_, query.c_str() , &max_frequency) == 0){
+    std::cout << max_frequency << "Hz \t| " << query_root  << "\n";
+  }else{
+    max_frequency =1.0;
+    std::cout << max_frequency << "Hz \t| " << query_root << " ###### not found. using default ######\n";
+  }
+  return max_frequency;
 }
 
 Eigen::Isometry3d KDLToEigen(KDL::Frame tf){
@@ -53,6 +97,9 @@ Eigen::Isometry3d KDLToEigen(KDL::Frame tf){
 }
 
 void joints2frames::publishPose(Eigen::Isometry3d pose, int64_t utime, std::string channel){
+  if (!shouldPublish(utime, channel) )
+    return; 
+  
   bot_core::pose_t pose_msg;
   pose_msg.utime =   utime;
   pose_msg.pos[0] = pose.translation().x();
@@ -68,6 +115,9 @@ void joints2frames::publishPose(Eigen::Isometry3d pose, int64_t utime, std::stri
 
 
 void joints2frames::publishRigidTransform(Eigen::Isometry3d pose, int64_t utime, std::string channel){
+  if (!shouldPublish(utime, channel) )
+    return; 
+
   bot_core::rigid_transform_t tf;
   tf.utime = utime;
   tf.trans[0] = pose.translation().x();
@@ -81,6 +131,35 @@ void joints2frames::publishRigidTransform(Eigen::Isometry3d pose, int64_t utime,
   lcm_->publish(channel, &tf);    
 }
 
+// find the channel and check to see if it should be published
+// true: publish | false: dont publish
+// then update the utime that it was last published at
+bool joints2frames::shouldPublish(int64_t utime, std::string channel){
+  #if DONT_LIMIT_FREQUENCY
+    return true;
+  #endif
+  
+  std::map<string,FrequencyLimit>::iterator it;
+  it = pub_frequency_.find( channel );
+  
+  if(it == pub_frequency_.end()){
+    std::cout << channel << " was not found in the pub_frequency list - not publishing\n";
+    return false; 
+  }
+  
+  if (utime <  it->second.last_utime ){
+    it->second.last_utime  = 0;
+    std::cout << utime << " detected negative time change for " << channel << " resetting last_utime\n";
+  }
+  
+  if (utime >  it->second.min_period + it->second.last_utime ){
+    it->second.last_utime = utime; 
+    return true;
+  }
+  
+  // if published recently - then dont publish again
+  return false;
+}
 
 void joints2frames::robot_state_handler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::robot_state_t* msg){
   
@@ -171,18 +250,14 @@ void joints2frames::robot_state_handler(const lcm::ReceiveBuffer* rbuf, const st
     Eigen::Isometry3d world_to_l_sole = world_to_body * body_to_l_foot * foot_to_sole;
     Eigen::Isometry3d world_to_r_sole = world_to_body * body_to_r_foot * foot_to_sole;
 
+    publishPose(world_to_l_sole, msg->utime,"POSE_LEFT_FOOT");
+    publishPose(world_to_r_sole, msg->utime,"POSE_RIGHT_FOOT");
+    
     // Publish lower of the soles at the ground occasionally
-    if (msg->utime - last_ground_publish_utime_  > 1E5){ // every 0.1sec
-      last_ground_publish_utime_ =msg->utime;
-      publishPose(world_to_l_sole, msg->utime,"POSE_LEFT_FOOT");
-      publishPose(world_to_r_sole, msg->utime,"POSE_RIGHT_FOOT");
-      if ( world_to_l_sole.translation().z() < world_to_r_sole.translation().z() ){
-        publishPose(world_to_l_sole, msg->utime,"POSE_GROUND");
-      }else{
-        publishPose(world_to_r_sole, msg->utime,"POSE_GROUND");
-      }
-
-
+    if ( world_to_l_sole.translation().z() < world_to_r_sole.translation().z() ){
+      publishPose(world_to_l_sole, msg->utime,"POSE_GROUND");
+    }else{
+      publishPose(world_to_r_sole, msg->utime,"POSE_GROUND");
     }
   }
   
--- software/motion_estimate/motion_estimate/src/joints2frames/joints2frames.hpp
+++ software/motion_estimate/motion_estimate/src/joints2frames/joints2frames.hpp
@@ -17,6 +17,9 @@
 #include "forward_kinematics/treefksolverposfull_recursive.hpp"
 #include <model-client/model-client.hpp>
 
+#include <bot_param/param_client.h>
+#include <bot_param/param_util.h>
+
 #include <pointcloud_tools/pointcloud_math.hpp>
 #include <pointcloud_tools/pointcloud_lcm.hpp>
 #include <pointcloud_tools/pointcloud_vis.hpp>
@@ -25,6 +28,14 @@
 #include <lcmtypes/bot_core.hpp>
 #include "lcmtypes/drc_lcmtypes.hpp"
 
+struct FrequencyLimit {
+  FrequencyLimit(){}
+  FrequencyLimit(int64_t last_utime , int64_t min_period):
+      last_utime(last_utime),min_period(min_period){}
+  
+  int64_t last_utime; // utime of last published message
+  int64_t min_period; // minimum time between publishes
+};
 
 ///////////////////////////////////////////////////////////////
 class joints2frames{
@@ -39,28 +50,24 @@ class joints2frames{
     
   private:
     boost::shared_ptr<lcm::LCM> lcm_;
+    BotParam* botparam_;
     boost::shared_ptr<ModelClient> model_;
     boost::shared_ptr<KDL::TreeFkSolverPosFull_recursive> fksolver_;
-
     pointcloud_vis* pc_vis_;
+    
+    map<string, FrequencyLimit > pub_frequency_;
+    bool show_labels_, show_triads_, ground_height_;
+    bool standalone_head_, bdi_motion_estimate_;
 
     void urdf_handler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::robot_urdf_t* msg);
     void robot_state_handler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::robot_state_t* msg);
     void foot_pos_est_handler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  drc::atlas_foot_pos_est_t* msg);
     
-    
-    bool show_labels_, show_triads_, ground_height_;
-
-    bool standalone_head_, bdi_motion_estimate_;
-    
     void publishPose(Eigen::Isometry3d pose, int64_t utime, std::string channel);
     void publishRigidTransform(Eigen::Isometry3d pose, int64_t utime, std::string channel);
+    bool shouldPublish(int64_t utime, std::string channel); 
     
-    double last_ground_height_;
-    int64_t last_ground_publish_utime_;
-    int64_t last_joint_publish_utime_;
-    
-    Eigen::Quaterniond  euler_to_quat(double yaw, double pitch, double roll);
+    double getMaxFrequency(std::string query_root);
 };    
 
 #endif
