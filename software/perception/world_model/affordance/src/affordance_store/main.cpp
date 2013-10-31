a1e73983a2bfe24547b0e157843091df78bdc2e3
Maurice Fallon <mfallon@csail.mit.edu>
2013-10-31 20:00:51 +0000

    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6962 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/affordance/src/affordance_store/main.cpp
+++ software/perception/world_model/affordance/src/affordance_store/main.cpp
@@ -16,31 +16,8 @@ int main(int argc, char ** argv)
   opt.add(role, "r", "role","Role - robot or base");
   opt.parse();
   std::cout << "role: " << role << "\n";
-  
 
-  string lcm_url="";
-  std::string role_upper;
-  for(ushort i = 0; i < role.size(); ++i)
-     role_upper+= (std::toupper(role[i]));
-  if((role.compare("robot") == 0) || (role.compare("base") == 0) ){
-    for(ushort i = 0; i < role_upper.size(); ++i)
-       role_upper[i] = (std::toupper(role_upper[i]));
-    string env_variable_name = string("LCM_URL_DRC_" + role_upper); 
-    char* env_variable;
-    env_variable = getenv (env_variable_name.c_str());
-    if (env_variable!=NULL){
-      //printf ("The env_variable is: %s\n",env_variable);      
-      lcm_url = string(env_variable);
-    }else{
-      std::cout << env_variable_name << " environment variable has not been set ["<< lcm_url <<"]\n";     
-      exit(-1);
-    }
-  }else{
-    std::cout << "Role not understood, choose: robot or base\n";
-    return 1;
-  }   
-
-  shared_ptr<lcm::LCM> theLcm(new lcm::LCM( lcm_url ));
+  shared_ptr<lcm::LCM> theLcm(new lcm::LCM( "" ));
   if (!theLcm->good())
   {
     cerr << "Cannot create lcm object" << endl;
