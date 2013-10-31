2132e2371a91bb249d9ee368c377bb0738f29dca
Maurice Fallon <mfallon@csail.mit.edu>
2013-10-31 19:53:54 +0000

    intentionally removing robot-base urls - the robot and base communities will not be serperated if their LCM can be bridged
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@6959 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/network/network_sim/src/network_bridge/network-bridge.cpp
+++ software/network/network_sim/src/network_bridge/network-bridge.cpp
@@ -271,6 +271,8 @@ int main (int argc, char ** argv) {
     std::cout << "robot only: " << cl_cfg.bot_only << "\n";
     std::cout << "base only: " << cl_cfg.base_only << "\n";
 
+    // Different URLS for different robots has been disabled:
+    /*
     char* lcm_url_robot;
     lcm_url_robot = getenv ( "LCM_URL_DRC_ROBOT" );
     
@@ -289,15 +291,14 @@ int main (int argc, char ** argv) {
         std::cout << "LCM_URL_DRC_BASE environment variable has not been set." << std::endl;
         exit(-1);
     }
+    */
 
 
-
-
-    boost::shared_ptr<lcm::LCM> robot_lcm(new lcm::LCM( lcm_url_robot )  );
+    boost::shared_ptr<lcm::LCM> robot_lcm(new lcm::LCM());// lcm_url_robot )  );
     if(!robot_lcm->good()){
         std::cerr <<"ERROR: lcm is not good()" <<std::endl;
     }
-    boost::shared_ptr<lcm::LCM> base_lcm(new lcm::LCM( lcm_url_base )  );
+    boost::shared_ptr<lcm::LCM> base_lcm(new lcm::LCM());// lcm_url_base )  );
     if(!base_lcm->good()){
         std::cerr <<"ERROR: lcm is not good()" <<std::endl;
     }
