b9984bd0c9219aad45014dfdb78b12b4f75f9ba1
Maurice Fallon <mfallon@csail.mit.edu>
2013-11-03 14:45:56 +0000

    adding argument to logger for robot/base
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7021 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/tools/tools/scripts/drc-lcm-logger.sh
+++ software/tools/tools/scripts/drc-lcm-logger.sh
@@ -2,7 +2,15 @@
 # drc-lcm-logger.sh
 # Author: Maurice Fallon
 # Writes LCM logs to a specific directory, but adds time as extension
+#echo "|$1|"
 
 date_str=$(date +"lcmlog-%Y-%m-%d-%H-%M")
 date_str="$DRC_BASE/logs/$date_str"
+
+
+if [ -n "$1" ]; then
+  #echo "1 is not empty"
+  date_str=$date_str"-$1" 
+fi
+#echo $date_str
 lcm-logger $date_str
