7acc2af5bb9aa34b5224099c8efe74a251d1b6b4
Twan Koolen <tkoolen@csail.mit.edu>
2013-11-03 18:46:43 +0000

    Improved mit_irobot_hand_control script: can now open and close using the same script (reduced code duplication) using program arguments, and can concatenate commands (e.g. mit_irobot_hand_control CLOSE OPEN. Also made it so that program terminates when done (the call rospy.spin() was misplaced for this simple script)
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7025 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/irobot-hand/Software/handle/handle_launch/launch/mit_irobot_hand_control.py
+++ ros_workspace/irobot-hand/Software/handle/handle_launch/launch/mit_irobot_hand_control.py
@@ -7,64 +7,72 @@ Created on Oct 30, 2013
 
 import roslib; roslib.load_manifest('handle_launch')
 import rospy
+import argparse
 
 from handle_msgs.msg import HandleControl
 
-def do_control(command_message, motor_indices, control_type, value):
+motor_indices = range(3) #range(3)
+
+def set_command_message(command_message, motor_indices, control_type, value):
     for motor_index in motor_indices:
         command_message.valid[motor_index] = True
         command_message.type[motor_index] = control_type
         command_message.value[motor_index] = value
-    
+
+def loop_control(rospy, publisher, rate, max_time, control):
+    start_time = rospy.get_time()
+
+    while rospy.get_time() - start_time < max_time and not rospy.is_shutdown():
+        command_message = HandleControl()
+        control(command_message)
+#         publisher.publish(command_message)
+        rate.sleep()
+
+def zero_current(publisher):
+    no_current_message = HandleControl()
+    set_command_message(no_current_message, motor_indices, HandleControl.CURRENT, 0)
+#     publisher.publish(no_current_message)
+
+def close_hand_current_control(rospy, publisher, rate):
+    grasp_time = 5
+    finger_close_current = 1 # milliamps?
+    def control(command_message):
+        set_command_message(command_message, motor_indices, HandleControl.CURRENT, finger_close_current)
+
+    loop_control(rospy, publisher, rate, grasp_time, control)
+
+def open_hand(rospy, publisher, rate):
+    open_time = 5
+    open_angle = 10
+    def control(command_message):
+        set_command_message(command_message, motor_indices, HandleControl.ANGLE, open_angle)
+        
+    loop_control(rospy, publisher, rate, open_time, control)
+
 
 if __name__ == '__main__':
-    node_name = "close_finger_current_control"
+    parser = argparse.ArgumentParser(description='Script for interacting with iRobot hand')
+    parser.add_argument('commands', nargs='+')
+    args = parser.parse_args()
+    commands = args.commands
+    print "Commands: " + ", ".join(commands)
+    
+    node_name = "mit_irobot_hand_control"
     publisher_name = "irobot_hands/r_hand/control"
     ros_rate = 100.0  # todo: something smarter
-
+    
     rospy.init_node(node_name)
     rate = rospy.Rate(ros_rate)
     publisher = rospy.Publisher(publisher_name, HandleControl)
-
-    start_time = rospy.get_time()
     
-    
-    grasp_time = 5
-    open_time = grasp_time + 5
-    open_angle = 10
-
-
-    finger_close_current = 1 # milliamps?
-    motor_indices = range(3) #range(3)
-
-
-    done = False
-    close_started = False
-    open_started = False
-    
-    while not rospy.is_shutdown() and not done:
-        time = rospy.get_time() - start_time
-        
-        command_message = HandleControl()
-        if time < grasp_time:
-            if not close_started:
-                print("Closing")
-                close_started = True
-            do_control(command_message, motor_indices, HandleControl.CURRENT, finger_close_current)
-#        elif time < open_time:
-#            if not open_started:
-#                print("Opening")
-#                open_started = True
-#            do_control(command_message, motor_indices, HandleControl.ANGLE, open_angle)
+    for command in commands:
+        print "Executing command: " + command
+        if command == 'CLOSE':
+            close_hand_current_control(rospy, publisher, rate)
+        elif command == 'OPEN':
+            open_hand(rospy, publisher, rate)
         else:
-            done = True
+            raise RuntimeError("Command not recognized: " + command)
 
-#         print command_message
-        publisher.publish(command_message)
-        rate.sleep()
-    
-    print("sending command: no current")
-    no_current_message = HandleControl()
-    do_control(no_current_message, motor_indices, HandleControl.CURRENT, 0)
-    publisher.publish(no_current_message)
-    rospy.spin()
+    print "Sending zero current message"
+    zero_current(publisher)
