5371f94a17fa8285dcea7bee31781f93ded2da78
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-11-07 20:05:45 +0000

    Adds a utility to echo back plans as EST_ROBOT_STATE for testing planners
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7136 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/planners/echo_plan_as_robot_state.m
@@ -0,0 +1,39 @@
+lc = lcm.lcm.LCM.getSingleton();
+plan_monitor = drake.util.MessageMonitor(drc.robot_plan_t(),'utime');
+state_monitor = drake.util.MessageMonitor(drc.robot_state_t(),'utime');
+lc.subscribe('COMMITTED_ROBOT_PLAN',plan_monitor);
+lc.subscribe('EST_ROBOT_STATE',state_monitor);
+Fs = 30;
+msg = state_monitor.getNextMessage(1);
+if isempty(msg)
+  r = Atlas();
+  load ../data/atlas_bdi_fp.mat
+  joint_names = r.getStateFrame.coordinates(1:getNumDOF(r));
+  robot_state_coder = LCMCoordinateFrameWCoder('AtlasState',r.getNumStates(),'x',JLCMCoder(drc.control.RobotStateCoder(joint_names)));
+  publish(robot_state_coder,0,xstar,'EST_ROBOT_STATE');
+  msg = state_monitor.getNextMessage(1);
+end
+current_robot_state = drc.robot_state_t(msg);
+
+display('Listening for committed plans ...');
+while true
+  msg = plan_monitor.getNextMessage(0);
+  
+  if ~isempty(msg)
+    display('Received plan. Echoing ...');
+    robot_plan = drc.robot_plan_t(msg);
+    lc.publish('EST_ROBOT_STATE',robot_plan.plan(1))
+    for i = 2:robot_plan.num_states
+      pause(1e-6*(robot_plan.plan(i).utime-robot_plan.plan(i-1).utime))
+      current_robot_state.joint_position = robot_plan.plan(i).joint_position;
+      current_robot_state.utime = now() * 24 * 60 * 60;
+      lc.publish('EST_ROBOT_STATE',current_robot_state)
+    end
+    current_robot_state = robot_plan.plan(end);
+    display('Listening for committed plans ...');
+  else
+    pause(1/Fs);
+    current_robot_state.utime = now() * 24 * 60 * 60;
+    lc.publish('EST_ROBOT_STATE',current_robot_state)
+  end
+end
