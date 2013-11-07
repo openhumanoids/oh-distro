49ed47b4fa9fc522b442a4494f4ea04f219c73b4
Robin Deits <rdeits@csail.mit.edu>
2013-11-07 23:42:27 +0000

    Actually fix crash when all terrain is bad
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7143 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/systems/@Biped/createInitialSteps.m
+++ software/control/matlab/systems/@Biped/createInitialSteps.m
@@ -70,6 +70,9 @@ function [X, foot_goals] = createInitialSteps(biped, x0, goal_pos, params)
       s_foot = 'right';
     end
     goal = foot_goals.(m_foot);
+    if isempty(goal)
+      break
+    end
 
     potential_poses = foot_centers.(m_foot)(:, last_ndx.(m_foot):end);
     feas_opts = struct('forward_step', params.nom_forward_step,...
@@ -134,9 +137,6 @@ function [X, foot_goals] = createInitialSteps(biped, x0, goal_pos, params)
 
     X(end+1) = struct('pos', pos_n, 'step_speed', 0, 'step_height', 0, 'id', 0, 'pos_fixed', zeros(6, 1), 'is_right_foot', is_right_foot, 'is_in_contact', true);
 
-    if isempty(goal)
-      break
-    end
     if ((all(abs(X(end).pos - goal) < 0.05) && all(abs(X(end-1).pos - foot_goals.(s_foot)) < 0.05)) || (length(X) - 2) >= params.max_num_steps) && ((length(X) - 2) >= params.min_num_steps)
       break
     end
