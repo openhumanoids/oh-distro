547b7d710d40e4141206847baba691305084f704
Hongkai Dai <hongkai@csail.mit.edu>
2013-11-07 18:09:47 +0000

    Forgot to include this in my last commit
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7132 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/planners/atlas_keyframe_planners/inverseKinRepeatSearch.m
@@ -0,0 +1,18 @@
+function [q,info,infeasible_constraint] = inverseKinRepeatSearch(obj,total_ik_attempt,q_seed,q_nom,varargin)
+% The same input and output as inverseKin. But search the solution repetitvely with some
+% random guess
+% @param total_ik_attempt   The maximum number of IK trials
+ik_attempt_count = 0;
+findFinalPostureFlag = false;
+while(~findFinalPostureFlag && ik_attempt_count< total_ik_attempt)
+  [q,info,infeasible_constraint] = inverseKin(obj,q_seed,q_nom,varargin{:});
+  ik_attempt_count = ik_attempt_count+1;
+  q_seed = q+1e-1*randn(size(q));
+  findFinalPostureFlag = info<=10;
+end
+if(findFinalPostureFlag)
+  display(sprintf('The IK succeeds after %d trials',ik_attempt_count));
+else
+  display(sprintf('The IK fails after %d trials',ik_attempt_count));
+end
+end
