d090bcc58585f542f10d698ad9d82bf74252bf94
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-11-08 17:59:03 +0000

    echo_plan_as_robot_state now handles floating-base states appropriately.
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7161 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/echo_plan_as_robot_state.m
+++ software/control/matlab/planners/echo_plan_as_robot_state.m
@@ -24,8 +24,16 @@ while true
     robot_plan = drc.robot_plan_t(msg);
     lc.publish('EST_ROBOT_STATE',robot_plan.plan(1))
     for i = 2:robot_plan.num_states
-      pause(1e-6*(robot_plan.plan(i).utime-robot_plan.plan(i-1).utime))
+      pause(1e-6*(robot_plan.plan(i).utime-robot_plan.plan(i-1).utime));
+      current_robot_state.pose.translation.x = robot_plan.plan(i).pose.translation.x;
+      current_robot_state.pose.translation.y = robot_plan.plan(i).pose.translation.y;
+      current_robot_state.pose.translation.z = robot_plan.plan(i).pose.translation.z;
+      current_robot_state.pose.rotation.w = robot_plan.plan(i).pose.rotation.w;
+      current_robot_state.pose.rotation.x = robot_plan.plan(i).pose.rotation.x;
+      current_robot_state.pose.rotation.y = robot_plan.plan(i).pose.rotation.y;
+      current_robot_state.pose.rotation.z = robot_plan.plan(i).pose.rotation.z;
       current_robot_state.joint_position = robot_plan.plan(i).joint_position;
+      
       current_robot_state.utime = now() * 24 * 60 * 60;
       lc.publish('EST_ROBOT_STATE',current_robot_state)
     end
