334a7a9cc703af865a9afe5f0848b3cd399589fb
Scott Kuindersma <kuindersma@gmail.com>
2013-11-12 22:14:28 +0000

    cleaning thing up a bit. made separate folder for calibration scripts
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7236 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/systems/HydraulicActuator.m
+++ software/control/matlab/systems/HydraulicActuator.m
@@ -3,9 +3,9 @@ classdef HydraulicActuator < DrakeSystem
   
   properties
     %%%% System properties %%%
-    p_supply = 3000 * 0.0689; % [bar] system supply pressure
-    p_return = 100 * 0.0689; % [bar] system return (tank) pressure
-    beta = 13000; % [bar] effective bulk modulus
+    p_supply = 3000 * 0.00689475729; % [MPa] system supply pressure
+    p_return = 100 * 0.00689475729; % [MPa] system return (tank) pressure
+    beta = 1200; % [MPa] effective bulk modulus
     %%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %%%% Valve properties %%%%
@@ -15,16 +15,17 @@ classdef HydraulicActuator < DrakeSystem
     max_input = 10; % [mA] maximum current supplied to valve
     nominal_flow = 0.000113071; % [m^3/s] should be available in valve datasheet 
     nominal_current = 10; % [mA] should be available in valve datasheet 
-    nominal_pressure = 68.95; % [bar] should be available in valve datasheet 
+    nominal_pressure = 1000 * 0.00689475729; % [MPa] should be available in valve datasheet 
     Kv; % valve gain, computed from nominal flow, current, and pressure
+    Kl = 0.0; % internal leakage coefficient
     %%%%%%%%%%%%%%%%%%%%%%%%%%
 
     %%%% Cylinder properties %
     Apos = 0.5891 / 1550; % [m^2] area on the positive side of the cylinder
     Aneg = 0.7854 / 1550; % [m^2]
     lc = 0.1; % [m] length of the cylinder
-    Vhose = 3e-5; % [m^3] hose pipeline volume
-    Fv = 10; % [Ns/m] viscous damping coefficient    
+    Vhose = 0; % [m^3] hose pipeline volume
+    Fv = 1000; % [Ns/m] viscous damping coefficient, very large to make simulation reasonable   
     mass = 10.0; % [kg] mass of the piston + fluid
     %%%%%%%%%%%%%%%%%%%%%%%%%%
   end
@@ -43,11 +44,13 @@ classdef HydraulicActuator < DrakeSystem
       % x = [x_v, dx_v, x_p, dx_p, p_pos, p_neg]
 
       f=zeros(6,1);
-      % second-order spool dynamics
+      % second-order spool dynamics: K*u = 1/omega^2*xdd + 2*xi/omega*xd + x
+      % Jelali and Kroll (2003) eq 4.9
       f(1) = x(2);
       f(2) = obj.omega^2*(obj.Kspool*u - 2*obj.xi/obj.omega * x(2) - x(1));
 
       % compute flow 
+      % Jelali and Kroll (2003) eq 4.2-4.3
       if x(1) > 0
         qpos = obj.Kv*x(1)*sign(obj.p_supply-x(5))*sqrt(abs(obj.p_supply-x(5)));
         qneg = -obj.Kv*x(1)*sign(x(6)-obj.p_return)*sqrt(abs(x(6)-obj.p_return));
@@ -56,22 +59,31 @@ classdef HydraulicActuator < DrakeSystem
         qneg = -obj.Kv*x(1)*sign(obj.p_supply-x(6))*sqrt(abs(obj.p_supply-x(6)));
       end
       
+      % flow from internal leakage
+      q_leakage = obj.Kl * (x(6)-x(5));
+      
       % compute chamber volumes
       Vpos = obj.Vhose + obj.Apos*x(3);
       Vneg = obj.Vhose + obj.Aneg*(obj.lc-x(3));
     
-      f(3) = x(4);
+      % piston motion
+      % Jelali and Kroll (2003) eq 4.56
       Fext = 0;
-      f(4) = (obj.Apos*x(5)*1e5 - obj.Aneg*x(6)*1e5 - obj.Fv*x(4) + Fext) / obj.mass;  
-      f(5) = obj.beta / Vpos * (qpos - obj.Apos*x(4));
-      f(6) = obj.beta / Vneg * (qneg + obj.Aneg*x(4));
+      f(3) = x(4);
+      f(4) = (obj.Apos*x(5)*1e6 - obj.Aneg*x(6)*1e6 - obj.Fv*x(4) - Fext) / obj.mass;  
 
+      % chamber pressure dynamics
+      % Jelali and Kroll (2003) eq 4.51-4.52
+      f(5) = obj.beta / Vpos * (qpos - obj.Apos*x(4) + q_leakage);
+      f(6) = obj.beta / Vneg * (qneg + obj.Aneg*x(4) - q_leakage);
+
+      t
       %assert(x(3)>=0 && x(3)<=obj.lc);
     end
     
     function y=output(obj,t,x,u)
       % outputs actuator force
-      f_out = obj.Apos*x(5)*1e5 - obj.Aneg*x(6)*1e5 - obj.Fv*x(4);
+      f_out = obj.Apos*x(5)*1e6 - obj.Aneg*x(6)*1e6 - obj.Fv*x(4);
       y=[x;f_out];
     end
     
@@ -80,13 +92,6 @@ classdef HydraulicActuator < DrakeSystem
       p_pos = obj.Aneg*p_neg/obj.Apos;
       x = [0;0;obj.lc/2;0;p_pos;p_neg];
     end
-    
-    function val = sg(obj,x)
-      val=0;
-      if x>=0
-        val=x;
-      end
-    end
-  end  
+  end
   
 end
