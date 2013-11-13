dc8af5b3e8b0bd51a4333ddf1af3eefb9bb09344
Mathew Antone <antone@csail.mit.edu>
2013-11-13 17:15:18 +0000

    added automatic reference frame update when registration overlap is poor, to increase longevity of valid alignment
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7248 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/maps/src/apps/PointCloudRegistrar.cpp
+++ software/perception/world_model/maps/src/apps/PointCloudRegistrar.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 #include <thread>
 
 #include <drc_utils/LcmWrapper.hpp>
@@ -35,7 +35,15 @@ struct State : public maps::Collector::DataListener {
   maps::PointCloud::Ptr mRefCloud;
   maps::PointCloud::Ptr mCurCloud;
   Eigen::Isometry3f mCurToRef;
-  std::mutex mMutex;
+  Eigen::Isometry3f mRefToOrig;
+  std::vector<int> mRefInliers;
+  bool mResetReference;
+  std::thread mThread;
+  std::mutex mCommandMutex;
+  std::mutex mRunMutex;
+  std::condition_variable mCondition;
+  bool mIsRunning;
+  bool mIsDataReady;
 
   State() {
     // initialize some variables
@@ -51,11 +59,16 @@ struct State : public maps::Collector::DataListener {
     mLcmGl = bot_lcmgl_init(mBotWrapper->getLcm()->getUnderlyingLCM(),
                             "maps-registrar");
     mCurToRef = Eigen::Isometry3f::Identity();
+    mRefToOrig = Eigen::Isometry3f::Identity();
 
     mLcm->subscribe("MAP_REGISTRATION_COMMAND", &State::onCommand, this);
+    mResetReference = false;
+    mIsRunning = false;
+    mIsDataReady = false;
   }
 
   ~State() {
+    stop();
     bot_lcmgl_destroy(mLcmGl);
   }
 
@@ -63,10 +76,82 @@ struct State : public maps::Collector::DataListener {
                  const std::string& iChannel,
                  const drc::map_registration_command_t* iMessage) {
     if (iMessage->command == drc::map_registration_command_t::RESET_REFERENCE) {
-      std::unique_lock<std::mutex> lock(mMutex);
-      mRefCloud.reset();
-      mCurToRef = Eigen::Isometry3f::Identity();
+      std::unique_lock<std::mutex> lock(mCommandMutex);
+      mResetReference = true;
+      mCondition.notify_one();
     }
+  }
+
+  void reset() {
+    std::unique_lock<std::mutex> lock(mCommandMutex);
+    mRefCloud.reset();
+    mCurToRef = Eigen::Isometry3f::Identity();
+    mRefToOrig = Eigen::Isometry3f::Identity();
+    mResetReference = false;
+  }
+
+  void operator()() {
+    while (mIsRunning) {
+      std::unique_lock<std::mutex> lock(mRunMutex);
+      mCondition.wait_for(lock,std::chrono::seconds(1));
+      if (mResetReference) reset();
+      if (!mIsDataReady) continue;
+      mIsDataReady = false;
+
+      // create space-time bounds from desired time range and around head
+      Eigen::Isometry3f headToLocal;
+      mBotWrapper->getTransform("head","local",headToLocal);
+      Eigen::Vector3f headPos = headToLocal.translation();
+      maps::LocalMap::SpaceTimeBounds bounds;
+      bounds.mTimeMin = mTimeMin;
+      bounds.mTimeMax = mTimeMax;
+      Eigen::Vector3f minPoint(-2,-2,-3), maxPoint(2,2,0.5);
+      bounds.mPlanes =
+        maps::Utils::planesFromBox(minPoint+headPos, maxPoint+headPos);
+
+      // get point cloud corresponding to this time range
+      maps::LocalMap::Ptr localMap =
+        mCollector->getMapManager()->getMap(mActiveMapId);
+      maps::PointCloudView::Ptr cloudView =
+        localMap->getAsPointCloud(0.01, bounds);
+      mCurCloud = cloudView->getAsPointCloud();
+
+      if (mRefCloud == NULL) {
+        mRefCloud = mCurCloud;
+      }
+
+      // align points
+      Eigen::Isometry3f curToRef;
+      double inlierFraction;
+      if (registerToReference(mCurCloud, curToRef, inlierFraction)) {
+        std::cout << "SUCCESS" << std::endl;
+        mCurToRef = curToRef;
+
+        // update reference if necessary
+        if (inlierFraction < 0.3) {
+          mRefCloud = mCurCloud;
+          mRefToOrig = mRefToOrig*mCurToRef;
+          mCurToRef = Eigen::Isometry3f::Identity();
+          std::cout << "*** inlier fraction " << inlierFraction << 
+            "; updating reference frame ***" << std::endl;
+        }
+
+        // publish pose update
+        publishMessage(mRefToOrig*mCurToRef, mTimeMax);
+      }
+      else {
+        std::cout << "FAILURE; resetting" << std::endl;
+        reset();
+      }
+    }
+  }
+
+  void stop() {
+    mIsRunning = false;
+    mLcmWrapper->stopHandleThread();
+    mCondition.notify_one();
+    if (mThread.joinable()) mThread.join();
+    mCollector->stop();
   }
 
   void start() {
@@ -87,6 +172,11 @@ struct State : public maps::Collector::DataListener {
     mCollector->addListener(*this);
     mCollector->start();
 
+    // start running processing thread
+    // (this avoids putting load on the data receiver thread)
+    mIsRunning = true;
+    mThread = std::thread(std::ref(*this));
+
     // start lcm thread running
     mLcmWrapper->startHandleThread(true);
   }
@@ -102,42 +192,8 @@ struct State : public maps::Collector::DataListener {
     if ((timeMin == mTimeMin) && (timeMax == mTimeMax)) return;
     mTimeMin = timeMin;
     mTimeMax = timeMax;
-    std::cout << "time range: " << timeMin << " " << timeMax << std::endl;
-
-    // create space-time bounds from desired time range and around head
-    Eigen::Isometry3f headToLocal;
-    mBotWrapper->getTransform("head","local",headToLocal);
-    Eigen::Vector3f headPos = headToLocal.translation();
-    std::cout << "HEAD POS " << headPos.transpose() << std::endl;
-    maps::LocalMap::SpaceTimeBounds bounds;
-    bounds.mTimeMin = timeMin;
-    bounds.mTimeMax = timeMax;
-    Eigen::Vector3f minPoint(-2,-2,-3), maxPoint(2,2,0.5);
-    bounds.mPlanes =
-      maps::Utils::planesFromBox(minPoint+headPos, maxPoint+headPos);
-
-    // get point cloud corresponding to this time range
-    maps::LocalMap::Ptr localMap =
-      mCollector->getMapManager()->getMap(mActiveMapId);
-    maps::PointCloudView::Ptr cloudView =
-      localMap->getAsPointCloud(0.01, bounds);
-    mCurCloud = cloudView->getAsPointCloud();
-
-    {
-      // align clouds
-      std::unique_lock<std::mutex> lock(mMutex);
-
-      if (mRefCloud == NULL) {
-        mRefCloud = mCurCloud;
-      }
-
-      Eigen::Isometry3f curToRef;
-      if (registerToReference(mCurCloud, curToRef)) {
-        std::cout << "SUCCESS" << std::endl;
-        mCurToRef = curToRef;
-        publishMessage(mCurToRef, timeMax);
-      }
-    }
+    mIsDataReady = true;
+    mCondition.notify_one();
   }
 
   void publishMessage(const Eigen::Isometry3f& iTransform,
@@ -153,43 +209,65 @@ struct State : public maps::Collector::DataListener {
     mLcm->publish(mUpdateChannel, &msg);
   }
 
+  void icpCallback(const maps::PointCloud& iSource,
+                   const std::vector<int>& iIndicesSource,
+                   const maps::PointCloud& iTarget,
+                   const std::vector<int>& iIndicesTarget) {
+    mRefInliers = iIndicesTarget;
+  }
+
   bool registerToReference(const maps::PointCloud::Ptr& iCurCloud,
-                           Eigen::Isometry3f& oCurToRef) {
+                           Eigen::Isometry3f& oCurToRef, double& oInlierFrac) {
     typedef maps::PointCloud::PointType PointType;
     if (mRefCloud == NULL) return false;
+
+    // set up icp object
     pcl::IterativeClosestPoint<PointType, PointType> icp;
     icp.setInputCloud(iCurCloud);
     icp.setInputTarget(mRefCloud);
     icp.setMaxCorrespondenceDistance(0.05);
+    //icp.setUseReciprocalCorrespondences(true);
+    boost::function
+      <void(const maps::PointCloud&, const std::vector<int>&,
+            const maps::PointCloud&, const std::vector<int>&)> functionObject;
+    functionObject = boost::bind(&State::icpCallback, this, _1, _2, _3, _4);
+    icp.registerVisualizationCallback(functionObject);
     pcl::PointCloud<PointType> finalCloud;
+
+    // align clouds
     icp.align(finalCloud, mCurToRef.matrix());
-    std::cout << "converged?:" << icp.hasConverged() << " score: " <<
-      icp.getFitnessScore() << std::endl;
-    std::cout << "matching points:" << finalCloud.size() << std::endl;
-    std::cout << icp.getFinalTransformation() << std::endl;
+    std::unordered_set<int> uniqueIndices;
+    for (auto ind : mRefInliers) uniqueIndices.insert(ind);
     oCurToRef.matrix() = icp.getFinalTransformation();
+    oInlierFrac = (double)uniqueIndices.size()/mRefCloud->size();
+
+    std::cout << "score: " << icp.getFitnessScore() << std::endl;
+    std::cout << oCurToRef.matrix() << std::endl;
+    std::cout << "inlier fraction: " << oInlierFrac << std::endl;
 
     // debug
     bot_lcmgl_t* lcmgl = mLcmGl;
+    /*
     bot_lcmgl_color4f(lcmgl, 0.5, 0, 0, 0.3);
     bot_lcmgl_point_size(lcmgl, 3);
-    for (int i = 0; i < iCurCloud->size(); ++i) {
+    for (int i = 0; i < iCurCloud->size(); i += 1) {
       maps::PointCloud::PointType point = (*iCurCloud)[i];
       bot_lcmgl_begin(lcmgl, LCMGL_POINTS);
       bot_lcmgl_vertex3f(lcmgl, point.x, point.y, point.z);
       bot_lcmgl_end(lcmgl);
     }
+    */
     bot_lcmgl_color4f(lcmgl, 0, 0.5, 0, 0.3);
     bot_lcmgl_point_size(lcmgl, 3);
-    for (int i = 0; i < mRefCloud->size(); ++i) {
+    for (int i = 0; i < mRefCloud->size(); i += 1) {
       maps::PointCloud::PointType point = (*mRefCloud)[i];
       bot_lcmgl_begin(lcmgl, LCMGL_POINTS);
       bot_lcmgl_vertex3f(lcmgl, point.x, point.y, point.z);
       bot_lcmgl_end(lcmgl);
     }
-    bot_lcmgl_color4f(lcmgl, 0, 0, 0.5,0.3);
+    bot_lcmgl_color4f(lcmgl, 1.0, 0.5, 0, 0.3);
     bot_lcmgl_point_size(lcmgl, 3);
-    for (int i = 0; i < finalCloud.size(); ++i) {
+    for (int i = 0; i < finalCloud.size(); i += 1) {
       maps::PointCloud::PointType point = finalCloud[i];
       bot_lcmgl_begin(lcmgl, LCMGL_POINTS);
       bot_lcmgl_vertex3f(lcmgl, point.x, point.y, point.z);
