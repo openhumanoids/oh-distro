6d0fbcdbcb46a9a0c9eb7ff983f5fe0527a0c111
Maurice Fallon <mfallon@csail.mit.edu>
2013-11-15 23:49:37 +0000

    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7376 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/drivers/multisense/scripts/spin-lidar-command.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os,sys
+home_dir =os.getenv("HOME")
+#print home_dir
+sys.path.append(home_dir + "/drc/software/build/lib/python2.7/site-packages")
+sys.path.append(home_dir + "/drc/software/build/lib/python2.7/dist-packages")
+
+import lcm
+from multisense.command_t import command_t
+import time
+
+def timestamp_now (): return int (time.time () * 1000000)
+
+print "Sleeping for 5 seconds"
+time.sleep(5)
+
+msg = command_t()
+msg.utime = timestamp_now()
+msg.rpm = 3
+msg.fps = -1
+msg.gain = -1
+
+lc = lcm.LCM()
+lc.publish("MULTISENSE_COMMAND", msg.encode())
+print "Publishing Multisense command to spin at 3rpm"
+print "Process exiting"
