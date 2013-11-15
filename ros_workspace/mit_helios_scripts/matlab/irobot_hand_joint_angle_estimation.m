1341f545b2156139a3a9c85d03889810179d8da2
Twan Koolen <tkoolen@csail.mit.edu>
2013-11-15 18:18:13 +0000

    tweaks to irobot_hand_joint_angle_estimation.
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7346 c7283977-0100-402a-a91a-fa70b306dbfe

--- ros_workspace/mit_helios_scripts/matlab/irobot_hand_joint_angle_estimation.m
+++ ros_workspace/mit_helios_scripts/matlab/irobot_hand_joint_angle_estimation.m
@@ -2,8 +2,8 @@ function irobot_hand_joint_angle_estimation()
 
 data = import_irobot_hand_data('../data/joint_angle_estimation_data.csv');
 
-xData = data.motorHallEncoder0;
-yData = data.proximalJointAngle0;
+xData = [data.motorHallEncoder0; data.motorHallEncoder1];
+yData = [data.proximalJointAngle0; data.proximalJointAngle1];
 
 validIndices = find(xData > 0);
 xData = xData(validIndices);
@@ -14,25 +14,45 @@ nKnotPoints = 3;
 
 [lb, ub] = getbounds(param0, xKnot1, xKnotFinal, nKnotPoints);
 
+param = lsqcurvefit(@piecewiseLinear, param0, xData, yData, lb, ub);
+
+xKnotSol = [xKnot1; param(1 : nKnotPoints - 2); xKnotFinal];
+yKnotSol = param(nKnotPoints - 1 : end);
+slopes = diff(yKnotSol) ./ diff(xKnotSol);
+intercepts = yKnotSol(2 : end) - slopes .* xKnotSol(2 : end);
+
+fprintf('xKnotSol:\n');
+disp(xKnotSol);
+
+fprintf('yKnotSol:\n');
+disp(yKnotSol);
+
+fprintf('slopes:\n')
+disp(slopes);
+
+fprintf('intercepts:\n')
+disp(intercepts);
+
+hold on;
+plot(data.motorHallEncoder0, data.proximalJointAngle0, 'b');
+plot(data.motorHallEncoder1, data.proximalJointAngle1, 'b');
+plot(data.motorHallEncoder0, piecewiseLinear(param, data.motorHallEncoder0), 'r');
+plot(data.motorHallEncoder1, piecewiseLinear(param, data.motorHallEncoder1), 'r');
+
+xlabel('motor tendon excursion')
+ylabel('proximal joint angle')
+% for i = 1 : length(slopes)
+%     slope = slopes(i);
+%     intercept = intercepts(i);
+%     plot(data.motorHallEncoder0, slope * data.motorHallEncoder0 + intercept, 'k');
+% end
+hold off;
+
   function y = piecewiseLinear(param, xDatai)
     xKnot = [xKnot1; param(1 : nKnotPoints - 2); xKnotFinal];
     yKnot = param(nKnotPoints - 1 : end);    
     y = interp1(xKnot, yKnot, xDatai, 'linear');
   end
-
-param = lsqcurvefit(@piecewiseLinear, param0, xData, yData, lb, ub);
-
-plot(xData, yData, 'b', xData, piecewiseLinear(param, xData), 'r');
-
-xKnotSol = [xKnot1; param(1 : nKnotPoints - 2); xKnotFinal];
-yKnotSol = param(nKnotPoints - 1 : end);
-
-disp('xKnotSol:');
-disp(num2str(xKnotSol));
-
-disp('yKnotSol:');
-disp(num2str(yKnotSol));
-
 end
 
 function [x0, xKnot1, xKnotFinal] = getParam0(xData, yData, nKnotPoints)
