5993310137963a9dc2c7646fcd7b3e6db3d1e665
Robin Deits <rdeits@csail.mit.edu>
2013-11-18 16:57:32 +0000

    Fix crash when using step sequence goals in line search mode
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7408 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/systems/@Biped/createInitialSteps.m
+++ software/control/matlab/systems/@Biped/createInitialSteps.m
@@ -4,6 +4,10 @@ if ~isfield(params, 'check_feasibility'); params.check_feasibility = false; end
 if ~isfield(params, 'allow_optimization'); params.allow_optimization = false; end
 if ~isstruct(goal_pos)
   goal_pos = struct('center', goal_pos);
+elseif ~isfield(goal_pos, 'center')
+  goal_pos.center = zeros(6,1);
+  goal_pos.center(1:3) = mean([goal_pos.right(1:3), goal_pos.left(1:3)], 2);
+  goal_pos.center(4:6) = goal_pos.right(4:6) + 0.5 * angleDiff(goal_pos.right(4:6), goal_pos.left(4:6));
 end
 
 if params.allow_optimization
