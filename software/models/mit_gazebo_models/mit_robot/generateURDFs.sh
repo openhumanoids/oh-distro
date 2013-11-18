75dfb928778194a5b0e2d079f619f911fda8d8ca
Maurice Fallon <mfallon@csail.mit.edu>
2013-11-18 19:13:32 +0000

    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7418 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -3,25 +3,29 @@
 echo "Generate Robot Model Configurations:"
 # 9 real robot configurations:
 echo "1/11 LI RI"
-rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
+#rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
 echo "2/11 LI RN"
-rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
+#rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
 echo "3/11 LI RS"
-rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
+#rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
 
 echo "4/11 LN RI"
-rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
+#rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
 echo "5/11 LN RN"
-rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
+#rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
 echo "6/11 LN RS"
-rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
+#rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
 
 echo "7/11 LS RI"
-rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
+#rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
 echo "8/11 LS RN"
-rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
+#rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
 echo "9/11 LS RS"
-rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
+#rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
+
+echo "10/11 LH RI"
+rosrun xacro xacro.py xacro/atlas_LH_RI.urdf.xacro > model_LH_RI.urdf
+
 
 # the simulated robot configuration - LCM facing and ROS/gazbo facing versions
 echo "10/11 sim - MIT Facing"
