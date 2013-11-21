70e8bb14fe359c814f9956c458c94736b602c96f
Scott Kuindersma <kuindersma@gmail.com>
2013-11-21 12:53:36 +0000

    takes controller type as arg
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7548 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/runAtlasStateMachine.m
+++ software/control/matlab/runAtlasStateMachine.m
@@ -1,10 +1,14 @@
-function runAtlasStateMachine()
+function runAtlasStateMachine(controller_type)
+
+if nargin < 1
+  controller_type = 1; % 1: PID, 2: PID+manip params, 3: PD+gravity comp, 4: inverse dynamics
+end
 
 options.floating = true;
 % options.floating = false;
 r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
 
-options.controller_type = 2; % 1: PID, 2: PID+manip params, 3: PD+gravity comp, 4: inverse dynamics
+options.controller_type = controller_type;
 init_controller = SilentInitController('init',r);
 manip_controller = AtlasManipController('manip',r,options);
 state_machine = DRCStateMachine(struct(manip_controller.name,manip_controller,...
