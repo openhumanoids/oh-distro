66aee9c12594cbd35f926b6489a49e240302f6f6
Scott Kuindersma <kuindersma@gmail.com>
2013-11-26 20:24:31 +0000

    option for turning off bdi manip control (for maurice)
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7713 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/controllers/BDIManipCommandBlock.m
+++ software/control/matlab/controllers/BDIManipCommandBlock.m
@@ -7,6 +7,7 @@ classdef BDIManipCommandBlock < MIMODrakeSystem
     robot;
     params_pub;
     params_listener;
+    plan_adjust_listener;
     controller_data;
   end
   
@@ -17,7 +18,13 @@ classdef BDIManipCommandBlock < MIMODrakeSystem
   
       ctrl_data = getData(controller_data);
       if ~isfield(ctrl_data,'qtraj')
-        error('QTrajEvalBlock: controller_data must contain qtraj field');
+        error('BDIManipCommandBlock: controller_data must contain qtraj field');
+      end
+      
+      if ~isfield(ctrl_data,'enable_bdi_manip')
+        error('BDIManipCommandBlock: controller_data must contain enable_bdi_manip field');
+      else
+        typecheck(ctrl_data.enable_bdi_manip,'logical');
       end
       
       if nargin<3
@@ -46,14 +53,24 @@ classdef BDIManipCommandBlock < MIMODrakeSystem
       obj.controller_data = controller_data;
       obj.params_pub = AtlasManipParamsPublisher('ATLAS_MANIPULATE_PARAMS');
       obj.params_listener = AtlasManipParamsListener('ATLAS_STATUS');
+      obj.plan_adjust_listener = PlanAdjustModeListener('PLAN_USING_BDI_HEIGHT');
     end
    
     function y=mimoOutput(obj,t,~,varargin)
       q_des=varargin{1};
       x=varargin{2};
       qtraj = obj.controller_data.data.qtraj;
+
+      plan_adjust = obj.plan_adjust_listener.getMessage();
+      if ~isempty(plan_adjust)
+        if plan_adjust.mode == 1
+          obj.controller_data.setField('enable_bdi_manip',true);
+        else
+          obj.controller_data.setField('enable_bdi_manip',false);
+        end
+      end
       
-      if isa(qtraj,'PPTrajectory') && t<=qtraj.tspan(end)
+      if obj.controller_data.data.enable_bdi_manip && isa(qtraj,'PPTrajectory') && t<=qtraj.tspan(end)
         cur_params = obj.params_listener.getMessage();
         
         % only support pelvis height for the time being
