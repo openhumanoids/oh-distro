b325f6585f580d920509a8dfff135c1451e437a0
Scott Kuindersma <kuindersma@gmail.com>
2013-11-26 22:10:29 +0000

    plan adjust mode listener uses MessageMonitor. added getMessage() method
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7725 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/util/PlanAdjustModeListener.m
+++ software/control/matlab/util/PlanAdjustModeListener.m
@@ -1,33 +1,47 @@
 classdef PlanAdjustModeListener
-    properties
-        lc
-        aggregator
+  properties
+    lc;
+		monitor;
+  end
+    
+  methods
+    function obj = PlanAdjustModeListener(channel)
+      obj.lc = lcm.lcm.LCM.getSingleton();
+ 		  obj.monitor = drake.util.MessageMonitor(drc.plan_adjust_mode_t(),'utime');
+      obj.lc.subscribe(channel,obj.monitor);
+    end
+        
+    function [x,t] = getNextMessage(obj, t_ms)
+			data = obj.monitor.getNextMessage(t_ms);
+			if isempty(data)
+				x = [];
+        t = -1;
+      else
+        t = obj.monitor.getLastTimestamp();
+        msg = drc.plan_adjust_mode_t(data);
+        x = PlanAdjustModeListener.decode(msg);
+			end
     end
     
-    methods
-        function obj = PlanAdjustModeListener(channel)
-            obj.lc = lcm.lcm.LCM.getSingleton();
-            obj.aggregator = lcm.lcm.MessageAggregator();
-            obj.lc.subscribe(channel, obj.aggregator);
-        end
-        
-        function [X] = getNextMessage(obj, t_ms)
-            modeset_msg = obj.aggregator.getNextMessage(t_ms);
-            if isempty(modeset_msg)
-                X = [];
-            else
-                [X] = PlanAdjustModeListener.decode(drc.plan_adjust_mode_t(modeset_msg.data));
-            end
-        end
-        
-    end
+    function [x,t] = getMessage(obj)
+			data = obj.monitor.getMessage();
+			if isempty(data)
+				x = [];
+        t = -1;
+      else
+        t = obj.monitor.getLastTimestamp();
+        msg = drc.plan_adjust_mode_t(data);
+        x = PlanAdjustModeListener.decode(msg);
+			end
+		end
+    
+  end
 
-    methods(Static)
-        function X = decode(msg)
-                X.time = double(msg.utime)/1000000; 
-                X.mode =msg.mode;
-         end
-            
+  methods(Static)
+    function x = decode(msg)
+      x.time = double(msg.utime)/1000000; 
+      x.mode =msg.mode;
     end
+  end
 
 end
