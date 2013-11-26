6b3bd8cdbc87aebb3fe9defb3014e0fb9306067c
Michael Posa <mposa@csail.mit.edu>
2013-11-26 19:17:37 +0000

    Added LH_RR urdf, for rotated irobot hand
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7706 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -2,37 +2,39 @@
 
 echo "Generate Robot Model Configurations:"
 # 9 real robot configurations:
-echo "1/14 LI RI"
+echo "1/15 LI RI"
 #rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
-echo "2/14 LI RN"
+echo "2/15 LI RN"
 #rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
-echo "3/14 LI RS"
+echo "3/15 LI RS"
 #rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
 
-echo "4/14 LN RI"
+echo "4/15 LN RI"
 #rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
-echo "5/14 LN RN"
+echo "5/15 LN RN"
 #rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
-echo "6/14 LN RS"
+echo "6/15 LN RS"
 #rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
-echo "7/14 LN RR"
+echo "7/15 LN RR"
 rosrun xacro xacro.py xacro/atlas_LN_RR.urdf.xacro > model_LN_RR.urdf
-echo "8/14 LR RN"
+echo "8/15 LR RN"
 rosrun xacro xacro.py xacro/atlas_LR_RN.urdf.xacro > model_LR_RN.urdf
 
-echo "9/14 LS RI"
+echo "9/15 LS RI"
 #rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
-echo "10/14 LS RN"
+echo "10/15 LS RN"
 #rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
-echo "11/14 LS RS"
+echo "11/15 LS RS"
 #rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
 
-echo "12/14 LH RI"
+echo "12/15 LH RI"
 rosrun xacro xacro.py xacro/atlas_LH_RI.urdf.xacro > model_LH_RI.urdf
 
+echo "13/15 LH RR"
+rosrun xacro xacro.py xacro/atlas_LH_RR.urdf.xacro > model_LH_RR.urdf
 
 # the simulated robot configuration - LCM facing and ROS/gazbo facing versions
-echo "13/14 sim - MIT Facing"
+echo "14/15 sim - MIT Facing"
 rosrun xacro xacro.py xacro/atlas_sandia_hands_sim.urdf.xacro > model_sim.urdf
-echo "14/14 sim - Gazebo Facing"
+echo "15/15 sim - Gazebo Facing"
 ./renameSimGazeboURDF.sh
