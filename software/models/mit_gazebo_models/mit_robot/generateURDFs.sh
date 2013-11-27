681e66b162045c3775440e143b095a212a885730
Maurice Fallon <mfallon@csail.mit.edu>
2013-11-27 23:06:33 +0000

    adding new config for robotiq and hooks
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7773 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -2,42 +2,36 @@
 
 echo "Generate Robot Model Configurations:"
 # 9 real robot configurations:
-echo "1/18 LI RI"
-#rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
-echo "2/18 LI RN"
-#rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
-echo "3/18 LI RS"
-#rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
+echo "[I]Robot, [N]one, [S]andia, [H]ook, [P]ointer, [R]obotiq"
 
-echo "4/18 LN RI"
-#rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
-echo "5/18 LN RN"
-#rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
-echo "6/18 LN RS"
-#rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
-echo "7/18 LN RR"
+echo "IRobot left - 3"
+rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
+rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
+rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
+
+echo "None left - 5"
+rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
+rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
+rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
 rosrun xacro xacro.py xacro/atlas_LN_RR.urdf.xacro > model_LN_RR.urdf
-echo "8/18 LR RN"
-rosrun xacro xacro.py xacro/atlas_LR_RN.urdf.xacro > model_LR_RN.urdf
-
-echo "9/18 LS RI"
-#rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
-echo "10/18 LS RN"
-#rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
-echo "11/18 LS RS"
-#rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
-
-echo "12/18 LH RI"
-rosrun xacro xacro.py xacro/atlas_LH_RI.urdf.xacro > model_LH_RI.urdf
-echo "13/18 LH RN"
-rosrun xacro xacro.py xacro/atlas_LH_RN.urdf.xacro > model_LH_RN.urdf
-echo "14/18 LN RH"
 rosrun xacro xacro.py xacro/atlas_LN_RH.urdf.xacro > model_LN_RH.urdf
-echo "15/18 LH RH"
-rosrun xacro xacro.py xacro/atlas_LH_RH.urdf.xacro > model_LH_RH.urdf
 
-echo "16/18 LH RR"
+echo "Sandia left - 3"
+rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
+rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
+rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
+
+echo "Hook left - 4"
+rosrun xacro xacro.py xacro/atlas_LH_RI.urdf.xacro > model_LH_RI.urdf
+rosrun xacro xacro.py xacro/atlas_LH_RN.urdf.xacro > model_LH_RN.urdf
+rosrun xacro xacro.py xacro/atlas_LH_RH.urdf.xacro > model_LH_RH.urdf
 rosrun xacro xacro.py xacro/atlas_LH_RR.urdf.xacro > model_LH_RR.urdf
+
+echo "Pointer left - 0"
+
+
+echo "Robotiq left - 4"
+rosrun xacro xacro.py xacro/atlas_LR_RN.urdf.xacro > model_LR_RN.urdf
 
 
 # the simulated robot configuration - LCM facing and ROS/gazbo facing versions
