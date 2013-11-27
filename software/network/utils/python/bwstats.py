25ff955b27dbf91715759c65d40b50b8958fbd50
Mathew Antone <antone@csail.mit.edu>
2013-11-27 21:08:40 +0000

    now reads from shaper csv files instead of logs, and can batch process a bunch of csvs in a directory; automatic handling of robot vs base via filename
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7767 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/network/utils/python/bwstats.py
+++ software/network/utils/python/bwstats.py
@@ -1,5 +1,6 @@
 import numpy
 
+# deprecated
 def readAllBandwidthMessages(fileName,channel):
   import lcm
   from drc import bandwidth_stats_t
@@ -13,11 +14,12 @@ def readAllBandwidthMessages(fileName,channel):
   print 'done reading log'
   return msgs
 
+# deprecated
 def createStats(msgs):
-  data = { 'utime' : [] }
+  data = { 'UTIME' : [] }
   for msg in msgs:
     zipped = zip(msg.sent_channels,msg.sent_bytes)
-    data['utime'].append(msg.utime)
+    data['UTIME'].append(msg.utime)
     for pair in zipped:
       if pair[0] not in data:
         data[pair[0]] = [pair[1]]
@@ -26,6 +28,42 @@ def createStats(msgs):
   for chan,dat in data.iteritems():
     data[chan] = numpy.array(dat)
   return data
+
+def readBandwidthCsv(fileName):
+  import csv
+  with open(fileName, 'rb') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = {}
+    for row in reader:
+
+      # check if row is corrupt
+      bad = False
+      for key,val in row.iteritems():
+        if type(val) is list:
+          bad = True
+          break
+      if bad:
+        continue
+        
+      for key,val in row.iteritems():
+        val = int(val)
+        if key not in data:
+          data[key] = [val]
+        else:
+          data[key].append(val)
+  for chan,dat in data.iteritems():
+    data[chan] = numpy.array(dat)
+
+  data0 = {'UTIME':data['UTIME']}
+  data1 = {'UTIME':data['UTIME']}
+  for chan,dat in data.iteritems():
+    if (chan[-1] == '0'):
+      data0[chan[0:-1]] = dat
+    elif (chan[-1] == '1'):
+      data1[chan[0:-1]] = dat
+
+  return data0,data1
+    
 
 def getBandwidthString(numBytes):
   if int(numBytes/1024)==0:
@@ -38,11 +76,11 @@ def getBandwidthString(numBytes):
     return (numBytes/1024.0**3, 'G')
 
 def printStats(data):
-  times = data['utime']
+  times = data['UTIME']
   dt = float(times[-1] - times[0])/1e6
   allBytes = 0
   for chan,dat in data.iteritems():
-    if (chan == 'utime'):
+    if (chan == 'UTIME'):
       continue
     totalBytes = dat[-1] - dat[0]
     totalVal,totalSuffix = getBandwidthString(totalBytes)
@@ -54,10 +92,10 @@ def printStats(data):
 
 def doPlots(data,title):
   import matplotlib.pyplot as plt
-  times = data['utime']
+  times = data['UTIME']
   times = (times-times[0])/1e6
   dt = numpy.diff(times)
-  channels = [chan for chan in data.keys() if chan != 'utime']
+  channels = [chan for chan in data.keys() if chan != 'UTIME']
   legendChannels = []
   fig = plt.figure()
   plt.hold(True)
@@ -69,7 +107,7 @@ def doPlots(data,title):
       legendChannels.append(chan)
       plt.plot(times[1:],ddat/1024.0,linewidth=3)
     totals += ddat/1024.0
-  plt.plot(times[1:],totals,linewidth=3)
+  plt.plot(times[1:],totals,linewidth=3,linestyle='dotted')
   legendChannels.append('TOTAL')
   plt.grid('on')
   plt.xlabel('time (s)')
@@ -78,19 +116,31 @@ def doPlots(data,title):
   plt.title(title + ' message bandwidths')
   return fig
   
-def go(fileNamePrefix):
+def go(csvFile):
   import matplotlib.pyplot as plt
-  fileNameRobot = fileNamePrefix + '-robot'
-  fileNameBase = fileNamePrefix + '-base'
-  msgs = readAllBandwidthMessages(fileNameRobot, 'ROBOT_BW_STATS')
-  dataRobot = createStats(msgs)
-  msgs = readAllBandwidthMessages(fileNameBase, 'BASE_BW_STATS')
-  dataBase = createStats(msgs)
-  #printStats(data)
-  fig = doPlots(dataRobot,'robot->base (downlink)')
+  import os.path
+  (dataIn,dataOut) = readBandwidthCsv(csvFile)
+  onRobot = 'usage-robot' in csvFile
+  if onRobot:
+    label = 'robot'
+    (dataIn,dataOut) = (dataOut,dataIn)
+  else:
+    label = 'base'
+  fileBase = os.path.splitext(csvFile)[0]
+  fig = doPlots(dataIn, label+': received')
   fig.set_size_inches(10,8)
-  plt.savefig(fileNameRobot + '.png', bbox_inches='tight', dpi=100)
-  fig = doPlots(dataBase, 'base->robot (uplink)')
+  plt.savefig(fileBase+'_'+label+'_rx.png', bbox_inches='tight', dpi=100)
+  plt.close(fig)
+  fig = doPlots(dataOut, label+': transmitted')
   fig.set_size_inches(10,8)
-  plt.savefig(fileNameBase + '.png', bbox_inches='tight', dpi=100)
-  return (dataRobot, dataBase)
+  plt.savefig(fileBase+'_'+label+'_tx.png', bbox_inches='tight', dpi=100)
+  plt.close(fig)
+  return (dataIn, dataOut)
+
+def processDir(dirName):
+  import glob
+  import time
+  for d in glob.glob(dirName + '/*.csv'):
+    print('processing %s...' % (d))
+    go(d)
+  
