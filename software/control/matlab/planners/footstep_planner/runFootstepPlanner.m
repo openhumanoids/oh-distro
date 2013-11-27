9488607ff16d5b7e4b30e22b91c2fed0730e9477
Sisir Karumanchi <sisir@mit.edu>
2013-11-27 01:30:11 +0000

    reverting my wrongly committed local patch
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7739 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/footstep_planner/runFootstepPlanner.m
+++ software/control/matlab/planners/footstep_planner/runFootstepPlanner.m
@@ -1,12 +1,23 @@
 function runFootstepPlanner
 %NOTEST
 
+%function runFootstepPlanner(varargin)
+% enable_terrainmaps= 0;% hardware mode
+%switch nargin
+%    case 1
+%        enable_terrainmaps= varargin{1}; 
+%    case 0
+%        enable_terrainmaps= 0;% hardware mode    
+%    otherwise
+%        error('ndefined number of varargin. (disable_terrainmaps)')
+%end
+
 while 1
   options.floating = true;
   options.dt = 0.001;
   r = Atlas(strcat(getenv('DRC_PATH'),'/models/mit_gazebo_models/mit_robot_drake/model_minimal_contact_point_hands.urdf'),options);
   r = removeCollisionGroupsExcept(r,{'heel','toe'});
-%   r = setTerrain(r,DRCTerrainMap(false,struct('name','Foot Plan','status_code',6,'fill', true,'normal_radius',2,'normal_method','ransac','auto_request',true)));
+  r = setTerrain(r,DRCTerrainMap(false,struct('name','Foot Plan','status_code',6,'fill', true,'normal_radius',2,'normal_method','ransac','auto_request',true)));
   r = compile(r);
 
   p = FootstepPlanner(r);
