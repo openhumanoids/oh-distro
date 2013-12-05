2b3f12cb03721e795d34c0bbe519d78a0b976409
Robin Deits <rdeits@csail.mit.edu>
2013-12-05 15:27:00 +0000

    Yet another URDF (left robotiq right irobot)
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@7932 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -31,9 +31,10 @@ rosrun xacro xacro.py xacro/atlas_LH_RQ.urdf.xacro > model_LH_RQ.urdf
 echo "Pointer left - 0"
 
 
-echo "Robotiq left - 2"
+echo "Robotiq left - 3"
 rosrun xacro xacro.py xacro/atlas_LR_RN.urdf.xacro > model_LR_RN.urdf
 rosrun xacro xacro.py xacro/atlas_LQ_RH.urdf.xacro > model_LQ_RH.urdf
+rosrun xacro xacro.py xacro/atlas_LQ_RI.urdf.xacro > model_LQ_RI.urdf
 
 # the simulated robot configuration - LCM facing and ROS/gazbo facing versions
 echo "17/18 sim - MIT Facing"
