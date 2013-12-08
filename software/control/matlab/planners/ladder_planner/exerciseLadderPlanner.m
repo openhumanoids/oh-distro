4f0f6d3ade592be42394948b3e960ce6ec12f8c4
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-12-08 16:16:22 +0000

    Adds utility to make running the ladder planner from saved lcm messages easier
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@8042 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/matlab/planners/ladder_planner/exerciseLadderPlanner.m
@@ -0,0 +1,6 @@
+function exerciseLadderPlanner(x0_msg,footstep_msg)
+  lc = lcm.lcm.LCM.getSingleton();
+  lc.publish('EST_ROBOT_STATE',drc.robot_state_t(x0_msg));
+  pause(1);
+  lc.publish('APPROVED_FOOTSTEP_PLAN',drc.footstep_plan_t(footstep_msg));
+end
