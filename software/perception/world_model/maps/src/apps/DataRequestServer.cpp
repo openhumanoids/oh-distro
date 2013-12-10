823df438247370483abef78d26203c1505388d08
Toby Schneider <tes@mit.edu>
2013-12-10 01:16:33 +0000

    changed default height map bounds to reduce message size and latency
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@8129 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/perception/world_model/maps/src/apps/DataRequestServer.cpp
+++ software/perception/world_model/maps/src/apps/DataRequestServer.cpp
@@ -201,8 +201,8 @@ struct Worker {
   }
 
   void sendHeightMapSceneRequest() {
-    const Eigen::Vector3f minPt(-2, -5, -3);
-    const Eigen::Vector3f maxPt(5, 5, 0.3);
+    const Eigen::Vector3f minPt(-0.5, -2, -3);
+    const Eigen::Vector3f maxPt(5, 2, 0.3);
     drc::map_request_t msg =
       prepareHeightRequestMessage(minPt, maxPt, 0.03, 0.03);
     Eigen::Isometry3f pelvisPose;
