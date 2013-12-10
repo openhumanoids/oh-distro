29c3f02ae106218126fdc111b08ccbc14e4db1e0
Maurice Fallon <mfallon@csail.mit.edu>
2013-12-10 18:50:20 +0000

    git-svn-id: https://svn.csail.mit.edu/drc/trunk@8146 c7283977-0100-402a-a91a-fa70b306dbfe

--- software/control/matlab/planners/atlas_keyframe_planners/KeyframePlanner.m
+++ software/control/matlab/planners/atlas_keyframe_planners/KeyframePlanner.m
@@ -121,7 +121,7 @@ classdef KeyframePlanner < handle
 
             % Hands with 6 inch extenders - 6 Dec 2013, mfallon:
             % mod1: rotates irobots by 180 due to calbe length
-            extenders =0;
+            extenders =1;
             if (extenders ==1)
               send_status(2,0,0,'Planner is assuming Arm extenders. Careful!');
               obj.T_hand_palm_l_irobot = HT([0;0.26756;0.015],1.57079, 0, -3.14159);
