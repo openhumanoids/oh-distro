cdb4603d24bc64d68d510604242919cc30cb7aa0
Andres Valenzuela <avalenzu@csail.mit.edu>
2013-12-11 15:11:49 +0000

    Adds python scripts to request ladder reach-to-next-step plans from the relative_ee_traj_planner
    git-svn-id: https://svn.csail.mit.edu/drc/trunk@8173 c7283977-0100-402a-a91a-fa70b306dbfe

---
+++ software/control/ladderLeftArmReach.py
@@ -0,0 +1,11 @@
+import lcm, os, sys
+sys.path.append(os.path.expanduser('~/drc/software/build/lib/python2.7/dist-packages'))
+import drc
+
+lc = lcm.LCM()
+
+msg = drc.atlas_behavior_command_t()
+msg.utime = 0
+msg.command = "ladder_reach_up:LEFT"
+lc.publish("LADDER_REACH", msg.encode())
+
