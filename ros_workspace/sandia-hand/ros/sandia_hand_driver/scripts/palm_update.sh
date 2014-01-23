1f5d1cadb4f8abb685b20452b24de6199f014ec2
Scott Kuindersma <kuindersma@gmail.com>
2014-01-23 15:56:46 -0500

    removing sandia-hand copy in repo. commented out camera streaming message in hand translator to compile against latest sandia-hand package

--- ros_workspace/sandia-hand/ros/sandia_hand_driver/scripts/palm_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#cd `rospack find sandia_hand_driver` && bin/sandia_hand_cli palmburn ../../firmware/build/palm/minimal/palm-minimal.bin
-#cd `rospack find sandia_hand_driver` && bin/sandia_hand_cli palmburn ../../firmware/build/palm/std/palm-std.bin
-cd `rospack find sandia_hand_driver` && bin/sandia_hand_cli $1 palmburn ../../firmware/build/palm/std/palm-std.bin
--- ros_workspace/sandia-hand/ros/sandia_hand_driver/scripts/view_stereo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosrun image_view stereo_view stereo:=/ image:=image_raw
