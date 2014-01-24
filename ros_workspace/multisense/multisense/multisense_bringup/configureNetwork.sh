11028185d615bbc1b01bf42675b9399c37fd7851
Scott Kuindersma <kuindersma@gmail.com>
2014-01-24 11:44:02 -0500

    removed ROS multisense driver (we use our own LCM driver)

--- ros_workspace/multisense/multisense/multisense_bringup/configureNetwork.sh
+++
@@ -1,6 +0,0 @@
-ifconfig eth0 10.66.171.20
-
-echo 16777215 > /proc/sys/net/core/rmem_max
-echo 16777215 > /proc/sys/net/core/wmem_max
-ifconfig eth0 mtu 7200
-
--- ros_workspace/multisense/multisense/multisense_dashboard/src/multisense_dashboard/__init__.py
+++
@@ -1,1 +0,0 @@
-from multisense_frame import *
