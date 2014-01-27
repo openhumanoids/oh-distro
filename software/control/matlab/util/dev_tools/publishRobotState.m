bd996cb83429e0ac9f3cd636fa2ea131af1ac079
Scott Kuindersma <kuindersma@gmail.com>
2014-01-27 12:27:12 -0500

    removing old dev tools

--- software/control/matlab/util/dev_tools/publishRobotState.m
+++
@@ -1,9 +0,0 @@
-function publishRobotState(x, channel)
-  r = RigidBodyManipulator('');
-  r = r.addRobotFromURDF('../../../models/mit_gazebo_models/mit_robot_drake/model_minimal_contact.urdf', [],[],struct('floating',true));
-  joint_names = r.getStateFrame.coordinates(1:getNumDOF(r));
-  robot_state_coder = LCMCoordinateFrameWCoder('AtlasState',r.getNumStates(), ...
-    'x',JLCMCoder(drc.control.RobotStateCoder(joint_names)));
-
-  publish(robot_state_coder,0,x,channel);
-end
