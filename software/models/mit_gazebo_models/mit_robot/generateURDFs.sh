8ea2394e89d7ba6de185eda97d7943a9cf21a452
Maurice Fallon <mfallon@csail.mit.edu>
2014-01-28 18:13:54 -0500

    a few changes to prepare for the amazon demo. moved the irobot sticky hands on the dewalt drill. added back in an irobot left and right combo into urdf and procma

--- software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
+++ software/models/mit_gazebo_models/mit_robot/generateURDFs.sh
@@ -26,33 +26,33 @@ rosrun xacro xacro.py xacro/atlas_LQ_RP.urdf.xacro > model_LQ_RP.urdf
 # was LQ_RP
 
 
-#echo "IRobot left - 3"
-#rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
+echo "IRobot left combinations - 1"
+rosrun xacro xacro.py xacro/atlas_LI_RI.urdf.xacro > model_LI_RI.urdf
 #rosrun xacro xacro.py xacro/atlas_LI_RN.urdf.xacro > model_LI_RN.urdf
 #rosrun xacro xacro.py xacro/atlas_LI_RS.urdf.xacro > model_LI_RS.urdf
 
-#echo "None left - 5"
+#echo "None left combinations - 5"
 #rosrun xacro xacro.py xacro/atlas_LN_RI.urdf.xacro > model_LN_RI.urdf
 #rosrun xacro xacro.py xacro/atlas_LN_RN.urdf.xacro > model_LN_RN.urdf
 #rosrun xacro xacro.py xacro/atlas_LN_RS.urdf.xacro > model_LN_RS.urdf
 #rosrun xacro xacro.py xacro/atlas_LN_RR.urdf.xacro > model_LN_RR.urdf
 #rosrun xacro xacro.py xacro/atlas_LN_RH.urdf.xacro > model_LN_RH.urdf
 
-#echo "Sandia left - 3"
+#echo "Sandia left combinations - 3"
 #rosrun xacro xacro.py xacro/atlas_LS_RI.urdf.xacro > model_LS_RI.urdf
 #rosrun xacro xacro.py xacro/atlas_LS_RN.urdf.xacro > model_LS_RN.urdf
 #rosrun xacro xacro.py xacro/atlas_LS_RS.urdf.xacro > model_LS_RS.urdf
 
-#echo "Hook left - 5"
+#echo "Hook left combinations - 5"
 #rosrun xacro xacro.py xacro/atlas_LH_RI.urdf.xacro > model_LH_RI.urdf
 #rosrun xacro xacro.py xacro/atlas_LH_RN.urdf.xacro > model_LH_RN.urdf
 #rosrun xacro xacro.py xacro/atlas_LH_RH.urdf.xacro > model_LH_RH.urdf
 #rosrun xacro xacro.py xacro/atlas_LH_RR.urdf.xacro > model_LH_RR.urdf
 #rosrun xacro xacro.py xacro/atlas_LH_RQ.urdf.xacro > model_LH_RQ.urdf
 
-#echo "Pointer left - 0"
+#echo "Pointer left combinations - 0"
 
-#echo "Robotiq left - 3"
+#echo "Robotiq left combinations - 3"
 #rosrun xacro xacro.py xacro/atlas_LR_RN.urdf.xacro > model_LR_RN.urdf
 #rosrun xacro xacro.py xacro/atlas_LQ_RH.urdf.xacro > model_LQ_RH.urdf
 #rosrun xacro xacro.py xacro/atlas_LQ_RI.urdf.xacro > model_LQ_RI.urdf
