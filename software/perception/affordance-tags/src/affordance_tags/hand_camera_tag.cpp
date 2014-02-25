9023701d0fb3722635b06e06d949ef8e15ba801c
Maurice Fallon <mfallon@mit.edu>
2014-02-24 19:29:51 -0500

    adding angualr poly configuration to camera rhand
    corrected rbotiq off line
    corrected camera rotation in robotiq
    clean up tool for gilwoo and peter

--- software/perception/affordance-tags/src/affordance_tags/hand_camera_tag.cpp
+++ software/perception/affordance-tags/src/affordance_tags/hand_camera_tag.cpp
@@ -40,18 +40,6 @@ using namespace Eigen;
 using namespace affordance;
 using namespace boost::assign; // bring 'operator+()' into scope
 
-// Details about a pairing between and tag and an affordance:
-class AffTag { 
-public:
-  AffTag(std::string name, double tag_size, bool active, AffPlusPtr aff_plus, KDL::Frame tag_to_aff ):
-    name_ (name), tag_size_ (tag_size), active_(active), aff_plus_(aff_plus), tag_to_aff_(tag_to_aff){};
-  std::string name_; // name of the object 
-  double tag_size_; // size of the tag
-  bool active_; // has this object been seen yet
-  AffPlusPtr aff_plus_; // details about the affordance
-  KDL::Frame tag_to_aff_; // transform from the tag to the affordance
-};
-
 class Tags{
   public:
     Tags(boost::shared_ptr<lcm::LCM> &lcm_, bool verbose_);
@@ -66,11 +54,7 @@ class Tags{
     double fx_, fy_, cx_, cy_;
     
     void imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg);   
-    void multisenseHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  multisense::images_t* msg);   
-
-    void getTagList();
-    void processTag();//const  bot_core::image_t* msg);
-    void updateAffordance(AffTag &tag, Eigen::Isometry3d tag_pose, int uid);
+    void processTag(int64_t utime_in);
 
     BotParam* botparam_;
     bot::frames* botframes_cpp_;
@@ -81,7 +65,6 @@ class Tags{
     uint8_t* img_buf_;
     
     AprilTags::TagDetector* tag_detector_;
-    std::map<int,AffTag> tags_; // map of tag details    
 };
 
 Tags::Tags(boost::shared_ptr<lcm::LCM> &lcm_, bool verbose_):
@@ -110,59 +93,15 @@ Tags::Tags(boost::shared_ptr<lcm::LCM> &lcm_, bool verbose_):
   pc_vis_ = new pointcloud_vis( lcm_->getUnderlyingLCM() );
   // obj: id name type reset
   // pts: id name type reset objcoll usergb rgb
-  pc_vis_->obj_cfg_list.push_back( obj_cfg(60000,"Pose - Laser",5,1) );
+  pc_vis_->obj_cfg_list.push_back( obj_cfg(60000,"Tag Detections",5,1) );
+  pc_vis_->obj_cfg_list.push_back( obj_cfg(60001,"Camera Pose",5,1) );
+  pc_vis_->obj_cfg_list.push_back( obj_cfg(60002,"Hand Face Pose",5,1) );
    
   tag_detector_ = new AprilTags::TagDetector (AprilTags::tagCodes16h5);
   img_buf_= (uint8_t*) malloc(3* width_  *  height_);
   imgutils_ = new image_io_utils( lcm_->getUnderlyingLCM(), width_, height_); // extra space for stereo tasks
-  
-  Tags::getTagList();
 }
     
-void Tags::getTagList(){
-
-  // TODO: read this information from a config file:
-  /// NB: uids count from 1  
-  /// cylinder constructor: length, radius, mass, uid, map id, position  
-  
-  // aff = 0 is reserved .... dont use it here
-  
-  AffPtr aff1(new AffordanceState());
-  aff1->setToCylinder( 0.01, 0.28, 2,0, KDL::Frame(KDL::Vector(0,0,1)), Eigen::Vector3f(0,1,0));  
-  AffPlusPtr aff_plus1(new AffordancePlusState());
-  aff_plus1->aff = aff1;   
-  KDL::Frame tag_to_aff1 = KDL::Frame(KDL::Rotation::RPY(0,0,0), KDL::Vector( 0, 0, 0));  
-  tags_.insert( make_pair( 1 , AffTag( "Table", 0.172244, false, aff_plus1 ,tag_to_aff1 ) ) ); 
-  
-  AffPtr aff2(new AffordanceState());
-  aff2->setToCylinder(0.122, 0.0325, 1,0, KDL::Frame(KDL::Vector(2.2,0,1)), Eigen::Vector3f(0,1,0));  
-  AffPlusPtr aff_plus2(new AffordancePlusState());
-  aff_plus2->aff = aff2;   
-  KDL::Frame tag_to_aff2 = KDL::Frame(KDL::Rotation::RPY(0,0,0), KDL::Vector( 0, 0, -0.061));  
-  tags_.insert( make_pair( 2 , AffTag( "CanB", 0.07778755, false, aff_plus2 , tag_to_aff2 ) ) ); 
-  
-  AffPtr aff3(new AffordanceState());
-  aff3->setToCylinder(0.122, 0.0325, 1,0, KDL::Frame(KDL::Vector(2,0,1)), Eigen::Vector3f(0,1,0));  
-  AffPlusPtr aff_plus3(new AffordancePlusState());
-  aff_plus3->aff = aff3;   
-  KDL::Frame tag_to_aff3 = KDL::Frame(KDL::Rotation::RPY(0,0,0), KDL::Vector( 0, 0, -0.061));  
-  tags_.insert( make_pair( 3 , AffTag( "Can", 0.07778755, false, aff_plus3 , tag_to_aff3 ) ) ); 
-  
-
-  std::map<int, AffTag >::iterator it;
-  for (int i=1; i<=3; i++){
-    it=tags_.find( i );
-    drc::affordance_plus_t msg;
-    it->second.aff_plus_->toMsg( &msg);
-    msg.aff.aff_store_control = drc::affordance_t::NEW;    
-    msg.aff.uid = i;
-    std::cout << msg.aff.uid << " fit sent\n";
-    lcm_->publish("AFFORDANCE_FIT",&msg);
-    it->second.active_ = true;
-  }
-  
-}
-
 // draw April tag detection on actual image
 // NB: because the conversion was skipped R and B will be switched...
 void draw_detection(cv::Mat& image, const AprilTags::TagDetection& detection) {
@@ -188,58 +127,29 @@ void draw_detection(cv::Mat& image, const AprilTags::TagDetection& detection) {
               cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0,0,255));
 }
 
+std::string printPose(Eigen::Isometry3d pose){
+  Eigen::Vector3d t(pose.translation());
+  Eigen::Quaterniond r(pose.rotation());
+  double rpy[3];
+  quat_to_euler(r, rpy[0], rpy[1], rpy[2]);
+  
+  std::stringstream ss;
+  ss <<t[0]<<", "<<t[1]<<", "<<t[2]<<", " 
+       <<r.w()<<", "<<r.x()<<", "<<r.y()<<", "<<r.z() << ", "
+       << rpy[0] <<", "<< rpy[1] <<", "<< rpy[2];
+  return ss.str();
+}
+
 
 void Tags::imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg){
   imgutils_->decodeImageToRGB((msg),  img_buf_ );
-  processTag();
+  processTag(msg->utime);
 }
 
-
-
-KDL::Frame makeKDLFrame( Eigen::Isometry3d input){
-  KDL::Frame out;
-  for (int i=0; i < 3; i++) {
-    for (int j=0; j < 3; j++)
-      out.M(i,j) = input.rotation()(i,j);
-  }
-  out.p[0] = input.translation().x();
-  out.p[1] = input.translation().y();
-  out.p[2] = input.translation().z();
-  return out;
-}
-
-
-// Apply offset to tag position and publish affordance position
-void Tags::updateAffordance(AffTag &tag, Eigen::Isometry3d tag_pose, int tag_uid){
-  KDL::Frame aff_pose = makeKDLFrame(tag_pose )* tag.tag_to_aff_;
-  tag.aff_plus_->aff->setFrame( aff_pose );
-  
-  if (!tag.active_){
-    // inicate the affordance:
-    // drc::affordance_plus_t msg;
-    // tag.aff_plus_->toMsg( &msg);
-    // msg.aff.aff_store_control = drc::affordance_t::NEW;    
-    // lcm_->publish("AFFORDANCE_FIT",&msg);
-    // tag.active_ = true;
-  }else{
-    drc::affordance_t msg;
-    tag.aff_plus_->aff->toMsg( &msg);
-    msg.uid = tag_uid;
-    msg.aff_store_control = drc::affordance_t::UPDATE;
-    lcm_->publish("AFFORDANCE_TRACK",&msg);
-  }
-}
-
-
-void Tags::processTag(){
-  int64_t utime_in =  0;//msg->utime;
-  
-  // TODO: support jpeg compression
+void Tags::processTag(int64_t utime_in){
   
   cv::Mat img = cv::Mat::zeros(height_, width_,CV_8UC3);
-  //memcpy(img.data,  msg->data.data() , width_*height_*3 );  
   img.data = img_buf_;
-  
   //cv::Mat img = cv::imdecode(cv::Mat(msg->images[0].data), -1);
   
   // I'm assuming incoming data has LCM RGB convention
@@ -250,30 +160,38 @@ void Tags::processTag(){
   
   if (verbose_) cout << detections.size() << " tags detected:" << endl;
   std::vector < Isometry3dTime > detected_posesT;
+  
   for (int i=0; i< detections.size(); i++) {
-    //cout << "  Id: " << detections[i].id << " -- " << "  Hamming distance: " << detections[i].hammingDistance << endl;
-
+    cout << "  Id: " << detections[i].id << " -- " << "  Hamming distance: " << detections[i].hammingDistance << endl;
     // also highlight in the image
     if (verbose_) draw_detection(img, detections[i]);
+   
+    // Get the relative transform, match with affordance and publish updated affordance:
+    Eigen::Matrix4d T = detections[i].getRelativeTransform( 0.053, fx_, fy_, cx_, cy_);  
+    Eigen::Isometry3d local_to_camera;
+    botframes_cpp_->get_trans_with_utime( camera_frame_ , "local", utime_in, local_to_camera);    
+    Eigen::Isometry3d tag_pose = local_to_camera*Eigen::Isometry3d(T)   ;
+    tag_pose.rotate( Eigen::Quaterniond(  euler_to_quat( 0 ,  (M_PI) ,  0 )  ) );
+
     
-    std::map<int, AffTag >::iterator it;
-    it=tags_.find( detections[i].id );
-    if(it!=tags_.end()) {
-      // Get the relative transform, match with affordance and publish updated affordance:
-      Eigen::Matrix4d T = detections[i].getRelativeTransform( it->second.tag_size_, fx_, fy_, cx_, cy_);  
-      Eigen::Isometry3d local_to_camera;
-      botframes_cpp_->get_trans_with_utime( camera_frame_ , "local", utime_in, local_to_camera);    
-      Eigen::Isometry3d tag_pose = local_to_camera*Eigen::Isometry3d(T);
-      updateAffordance( it->second , tag_pose, it->first );
+    if (verbose_){
+      Isometry3dTime cameraT = Isometry3dTime(utime_in, local_to_camera);
+      pc_vis_->pose_to_lcm_from_list(60001, cameraT);
       
-      if (verbose_){
-        Isometry3dTime poseT = Isometry3dTime(utime_in + detections[i].id, tag_pose);
-        detected_posesT.push_back( poseT);
-        std::cout << "detected: " << detections[i].id << "|" 
-          << it->first << " is tag"
-          << it->second.name_ << " is name\n";
-      }
-    }
+      Isometry3dTime poseT = Isometry3dTime(utime_in, tag_pose);
+      detected_posesT.push_back( poseT);
+      std::cout << "detected: " << detections[i].id << "\n";
+      
+      Eigen::Isometry3d local_to_rhand_face;
+      botframes_cpp_->get_trans_with_utime( "RHAND_FACE" , "local", utime_in, local_to_rhand_face);    
+      Isometry3dTime hand_faceT = Isometry3dTime(utime_in, local_to_rhand_face);
+      pc_vis_->pose_to_lcm_from_list(60002, hand_faceT);
+      
+      std::cout << printPose(local_to_camera) << " camera in world frame\n";
+      std::cout << printPose(tag_pose) << " tag in world frame\n";
+      std::cout << printPose(local_to_rhand_face) << " hand face in world frame\n";
+    }    
+    
   }
   
   if (verbose_){
