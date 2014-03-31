55732bf6e0b020ad915c82381fa9786a8583d92d
Robin Deits <robin.deits@gmail.com>
2014-03-31 16:53:06 -0400

    BDI translator now listens for a frame transform and applies it

--- software/bdi_walking/py_drake_utils/python/py_drake_utils/utils.py
+++ software/bdi_walking/py_drake_utils/python/py_drake_utils/utils.py
@@ -7,7 +7,7 @@ import lcm
 Pos = namedtuple('Pos', 'x y z roll pitch yaw')
 
 def rpy2rotmat(rpy):
-    rotMat = np.matrix([[np.cos(rpy[2])*np.cos(rpy[1]),
+    rotMat = np.array([[np.cos(rpy[2])*np.cos(rpy[1]),
                          np.cos(rpy[2])*np.sin(rpy[1])*np.sin(rpy[0])-np.sin(rpy[2])*np.cos(rpy[0]),
                          np.cos(rpy[2])*np.sin(rpy[1])*np.cos(rpy[0])+np.sin(rpy[2])*np.sin(rpy[0])],
                         [np.sin(rpy[2])*np.cos(rpy[1]),
@@ -21,10 +21,38 @@ def rpy2rotmat(rpy):
 def quat2rpy(q):
     q = q / np.linalg.norm(q)
     w, x, y, z = q
-    rpy = np.array([[np.arctan2(2*(w*x + y*z), w*w + z*z -(x*x +y*y))],
-                    [np.arcsin(2*(w*y - z*x))],
-                    [np.arctan2(2*(w*z + x*y), w*w + x*x-(y*y+z*z))]])
+    rpy = np.array([np.arctan2(2*(w*x + y*z), w*w + z*z -(x*x +y*y)),
+                    np.arcsin(2*(w*y - z*x)),
+                    np.arctan2(2*(w*z + x*y), w*w + x*x-(y*y+z*z))])
     return rpy
+
+def rotmat2rpy(R):
+    if R.shape == (2,2):
+        return np.arctan2(R[1,0], R[0,0])
+    else:
+        # NOTE: assumes we're using an X-Y-Z convention to construct R
+        return np.array([np.arctan2(R[2,1],R[2,2]),
+                         np.arctan2(-R[2,0],np.sqrt(R[2,1]**2 + R[2,2]**2)),
+                         np.arctan2(R[1,0],R[0,0])])
+
+def rpy2quat(rpy):
+    # converts ROS's rpy representation of angles to unit quaternions
+    # from https://code.ros.org/trac/ros-pkg/ticket/4247, but with
+    #  q = [w;x;y;z]
+    rpy = np.array(rpy)
+    s=np.sin(rpy/2)
+    c=np.cos(rpy/2);
+
+    q = np.array([c[0]*c[1]*c[2] + s[0]*s[1]*s[2],
+                  s[0]*c[1]*c[2] - c[0]*s[1]*s[2],
+                  c[0]*s[1]*c[2] + s[0]*c[1]*s[2],
+                  c[0]*c[1]*s[2] - s[0]*s[1]*c[2]])
+
+    return q/(np.linalg.norm(q)+np.spacing(1));
+
+def mk_transform(xyz, rpy):
+    return np.vstack((np.hstack((rpy2rotmat(rpy), np.array(xyz).reshape((3,1)))),
+                      np.array([0,0,0,1])))
 
 def angleDiff(phi1, phi2):
     return np.mod(phi2 - phi1 + np.pi, 2 * np.pi) - np.pi
