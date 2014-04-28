76d04b1ed6a8ad7ee921f3781bb3879837d053ff
Scott Kuindersma <kuindersma@gmail.com>
2014-04-28 10:48:47 -0400

    walking pd block clean up/add contact input

--- software/control/matlab/controllers/WalkingPDBlock.m
+++ software/control/matlab/controllers/WalkingPDBlock.m
@@ -4,34 +4,43 @@ classdef WalkingPDBlock < MIMODrakeSystem
     nq;
     Kp;
     Kd;
-    dt;
-    controller_data; % pointer to shared data handle containing qtraj
+    controller_data; % pointer to shared data handle com/foot trajectories
     ikoptions;
     robot;
     max_nrm_err;
-    contact_est_monitor;
-    l_ank;
-    r_ank;
-    ik_qnom;
+		input_foot_contacts;
+		r_ankle_idx;
+		l_ankle_idx;
   end
   
   methods
     function obj = WalkingPDBlock(r,controller_data,options)
       typecheck(r,'Atlas');
       typecheck(controller_data,'SharedDataHandle');
-            
-      input_frame = MultiCoordinateFrame({AtlasCoordinates(r),r.getStateFrame});
-      coords = AtlasCoordinates(r);
+      
+			if nargin<3
+				options = struct();
+			end
+
+			if ~isfield(options,'input_foot_contacts')
+				options.input_foot_contacts = false;
+			else
+				typecheck(options.input_foot_contacts,'logical');
+			end
+			
+			if options.input_foot_contacts
+				input_frame = MultiCoordinateFrame({AtlasCoordinates(r),r.getStateFrame,FootContactState});
+			else
+				input_frame = MultiCoordinateFrame({AtlasCoordinates(r),r.getStateFrame});
+			end
+			coords = AtlasCoordinates(r);
       obj = obj@MIMODrakeSystem(0,0,input_frame,coords,true,true);
       obj = setInputFrame(obj,input_frame);
       obj = setOutputFrame(obj,coords);
 
       obj.controller_data = controller_data;
       obj.nq = getNumDOF(r);
-
-      if nargin<3
-        options = struct();
-      end
+			obj.input_foot_contacts = options.input_foot_contacts;
       
       if isfield(options,'Kp')
         typecheck(options.Kp,'double');
@@ -56,27 +65,15 @@ classdef WalkingPDBlock < MIMODrakeSystem
       if isfield(options,'dt')
         typecheck(options.dt,'double');
         sizecheck(options.dt,[1 1]);
-        obj.dt = options.dt;
+        dt = options.dt;
       else
-        obj.dt = 0.001;
+        dt = 0.001;
       end
-      obj = setSampleTime(obj,[obj.dt;0]); % sets controller update rate
+      obj = setSampleTime(obj,[dt;0]); % sets controller update rate
 
-      state_names = r.getStateFrame.coordinates(1:getNumDOF(r));
-      obj.l_ank = find(~cellfun(@isempty,strfind(state_names,'l_leg_akx')) | ~cellfun(@isempty,strfind(state_names,'l_leg_aky')));
-      obj.r_ank = find(~cellfun(@isempty,strfind(state_names,'r_leg_akx')) | ~cellfun(@isempty,strfind(state_names,'r_leg_aky')));
-      
-      if isfield(options,'q_nom')
-        typecheck(options.q_nom,'double');
-        sizecheck(options.q_nom,[obj.nq 1]);
-        q_nom = options.q_nom;
-        obj.controller_data.setField('qtraj',q_nom);
-      else
-        d = load(strcat(getenv('DRC_PATH'),'/control/matlab/data/atlas_fp.mat'));
-        q_nom = d.xstar(1:obj.nq);
-        obj.controller_data.setField('qtraj',q_nom);
-      end
-      
+			obj.r_ankle_idx = findJointIndices(r,'r_leg_ak');
+			obj.l_ankle_idx = findJointIndices(r,'l_leg_ak');
+            
       if ~isfield(obj.controller_data,'trans_drift')
         obj.controller_data.setField('trans_drift',[0;0;0]);
       end
@@ -95,11 +92,8 @@ classdef WalkingPDBlock < MIMODrakeSystem
 
       cost = double(cost);
       
-%      arm_idx = find(~cellfun(@isempty,strfind(state_names,'arm')));
-%      cost(arm_idx) = 0.1*ones(length(arm_idx),1);
       obj.ikoptions = struct();
       obj.ikoptions.Q = diag(cost(1:obj.nq));
-      obj.ikoptions.q_nom = q_nom;
 
       % Prevent the knee from locking
       %[obj.ikoptions.jointLimitMin, obj.ikoptions.jointLimitMax] = r.getJointLimits();
@@ -107,49 +101,37 @@ classdef WalkingPDBlock < MIMODrakeSystem
       %obj.ikoptions.jointLimitMin(~cellfun(@isempty,strfind(joint_names,'kny'))) = 0.6;
 
       obj.robot = r;
-      obj.max_nrm_err = 1.5;
-      
-      obj.contact_est_monitor = drake.util.MessageMonitor(drc.foot_contact_estimate_t,'utime');
-      lc = lcm.lcm.LCM.getSingleton();
-      lc.subscribe('FOOT_CONTACT_ESTIMATE',obj.contact_est_monitor);
-      
-      
+      obj.max_nrm_err = 1.5;      
     end
    
     function y=mimoOutput(obj,t,~,varargin)
-      global infocount 
-      if isempty(infocount)
-        infocount = 0;
-      end
+%       global infocount 
+%       if isempty(infocount)
+%         infocount = 0;
+%       end
       
-      x = varargin{2};
+      obj.ikoptions.q_nom = varargin{1};
+
+			x = varargin{2};
       q = x(1:obj.nq);
       qd = x(obj.nq+1:end);
 
-      obj.ikoptions.q_nom = varargin{1};
-      cdata = obj.controller_data.data;
+      if obj.input_foot_contacts
+				fc = varargin{3};
+				
+				if fc(1) > 0.5 % left foot in contact
+%         Kp(obj.l_ankle_idx,obj.l_ankle_idx) = 0*eye(2);
+%         Kd(obj.l_ankle_idx,obj.l_ankle_idx) = 0*eye(2);
+				end
+				if fc(2) > 0.5 % right foot in contact
+%         Kp(obj.r_ankle_idx,obj.r_ankle_idx) = 0*eye(2);
+%         Kd(obj.r_ankle_idx,obj.r_ankle_idx) = 0*eye(2);
+				end
+			end
+			
+			cdata = obj.controller_data.data;
 
       
-%       % get foot contact state over LCM
-%       contact_data = obj.contact_est_monitor.getMessage();
-%       if isempty(contact_data)
-%         lfoot_contact_state = 0;
-%         rfoot_contact_state = 0;
-%       else
-%         msg = drc.foot_contact_estimate_t(contact_data);
-%         lfoot_contact_state = msg.left_contact > 0.5;
-%         rfoot_contact_state = msg.right_contact > 0.5;
-%       end
-      
-%       if lfoot_contact_state
-%         Kp(obj.l_ank,obj.l_ank) = 0*eye(2);
-%         Kd(obj.l_ank,obj.l_ank) = 0*eye(2);
-%       end
-%       if rfoot_contact_state
-%         Kp(obj.r_ank,obj.r_ank) = 0*eye(2);
-%         Kd(obj.r_ank,obj.r_ank) = 0*eye(2);
-%       end
-%       
       approx_args = {};
       for j = 1:length(cdata.link_constraints)
         if ~isempty(cdata.link_constraints(j).traj)
@@ -177,8 +159,7 @@ classdef WalkingPDBlock < MIMODrakeSystem
 %       if infocount > 0
 %   		  save(sprintf('pd_dump_t=%2.3f.mat',t),'q','q_des','err_q','y','compos','approx_args');
 %       end
-      
-      
+     
 		end
   end
   
