006e979c0ab78d5cbb88fa00344b87ed519fffe0
Robin Deits <robin.deits@gmail.com>
2014-05-09 11:18:07 -0400

    pass more walking params through from the GUI to walk planner

--- software/control/matlab/systems/@Biped/planZMPTraj.m
+++ software/control/matlab/systems/@Biped/planZMPTraj.m
@@ -169,14 +169,15 @@ supps = [zmp_knots.supp];
 foot_supports = [[supps.right] * rfoot_body_idx;
                  [supps.left] * lfoot_body_idx];
 zmp_ts = [zmp_knots.t];
-supports = cell(length(zmp_ts),1);
+supports = SupportState.empty(0,1);
+% supports = cell(length(zmp_ts),1);
 for i=1:length(zmp_ts)
   if all(foot_supports(:,i)~=0)
     cpts = {1:4,1:4};
   else
     cpts = {1:4};
   end
-  supports{i} = SupportState(biped,foot_supports(:,i),cpts);
+  supports(i) = SupportState(biped,foot_supports(:,i),cpts);
 end
 
 if options.debug
