b8940b3e3f592f35e2e041627b92724ed521f83a
Robin Deits <robin.deits@gmail.com>
2014-05-09 19:07:55 -0400

    allow user to toggle walking volume regions, and upgrade IRIS

--- software/externals/cmake/externals.cmake
+++ software/externals/cmake/externals.cmake
@@ -104,7 +104,7 @@ set(gurobi_revision 46d1985d5a86f7bc660e363cb2d5ff76af4b6fd9)
 set(gurobi_depends)
 
 set(iris_url ssh://git@github.com/rdeits/iris-distro.git)
-set(iris_revision 81171bcb897ac5fde30012776cd3db6ff0a9fa3c)
+set(iris_revision 246f033b553120e878689167a4e18212473e179f)
 set(iris_depends)
 
 set(mosek_url ssh://git@github.com/rdeits/mosek.git)
