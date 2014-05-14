e63a65a6f0368d767e7d658a5c4a011e16b53b2e
Maurice Fallon <mfallon@mit.edu>
2014-05-13 22:01:34 -0400

    clean up and adding feature publishing

--- software/motion_estimate/vo_estimate/src/fovision/fovision_simple_odometry.cpp
+++ software/motion_estimate/vo_estimate/src/fovision/fovision_simple_odometry.cpp
@@ -1,7 +1,3 @@
-// Main VO module
-
-
-
 #include <zlib.h>
 #include <lcm/lcm-cpp.hpp>
 #include <bot_param/param_client.h>
@@ -10,9 +6,7 @@
 
 #include <lcmtypes/bot_core.hpp>
 #include <lcmtypes/multisense.hpp>
-#include <lcmtypes/microstrain_comm.hpp>
 #include "lcmtypes/drc/robot_state_t.hpp"
-#include "lcmtypes/drc/imu_t.hpp"
 
 #include "drcvision/voconfig.hpp"
 #include "drcvision/vofeatures.hpp"
@@ -34,10 +28,16 @@
 using namespace std;
 using namespace cv; // for disparity ops
 
+struct CommandLineConfig
+{
+  std::string camera_config; // which block from the cfg to read
+  bool output_signal;
+  bool feature_analysis;
+};
+
 class StereoOdom{
   public:
-    StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, string camera_config_,
-      bool output_signal_);
+    StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, const CommandLineConfig& cl_cfg_);
     
     ~StereoOdom(){
       free (left_buf_);
@@ -45,12 +45,15 @@ class StereoOdom{
     }
 
   private:
+    const CommandLineConfig cl_cfg_;    
+    
     int image_size_; // just the resolution of the image
     uint8_t* left_buf_;
     uint8_t* right_buf_;
-
-    // Is mutable necessary?
-    mutable std::vector<float> disparity_buf_;    
+    mutable std::vector<float> disparity_buf_; // Is mutable necessary?
+    uint8_t* rgb_buf_ ;
+    uint8_t* decompress_disparity_buf_;    
+    image_io_utils*  imgutils_;    
     
     int64_t utime_cur_;
 
@@ -71,12 +74,8 @@ class StereoOdom{
     Eigen::Isometry3d ref_camera_pose_; // [pose of the camera when the reference frames changed
     bool changed_ref_frames_;
 
-    bool output_signal_;
-
     void featureAnalysis();
     void updateMotion(int64_t utime);
-    
-    string camera_config_; // which block from the cfg to read
     
     void unpack_multisense(const multisense::images_t *msg);
     void imageHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const  bot_core::image_t* msg);   
@@ -88,28 +87,16 @@ class StereoOdom{
     boost::shared_ptr<KDL::TreeFkSolverPosFull_recursive> fksolver_;
     
     bool pose_initialized_;
-    int imu_counter_;
 
     drc::robot_state_t last_robot_state_msg_;
     
-  
-    image_io_utils*  imgutils_;    
-    
-    uint8_t* rgb_buf_ ;
-    uint8_t* decompress_disparity_buf_;    
-    
     Eigen::Isometry3d world_to_camera_;
-    Eigen::Isometry3d world_to_body_;
-    
+    Eigen::Isometry3d world_to_body_;    
 };    
 
-StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, string camera_config_,
-       bool output_signal_) : 
-       lcm_(lcm_), utime_cur_(0), utime_prev_(0), 
-       ref_utime_(0), changed_ref_frames_(false), 
-       camera_config_(camera_config_),
-       output_signal_(output_signal_)
-{
+StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, const CommandLineConfig& cl_cfg_) : 
+       lcm_(lcm_), cl_cfg_(cl_cfg_), utime_cur_(0), utime_prev_(0), 
+       ref_utime_(0), changed_ref_frames_(false){
   botparam_ = bot_param_new_from_server(lcm_->getUnderlyingLCM(), 0);
   botframes_= bot_frames_get_global(lcm_->getUnderlyingLCM(), botparam_);
   model_ = boost::shared_ptr<ModelClient>(new ModelClient(lcm_->getUnderlyingLCM(), 0));
@@ -121,16 +108,16 @@ StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, string camera_config_,
   fksolver_ = boost::shared_ptr<KDL::TreeFkSolverPosFull_recursive>(new KDL::TreeFkSolverPosFull_recursive(tree));
   
   // Read config from file:
-  config = new voconfig::KmclConfiguration(botparam_, camera_config_);
-  std::cout << "tip\n";
+  config = new voconfig::KmclConfiguration(botparam_, cl_cfg_.camera_config);
 
   boost::shared_ptr<fovis::StereoCalibration> stereo_calibration_;
   stereo_calibration_ = boost::shared_ptr<fovis::StereoCalibration>(config->load_stereo_calibration());
   image_size_ = stereo_calibration_->getWidth() * stereo_calibration_->getHeight();
   left_buf_ = (uint8_t*) malloc(3*image_size_);
   right_buf_ = (uint8_t*) malloc(3*image_size_);
-
   left_buf_ref_ = (uint8_t*) malloc(3*image_size_); // used of feature output 
+  rgb_buf_ = (uint8_t*) malloc(10*image_size_ * sizeof(uint8_t)); 
+  decompress_disparity_buf_ = (uint8_t*) malloc( 4*image_size_*sizeof(uint8_t));  // arbitary size chosen..
   
   vo_ = new FoVision(lcm_ , stereo_calibration_);
   features_ = new VoFeatures(lcm_, stereo_calibration_->getWidth(), stereo_calibration_->getHeight() );
@@ -140,16 +127,8 @@ StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, string camera_config_,
   //lcm_->subscribe("MULTISENSE_LR",&StereoOdom::multisenseLRHandler,this);
   lcm_->subscribe("CAMERA",&StereoOdom::multisenseLDHandler,this);
   
-  cout <<"StereoOdom Constructed\n";
-  
   imgutils_ = new image_io_utils( lcm_->getUnderlyingLCM(), stereo_calibration_->getWidth(), 2*stereo_calibration_->getHeight()); // extra space for stereo tasks
  
-  
-  rgb_buf_ = (uint8_t*) malloc(10*1024 * 1024 * sizeof(uint8_t)); 
-  decompress_disparity_buf_ = (uint8_t*) malloc( 4*1024*1204*sizeof(uint8_t));  // arbitary size chosen..
-  
-  
-  
   // Initialise the nominal camera frame with the head pointing horizontally
   Eigen::Matrix3d M;
   M <<  0,  0, 1,
@@ -161,13 +140,51 @@ StereoOdom::StereoOdom(boost::shared_ptr<lcm::LCM> &lcm_, string camera_config_,
   world_to_camera_.translation().y() = 0;
   world_to_camera_.translation().z() = 1.65; // nominal head height
   
-  /*
-  Eigen::Vector3d translation(cam_to_local.translation());
-  Eigen::Quaterniond rotation;
-  rotation  << 0,0,0,0;
-  rotation = rotation * M.transpose();  
-  */
+  cout <<"StereoOdom Constructed\n";  
+}
+
+
+int counter =0;
+void StereoOdom::featureAnalysis(){
+
+  /// Incremental Feature Output:
+  if (counter%5 == 0 ){
+    features_->setFeatures(vo_->getMatches(), vo_->getNumMatches() , utime_cur_);
+    features_->setCurrentImage(left_buf_);
+    //features_->setCurrentImages(left_buf_, right_buf_);
+    //features_->setCurrentCameraPose( estimator_->getCameraPose() );
+    features_->setCurrentCameraPose( world_to_camera_ );
+    features_->doFeatureProcessing(1); // 1 = send the FEATURES_CUR
+  }
   
+  /// Reference Feature Output: ///////////////////////////////////////////////
+  // Check we changed reference frame last iteration, if so output the set of matching inliers:
+  if (changed_ref_frames_) {
+    if (ref_utime_ > 0){ // skip the first null image
+      if(vo_->getNumMatches() > 200){ // if less than 50 features - dont bother writing
+      // was:      if(featuresA.size() > 50){ // if less than 50 features - dont bother writing
+        cout << "ref frame from " << utime_prev_ << " at " << utime_cur_ <<  " with " <<vo_->getNumMatches()<<" matches\n";
+        features_->setFeatures(vo_->getMatches(), vo_->getNumMatches() , ref_utime_);
+        features_->setReferenceImage(left_buf_ref_);
+        features_->setReferenceCameraPose( ref_camera_pose_ );
+        features_->doFeatureProcessing(0); // 0 = send the FEATURES_REF
+      }
+    }
+    changed_ref_frames_=false;
+  }
+
+  
+  if (vo_->getChangeReferenceFrames()){ // If we change reference frame, note the change for the next iteration.
+    std::cout << "ref frame from " << ref_utime_ << " to " << utime_cur_  << " " << (utime_cur_-ref_utime_)*1E-6 << "sec\n";
+    ref_utime_ = utime_cur_;
+    // ref_camera_pose_ = estimator_->getCameraPose(); // publish this pose when the 
+    ref_camera_pose_ = world_to_camera_; // publish this pose when the 
+    // TODO: only copy gray data if its grey
+    std::copy( left_buf_ , left_buf_ + 3*image_size_  , left_buf_ref_); // Keep the image buffer to write with the features:
+    changed_ref_frames_=true;
+  }
+  
+  counter++;
 }
 
 
@@ -205,7 +222,7 @@ void StereoOdom::updateMotion(int64_t utime){
   // Find the resultant delta by comparing the body position estimate with its previous
   Eigen::Isometry3d delta_body =  new_world_to_body * ( world_to_body_.inverse() );
 
-  if (output_signal_){
+  if (cl_cfg_.output_signal ){
     publishPose(world_to_camera_, utime, "POSE_BODY_ALT"); // same as CAMERA_LEFT frame solved by state-sync
     publishPose(new_world_to_body, utime, "POSE_BODY");
   }
@@ -249,101 +266,17 @@ int pixel_convert_8u_rgb_to_8u_gray (uint8_t *dest, int dstride, int width,
   return 0;
 }
 
-////// Camera handlers:
-int counter_cam=0;
-// stacked LR image
-void StereoOdom::imageHandler(const lcm::ReceiveBuffer* rbuf, 
-     const std::string& channel, const  bot_core::image_t* msg){
-  utime_prev_ = utime_cur_;
-  utime_cur_ = msg->utime;
-  
-  if (msg->pixelformat == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG){
-    if (counter_cam % 10 ==0){
-      std::cout << channel << " "<< msg->utime << " frame:"<< counter_cam << " MJPEG received\n"; 
-    }
-    counter_cam++;
-    imgutils_->decodeStereoImageToGray(msg, left_buf_, right_buf_);
-   
-  }else{
-    std::cout << "camera not MJPEG - not supported/tested\n"; 
-    exit(-1);
-    //memcpy(left_buf_,  msg->data.data() , msg->size/2);
-    //memcpy(right_buf_,  msg->data.data() + msg->size/2 , msg->size/2);
-  }
-  
-  
-  vo_->doOdometry(left_buf_,right_buf_, msg->utime);
-  updateMotion(msg->utime);
-  
-  return; // full estimator VO disabled for VRC competition, 8 june 2013
-}
-
-void StereoOdom::multisenseLRHandler(const lcm::ReceiveBuffer* rbuf, 
-     const std::string& channel, const  multisense::images_t* msg){
-  utime_prev_ = utime_cur_;
-  utime_cur_ = msg->utime;
-  
-  
-  int w = msg->images[0].width;
-  int h = msg->images[0].height;
-  
-  // extract image data
-  switch (msg->images[0].pixelformat) {
-    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_GRAY:
-      memcpy(left_buf_,  msg->images[0].data.data() , msg->images[0].size);
-      memcpy(right_buf_,  msg->images[1].data.data() , msg->images[1].size);
-      break;
-    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB:
-      // image came in as raw RGB buffer.  convert to grayscale:
-      pixel_convert_8u_rgb_to_8u_gray(  left_buf_, w,          w, h, msg->images[0].data.data(),  w*3);
-      pixel_convert_8u_rgb_to_8u_gray(  right_buf_, w,          w, h, msg->images[1].data.data(),  w*3);
-      break;
-    case BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG:
-      fprintf(stderr, "MPEG not supported yet\n");
-      /*// for some reason msg->row_stride is 0, so we use msg->width instead.
-      jpeg_decompress_8u_gray(msg->data,
-                              msg->size,
-                              _images_buf,
-                              msg->width,
-                              msg->height,
-                              msg->width);
-      std::copy(_images_buf           , _images_buf+_buf_size   , _image_left_buf);
-      std::copy(_images_buf+_buf_size , _images_buf+2*_buf_size , _image_right_buf);
-      */
-      break;
-    default:
-      fprintf(stderr, "Unrecognized image format\n");
-      break;
-  }  
-  
-  vo_->doOdometry(left_buf_,right_buf_, msg->utime);
-  updateMotion(msg->utime);
-  
-  return; // full estimator VO disabled for VRC competition, 8 june 2013
-}
-
-
-
-
-
 void StereoOdom::unpack_multisense(const multisense::images_t *msg){
-  bool is_rgb=true;
   if (msg->images[0].pixelformat == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB ){
     rgb_buf_ = (uint8_t*) msg->images[0].data.data();
-    is_rgb = true;
   }else if (msg->images[0].pixelformat == BOT_CORE_IMAGE_T_PIXEL_FORMAT_GRAY ){
     rgb_buf_ = (uint8_t*) msg->images[0].data.data();
-    is_rgb = false;
   }else if (msg->images[0].pixelformat == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG ){
     jpeg_decompress_8u_rgb ( msg->images[0].data.data(), msg->images[0].size,
         rgb_buf_, msg->images[0].width, msg->images[0].height, msg->images[0].width* 3);
     
     pixel_convert_8u_rgb_to_8u_gray(  left_buf_, msg->images[0].width, msg->images[0].width, msg->images[0].height, 
                                       rgb_buf_,  msg->images[0].width*3);
-    
-    //jpegijg_decompress_8u_rgb(msg->image.image_data, msg->image.image_data_nbytes,
-    //        rgb_data, msg->image.width, msg->image.height, msg->image.width* 3);
-    is_rgb = true;
   }else{
     std::cout << "pointcloud_lcm::unpack_multisense | image type not understood\n";
     exit(-1);
@@ -357,7 +290,6 @@ void StereoOdom::unpack_multisense(const multisense::images_t *msg){
     std::cout << "pointcloud_lcm::unpack_multisense | depth type not understood\n";
     exit(-1);
   }
- 
 }
 
 
@@ -371,11 +303,8 @@ void StereoOdom::multisenseLDHandler(const lcm::ReceiveBuffer* rbuf,
   
   unpack_multisense(msg);
   
-//  memcpy(left_buf_,  msg->images[0].data.data() , msg->images[0].size);
-  
   // Convert Carnegie disparity format into floating point disparity. Store in local buffer
   Mat disparity_orig_temp = Mat::zeros(h,w,CV_16UC1); // h,w
-//  const uint8_t* raw_data = msg->images[1].data.data();
   disparity_orig_temp.data = (uchar*) decompress_disparity_buf_;   // ... is a simple assignment possible?  
   cv::Mat_<float> disparity_orig(h, w);
   disparity_orig = disparity_orig_temp;
@@ -385,26 +314,29 @@ void StereoOdom::multisenseLDHandler(const lcm::ReceiveBuffer* rbuf,
   
   vo_->doOdometry(left_buf_,disparity_buf_.data(), msg->utime );
   updateMotion(msg->utime);
+  
+  if(cl_cfg_.feature_analysis)
+    featureAnalysis();
 
-  return; // full estimator VO disabled for VRC competition, 8 june 2013
+  return;
 }
 
-
-
 int main(int argc, char **argv){
+  CommandLineConfig cl_cfg;
+  cl_cfg.camera_config = "CAMERA";
+  cl_cfg.output_signal = FALSE; 
+  cl_cfg.feature_analysis = FALSE; 
   ConciseArgs parser(argc, argv, "fovision-odometry");
-  string camera_config = "CAMERA";
-  bool output_signal = FALSE;
-  parser.add(camera_config, "c", "camera_config", "Camera Config block to use: CAMERA, stereo, stereo_with_letterbox");
-  parser.add(output_signal, "o", "output_signal", "Output POSE_BODY and POSE_BODY_ALT signals");
+  parser.add(cl_cfg.camera_config, "c", "camera_config", "Camera Config block to use: CAMERA, stereo, stereo_with_letterbox");
+  parser.add(cl_cfg.output_signal, "o", "output_signal", "Output POSE_BODY and POSE_BODY_ALT signals");
+  parser.add(cl_cfg.feature_analysis, "f", "feature_analysis", "Publish Feature Analysis Data");
   parser.parse();
-  cout << camera_config << " is camera_config\n"; 
-  
+  cout << cl_cfg.camera_config << " is camera_config\n"; 
   
   boost::shared_ptr<lcm::LCM> lcm(new lcm::LCM);
   if(!lcm->good()){
     std::cerr <<"ERROR: lcm is not good()" <<std::endl;
   }
-  StereoOdom fo= StereoOdom(lcm,camera_config, output_signal);    
+  StereoOdom fo= StereoOdom(lcm,cl_cfg);    
   while(0 == lcm->handle());
 }
