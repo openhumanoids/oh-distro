cd65fcc42504c5cd616dffa0c285ff283c372c3f
Robin Deits <robin.deits@gmail.com>
2014-05-16 11:06:33 -0400

    an interesting result using the spring-network idea
    the concept here was to try to keep the spacing between steps near
    dx_nom for straight plans just by carefully balancing the quadratic cost
    terms. It works, but it doesn't have the additional necessary quality of
    causing 'extra' steps to pile up at the end of the plan, and overall its
    results aren't any better

--- software/control/matlab/planners/footstep_planner/footstepCostFun.m
+++ software/control/matlab/planners/footstep_planner/footstepCostFun.m
@@ -1,10 +1,10 @@
 function [c, dc] = footstepCostFun(steps, steps_rel, goal_pos, right_foot_lead, dgoal, nominal_dxy)
-w_goal = [10;10;1;1;1;100];
-w_rot = 10;
-w_dx_final = 100;
-w_rel = [1; 1];
-dx_power = 8;
-dy_power = 2;
+w_goal = [1;1;0;0;0;10];
+w_rot = 1;
+w_dx_final = 10;
+w_rel = [10; 10];
+% dx_power = 2;
+% dy_power = 2;
 nominal_y_var = 0.05;
 
 nsteps = size(steps, 2);
@@ -18,73 +18,60 @@ else
 %   dgoal = norm(steps(1:2,1) - goal_pos.left(1:2));
 end
 
-
 % Normalize the goal weight so that the plans don't stretch out as the goal
 % gets farther away
-extra_distance = max(dgoal - (nsteps - 2) * nominal_dxy(1), 0.01);
-w_goal(1:2) = w_rel(1) / ((4 / dx_power) * nominal_dxy(1) * extra_distance);
+extra_distance = max(dgoal - (nsteps - 1) * nominal_dxy(1), 0.01);
+%   w_goal(1:2) = w_rel(1) / ((4 / dx_power) * nominal_dxy(1) * extra_distance * (nsteps - j + 1));
 
-% c = c + sqrt(sum((w_goal .* (steps(:,r_ndx(end)) - goal_pos.right)).^2));
-% dc(1:6,r_ndx(end)) = 0.5 * 1 / sqrt(sum((w_goal .* (steps(:,r_ndx(end)) - goal_pos.right)).^2)) ...
-%       * 2 * (w_goal.^2 .* (steps(:,r_ndx(end)) - goal_pos.right));
-% c = c + sqrt(sum((w_goal .* (steps(:,l_ndx(end)) - goal_pos.left)).^2));
-% dc(1:6,l_ndx(end)) = 0.5 * 1 / sqrt(sum((w_goal .* (steps(:,l_ndx(end)) - goal_pos.left)).^2)) ...
-%       * 2 * (w_goal.^2 .* (steps(:,l_ndx(end)) - goal_pos.left));
-% c = sum(w_goal .* sum(bsxfun(@minus, steps(:,r_ndx(end)), goal_pos.right).^2,2)) ...
-%            + sum(w_goal .* sum(bsxfun(@minus, steps(:,l_ndx(end)), goal_pos.left).^2,2));
-c = sum(w_goal .* (steps(:,r_ndx(end)) - goal_pos.right).^2) + ...
-    sum(w_goal .* (steps(:,l_ndx(end)) - goal_pos.left).^2);
+
+c = 0;
 dc = zeros(12,nsteps);
-for j = nsteps-1:nsteps
+for j = 2:nsteps
   if mod(right_foot_lead + j, 2)
-    dc(1:6,j) = w_goal .* (2*(steps(:,j) - goal_pos.right));
+    c = c + 0.5 * sum(w_goal .* (steps(:,j) - goal_pos.right).^2);
+    dc(1:6,j) = w_goal .* (steps(:,j) - goal_pos.right);
   else
-    dc(1:6,j) = w_goal .* (2*(steps(:,j) - goal_pos.left));
+    c = c + 0.5 * sum(w_goal .* (steps(:,j) - goal_pos.left).^2);
+    dc(1:6,j) = w_goal .* (steps(:,j) - goal_pos.left);
   end
 end
 
 nominal_dxy = repmat(nominal_dxy, 1, nsteps);
 nominal_dxy(2,r_ndx) = -nominal_dxy(2,r_ndx);
 
-c = c + (-1) * w_rot * sum(steps_rel(6,r_ndx)) + w_rot * sum(steps_rel(6,l_ndx));
-dc(12,r_ndx) = -w_rot;
-dc(12,l_ndx) = w_rot;
+% c = c + (-1) * w_rot * sum(steps_rel(6,r_ndx)) + w_rot * sum(steps_rel(6,l_ndx));
+% dc(12,r_ndx) = -w_rot;
+% dc(12,l_ndx) = w_rot;
 
-c = c + sum(w_rel(2) * ((steps_rel(2,2:end) - nominal_dxy(2,2:end))/nominal_y_var).^dy_power);
-dc(8,2:end) = w_rel(2) * dy_power*((steps_rel(2,2:end) - nominal_dxy(2,2:end))/nominal_y_var).^(dy_power-1) .* (1 / nominal_y_var);
-c = c + sum(w_rel(1) * (steps_rel(1,2:end) ./ nominal_dxy(1,2:end)).^dx_power);
-dc(7,2:end) = w_rel(1) * dx_power*(steps_rel(1,2:end) ./ nominal_dxy(1,2:end)).^(dx_power-1) .* (1 ./ nominal_dxy(1,2:end));
+
+c_dex = 1;
+c_dnom = 0;
+assert(w_goal(1) == w_goal(2), 'Currently I assume that world x and y coordinate goal weights are the same');
+for j = nsteps:-1:2
+  w_rel(1) = w_goal(1) / nominal_dxy(1) * (c_dex * extra_distance + c_dnom * nominal_dxy(1));
+  c = c + 0.5 * w_rel(1) * steps_rel(1,j).^2;
+  dc(7,j) = w_rel(1) * steps_rel(1,j);
+  
+  w_rel(2) = w_goal(1) / nominal_dxy(1) * (c_dex * extra_distance + c_dnom * nominal_dxy(1));
+  c = c + 0.5 * w_rel(2) * (steps_rel(2,j) - nominal_dxy(2,j)).^2;
+  dc(8,j) = w_rel(2) * (steps_rel(2,j) - nominal_dxy(2,j));
+  
+  w_rot = w_goal(6) /  nominal_dxy(1) * (c_dex * extra_distance + c_dnom * nominal_dxy(1));
+  c = c + 0.5 * w_rot * (steps_rel(6,j)).^2;
+  dc(12,j) = w_rot * steps_rel(6,j);
+  
+  c_dnom = c_dnom + c_dex;
+  c_dex = c_dex + 1;
+end
+
+% c = c + sum(w_rel(2) * ((steps_rel(2,2:end) - nominal_dxy(2,2:end))/nominal_y_var).^dy_power);
+% dc(8,2:end) = w_rel(2) * dy_power*((steps_rel(2,2:end) - nominal_dxy(2,2:end))/nominal_y_var).^(dy_power-1) .* (1 / nominal_y_var);
+% c = c + sum(w_rel(1) * (steps_rel(1,2:end) ./ nominal_dxy(1,2:end)).^dx_power);
+% dc(7,2:end) = w_rel(1) * dx_power*(steps_rel(1,2:end) ./ nominal_dxy(1,2:end)).^(dx_power-1) .* (1 ./ nominal_dxy(1,2:end));
 
 c = c + w_dx_final * steps_rel(1,end)^2;
 dc(7,end) = dc(7,end) + w_dx_final * 2 * steps_rel(1,end);
 
 dc = reshape(dc, [], 1);
 
-% w_goal = [1;1;1;1;1;100];
-% w_rot = 10;
-% 
-% nsteps = size(steps, 2);
-% if ~right_foot_lead
-%   r_ndx = 1:2:nsteps;
-%   l_ndx = 2:2:nsteps;
-% else
-%   r_ndx = 2:2:nsteps;
-%   l_ndx = 1:2:nsteps;
-% end
-% c = sum(w_goal .* sum(bsxfun(@minus, steps(:,r_ndx(end)), goal_pos.right).^2,2)) ...
-%            + sum(w_goal .* sum(bsxfun(@minus, steps(:,l_ndx(end)), goal_pos.left).^2,2));
-% dc = zeros(12,nsteps);
-% for j = nsteps-1:nsteps
-%   if mod(right_foot_lead + j, 2)
-%     dc(1:6,j) = w_goal .* (2*(steps(:,j) - goal_pos.right));
-%   else
-%     dc(1:6,j) = w_goal .* (2*(steps(:,j) - goal_pos.left));
-%   end
-% end
-% 
-% c = c + (-1) * w_rot * sum(steps_rel(6,r_ndx)) + w_rot * sum(steps_rel(6,l_ndx));
-% dc(12,r_ndx) = -w_rot;
-% dc(12,l_ndx) = w_rot;
-% 
-% dc = reshape(dc, [], 1);
 end
