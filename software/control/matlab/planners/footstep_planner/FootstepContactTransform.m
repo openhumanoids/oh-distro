9d75f039159f0d95a30cd13ed2ff85d7617a41b9
Robin Deits <robin.deits@gmail.com>
2014-05-22 15:10:01 -0400

    explain my reasoning

--- software/control/matlab/planners/footstep_planner/FootstepContactTransform.m
+++ software/control/matlab/planners/footstep_planner/FootstepContactTransform.m
@@ -1,6 +1,14 @@
 classdef FootstepContactTransform < CoordinateTransform
   % Transform from 6-dof footstep pose expressed at foot origin to a particular
-  % contact location on the foot (like the center of the sole).
+  % contact location on the foot (like the center of the sole). This lets us
+  % work with footstep positions expressed as poses of the center of the foot
+  % when it's convenient but then ensure that we publish footsteps expressed
+  % as the origin of the foot.
+  %
+  % Note: this is trivially equivalent to constructing a RigidBody for every
+  % footstep and then using forwardKin and inverseKin to swap between center
+  % poses and origin poses, but that seems like overkill just to apply a
+  % single transform.
   methods
     function obj = FootstepContactTransform(from,to,contact_offset)
       obj=obj@CoordinateTransform(from,to,true,true);
