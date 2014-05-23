64e94448cdd354eece06fb6a302eca91f0c3f5c2
Scott Kuindersma <kuindersma@gmail.com>
2014-05-23 18:49:06 -0400

    re-reverting deboucing in contact monitor

--- software/control/contact_monitor/python/contact_monitor/main.py
+++ software/control/contact_monitor/python/contact_monitor/main.py
@@ -12,12 +12,20 @@ class ContactMonitor:
     self.fz_threshold = 100 # newtons
     self.l_foot_contact = False
     self.r_foot_contact = False
+    self.last_left_change_t = 0;
+    self.last_right_change_t = 0;
+    self.debounce_time = 0.1 # seconds
+      # note: could also require force be greater than X for at least t secs
 
   def state_handle(self, channel, data):
     msg = drc.robot_state_t.decode(data)
 
-    self.l_foot_contact = msg.force_torque.l_foot_force_z > self.fz_threshold
-    self.r_foot_contact = msg.force_torque.r_foot_force_z > self.fz_threshold
+    if (msg.utime-self.last_left_change_t > self.debounce_time*1e6):
+      self.l_foot_contact = msg.force_torque.l_foot_force_z > self.fz_threshold
+      self.last_left_change_t = msg.utime
+    if (msg.utime-self.last_right_change_t > self.debounce_time*1e6):
+      self.r_foot_contact = msg.force_torque.r_foot_force_z > self.fz_threshold
+      self.last_right_change_t = msg.utime
 
     output = drc.foot_contact_estimate_t()
     output.utime = msg.utime
@@ -35,4 +43,4 @@ def main():
     lc.handle()
 
 if __name__ == '__main__':
-   main()
+  main()
