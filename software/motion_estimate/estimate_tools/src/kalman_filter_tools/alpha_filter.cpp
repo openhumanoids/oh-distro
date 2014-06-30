72cb646278c847fd0923f8f94b1e4d0393ea9f38
Maurice Fallon <mfallon@mit.edu>
2014-06-29 21:52:25 -0400

    alpha filter code

---
+++ software/motion_estimate/estimate_tools/src/kalman_filter_tools/alpha_filter.cpp
@@ -0,0 +1,29 @@
+#include <estimate_tools/alpha_filter.hpp>
+using namespace Eigen;
+using namespace std;
+
+namespace EstimateTools {
+
+AlphaFilter::AlphaFilter(double alpha_): 
+     alpha_(alpha_){
+  init_ = false;
+  verbose_ = false;
+
+}
+
+
+void AlphaFilter::processSample(Eigen::VectorXd& x, 
+                    Eigen::VectorXd &x_filtered){
+  if (!init_){
+     x_filtered = x;
+     x_filtered_prev_ = x_filtered;
+     init_ = true;
+     return;
+  }
+
+  x_filtered <<  alpha_*x_filtered_prev_  + (1.0 - alpha_)*x;
+  x_filtered_prev_ = x_filtered;
+}
+
+
+}
