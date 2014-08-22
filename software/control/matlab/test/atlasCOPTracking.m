eebdac3725e44fd8fe591360f9b16628d637da20
Scott Kuindersma <kuindersma@gmail.com>
2014-08-22 16:38:27 -0400

    COP tracking script runs again. fixes #458

--- software/control/matlab/test/atlasCOPTracking.m
+++ software/control/matlab/test/atlasCOPTracking.m
@@ -2,8 +2,6 @@ function atlasCOPTracking
 %NOTEST
 addpath(fullfile(getDrakePath,'examples','ZMP'));
 
-joint_str = {'leg','back'};% <---- cell array of (sub)strings
-
 r = Atlas();
 r = removeCollisionGroupsExcept(r,{'toe','heel'});
 r = compile(r);
@@ -13,7 +11,6 @@ r = r.setInitialState(xstar);
 % setup frames
 state_frame = AtlasState(r);
 state_frame.subscribe('EST_ROBOT_STATE');
-input_frame = getInputFrame(r);
 ref_frame = AtlasPosVelTorqueRef(r);
 
 nu = getNumInputs(r);
@@ -22,12 +19,14 @@ nq = getNumDOF(r);
 act_idx_map = getActuatedJoints(r);
 gains = getAtlasGains(); % change gains in this file
 
-joint_ind = [];
-joint_act_ind = [];
-for i=1:length(joint_str)
-  joint_ind = union(joint_ind,find(~cellfun(@isempty,strfind(state_frame.coordinates(1:nq),joint_str{i}))));
-  joint_act_ind = union(joint_act_ind,find(~cellfun(@isempty,strfind(input_frame.coordinates,joint_str{i}))));
+force_control_joint_str = {'leg'};% <---- cell array of (sub)strings  
+force_controlled_joints = [];
+for i=1:length(force_control_joint_str)
+  force_controlled_joints = union(force_controlled_joints,find(~cellfun(@isempty,strfind(r.getInputFrame.coordinates,force_control_joint_str{i}))));
 end
+
+act_ind = (1:r.getNumInputs)';
+position_controlled_joints = setdiff(act_ind,force_controlled_joints);
 
 % zero out force gains to start --- move to nominal joint position
 gains.k_f_p = zeros(nu,1);
@@ -42,14 +41,14 @@ atlasLinearMoveToPos(qdes,state_frame,ref_frame,act_idx_map,5);
 
 gains_copy = getAtlasGains();
 % reset force gains for joint being tuned
-gains.k_f_p(joint_act_ind) = gains_copy.k_f_p(joint_act_ind);
-gains.ff_f_d(joint_act_ind) = gains_copy.ff_f_d(joint_act_ind);
-gains.ff_qd(joint_act_ind) = gains_copy.ff_qd(joint_act_ind);
-gains.ff_qd_d(joint_act_ind) = gains_copy.ff_qd_d(joint_act_ind);
+gains.k_f_p(force_controlled_joints) = gains_copy.k_f_p(force_controlled_joints);
+gains.ff_f_d(force_controlled_joints) = gains_copy.ff_f_d(force_controlled_joints);
+gains.ff_qd(force_controlled_joints) = gains_copy.ff_qd(force_controlled_joints);
+gains.ff_qd_d(force_controlled_joints) = gains_copy.ff_qd_d(force_controlled_joints);
 % set joint position gains to 0 for joint being tuned
-gains.k_q_p(joint_act_ind) = 0;
-gains.k_q_i(joint_act_ind) = 0;
-gains.k_qd_p(joint_act_ind) = 0;
+gains.k_q_p(force_controlled_joints) = 0;
+gains.k_q_i(force_controlled_joints) = 0;
+gains.k_qd_p(force_controlled_joints) = 0;
 
 ref_frame.updateGains(gains);
 
@@ -59,30 +58,30 @@ x0 = x(1:2*nq);
 q0 = x0(1:nq);
 kinsol = doKinematics(r,q0);
 
-T = 30;
+T = 10;
 if 0
   % create figure 8 zmp traj
   dt = 0.01;
   ts = 0:dt:T;
   nt = T/dt;
-  radius = 0.04; % 8 loop radius
+  radius = 0.05; % 8 loop radius
   zmpx = [radius*sin(4*pi/T * ts(1:nt/2)), radius*sin(4*pi/T * ts(1:nt/2+1))];
   zmpy = [radius-radius*cos(4*pi/T * ts(1:nt/2)), -radius+radius*cos(4*pi/T * ts(1:nt/2+1))];
 else
-%   % back and forth
-%   w=0.1;
-%   zmpx = [0 0  0 0  0 0  0 0  0 0];
-%   zmpy = [0 w -w w -w w -w w -w 0];
-%
-%   np=length(zmpy);
-%   ts = linspace(0,T,np);
+  % back and forth
+  w=0.2;
+  zmpx = [0 0  0 0  0 0  0 0  0 0];
+  zmpy = [0 w -w w -w w -w w -w 0];
 
-  % rectangle
-  h=0.015; % height/2
-  w=0.08; % width/2
-  zmpx = [0 h h -h -h 0];
-  zmpy = [0 w -w -w w 0];
-  ts = [0 T/5 2*T/5 3*T/5 4*T/5 T];
+  np=length(zmpy);
+  ts = linspace(0,T,np);
+
+%   % rectangle
+%   h=0.015; % height/2
+%   w=0.08; % width/2
+%   zmpx = [0 h h -h -h 0];
+%   zmpy = [0 w -w -w w 0];
+%   ts = [0 T/5 2*T/5 3*T/5 4*T/5 T];
 end
 
 zmpknots = [zmpx;zmpy;0*zmpx];
@@ -100,7 +99,7 @@ zmptraj = zmptraj.setOutputFrame(desiredZMP);
 com = getCOM(r,kinsol);
 options.com0 = com(1:2);
 zfeet = min(foot_pos(3,:));
-[K,~,comtraj] = LinearInvertedPendulum.ZMPtrackerClosedForm(com(3)-zfeet,zmptraj,options);
+[~,V,comtraj] = LinearInvertedPendulum.ZMPtrackerClosedForm(com(3)-zfeet,zmptraj,options);
 
 % plot zmp/com traj in drake viewer
 lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(),'zmp-traj');
@@ -113,22 +112,30 @@ for i=1:length(ts)
 end
 lcmgl.switchBuffers();
 
-foot_support = RigidBodySupportState(r,find(~cellfun(@isempty,strfind(r.getLinkNames(),'foot'))));
-foottraj.right.orig = ConstantTrajectory(forwardKin(r,kinsol,rfoot_ind,[0;0;0],1));
-foottraj.left.orig = ConstantTrajectory(forwardKin(r,kinsol,lfoot_ind,[0;0;0],1));
-link_constraints = buildLinkConstraints(r, q0, foottraj);
+foot_support = RigidBodySupportState(r,[rfoot_ind,lfoot_ind]);
 
+pelvis_idx = findLinkInd(r,'pelvis');
 
-ctrl_data = AtlasQPControllerData(false,struct(...
+link_constraints(1).link_ndx = pelvis_idx;
+link_constraints(1).pt = [0;0;0];
+link_constraints(1).traj = ConstantTrajectory(forwardKin(r,kinsol,pelvis_idx,[0;0;0],1));
+link_constraints(2).link_ndx = rfoot_ind;
+link_constraints(2).pt = [0;0;0];
+link_constraints(2).traj = ConstantTrajectory(forwardKin(r,kinsol,rfoot_ind,[0;0;0],1));
+link_constraints(3).link_ndx = lfoot_ind;
+link_constraints(3).pt = [0;0;0];
+link_constraints(3).traj = ConstantTrajectory(forwardKin(r,kinsol,lfoot_ind,[0;0;0],1));
+
+ctrl_data = AtlasQPControllerData(true,struct(...
   'acceleration_input_frame',AtlasCoordinates(r),...
   'D',-getAtlasNominalCOMHeight()/9.81*eye(2),...
   'Qy',eye(2),...
   'S',V.S,...
-  's1',zeros(4,1),...
-  's2',0,...
-  'x0',[comgoal;0;0],...
-  'u0',zeros(2,1),...
-  'y0',comgoal,...
+  's1',V.s1,...
+  's2',V.s2,...
+  'x0',ConstantTrajectory([zmptraj.eval(zmptraj.tspan(2));0;0]),...
+  'u0',ConstantTrajectory(zeros(2,1)),...
+  'y0',zmptraj,...
   'qtraj',q0,...
   'support_times',0,...
   'supports',foot_support,...
@@ -152,10 +159,6 @@ if ~strcmp(resp,{'y','yes'})
 end
 
 xtraj = [];
-
-udes = zeros(nu,1);
-qddes = zeros(nu,1);
-qd_int_state = zeros(nq+4,1);
 while tt<T
   [x,t] = getNextMessage(state_frame,1);
   if ~isempty(x)
@@ -163,6 +166,7 @@ while tt<T
       toffset=t;
     end
     tt=t-toffset;
+    xtraj = [xtraj,x];
     y = sys.output(tt,[],x);
     ref_frame.publish(t,y,'ATLAS_COMMAND');
   end
