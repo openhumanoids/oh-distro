969d0387634c0f052a3239bd31c6e9e6a9956bf0
Twan Koolen <tkoolen@csail.mit.edu>
2014-09-16 00:00:41 -0400

    added measured pelvis marker position

---
+++ software/control/matlab/calibration/viconMarkerMeasurements/pelvis_measurement.pp
@@ -0,0 +1,11 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="12:41:37" date="2014-08-22"/>
+  <User name="twan"/>
+  <DataFileName name="pelvis.obj"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point x="0.171638" y="0.000000" z="0.119530" active="1" name="reference"/>
+ <point x="0.188638" y="-0.037000" z="0.013530" active="1" name="marker"/>
+</PickedPoints>
