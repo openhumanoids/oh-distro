6c91da303d73372fb3800068a661dda5efa9cc42
Twan Koolen <tkoolen@csail.mit.edu>
2014-09-15 20:48:02 -0400

    adapted runArmOffsetEstimation.m to new jointOffsetEstimation function.

--- software/control/matlab/calibration/runArmOffsetEstimation.m
+++ software/control/matlab/calibration/runArmOffsetEstimation.m
@@ -1,6 +1,4 @@
 function dq = runArmOffsetEstimation(side, logfile, v_norm_limit, num_poses, torso_marker_function, torso_num_params, hand_marker_function, hand_num_params, options)
-%NOTEST
-
 show_pose_indices = getOption(options, 'show_pose_indices', false);
 show_data_synchronization = getOption(options, 'show_data_synchronization', false);
 visualize_result = getOption(options, 'visualize_result', false);
@@ -15,21 +13,25 @@ t_x = t_x - t_x(1);
 t_offset = min(t_x) - min(t_vicon) - 0;
 t_vicon = t_vicon - t_x(1) + t_offset;  %%OFFSET TIME OF THE DATA! THIS MAY CHANGE
 
-torso_body_name = 'utorso';
-torso_vicon_object_name = 'UTORSO';
+body_names = {'utorso'};
+vicon_object_names = {'UTORSO'};
 
 if strcmp(side, 'r')
-  hand_body_name = 'r_hand';
-  hand_vicon_object_name = 'RIGHTHAND';
+  body_names{end + 1} = 'r_hand';
+  vicon_object_names{end + 1} = 'RIGHTHAND';
 elseif strcmp(side, 'l')
-  hand_body_name = 'l_hand';
-  hand_vicon_object_name = 'LEFTHAND';
+  body_names{end + 1} = 'l_hand';
+  vicon_object_names{end + 1} = 'LEFTHAND';
 else
   error('side not recognized');
 end
+marker_functions = {torso_marker_function, hand_marker_function};
+marker_function_num_params = {torso_num_params, hand_num_params};
+scales = {100, 1};
 
 nq = r.getNumPositions();
 nv = r.getNumVelocities();
+nb = length(body_names);
 
 v_data = x_data(nq + (1 : nv), :);
 pose_indices = findCalibrationPoseIndices(v_data, num_poses, v_norm_limit, show_pose_indices);
@@ -53,35 +55,33 @@ if show_data_synchronization
   title('time synchronization helper plot')
 end
 
-torso_body = r.findLinkInd(torso_body_name);
-hand_body = r.findLinkInd(hand_body_name);
+bodies = cell(nb, 1);
+for i = 1 : nb
+  bodies{i} = r.findLinkInd(body_names{i});
+end
 
-[~, joint_indices] = r.findKinematicPath(torso_body, hand_body);
+[~, joint_indices] = r.findKinematicPath(bodies{1}, bodies{2});
 q_indices = [r.getBody(joint_indices).position_num];
-
-torso_markers = getViconObjectData(vicon_data_struct, torso_vicon_object_name);
-hand_markers = getViconObjectData(vicon_data_struct, hand_vicon_object_name);
-
 q_data = zeros(nq, num_poses);
-torso_data = zeros(3, size(torso_markers, 2), num_poses);
-hand_data = zeros(3, size(hand_markers, 2), num_poses);
+
+motion_capture_data = cell(nb, 1);
 for i = 1 : num_poses
   x_pose_index = pose_indices(i);
   vicon_pose_index = find(t_vicon > t_x(x_pose_index),1);
   q_data(:,i) = x_data(1 : nq, x_pose_index);
   
-  torso_data(:,:,i) = torso_markers(1:3,:,vicon_pose_index);
-  occluded_torso_markers = logical(torso_markers(4,:,vicon_pose_index));
-  torso_data(:, occluded_torso_markers, i) = nan(3, sum(occluded_torso_markers));
-  
-  hand_data(:,:,i) = hand_markers(1:3,:,vicon_pose_index);
-  occluded_hand_markers = logical(hand_markers(4,:,vicon_pose_index));
-  hand_data(:, occluded_hand_markers, i) = nan(3, sum(occluded_hand_markers));
+  for j = 1 : nb
+    raw_vicon_data = getViconObjectData(vicon_data_struct, vicon_object_names{j});
+    motion_capture_data{j}(:, :, i) = raw_vicon_data(1:3,:,vicon_pose_index);
+    occluded_markers = logical(raw_vicon_data(4,:,vicon_pose_index));
+    motion_capture_data{j}(:, occluded_markers, i) = nan(3, sum(occluded_markers));
+  end
 end
 
 options.search_floating = true;
-[dq, body1_params, body2_params, floating_states, residuals, info, J, body1_resids, body2_resids] = ...
-  jointOffsetCalibration(r, q_data, q_indices,torso_body, torso_marker_function, torso_num_params, torso_data, hand_body, hand_marker_function, hand_num_params, hand_data, options);
+[dq, marker_params, floating_states, objective_value, marker_residuals, info] = jointOffsetCalibration(r, q_data, q_indices,...
+    bodies, marker_functions, marker_function_num_params, motion_capture_data, scales, options);
+
 dq = unwrap([0;dq]);
 dq = dq(2:end);
 
@@ -95,10 +95,10 @@ if visualize_result
     v.draw(0, q);
     
     kinsol = r.doKinematics(q);
-    hand_pts = r.forwardKin(kinsol,hand_body,hand_marker_function(body2_params));
-    hand_pts_fixed = hand_marker_function(nan(hand_num_params, 1));
-    torso_pts = r.forwardKin(kinsol,torso_body,torso_marker_function(body1_params));
+    torso_pts = r.forwardKin(kinsol,torso_body,torso_marker_function(marker_params{1}));
     torso_pts_fixed = torso_marker_function(nan(torso_num_params, 1));
+    hand_pts = r.forwardKin(kinsol,hand_body,hand_marker_function(marker_params{2}));
+    hand_pts_fixed = hand_marker_function(nan(hand_num_params, 1));
     
     all_pts = [torso_pts hand_pts];
     all_pts_fixed = [torso_pts_fixed hand_pts_fixed];
--- software/control/matlab/calibration/runArmOffsetEstimation_20140827.m
+++ software/control/matlab/calibration/runArmOffsetEstimation_20140827.m
@@ -10,12 +10,12 @@ v_norm_limit = 0.05;
 num_poses = 20;
 marker_data = measuredViconMarkerPositions();
 
-logfile_left = strcat(getenv('DRC_PATH'),'/../logs/arm_calibration/lcmlog-2014-08-27.03-vicon-left-arm-offsets');
-dq_left = runArmOffsetEstimation('l', logfile_left, v_norm_limit, num_poses, ...
-  marker_data.utorso.marker_positions, marker_data.utorso.num_params, ...
-  marker_data.l_hand.marker_positions, marker_data.l_hand.num_params, options);
-disp('left offsets:')
-disp(dq_left);
+% logfile_left = strcat(getenv('DRC_PATH'),'/../logs/arm_calibration/lcmlog-2014-08-27.03-vicon-left-arm-offsets');
+% dq_left = runArmOffsetEstimation('l', logfile_left, v_norm_limit, num_poses, ...
+%   marker_data.utorso.marker_positions, marker_data.utorso.num_params, ...
+%   marker_data.l_hand.marker_positions, marker_data.l_hand.num_params, options);
+% disp('left offsets:')
+% disp(dq_left);
 
 logfile_right = strcat(getenv('DRC_PATH'),'/../logs/arm_calibration/lcmlog-2014-08-27.06-vicon-right-arm-offsets');
 dq_right = runArmOffsetEstimation('r', logfile_right, v_norm_limit, num_poses, ...
