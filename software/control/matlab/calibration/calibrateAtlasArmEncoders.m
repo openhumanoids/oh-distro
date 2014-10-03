c68f7b1024364bd433d5040e25da2a03e61d63f5
Maurice Fallon <mfallon@mit.edu>
2014-10-02 21:54:15 -0400

    meah

--- software/control/matlab/calibration/calibrateAtlasArmEncoders.m
+++ software/control/matlab/calibration/calibrateAtlasArmEncoders.m
@@ -65,6 +65,7 @@ calib_val = Point(state_frame);
 calib_val.r_arm_usy = 0.79804;
 calib_val.r_arm_shx = 1.57022;
 calib_val.r_arm_ely = -0.01648;
+calib_val.r_arm_elx = -0.00297;
 calib_val.r_arm_uwy = 0.02374;
 calib_val.r_arm_mwx = -1.22977;
 calib_val.l_arm_usy = 0.79858;
