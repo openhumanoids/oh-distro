190aebfdc8d25846012d90623fdf3bf4ad18b11c
Maurice Fallon <mfallon@mit.edu>
2014-12-15 14:58:12 -0500

    a few utils for moving the hands, moving left hand works now

--- software/valkyrie/python/move_arms_to_set_point.py
+++ software/valkyrie/python/move_arms_to_set_point.py
@@ -12,33 +12,33 @@ from valkyrie.hand_pose_packet_message_t import hand_pose_packet_message_t
 
 def timestamp_now (): return int (time.time () * 1000000)
 
+
+lc = lcm.LCM()
+
 msg = hand_pose_packet_message_t()
 msg.utime = timestamp_now()
 msg.reference_frame = 0 # not used with joints
 msg.position = [0,0,0] # ee position and orientation
 msg.orientation = [1,0,0,0]
-msg.robot_side = 1
 msg.trajectory_time = 2
 
-if len(sys.argv)>1:
-  cmd = sys.argv[1]
-  msg.data_type =  1 # ee pos=0 | joint angles =1
-  msg.to_home_position = True
-  msg.n_angles = 0
-  msg.joint_angles = []
+msg.data_type =  1 # ee pos=0 | joint angles =1
+msg.to_home_position = False
+msg.n_angles = 6;
 
-else:
-  print 'No mode specified - sending null config'
-  msg.data_type =  1 # ee pos=0 | joint angles =1
-  msg.to_home_position = False
-  msg.n_angles = 6;
-  # left arm set point:
-  msg.joint_angles = [0.2705597,-1.33021509, 2.099817,  0.50086, 0.02374, 0.00058]
-  #right arm set point:
-  # msg.joint_angles = [0.2705597, 1.33021509, 2.099817, -0.50086, 0.02374, 0.00058]
-
-lc = lcm.LCM()
+# left arm set point:
+msg.robot_side = 0
+msg.joint_angles = [0.2705597,-1.33021509, 2.099817,  0.50086, 0.02374, 0.00058]
 lc.publish("VAL_COMMAND_HAND_POSE", msg.encode())
+
+time.sleep(2)
+
+#right arm set point:
+msg.robot_side = 1
+msg.joint_angles = [0.2705597, 1.33021509, 2.099817, -0.50086, 0.02374, 0.00058]
+lc.publish("VAL_COMMAND_HAND_POSE", msg.encode())
+
+
 print "Commanding arm"
 
 
