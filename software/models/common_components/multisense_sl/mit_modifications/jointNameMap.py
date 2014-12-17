b05f0e4da80c8729f96279c9e35b3cae3cb5de74
Andrés Valenzuela <avalenzu@mit.edu>
2014-12-17 10:56:40 -0500

    Add new models/common_components directory

---
+++ software/models/common_components/multisense_sl/mit_modifications/jointNameMap.py
@@ -0,0 +1,1 @@
+jointNameMap = {"head_hokuyo_joint": "head_hokuyo_fixed_joint"}
---
+++ software/models/common_components/multisense_sl/mit_modifications/multisense_sl_v4.py
@@ -0,0 +1,48 @@
+import os
+drc_base_path = os.getenv("DRC_BASE")
+
+import sys
+sys.path.append(os.path.join(drc_base_path, "software", "models",
+                             "model_transformation"))
+
+import mitUrdfUtils as mit
+from jointNameMap import jointNameMap
+from lxml import etree
+import tempfile
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+original_urdf_path = "../multisense_sl_original.urdf"
+urdf_path = "../multisense_sl.urdf"
+no_joint_urdf_path = "../multisense_sl_no_joint.urdf"
+convex_hull_urdf_path = "../multisense_sl_convex_hull.urdf"
+no_collision_urdf_path = "../multisense_sl_no_collision.urdf"
+
+# Expand all includes to allow us to appropriately change mesh filenames
+tmp = tempfile.NamedTemporaryFile()
+mit.xacro(original_urdf_path, tmp.name, includes_only=True,
+          recursive_includes=True)
+
+# Load urdf
+urdf = etree.parse(tmp.name)
+
+# Replace package:// syntax
+mit.replacePackageWithPathInMeshPaths(urdf, "../common_components")
+
+# Use MITDRC joint names and .obj meshes
+mit.useObjMeshes(urdf)
+mit.renameJoints(urdf, jointNameMap)
+urdf.write(urdf_path, pretty_print=True)
+
+# Generate no-joint urdf
+mit.weldAllJoints(urdf)
+urdf.write(no_joint_urdf_path, pretty_print=True)
+
+# Generate no-joint, no-collision urdf
+mit.removeAllCollisions(urdf)
+urdf.write(no_collision_urdf_path, pretty_print=True)
+
+# Generate convex hull urdf
+mit.addCollisionsFromVisuals(urdf)
+mit.useConvexHullMeshes(urdf)
+urdf.write(convex_hull_urdf_path, pretty_print=True)
