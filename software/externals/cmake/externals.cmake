f236ef8b9a4a4c1758ea542b846852e5f690afd0
Robin Deits <robin.deits@gmail.com>
2015-01-23 17:03:33 -0500

    update iris

--- software/externals/cmake/externals.cmake
+++ software/externals/cmake/externals.cmake
@@ -118,7 +118,7 @@ set(gurobi_environment_args GUROBI_DISTRO=${CMAKE_CURRENT_SOURCE_DIR}/cmake/guro
 set(gurobi_depends)
 
 set(iris_url ssh://git@github.com/rdeits/iris-distro.git)
-set(iris_revision 0b34b9e4beac1cf0d319cb72c809b087b02bef53)
+set(iris_revision b3e9e663e2ac89bc0bd607091e50d598b5914956)
 set(iris_depends)
 
 set(mosek_url ssh://git@github.com/RobotLocomotion/mosek.git)
