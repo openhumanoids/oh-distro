0eec47265c84112ffacdfb0bc9d913f9500d4508
Scott Kuindersma <kuindersma@gmail.com>
2015-02-10 12:53:09 -0500

    removed drcFrames.AtlasCoordinateFrame (not needed and was leading to conflicts between drake/drc packages).

--- software/control/matlab/controllers/AtlasManipController.m
+++ software/control/matlab/controllers/AtlasManipController.m
@@ -96,7 +96,7 @@ classdef AtlasManipController < DRCController
       elseif options.controller_type == 4 % use inverse dynamics
 
         % cascade eval block with signal duplicator
-        dupl = SignalDuplicator(drcFrames.AtlasCoordinates(r),2);
+        dupl = SignalDuplicator(atlasFrames.AtlasCoordinates(r),2);
         ins(1).system = 1;
         ins(1).input = 1;
         outs(1).system = 2;
@@ -165,7 +165,7 @@ classdef AtlasManipController < DRCController
       % use saved nominal pose 
       d = load(strcat(getenv('DRC_PATH'),'/control/matlab/data/atlas_fp.mat'));
       q0 = d.xstar(1:getNumPositions(obj.robot));
-      obj.controller_data.qtraj = q0;
+      obj.controller_data.qtraj = q0((1+~obj.robot.floating*6):end);
       obj.controller_data.qddtraj = ConstantTrajectory(zeros(getNumPositions(r),1));
       
       obj = addLCMTransition(obj,'COMMITTED_ROBOT_PLAN',drc.robot_plan_t(),name); % for standing/reaching tasks
--- software/control/matlab/controllers/VelocityOutputIntegratorBlock.m
+++ software/control/matlab/controllers/VelocityOutputIntegratorBlock.m
@@ -21,7 +21,7 @@ classdef VelocityOutputIntegratorBlock < MIMODrakeSystem
       typecheck(r,'Atlas');
 
       num_q = getNumPositions(r);
-      input_frame = MultiCoordinateFrame({getStateFrame(r),drcFrames.AtlasCoordinates(r),drcFrames.FootContactState});
+      input_frame = MultiCoordinateFrame({getStateFrame(r),atlasFrames.AtlasCoordinates(r),drcFrames.FootContactState});
       output_frame = drcFrames.AtlasVelocityRef(r);      
       obj = obj@MIMODrakeSystem(0,4+num_q,input_frame,output_frame,true,true);
       obj = setInputFrame(obj,input_frame);
