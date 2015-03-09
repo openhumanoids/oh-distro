e44c657f44928b94d59c718344c175373ccdbdba
Toby Schneider <toby@gobysoft.org>
2015-03-09 17:38:38 -0400

    Config updates for testbed

--- software/config/testbed/field-startup.sh
+++ software/config/testbed/field-startup.sh
@@ -14,7 +14,7 @@ export LCM_DEFAULT_URL=$LCM_URL_DRC_ROBOT
 
 screen -D -m -S shaper-link3 drc-network-shaper -r robot -c drc_robot.cfg -l -p /tmp  -i link3 -v &
 screen -D -m -S shaper-link2 drc-network-shaper -r robot -c drc_robot.cfg -l -p /tmp  -i link2 -v &
-screen -D -m -S deputy bot-procman-deputy -n field &
+#screen -D -m -S deputy bot-procman-deputy -n field &
 screen -D -m -S tunnel bot-lcm-tunnel & 
 
 sleep 1
