f2be64597308d7feb154ae0030bc2e9302a15113
marcoCar <marcocaravagna@gmail.com>
2015-03-10 16:25:58 +0000

    fix smoothing bug and improve batchRRT

--- software/valkyrie/matlab/batchRRT.m
+++ software/valkyrie/matlab/batchRRT.m
@@ -1,16 +1,20 @@
-function batchRRT(scene, n, outfilename)
-
-options.scene = scene;
-options.visualize = false;
-rrtTimes = zeros(1,n);
-smoothingTimes = zeros(1,n);
-for i = 1:n
-    [xtraj, info, v, simVars] = exploringRRT(options);
-    save(sprintf('%s_%03d',outfilename, i), 'simVars')
-    rrtTimes(i) = simVars.rrtTime;
-    smoothingTimes(i) = simVars.smoothingTime;
-end
-for i=1:n
-    fprintf('Iteration %d:\n\tRRT time: %5.2f s\n\tSmoothing time: %5.2f s\n',...
-        i, rrtTimes(i), smoothingTimes(i))
+function batchRRT(n)
+    scenes = {'scene1', 'scene2', 'scene3'};
+    warning('off', 'MATLAB:MKDIR:DirectoryExists');
+    for scene = 1:length(scenes)
+        %Create a directory to save the results
+        mkdir(scenes{scene});
+        cd(scenes{scene});
+        
+        %Set up the simulation, run it n times and save the results
+        options.scene = scenes{scene};
+        options.visualize = false;
+        for i = 1:n
+            [xtraj, info, v, simVars] = exploringRRT(options);
+            save(sprintf('%03d', i), 'simVars')
+        end
+        % back to the parent dir
+        cd ..
+    end
+    warning('on', 'MATLAB:MKDIR:DirectoryExists');
 end
