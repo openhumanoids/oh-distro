9b96a2de5a675f3bd0c1f7d7ee2697242cea3fdb
Mathew Antone <antone@csail.mit.edu>
2015-03-12 15:35:05 -0400

    changed deputy to run as daemon process

--- software/tools/tools/scripts/atlas_restart_deputy.sh
+++ software/tools/tools/scripts/atlas_restart_deputy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pidof bot-procman-deputy && killall bot-procman-deputy
-bot-procman-deputy -n `hostname` > $HOME/deputy_output.txt 2>&1  &
+daemon --command="bot-procman-deputy -n `hostname`" > $HOME/deputy_output.txt 2>&1
 sleep 0.1
 pidof bot-procman-deputy && echo started deputy: `hostname` with LCM url: $LCM_DEFAULT_URL
