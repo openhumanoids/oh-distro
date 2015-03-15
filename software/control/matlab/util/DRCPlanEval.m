1914967e760fdc745f5295762c4d86872ee29d3a
Robin Deits <robin.deits@gmail.com>
2015-03-15 02:19:36 -0400

    smoothly transition to next manip plan

--- software/control/matlab/util/DRCPlanEval.m
+++ software/control/matlab/util/DRCPlanEval.m
@@ -43,7 +43,10 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
       obj.plan_channels{end+1} = 'START_MIT_STAND';
       obj.plan_handlers{end+1} = @obj.handle_stand_default;
 
-     % ,'stand');
+      obj.plan_monitors{end+1} = drake.util.MessageMonitor(drc.atlas_behavior_command_t, 'utime');
+      obj.plan_channels{end+1} = 'ATLAS_BEHAVIOR_COMMAND';
+      obj.plan_handlers{end+1} = @obj.handle_atlas_behavior_command;
+
      % obj = addLCMTransition(obj,'ATLAS_BEHAVIOR_COMMAND',drc.atlas_behavior_command_t(),'init');
      % obj = addLCMTransition(obj,'CONFIGURATION_TRAJ
 
@@ -68,11 +71,48 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
 
     function new_plan = handle_atlas_behavior_command(obj, msg)
       msg = drc.atlas_behavior_command_t(msg);
-
-      new_plan = SilentPlan(obj.robot);
+      if strcmp(char(msg.command), 'stop') || strcmp(char(msg.command), 'freeze')
+        disp('Got an atlas behavior command...going into silent mode');
+        new_plan = SilentPlan(obj.robot);
+      else
+        new_plan = [];
+      end
     end
 
-    function current_plan = getCurrentPlan(obj, t, x)
+    function new_plan = smoothPlanTransition(obj, t, x, new_plan)
+      % Make the transition to the new plan as smooth as possible
+      new_plan.start_time = t;
+
+      % Use the previously commanded state to override the first (or only) knot point in the new trajectory. This is necessary to prevent the robot from suddenly drooping or jerking at the start of a plan, since the initial state of the plan may not match the robot's current desired state. 
+      if ~isempty(obj.data.last_qp_input)
+        if isnumeric(new_plan.qtraj)
+          new_plan.qtraj(7:end) = obj.data.last_qp_input.whole_body_data.q_des(7:end);
+        else
+          if isa(new_plan.qtraj, 'PPTrajectory')
+            % The qtraj is a piecewise polynomial, so we'll just add a constant offset to its first segment to ensure that its initial qdes is the same as our previously published qdes.
+            [ts, coefs, l, k, d] = unmkpp(new_plan.qtraj.pp);
+            coefs = reshape(coefs, [d, l, k]);
+            q0 = fasteval(new_plan.qtraj, ts(1));
+            delta_q = obj.data.last_qp_input.whole_body_data.q_des - q0;
+            coefs(7:end,1,end) = coefs(7:end,1,end) + delta_q(7:end);
+            if d > 1
+              new_plan.qtraj = PPTrajectory(pchipDeriv(ts, [coefs(:,:,end), fasteval(new_plan.qtraj, ts(end))], coefs(:,:,end-1)));
+            else
+              new_plan.qtraj = PPTrajectory(pchip(ts, coefs(:,:,end)));
+            end
+          elseif isa(new_plan.qtraj, 'ConstantTrajectory')
+            q0 = double(new_plan.qtraj.pt);
+            delta_q = obj.data.last_qp_input.whole_body_data.q_des - q0;
+            q_smooth = [q0(1:6); q0(7:end) + delta_q(7:end)];
+            new_plan.qtraj = ConstantTrajectory(q_smooth);
+          else
+            warning('Got a new plan whose qtraj is neither a constant nor a PPTrajectory. I can not smooth the transition to this plan.');
+          end
+        end
+      end
+    end
+
+    function updateQueueLCM(obj, t, x)
       % check for new plans, and use them to modify the queue
       for j = 1:length(obj.plan_monitors)
         msg = obj.plan_monitors{j}.getNextMessage(0);
@@ -84,17 +124,16 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
           disp('handling new plan on channel:')
           obj.plan_channels{j}
           new_plan = obj.plan_handlers{j}(msg);
-          new_plan.start_time = t;
-          obj.switchToPlan(new_plan);
-          break;
+          if ~isempty(new_plan)
+            new_plan = obj.smoothPlanTransition(t, x, new_plan);
+            obj.switchToPlan(new_plan);
+            break;
+          end
         catch e
           report = e.getReport();
           disp(report)
         end
       end
-
-      % Pass the actual queue handling off to the superclass
-      current_plan = getCurrentPlan@atlasControllers.AtlasPlanEval(obj, t, x);
     end
 
     function run(obj)
@@ -106,21 +145,25 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
 
       % state_data = obj.atlas_state_monitor.getNextMessage(5);
       % [x, t] = obj.atlas_state_coder.decode(drc.robot_state_t(state_data));
-      obj.data.plan_queue = {SilentPlan(obj.robot)};
+      obj.data.plan_queue = {};
       
       disp('DRCPlanEval now listening');
 
       while 1
         % check for new state
-        state_data = obj.atlas_state_monitor.getNextMessage(5);
+        state_data = obj.atlas_state_monitor.getNextMessage(0);
         if isempty(state_data)
           continue
         end
         [x, t] = obj.atlas_state_coder.decode(drc.robot_state_t(state_data));
-        qp_input = obj.getQPControllerInput(t, x);
-        if ~isempty(qp_input)
-          qp_input.param_set_name = [qp_input.param_set_name, '_', obj.mode]; % send _sim or _hardware param variant
-          encodeQPInputLCMMex(qp_input);
+        obj.updateQueueLCM(t, x);
+        if ~isempty(obj.data.plan_queue)
+          qp_input = obj.getQPControllerInput(t, x);
+          if ~isempty(qp_input)
+            qp_input.param_set_name = [qp_input.param_set_name, '_', obj.mode]; % send _sim or _hardware param variant
+            encodeQPInputLCMMex(qp_input);
+            obj.data.last_qp_input = qp_input;
+          end
         end
       end
     end
