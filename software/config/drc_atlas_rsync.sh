f27bdfacf03208104083cab96f96ea3523014b26
Robin Deits <robin.deits@gmail.com>
2015-03-19 16:29:12 -0400

    fix paths for sync script

--- software/config/drc_atlas_rsync.sh
+++ software/config/drc_atlas_rsync.sh
@@ -3,7 +3,7 @@
 read -p "WARNING: This will copy all files from ~/drc to ~/drc on atlas0. Okay? [yN]: " answer
 case ${answer:0:1} in
 	y|Y )
-		rsync -av --exclude-from=$HOME/drc/software/config/drc_atlas_rsync_excludes.txt $HOME/drc/ drc@atlas0:/home/drc/drc
+		rsync -av --exclude-from=$HOME/drc/software/config/drc_atlas_rsync_excludes.txt $HOME/drc/ atlas0:drc
 	;;
 	* )
 		echo "Cancelled."
