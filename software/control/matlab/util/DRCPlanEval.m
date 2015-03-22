fd9275dedcef3687035eb997ff563efc688691b8
Robin Deits <rdeits@csail.mit.edu>
2015-03-22 15:33:25 -0400

    remove matlab struct arrays (because they're ridiculously slow)

--- software/control/matlab/util/DRCPlanEval.m
+++ software/control/matlab/util/DRCPlanEval.m
@@ -7,16 +7,33 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
   properties  
     lc
     mode = 'sim';
-    lcm_interfaces = struct('name', {}, 'channel', {}, 'msg_constructor', {}, 'timeout', {}, 'handler', {}, 'monitor', {});
+
+    msg_monitors = {};
+    msg_constructors = {};
+    msg_handlers = {};
+    msg_channels = {};
+    msg_timeouts = {};
+    msg_names = {};
+
     atlas_state_coder;
+
+    pause_state = 0;
+    contact_force_detected;
+    last_status_msg_time;
   end
+
+
+  properties(Constant)
+   PAUSE_NONE = 0;
+   PAUSE_NOW = 1;
+   STOP_WALKING_ASAP = 2;
+ end
 
   methods
     function obj = DRCPlanEval(r, mode, varargin)
       typecheck(r,'DRCAtlas');
       obj = obj@atlasControllers.AtlasPlanEval(r, varargin{:});
-      obj.data = DRCPlanEvalData(struct('plan_queue', {obj.data.plan_queue}));
-      obj.data.contact_force_detected = zeros(obj.robot_property_cache.num_bodies, 1);
+      obj.contact_force_detected = zeros(obj.robot_property_cache.num_bodies, 1);
       assert(strcmp(obj.mode, 'sim') || strcmp(obj.mode, 'hardware'), 'bad mode: %s', mode);
       obj.mode = mode;
 
@@ -33,45 +50,45 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
     end
 
     function obj = addLCMInterface(obj, name, channel, msg_constructor, timeout, handler)
-      obj.lcm_interfaces(end+1) = struct('name', name,...
-                                         'channel', channel,...
-                                         'msg_constructor', msg_constructor,... 
-                                         'timeout', timeout,...
-                                         'handler', handler,...
-                                         'monitor', drake.util.MessageMonitor(msg_constructor(), 'utime'));
+      obj.msg_names{end+1} = name;
+      obj.msg_constructors{end+1} = msg_constructor;
+      obj.msg_handlers{end+1} = handler;
+      obj.msg_timeouts{end+1} = timeout;
+      obj.msg_channels{end+1} = channel;
+      obj.msg_monitors{end+1} = drake.util.MessageMonitor(msg_constructor(), 'utime');
     end
 
     function obj = setupSubscriptions(obj)
       % subscribe to everything we're listening to -- plan channels + robot state
-      for j = 1:length(obj.lcm_interfaces)
-        obj.lc.subscribe(obj.lcm_interfaces(j).channel, obj.lcm_interfaces(j).monitor);
+      for j = 1:length(obj.msg_channels)
+        obj.lc.subscribe(obj.msg_channels{j}, obj.msg_monitors{j});
       end
     end
 
     function LCMReceive(obj)
-      for j = 1:length(obj.lcm_interfaces)
-        msg_data = obj.lcm_interfaces(j).monitor.getNextMessage(obj.lcm_interfaces(j).timeout);
+      for j = 1:length(obj.msg_channels)
+        msg_data = obj.msg_monitors{j}.getNextMessage(0);
         if ~isempty(msg_data)
-          msg = obj.lcm_interfaces(j).msg_constructor(msg_data);
-          obj.lcm_interfaces(j).handler(msg);
+          msg = obj.msg_constructors{j}(msg_data);
+          obj.msg_handlers{j}(msg);
         end
       end
     end
 
     function handle_state(obj, msg)
       [x, t] = obj.atlas_state_coder.decode(msg);
-      obj.data.t = t;
-      obj.data.x = x;
+      obj.t = t;
+      obj.x = x;
     end
 
     function handle_foot_contact(obj, msg)
-      obj.data.contact_force_detected(obj.robot.foot_body_id.right) = double(msg.right_contact > 0.5);
-      obj.data.contact_force_detected(obj.robot.foot_body_id.left) = double(msg.left_contact > 0.5);
+      obj.contact_force_detected(obj.robot.foot_body_id.right) = double(msg.right_contact > 0.5);
+      obj.contact_force_detected(obj.robot.foot_body_id.left) = double(msg.left_contact > 0.5);
     end
     
     function handle_stand_default(obj, msg)
       disp('Got a default stand plan')
-      new_plan = DRCQPLocomotionPlan.from_standing_state(obj.data.x, obj.robot);
+      new_plan = DRCQPLocomotionPlan.from_standing_state(obj.x, obj.robot);
       obj.switchToPlan(obj.smoothPlanTransition(new_plan));
     end
 
@@ -81,7 +98,10 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
       obj.switchToPlan(obj.smoothPlanTransition(new_plan));
       if isa(new_plan.qtraj, 'Trajectory')
         disp('Automatically generating a standing plan from the end of this plan.')
-        obj.appendPlan(QPLocomotionPlan.from_standing_state(new_plan.qtraj.eval(new_plan.qtraj.tspan(end)), obj.robot, new_plan.supports(end)));
+        obj.appendPlan(QPLocomotionPlan.from_standing_state(new_plan.qtraj.eval(new_plan.qtraj.tspan(end)),...
+                                                            obj.robot,...
+                                                            new_plan.supports(end),...
+                                                            struct('center_pelvis', false)));
       end
     end
 
@@ -95,10 +115,10 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
     function handle_pause(obj, msg)
       if msg.control == msg.PAUSE
         disp('Got a plan pause NOW');
-        obj.data.pause_state = obj.data.PAUSE_NOW;
+        obj.pause_state = obj.PAUSE_NOW;
       elseif msg.control == msg.TERMINATE
         disp('Got a stop walking ASAP');
-        obj.data.pause_state = obj.data.STOP_WALKING_ASAP;
+        obj.pause_state = obj.STOP_WALKING_ASAP;
       else
         % handle this somehow
         disp('I want to resume but I dont know how yet');
@@ -107,19 +127,19 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
 
     function new_plan = smoothPlanTransition(obj, new_plan)
       % Make the transition to the new plan as smooth as possible
-      new_plan.start_time = obj.data.t + 1;
+      new_plan.start_time = [];
 
       % Use the previously commanded state to override the first (or only) knot point in the new trajectory. This is necessary to prevent the robot from suddenly drooping or jerking at the start of a plan, since the initial state of the plan may not match the robot's current desired state. 
-      if ~isempty(obj.data.qp_input) && isfield(new_plan, 'qtraj')
+      if ~isempty(obj.qp_input) && isfield(new_plan, 'qtraj')
         if isnumeric(new_plan.qtraj)
-          new_plan.qtraj(1:end) = obj.data.qp_input.whole_body_data.q_des(1:end);
+          new_plan.qtraj(1:end) = obj.qp_input.whole_body_data.q_des(1:end);
         else
           if isa(new_plan.qtraj, 'PPTrajectory')
             % The qtraj is a piecewise polynomial, so we'll just add a constant offset to its first segment to ensure that its initial qdes is the same as our previously published qdes.
             [ts, coefs, l, k, d] = unmkpp(new_plan.qtraj.pp);
             coefs = reshape(coefs, [d, l, k]);
             q0 = fasteval(new_plan.qtraj, ts(1));
-            delta_q = obj.data.qp_input.whole_body_data.q_des - q0;
+            delta_q = obj.qp_input.whole_body_data.q_des - q0;
             coefs(1:end,1,end) = coefs(1:end,1,end) + delta_q(1:end);
             if d > 1
               new_plan.qtraj = PPTrajectory(pchipDeriv(ts, [coefs(:,:,end), fasteval(new_plan.qtraj, ts(end))], coefs(:,:,end-1)));
@@ -127,10 +147,7 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
               new_plan.qtraj = PPTrajectory(pchip(ts, coefs(:,:,end)));
             end
           elseif isa(new_plan.qtraj, 'ConstantTrajectory')
-            % q0 = double(new_plan.qtraj.pt);
-            % delta_q = obj.data.qp_input.whole_body_data.q_des - q0;
-            % q_smooth = [q0(1:end) + delta_q(1:end)];
-            q_smooth = obj.data.qp_input.whole_body_data.q_des;
+            q_smooth = obj.qp_input.whole_body_data.q_des;
             new_plan.qtraj = q_smooth;
           else
             warning('Got a new plan whose qtraj is neither a constant nor a PPTrajectory. I can not smooth the transition to this plan.');
@@ -141,36 +158,36 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
 
     function pauseIfRequested(obj)
       % TODO: should this use sensed foot contact instead of planned contact?
-      if obj.data.pause_state == obj.data.STOP_WALKING_ASAP && ~isempty(obj.data.qp_input)
+      if obj.pause_state == obj.STOP_WALKING_ASAP && ~isempty(obj.qp_input)
         have_right_foot = 0;
         have_left_foot = 0;
-        for i=1:length(obj.data.qp_input.support_data)
+        for i=1:length(obj.qp_input.support_data)
           % If this support is not forbidden
-          if (any(obj.data.qp_input.support_data(i).support_logic_map))
+          if (any(obj.qp_input.support_data(i).support_logic_map))
             % I'm sure this can be vectorized better
-            if (obj.data.qp_input.support_data(i).body_id == obj.robot.foot_body_id.right)
+            if (obj.qp_input.support_data(i).body_id == obj.robot.foot_body_id.right)
               have_right_foot = 1;
-            elseif (obj.data.qp_input.support_data(i).body_id == obj.robot.foot_body_id.left)
+            elseif (obj.qp_input.support_data(i).body_id == obj.robot.foot_body_id.left)
               have_left_foot = 1;
             end
           end
         end
         if have_right_foot && have_left_foot
           disp('Got double support, pausing now.')
-          obj.data.pause_state = obj.data.PAUSE_NONE;
-          obj.switchToPlan(DRCQPLocomotionPlan.from_standing_state(obj.data.x, obj.robot));
+          obj.pause_state = obj.PAUSE_NONE;
+          obj.switchToPlan(DRCQPLocomotionPlan.from_standing_state(obj.x, obj.robot));
         end
-      elseif obj.data.pause_state == obj.data.PAUSE_NOW
+      elseif obj.pause_state == obj.PAUSE_NOW
         disp('freezing now')
-        obj.switchToPlan(FrozenPlan(obj.data.qp_input));
-        obj.data.pause_state = obj.data.PAUSE_NONE;
+        obj.switchToPlan(FrozenPlan(obj.qp_input));
+        obj.pause_state = obj.PAUSE_NONE;
       end
     end
 
     function sendStatus(obj)
-      if isempty(obj.data.last_status_msg_time) || (obj.data.t - obj.data.last_status_msg_time) > 0.2
-        if ~isempty(obj.data.plan_queue)
-          current_plan = obj.data.plan_queue{1};
+      if isempty(obj.last_status_msg_time) || (obj.t - obj.last_status_msg_time) > 0.2
+        if ~isempty(obj.plan_queue)
+          current_plan = obj.plan_queue{1};
           if strcmp(current_plan.gain_set, 'standing')
             state_flag = drc.controller_status_t.STANDING;
           elseif strcmp(current_plan.gain_set, 'walking')
@@ -183,9 +200,9 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
         else
           state_flag = drc.controller_status_t.DUMMY;
         end
-        obj.data.last_status_msg_time = obj.data.t;
+        obj.last_status_msg_time = obj.t;
         status_msg = drc.controller_status_t();
-        status_msg.utime = obj.data.t * 1e6;
+        status_msg.utime = obj.t * 1e6;
         status_msg.state = state_flag;
         obj.lc.publish('CONTROLLER_STATUS', status_msg);
       end
@@ -194,25 +211,35 @@ classdef DRCPlanEval < atlasControllers.AtlasPlanEval
     function run(obj)
       obj = obj.setupSubscriptions();
 
-      obj.data.plan_queue = {};
+      obj.plan_queue = {};
       
       disp('DRCPlanEval now listening');
 
       while 1
         try
+          % t0 = tic();
           obj.LCMReceive();
+          % fprintf(1, 'recieve: %fs\n', toc(t0))
+          % t0 = tic();
           obj.pauseIfRequested();
-          if ~isempty(obj.data.plan_queue)
-            qp_input = obj.getQPControllerInput(obj.data.t, obj.data.x, obj.data.contact_force_detected);
+          % fprintf(1, 'pause: %fs\n', toc(t0))
+          if ~isempty(obj.plan_queue)
+            % t0 = tic()
+            qp_input = obj.getQPControllerInput(obj.t, obj.x, obj.contact_force_detected);
+            % fprintf(1, 'get input: %fs\n', toc(t0));
             if ~isempty(qp_input)
               if length(qp_input.param_set_name) <= length(obj.mode) + 1 || ~strcmp(qp_input.param_set_name(end-length(obj.mode):end), ['_', obj.mode])
                 qp_input.param_set_name = [qp_input.param_set_name, '_', obj.mode]; % send _sim or _hardware param variant
               end
+              % t0 = tic();
               encodeQPInputLCMMex(qp_input);
-              obj.data.qp_input = qp_input;
+              % fprintf(1, 'encode: %fs\n', toc(t0));
+              obj.qp_input = qp_input;
             end
           end
+          % t0 = tic();
           obj.sendStatus();
+          % fprintf(1, 'send status: %fs\n', toc(t0));
         catch e
           disp('error in planEval loop:')
           disp(e.getReport())
