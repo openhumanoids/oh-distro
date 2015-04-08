a657b2b08fead36bab1cd79ebd82065e8b19979d
Gregory Izatt <gizatt@mit.edu>
2015-04-08 15:18:51 -0400

    Bring in new swingtraj, make controller more sane

--- software/control/src/threadedController.cpp
+++ software/control/src/threadedController.cpp
@@ -332,6 +332,7 @@ void threadLoop(std::shared_ptr<ThreadedControllerOptions> ctrl_opts)
 
       if (info < 0 && ctrl_opts->max_infocount > 0) {
         infocount++;
+        std::cout << "Incremented infocount to " << infocount << std::endl;
         if (infocount >= ctrl_opts->max_infocount) {
           if (infocount == ctrl_opts->max_infocount) {
             std::cout << "Infocount exceeded. Freezing Atlas!" << std::endl;
