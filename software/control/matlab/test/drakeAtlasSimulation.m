4a84b214226765ded0f042bde1662a53a4563d52
Robin Deits <robin.deits@gmail.com>
2015-04-20 20:24:34 -0400

    support arbitrary initial pose in sim

--- software/control/matlab/test/drakeAtlasSimulation.m
+++ software/control/matlab/test/drakeAtlasSimulation.m
@@ -13,6 +13,8 @@ if nargin < 7, box_height = 1.2; end
 % an argument)
 use_mass_est = false;
 
+initial_offset_xyzrpy = [0;0;0;0;0;0];
+
 % silence some warnings
 warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints')
 warning('off','Drake:RigidBodyManipulator:UnsupportedJointLimits')
@@ -39,7 +41,7 @@ if (strcmp(world_name,'steps'))
     1.2, 0.0, 0.8, 1, 0.30;];
   options.terrain = RigidBodyStepTerrain(boxes);
 else
-  options.terrain = RigidBodyFlatTerrain();
+  options.terrain = RigidBodyFlatTerrain(initial_offset_xyzrpy(3));
 end
 r_pure = DRCAtlas([],options);
 % And construct a complete one
@@ -75,7 +77,7 @@ elseif (strcmp(world_name, 'stairs'))
   options.terrain = height_map;
   options.use_bullet = false;
 else
-  options.terrain = RigidBodyFlatTerrain();
+  options.terrain = RigidBodyFlatTerrain(initial_offset_xyzrpy(3));
 end
 options.replace_cylinders_with_capsules = false;
 r_complete = DRCAtlas([],options);
@@ -95,33 +97,33 @@ extra_height = 0;
 % Add world if relevant
 if (strcmp(world_name, 'valve_wall'))
   % Add valve DRC environment
-  r_complete = r_complete.addRobotFromURDF('single_valve_wall.urdf', [1.0; 0.0; 0.0], [0;0;pi]);
+  r_complete = r_complete.addRobotFromURDF('single_valve_wall.urdf', [1.0; 0.0; 0.0] + initial_offset_xyzrpy(1:3), [0;0;pi] + initial_offset_xyzrpy(4:6));
 elseif (strcmp(world_name, 'drill_frame'))
-  r_complete = r_complete.addRobotFromURDF([getDrakePath(), '/examples/Atlas/urdf/drill_frame.urdf'], [1.0; 0.0; 0], [0;0;pi]);
+  r_complete = r_complete.addRobotFromURDF([getDrakePath(), '/examples/Atlas/urdf/drill_frame.urdf'], [1.0; 0.0; 0] + initial_offset_xyzrpy(1:3), [0;0;pi] + initial_offset_xyzrpy(4:6));
   options_cyl.floating = true;
-  r_complete = r_complete.addRobotFromURDF('table.urdf', [0.225; -1.5; 0.5]);
-  r_complete = r_complete.addRobotFromURDF('drill_box.urdf', [0.225; -1.2; 1.2], [], options_cyl);
+  r_complete = r_complete.addRobotFromURDF('table.urdf', [0.225; -1.5; 0.5] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6));
+  r_complete = r_complete.addRobotFromURDF('drill_box.urdf', [0.225; -1.2; 1.2] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy, options_cyl);
 elseif (strcmp(world_name, 'door'))
-  r_complete = r_complete.addRobotFromURDF([getDrakePath(), '/examples/Atlas/urdf/door.urdf'], [1.0; 0.0; 0], [0;0;pi]);
+  r_complete = r_complete.addRobotFromURDF([getDrakePath(), '/examples/Atlas/urdf/door.urdf'], [1.0; 0.0; 0] + initial_offset_xyzrpy(1:3), [0;0;pi] + initial_offset_xyzrpy(4:6));
 elseif (strcmp(world_name, 'manip_ex'))
   options_cyl.floating = true;
-  r_complete = r_complete.addRobotFromURDF('table.urdf', [1.225; 0.0; 0.5]);
-  r_complete = r_complete.addRobotFromURDF('drill_box.urdf', [0.775; -0.2; 1.2], [], options_cyl);
+  r_complete = r_complete.addRobotFromURDF('table.urdf', [1.225; 0.0; 0.5] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6));
+  r_complete = r_complete.addRobotFromURDF('drill_box.urdf', [0.775; -0.2; 1.2] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6), options_cyl);
 elseif(strcmp(world_name, 'terrain'))
-  r_complete = r_complete.addRobotFromSDF(terrainSDF);
+  r_complete = r_complete.addRobotFromSDF(terrainSDF, initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6));
 elseif(strcmp(world_name, 'plug'))
   options_cyl.floating = true;
-  r_complete = r_complete.addRobotFromURDF('plug_frame.urdf', [1.0 ; 0.0 ; 0.0]);
-  r_complete = r_complete.addRobotFromURDF('table.urdf', [0; -1.5; 0.5]);
-  r_complete = r_complete.addRobotFromURDF('big_plug.urdf', [-0.2; -1.2; 1.2], [], options_cyl);
-  r_complete = r_complete.addRobotFromURDF('small_plug.urdf', [0.2; -1.2; 1.2], [], options_cyl);
+  r_complete = r_complete.addRobotFromURDF('plug_frame.urdf', [1.0 ; 0.0 ; 0.0] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6));
+  r_complete = r_complete.addRobotFromURDF('table.urdf', [0; -1.5; 0.5] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6));
+  r_complete = r_complete.addRobotFromURDF('big_plug.urdf', [-0.2; -1.2; 1.2] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6), options_cyl);
+  r_complete = r_complete.addRobotFromURDF('small_plug.urdf', [0.2; -1.2; 1.2] + initial_offset_xyzrpy(1:3), initial_offset_xyzrpy(4:6), options_cyl);
 elseif(strcmp(world_name, 'stairs'))
-  r_complete = r_complete.addRobotFromURDF('stairs.urdf', [1.5 ; 0.0; 0.0], [0 ; 0 ; pi]);
+  r_complete = r_complete.addRobotFromURDF('stairs.urdf', [1.5 ; 0.0; 0.0] + initial_offset_xyzrpy(1:3), [0 ; 0 ; pi] + initial_offset_xyzrpy(4:6));
 elseif(strcmp(world_name, 'runningboard'))  
-  r_complete = r_complete.addRobotFromURDF('springboard.urdf', [0.0 ; 0.0; 0.1], [0 ; 0 ; pi]);
+  r_complete = r_complete.addRobotFromURDF('springboard.urdf', [0.0 ; 0.0; 0.1] + initial_offset_xyzrpy(1:3), [0 ; 0 ; pi] + initial_offset_xyzrpy(4:6));
   extra_height = 0.12;
 elseif(strcmp(world_name,'box'))
-  box = RigidBodyBox([1;2;box_height;]);
+  box = RigidBodyBox([1;2;box_height;] + initial_offset_xyzrpy(1:3));
   r_complete = r_complete.addCollisionGeometryToBody(1,box);
   r_complete = r_complete.addVisualGeometryToBody(1,box);
   handle = addpathTemporary([getenv('DRC_BASE'),'/software/control/matlab/planners/prone']);
@@ -137,12 +139,14 @@ xstar(1) = 0;
 xstar(2) = 0;
 xstar(3) = xstar(3) + 0.08 + extra_height ;
 xstar(6) = 0;
+xstar(1:6) = xstar(1:6) + initial_offset_xyzrpy;
 x0 = zeros(r_pure.getNumStates, 1);
 x0(1:length(xstar)) = xstar;
 r_pure = r_pure.setInitialState(x0);
 
 % load the correct initial state in the box world
 if strcmp(world_name,'box')
+  valuecheck(initial_offset_xyzrpy, zeros(6,1));
   % load the correct fixed point file
   handle = addpathTemporary([getenv('DRC_BASE'),'/software/control/matlab/planners/chair_standup']);
   if atlas_version == 4
