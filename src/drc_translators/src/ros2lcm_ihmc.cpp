6301e932d5f47611eb8caa06e2e9ac8f58cade95
Maurice Fallon <mfallon@mit.edu>
2015-04-24 15:51:59 +0100

    moving pronto scs to use MICROSTRAIN_INS located at correct imu frame
    paired with SCS version 29878

--- src/drc_translators/src/ros2lcm_ihmc.cpp
+++ src/drc_translators/src/ros2lcm_ihmc.cpp
@@ -27,9 +27,10 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Int32.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Imu.h>
 
-#include <trooper_mlc_msgs/CachedRawIMUData.h>
-#include <trooper_mlc_msgs/RawIMUData.h>
+#include <ihmc_msgs/BatchRawImuData.h>
+#include <ihmc_msgs/RawImuData.h>
 //#include <pronto_msgs/CachedRawIMUData.h>
 //#include <pronto_msgs/RawIMUData.h>
 //#include <pronto_msgs/FootSensor.h>
@@ -42,6 +43,7 @@
 #include "lcmtypes/pronto/atlas_raw_imu_batch_t.hpp"
 #include "lcmtypes/pronto/atlas_behavior_t.hpp"
 #include "lcmtypes/pronto/multisense_state_t.hpp"
+#include "lcmtypes/mav/ins_t.hpp"
 
 #include <tf/transform_listener.h>
 
@@ -61,7 +63,7 @@ struct Joints {
 
 class App{
 public:
-  App(ros::NodeHandle node_, int mode_, std::string robotName_);
+  App(ros::NodeHandle node_, int mode_, std::string robotName_, std::string imuSensor_);
   ~App();
 
 private:
@@ -69,6 +71,7 @@ private:
   ros::NodeHandle node_;
   int mode_;
   string robotName_;
+  string imuSensor_;
 
 //  tf::TransformListener listener_;
   
@@ -77,6 +80,7 @@ private:
   ros::Subscriber poseSub_;
   ros::Subscriber laserScanSub_;
   ros::Subscriber imuBatchSub_;
+  ros::Subscriber imuSensorSub_;
   ros::Subscriber leftFootSensorSub_;
   ros::Subscriber rightFootSensorSub_;
   ros::Subscriber behaviorSub_;
@@ -85,7 +89,9 @@ private:
   void headJointStatesCallback(const sensor_msgs::JointStateConstPtr& msg);
   void poseCallBack(const nav_msgs::OdometryConstPtr& msg);
   void laserScanCallback(const sensor_msgs::LaserScanConstPtr& msg);
-  void imuBatchCallback(const trooper_mlc_msgs::CachedRawIMUDataConstPtr& msg);
+  void imuBatchCallback(const ihmc_msgs::BatchRawImuDataConstPtr& msg);
+  void imuSensorCallback(const sensor_msgs::ImuConstPtr& msg);
+
   void leftFootSensorCallback(const geometry_msgs::WrenchConstPtr& msg);
   void rightFootSensorCallback(const geometry_msgs::WrenchConstPtr& msg);
   void behaviorCallback(const std_msgs::Int32ConstPtr& msg);
@@ -102,8 +108,8 @@ private:
   bool verbose_;
 };
 
-App::App(ros::NodeHandle node_, int mode_, std::string robotName_) :
-    node_(node_), mode_(mode_), robotName_(robotName_){
+App::App(ros::NodeHandle node_, int mode_, std::string robotName_, std::string imuSensor_) :
+    node_(node_), mode_(mode_), robotName_(robotName_), imuSensor_(imuSensor_){
   ROS_INFO("Initializing Translator");
   if(!lcmPublish_.good()){
     std::cerr <<"ERROR: lcm is not good()" <<std::endl;
@@ -126,7 +132,10 @@ App::App(ros::NodeHandle node_, int mode_, std::string robotName_) :
   // Robot joint angles
   jointStatesSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/joint_states"), queue_size, &App::jointStatesCallback,this);
   poseSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/robot_pose"), queue_size, &App::poseCallBack,this);
-  imuBatchSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/batch_raw_imu"), queue_size, &App::imuBatchCallback,this);
+
+  imuBatchSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/imu/" + imuSensor_ + "_batch"), queue_size, &App::imuBatchCallback,this);
+  imuSensorSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/imu/" + imuSensor_), queue_size, &App::imuSensorCallback,this);
+
   leftFootSensorSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/foot_force_sensor/left"), queue_size, &App::leftFootSensorCallback,this);
   rightFootSensorSub_ = node_.subscribe(string("/ihmc_ros/" + robotName_ + "/output/foot_force_sensor/right"), queue_size, &App::rightFootSensorCallback,this);
   // using previously used queue_size for scan:
@@ -229,7 +238,7 @@ void App::behaviorCallback(const std_msgs::Int32ConstPtr& msg){
 }
 
 
-void App::imuBatchCallback(const trooper_mlc_msgs::CachedRawIMUDataConstPtr& msg){
+void App::imuBatchCallback(const ihmc_msgs::BatchRawImuDataConstPtr& msg){
 
   pronto::atlas_raw_imu_batch_t imu;
   imu.utime = (int64_t) floor(msg->header.stamp.toNSec()/1000);
@@ -260,6 +269,32 @@ void App::imuBatchCallback(const trooper_mlc_msgs::CachedRawIMUDataConstPtr& msg
   }
   lcmPublish_.publish( ("ATLAS_IMU_BATCH") , &imu);
 }
+
+
+void App::imuSensorCallback(const sensor_msgs::ImuConstPtr& msg){
+
+  mav::ins_t imu;
+  imu.utime = (int64_t) floor(msg->header.stamp.toNSec()/1000);
+  imu.device_time = imu.utime;
+  imu.gyro[0] = msg->angular_velocity.x;
+  imu.gyro[1] = msg->angular_velocity.y;
+  imu.gyro[2] = msg->angular_velocity.z;
+  imu.mag[0] = 0;
+  imu.mag[1] = 0;
+  imu.mag[2] = 0;
+  imu.accel[0] = msg->linear_acceleration.x;
+  imu.accel[1] = msg->linear_acceleration.y;
+  imu.accel[2] = msg->linear_acceleration.z;
+  imu.quat[0] = msg->orientation.w;
+  imu.quat[1] = msg->orientation.x;
+  imu.quat[2] = msg->orientation.y;
+  imu.quat[2] = msg->orientation.z;
+  imu.pressure = 0;
+  imu.rel_alt = 0;
+
+  lcmPublish_.publish( ("MICROSTRAIN_INS") , &imu);
+}
+
 
 int scan_counter=0;
 void App::laserScanCallback(const sensor_msgs::LaserScanConstPtr& msg){
@@ -487,12 +522,15 @@ void App::appendFootSensors(pronto::force_torque_t& msg_out, geometry_msgs::Wren
 int main(int argc, char **argv){
   std::string robotName;// = "valkyrie"; // "atlas"
   std::string modeArgument;
+  std::string imuSensor = "pelvis_imu_sensor_at_pelvis_frame"; // pelvis_imu_sensor_at_imu_frame
 
-  if (argc >= 3){
+
+  if (argc >= 4){
      modeArgument = argv[1];
      robotName = argv[2];
+     imuSensor = argv[3];
   }else {
-    ROS_ERROR("Need to have two arguments: mode and robot name");
+    ROS_ERROR("Need to have three arguments: mode, robotName imuSensor");
     exit(-1);
   }
 
@@ -508,8 +546,8 @@ int main(int argc, char **argv){
 
   ros::init(argc, argv, "ros2lcm");
   ros::NodeHandle nh;
-  new App(nh, mode, robotName);
-  ROS_ERROR("ROS2LCM Translator Ready [mode: %d, %s] [robotName: %s]", mode, modeArgument.c_str(), robotName.c_str());
+  new App(nh, mode, robotName, imuSensor);
+  ROS_ERROR("ROS2LCM Translator Ready [mode: %d, %s] [robotName: %s] [imuSensor: %s]", mode, modeArgument.c_str(), robotName.c_str(), imuSensor.c_str());
   ros::spin();
   return 0;
 }
