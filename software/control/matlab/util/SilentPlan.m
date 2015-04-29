1c7dad2c0447e48244b5d99d7f825bce140dbd6f
Robin Deits <robin.deits@gmail.com>
2015-04-28 20:05:28 -0400

    fix silentPlan for new interface

--- software/control/matlab/util/SilentPlan.m
+++ software/control/matlab/util/SilentPlan.m
@@ -1,13 +1,13 @@
-classdef SilentPlan < QPControllerPlan
+classdef SilentPlan < QPControllerPlanMatlabImplementation
   methods
     function obj = SilentPlan(robot)
-      obj.default_qp_input = atlasControllers.QPInputConstantHeight();
-      obj.default_qp_input.be_silent = true;
-      obj.default_qp_input.whole_body_data.q_des = zeros(robot.getNumPositions(), 1);
+      obj.default_qp_input_ = atlasControllers.QPInputConstantHeight();
+      obj.default_qp_input_.be_silent = true;
+      obj.default_qp_input_.whole_body_data.q_des = zeros(robot.getNumPositions(), 1);
     end
 
     function qp_input = getQPControllerInput(obj, varargin)
-      qp_input = obj.default_qp_input;
+      qp_input = obj.default_qp_input();
     end
   end
 end
