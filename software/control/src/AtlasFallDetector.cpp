12de12487651ed94c893d4fd28fed7cfe0757b1f
Robin Deits <robin.deits@gmail.com>
2015-05-04 13:12:34 -0400

    bug fixes

--- software/control/src/AtlasFallDetector.cpp
+++ software/control/src/AtlasFallDetector.cpp
@@ -8,15 +8,19 @@ AtlasFallDetector::AtlasFallDetector(std::shared_ptr<RigidBodyManipulator> model
   std::vector<std::string> state_coordinate_names;
   int num_states = model->num_positions + model->num_velocities;
   state_coordinate_names.reserve(num_states);
+  this->robot_state.q.resize(num_states);
+  this->robot_state.qd.resize(num_states);
 
   for (int i=0; i < num_states; ++i) {
     state_coordinate_names.push_back(model->getStateName(i));
-    // std::cout << state_coordinate_names[i] << std::endl;
   }
 
   this->state_driver.reset(new RobotStateDriver(state_coordinate_names));
 
   this->findFootIDS();
+
+  this->foot_contact[RIGHT] = false;
+  this->foot_contact[LEFT] = false;
 
   this->debounce.reset(new Debounce());
   this->debounce->t_low_to_high = 0.0;
@@ -58,16 +62,28 @@ void AtlasFallDetector::handleRobotState(const lcm::ReceiveBuffer* rbuf,
                        const std::string& chan,
                        const drc::robot_state_t* msg) {
   this->state_driver->decode(msg, &(this->robot_state));
-  this->model->doKinematics(robot_state.q);
+  this->model->doKinematicsNew(robot_state.q, robot_state.qd);
   bool icp_is_ok = this->debounce->update(this->robot_state.t, this->isICPCaptured());
 
-  drc::atlas_fall_detector_status_t fall_msg;
-  fall_msg.utime = static_cast<int64_t> (robot_state.t * 1e6);
-  fall_msg.falling = ~icp_is_ok;
-  Vector2d icp = this->getICP();
-  fall_msg.icp[0] = icp(0);
-  fall_msg.icp[1] = icp(1);
-  fall_msg.icp[2] = this->getSupportFootHeight();
+  if (this->controller_is_active) {
+    drc::atlas_fall_detector_status_t fall_msg;
+    fall_msg.utime = static_cast<int64_t> (robot_state.t * 1e6);
+    fall_msg.falling = !icp_is_ok;
+    Vector2d icp = this->getICP();
+    fall_msg.icp[0] = icp(0);
+    fall_msg.icp[1] = icp(1);
+    fall_msg.icp[2] = this->getSupportFootHeight();
+    fall_msg.measured_cop[0] = NAN;
+    fall_msg.measured_cop[1] = NAN;
+    fall_msg.measured_cop[2] = NAN;
+    this->lcm.publish("ATLAS_FALL_STATE", &fall_msg);
+
+    if (!icp_is_ok) {
+      drc::utime_t trigger_msg;
+      trigger_msg.utime = static_cast<int64_t> (robot_state.t * 1e6);
+      this->lcm.publish("RECOVERY_TRIGGER_ON", &trigger_msg);
+    }
+  }
 }
 
 void AtlasFallDetector::handleControllerStatus(const lcm::ReceiveBuffer* rbuf,
@@ -91,7 +107,11 @@ double AtlasFallDetector::getSupportFootHeight() {
   for (std::map<FootID, int>::iterator foot = this->foot_body_ids.begin(); foot != foot_body_ids.end(); ++foot) {
     Matrix3Xd contact_pts;
     this->model->getTerrainContactPoints(*this->model->bodies[foot->second], contact_pts);
-    sole_zs[foot->first] = contact_pts.row(2).mean();
+    Matrix3Xd contact_pts_in_world = this->model->forwardKinNew(contact_pts, foot->second, 0, 0, 0).value();
+    // std::set<int> body_ids = {foot->second};
+    // Matrix3Xd contact_pts_in_world(3, this->model->bodies[foot->second]->contact_pts.cols());
+    // this->model->getContactPositions(contact_pts_in_world, body_ids);
+    sole_zs[foot->first] = contact_pts_in_world.row(2).mean();
   }
   if ((this->foot_contact.at(RIGHT) && this->foot_contact.at(LEFT)) || (!this->foot_contact.at(RIGHT) && !this->foot_contact.at(LEFT))) {
     return std::min(sole_zs.at(RIGHT), sole_zs.at(LEFT));
@@ -108,19 +128,23 @@ Matrix3Xd AtlasFallDetector::getVirtualSupportPolygon () {
   Matrix3Xd all_contact_pts(3, 0);
 
   for (std::map<FootID, int>::iterator foot = this->foot_body_ids.begin(); foot != foot_body_ids.end(); ++foot) {
+    // std::set<int> body_ids = {foot->second};
+    // Matrix3Xd contact_pts_in_world(3, this->model->bodies[foot->second]->contact_pts.cols());
+    // this->model->getContactPositions(contact_pts_in_world, body_ids);
+
     Matrix3Xd contact_pts;
     this->model->getTerrainContactPoints(*this->model->bodies[foot->second], contact_pts);
+    Matrix3Xd contact_pts_in_world = this->model->forwardKinNew(contact_pts, foot->second, 0, 0, 0).value();
     if (!this->foot_contact.at(foot->first)) {
       // If foot is out of contact, only use its center to define the support polygon
       for (int i=0; i < 3; ++i) {
-        contact_pts(i, 0) = contact_pts.row(i).mean();
+        contact_pts_in_world(i, 0) = contact_pts_in_world.row(i).mean();
       }
-      contact_pts.conservativeResize(3, 1);
+      contact_pts_in_world.conservativeResize(3, 1);
     }
-    all_contact_pts.conservativeResize(3, all_contact_pts.cols() + contact_pts.cols());
-    all_contact_pts.block(0, all_contact_pts.cols() - contact_pts.cols(), 3, contact_pts.cols()) = contact_pts;
+    all_contact_pts.conservativeResize(3, all_contact_pts.cols() + contact_pts_in_world.cols());
+    all_contact_pts.block(0, all_contact_pts.cols() - contact_pts_in_world.cols(), 3, contact_pts_in_world.cols()) = contact_pts_in_world;
   }
-
   return all_contact_pts;
 }
 
@@ -132,7 +156,6 @@ bool AtlasFallDetector::isICPCaptured() {
 
 int main(int argc, char** argv) {
   const char* drc_path = std::getenv("DRC_BASE");
-  std::cout << "drc path: " << std::endl;
   if (!drc_path) {
     throw std::runtime_error("environment variable DRC_BASE is not set");
   }
--- software/control/src/convexHull.cpp
+++ software/control/src/convexHull.cpp
@@ -15,19 +15,28 @@ using namespace Eigen;
 // negative for clockwise turn, and zero if the points are collinear.
 coord2_t cross(const Point &O, const Point &A, const Point &B)
 {
-  return (long)(A.x - O.x) * (B.y - O.y) - (long)(A.y - O.y) * (B.x - O.x);
+  return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
 }
  
 // Returns a list of points on the convex hull in counter-clockwise order.
 // Note: the last point in the returned list is the same as the first one.
 vector<Point> convex_hull(vector<Point> P)
 {
+
   int n = P.size(), k = 0;
   vector<Point> H(2*n);
+
+  if (n == 2) {
+    H.reserve(3);
+    H.push_back(P[0]); 
+    H.push_back(P[1]); 
+    H.push_back(P[0]); 
+    return H;
+  }
  
   // Sort points lexicographically
   sort(P.begin(), P.end());
- 
+
   // Build lower hull
   for (int i = 0; i < n; ++i) {
     while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
