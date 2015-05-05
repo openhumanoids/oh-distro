babe3ec5753cfe8bd1507786a4b3205a2e843ead
Robin Deits <rdeits@csail.mit.edu>
2015-05-04 23:00:08 -0400

    fix package name

--- software/control/matlab/util/SilentPlan.m
+++ software/control/matlab/util/SilentPlan.m
@@ -1,7 +1,7 @@
 classdef SilentPlan < QPControllerPlanMatlabImplementation
   methods
     function obj = SilentPlan(robot)
-      obj.default_qp_input_ = QPInputConstantHeight();
+      obj.default_qp_input_ = atlasControllers.QPInputConstantHeight();
       obj.default_qp_input_.be_silent = true;
       obj.default_qp_input_.whole_body_data.q_des = zeros(robot.getNumPositions(), 1);
     end
