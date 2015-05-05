9e25343d87173f4eafcef7fb2c01f0d085d75905
Robin Deits <robin.deits@gmail.com>
2015-05-04 22:18:49 -0400

    more silent plan fixes

--- software/control/matlab/util/SilentPlan.m
+++ software/control/matlab/util/SilentPlan.m
@@ -1,13 +1,13 @@
 classdef SilentPlan < QPControllerPlanMatlabImplementation
   methods
     function obj = SilentPlan(robot)
-      obj.default_qp_input_ = drake.lcmt_qp_controller_input();
+      obj.default_qp_input_ = QPInputConstantHeight();
       obj.default_qp_input_.be_silent = true;
       obj.default_qp_input_.whole_body_data.q_des = zeros(robot.getNumPositions(), 1);
     end
 
     function qp_input = getQPControllerInput(obj, varargin)
-      qp_input = obj.default_qp_input();
+      qp_input = obj.default_qp_input().to_lcm();
     end
   end
 end
