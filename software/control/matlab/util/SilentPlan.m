9ef9a3e0446a3a697660454f97a99e0d0ed80e06
Robin Deits <robin.deits@gmail.com>
2015-05-06 13:25:51 -0400

    silentPlan doesn't need a robot

--- software/control/matlab/util/SilentPlan.m
+++ software/control/matlab/util/SilentPlan.m
@@ -1,13 +1,13 @@
 classdef SilentPlan < QPControllerPlanMatlabImplementation
   methods
-    function obj = SilentPlan(robot)
+    function obj = SilentPlan()
       obj.default_qp_input_ = atlasControllers.QPInputConstantHeight();
       obj.default_qp_input_.be_silent = true;
-      obj.default_qp_input_.whole_body_data.q_des = zeros(robot.getNumPositions(), 1);
+      obj.default_qp_input_ = obj.default_qp_input_.to_lcm();
     end
 
     function qp_input = getQPControllerInput(obj, varargin)
-      qp_input = obj.default_qp_input().to_lcm();
+      qp_input = obj.default_qp_input_;
     end
   end
 end
