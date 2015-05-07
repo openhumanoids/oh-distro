1aa6de6b9ef6461d78ce0b0297cecffc3f587f08
Twan Koolen <tkoolen@csail.mit.edu>
2015-05-06 23:21:49 -0400

    turned add_mex into add_library to fix compile error

--- software/control/src/AtlasFallDetector.cpp
+++ software/control/src/AtlasFallDetector.cpp
@@ -167,17 +167,3 @@ bool AtlasFallDetector::isICPCaptured() {
   return in_convex_hull(support_pts.topRows(2), icp);
 }
 
-int main(int argc, char** argv) {
-  const char* drc_path = std::getenv("DRC_BASE");
-  if (!drc_path) {
-    throw std::runtime_error("environment variable DRC_BASE is not set");
-  }
-  std::shared_ptr<RigidBodyManipulator> model(new RigidBodyManipulator(std::string(drc_path) + "/software/models/atlas_v5/model_minimal_contact.urdf"));
-  model->setUseNewKinsol(true);
-  model->compile();
-
-  std::unique_ptr<AtlasFallDetector> fall_detector(new AtlasFallDetector(model));
-  std::cout << "Atlas fall detector running" << std::endl;
-  fall_detector->run();
-  return 0;
-}
