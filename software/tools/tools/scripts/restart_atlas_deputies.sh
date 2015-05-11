c30238e0bae209597cc2e7243faed324e0e973ef
Mathew Antone <antone@csail.mit.edu>
2015-05-11 17:11:23 -0400

    moved time sync into restart deputies script

--- software/tools/tools/scripts/restart_atlas_deputies.sh
+++ software/tools/tools/scripts/restart_atlas_deputies.sh
@@ -3,3 +3,10 @@
 ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
 ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
 ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
+echo deputies complete
+
+# time sync (atlas0 syncs to server, atlas1+2 sync to atlas0)
+ssh atlas0 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas1 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas2 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+echo time sync complete
