baf85f8c748762e6efc5ec5145c7b77cdf70ad82
Matthew Antone <antone@csail.mit.edu>
2015-05-11 15:23:03 -0400

    initial script to synchronize clocks upon daemon setup

--- software/tools/tools/scripts/restart_atlas_all_daemons.sh
+++ software/tools/tools/scripts/restart_atlas_all_daemons.sh
@@ -10,3 +10,9 @@ screen -D -m -S shaper-link3 drc-network-shaper -r base -c drc_robot.cfg  -i lin
 
 until pids=$(pidof drc-network-shaper); do sleep 0.1; done
 echo started network shaper on `hostname`
+
+# time sync (atlas0 syncs to server, atlas1+2 sync to atlas0)
+ssh atlas0 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas1 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas2 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+echo time sync complete
