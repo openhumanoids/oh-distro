85f0d3b6cbb44bf2a4477383bd5c3298fbd1ea51
Wolfgang Merkt <w.merkt@gmail.com>
2015-05-14 19:15:15 +0100

    Updated state-sync-kuka to fix hand in drake-designer

--- software/ipab/tools/src/state_sync_kuka/state-sync-kuka.cpp
+++ software/ipab/tools/src/state_sync_kuka/state-sync-kuka.cpp
@@ -54,10 +54,10 @@ App::App(boost::shared_ptr<lcm::LCM> &lcm_) : lcm_(lcm_) {
   kukaState_.joint_effort.assign(7 , 0  );
   kukaState_.num_joints = 7;
 
-  sdhState_.joint_position.assign(7 , 0  );
-  sdhState_.joint_velocity.assign(7 , 0  );
-  sdhState_.joint_effort.assign(7 , 0  );
-  sdhState_.num_joints = 7;
+  sdhState_.joint_position.assign(8 , 0  );
+  sdhState_.joint_velocity.assign(8 , 0  );
+  sdhState_.joint_effort.assign(8 , 0  );
+  sdhState_.num_joints = 8;
 };
 
 App::~App()  {
@@ -87,26 +87,26 @@ void App::broadcastRobotState() {
   msg_out.utime =  bot_timestamp_now();
   msg_out.pose.translation.x = 0;  msg_out.pose.translation.y = 0;  msg_out.pose.translation.z = 0;
   msg_out.pose.rotation.w = 1;  msg_out.pose.rotation.x = 0;  msg_out.pose.rotation.y = 0;  msg_out.pose.rotation.z = 0;
-  int n_joints = 14;
+  int n_joints = 15;
   msg_out.joint_position.assign(n_joints , 0  );
   msg_out.joint_velocity.assign(n_joints , 0  );
   msg_out.joint_effort.assign(n_joints , 0  );
   msg_out.num_joints = n_joints;
 
-  msg_out.joint_name = {"lwr_arm_0_joint", "lwr_arm_1_joint", "lwr_arm_2_joint", "lwr_arm_3_joint", "lwr_arm_4_joint", "lwr_arm_5_joint", "lwr_arm_6_joint", "sdh_knuckle_joint", "sdh_thumb_2_joint", "sdh_thumb_3_joint", "sdh_finger_12_joint", "sdh_finger_13_joint", "sdh_finger_22_joint", "sdh_finger_23_joint"};
+  msg_out.joint_name = {"lwr_arm_0_joint", "lwr_arm_1_joint", "lwr_arm_2_joint", "lwr_arm_3_joint", "lwr_arm_4_joint", "lwr_arm_5_joint", "lwr_arm_6_joint", "sdh_knuckle_joint", "sdh_thumb_2_joint", "sdh_thumb_3_joint", "sdh_finger_12_joint", "sdh_finger_13_joint", "sdh_finger_22_joint", "sdh_finger_23_joint", "sdh_finger_21_joint"};
 
-  // Add kukaState_
+  // Add individual joint state messages
   int kuka_joints = 7; //kukaState_->joint_position.size();
-  int sdh_joints = 7; //sdhState_->joint_position.size();
+  int sdh_joints = 8; //sdhState_->joint_position.size();
 
   mtx_.lock();
-  for (int i = 0; i < kukaState_.joint_position.size(); i++)  {
+  for (int i = 0; i < kuka_joints; i++)  {
     msg_out.joint_position[ i ] = kukaState_.joint_position[ i ];
     msg_out.joint_velocity[ i ] = kukaState_.joint_velocity[ i ];
     msg_out.joint_effort[ i ] = kukaState_.joint_effort[ i ];
   }
 
-  for (int j = 0; j < 7; j++)  {
+  for (int j = 0; j < sdh_joints; j++)  {
     msg_out.joint_position[ j+7 ] = sdhState_.joint_position[ j ];
     msg_out.joint_velocity[ j+7 ] = sdhState_.joint_velocity[ j ];
     msg_out.joint_effort[ j+7 ] = sdhState_.joint_effort[ j ];
