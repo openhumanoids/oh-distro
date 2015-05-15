9a8e05796c67fea8428abb91cd7a93d5232fafae
Robin Deits <robin.deits@gmail.com>
2015-05-15 11:05:56 -0400

    use DRC convexHull library

--- software/control/src/AtlasFallDetector.cpp
+++ software/control/src/AtlasFallDetector.cpp
@@ -198,7 +198,7 @@ Matrix3Xd AtlasFallDetector::getVirtualSupportPolygon () {
 
 bool AtlasFallDetector::isICPCaptured(Vector2d icp) {
   Matrix3Xd support_pts = this->getVirtualSupportPolygon();
-  return inConvexHull(support_pts.topRows<2>(), icp);
+  return in_convex_hull(support_pts.topRows<2>(), icp);
 }
 
 bool AtlasFallDetector::isICPCapturable(Vector2d icp) {
