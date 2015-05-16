f0e3db683ad4281ee5fe7fa3d108ac7d85301d48
manuelli <lucas.t.manuelli@gmail.com>
2015-05-16 13:19:13 -0400

    code cleanup

--- software/control/matlab/+drcAtlasParams/PositionControlSim.m
+++ software/control/matlab/+drcAtlasParams/PositionControlSim.m
@@ -11,17 +11,15 @@ classdef PositionControlSim < atlasParams.Base
       % integral gains for position controlled joints
       integral_gains = zeros(getNumPositions(r),1);
       integral_clamps = zeros(getNumPositions(r),1);
-      arm_ind = findPositionIndices(r,'arm');
+      upper_arm_ind = [findPositionIndices(r,'shz'); findPositionIndices(r,'shx'); findPositionIndices(r,'ely'); findPositionIndices(r,'elx')];
       back_ind = findPositionIndices(r,'back');
       leg_ind = findPositionIndices(r,'leg');
       electric_ind = [findPositionIndices(r,'uwy'); findPositionIndices(r,'mwx'); findPositionIndices(r,'lwy')];
-      integral_gains(arm_ind) = 1.75; % TODO: generalize this
-      integral_gains(electric_ind) = 0;
+      integral_gains(upper_arm_ind) = 1.75; % TODO: generalize this
       integral_gains(back_ind) = 0.3;
       integral_gains(leg_ind) = 0.3;
 
-      integral_clamps(arm_ind) = 0.2;
-      integral_clamps(electric_ind) = 0;
+      integral_clamps(upper_arm_ind) = 0.2;
       integral_clamps(back_ind) = 0.2;
       integral_clamps(leg_ind) = 0.1;
 
