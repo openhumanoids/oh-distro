8de2952579039d51de436dd582f1ea0c85578ecc
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-05-21 17:59:22 +0100

    EXOTica-DrakeDesigner bug fixes  - runIK sends request over LCM and awaits replay over LCM  - runIKtraj does the same  - ikPlanner.addPose publishes the pose over LCM  - ikPlanner publishes joint names on initialisation (exotica node has to be running at this point)

--- catkin_ws/drc_translators/src/drc_translators/src/lcm2ros_kuka.cpp
+++ catkin_ws/drc_translators/src/drc_translators/src/lcm2ros_kuka.cpp
@@ -12,9 +12,11 @@ http://docs.ros.org/indigo/api/sensor_msgs/html/msg/JointState.html
 #include "lcmtypes/drc/robot_plan_t.hpp"
 #include "lcmtypes/drc/plan_control_t.hpp"
 #include "lcmtypes/drc/planner_request_t.hpp"
+#include "lcmtypes/drc/robot_state_t.hpp"
 #include <trajectory_msgs/JointTrajectory.h>
 #include <ipab_msgs/PlannerRequest.h>
 #include <std_srvs/Empty.h>
+#include <std_msgs/String.h>
 
 using namespace std;
 
@@ -36,6 +38,15 @@ class LCM2ROS{
     void robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::plan_control_t* msg);
     ros::ServiceClient robot_plan_pause_service_;
     ros::Publisher ik_request_pub_;
+    ros::Publisher poses_pub_;
+    ros::Publisher joint_names_pub_;
+
+    void robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg);
+    void plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg);
+
 
 };
 
@@ -44,14 +55,20 @@ LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(
   robot_plan_pub_ = nh_.advertise<trajectory_msgs::JointTrajectory>("/kuka/robot_plan",10);
 
   lcm_->subscribe("PLANNER_REQUEST",&LCM2ROS::plannerRequestHandler, this);
-  planner_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/planner_request",10);
+  planner_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_request",10);
 
   lcm_->subscribe("COMMITTED_PLAN_PAUSE",&LCM2ROS::robotPlanPauseHandler, this);
   robot_plan_pause_service_ = nh_.serviceClient<std_srvs::Empty>("stop_trajectory_execution");
   
   rosnode = new ros::NodeHandle();
   lcm_->subscribe("IK_REQUEST",&LCM2ROS::ikRequestHandler, this);
-  ik_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/ik_request",10);
+  ik_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/ik_request",10);
+
+  lcm_->subscribe("PLANNER_SETUP_POSES",&LCM2ROS::posesHandler, this);
+  poses_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_poses",10);
+
+  lcm_->subscribe("PLANNER_SETUP_JOINT_NAMES",&LCM2ROS::jointNamesHandler, this);
+  joint_names_pub_ = nh_.advertise<std_msgs::String>("/exotica/planner_joint_names",10);
 }
 
 void LCM2ROS::robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg) {
@@ -103,6 +120,25 @@ void LCM2ROS::ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string
   m.poses = msg->poses;
   m.constraints = msg->constraints;
   ik_request_pub_.publish(m);
+}
+
+void LCM2ROS::posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+  ROS_ERROR("LCM2ROS got PLANNER_SETUP_POSES");
+  ipab_msgs::PlannerRequest m;
+  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
+  m.poses = msg->poses;
+  m.constraints = msg->constraints;
+  poses_pub_.publish(m);
+}
+
+void LCM2ROS::jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg) {
+  ROS_ERROR("LCM2ROS got PLANNER_SETUP_JOINT_NAMES");
+  std_msgs::String m;
+  m.data="[";
+  bool first=true;
+  for(auto& s : msg->joint_name) {m.data += (first?"\"":", \"")+s+"\"";first=false;}
+  m.data += "]";
+  joint_names_pub_.publish(m);
 }
 
 int main(int argc,char** argv) {
