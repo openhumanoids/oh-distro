6684cf71c164cba6b0067e7b3091810f4bf0dc10
Andrés Valenzuela <avalenzu@mit.edu>
2015-05-23 08:26:36 -0400

    Fix typos in concatAndRescaleTrajectories

--- software/control/matlab/util/concatAndRescaleTrajectories.m
+++ software/control/matlab/util/concatAndRescaleTrajectories.m
@@ -2,11 +2,12 @@ function [qtraj_rescaled, traj_breaks_rescaled] = concatAndRescaleTrajectories(q
     qtraj = qtraj_cell{1};
     traj_breaks = qtraj.tspan();
 
-    for j = 2:N
+    for j = 2:numel(qtraj_cell)
       qtraj_cell{j} = qtraj_cell{j}.shiftTime(qtraj.tspan(2));
       qtraj = qtraj.append(qtraj_cell{j});
       traj_breaks(end+1) = qtraj.tspan(2);
     end
 
-    [qtraj_rescaled, traj_breaks_rescaled] = rescalePlanTimingGetOriginalTimes(qtraj,qd_max, traj_breaks, varargin);
+    [qtraj_rescaled, traj_breaks_rescaled] = rescalePlanTimingGetOriginalTimes(qtraj,qd_max, traj_breaks, varargin{:});
+    traj_breaks_rescaled = traj_breaks_rescaled';
 end
