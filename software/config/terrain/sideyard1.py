27774f39bda5f67a44984918791c75d58dced143
Robin Deits <robin.deits@gmail.com>
2015-05-23 16:26:34 -0400

    configure prefab footsteps for DRC terrain & our slice

--- software/config/terrain/sideyard1.py
+++ software/config/terrain/sideyard1.py
@@ -10,11 +10,12 @@ blockTiltAngle = 15 # degrees
 # last row is closest to robot (robot is on bottom looking up)
 # column order is left-to-right on robot (+y to -y)
 blockTypes = [
-    [ 'F', 'R' ],
-    [ 'L', 'B' ],
-    [ 'F', 'L' ],
+    [ 'B', 'L' ],
     [ 'L', 'F' ],
+    [ 'F', 'R' ],
     [ 'R', 'B' ],
+    [ 'B', 'L' ],
+    [ 'L', 'F' ],
     [ 'F', 'R' ]
 ]
 blockTypes.reverse()
@@ -22,10 +23,11 @@ blockTypes.reverse()
 # 0=ground level, 1=one cinderblock offset, etc
 blockLevels = [
     [ 0, 0 ],
-    [ 1, 0 ],
     [ 1, 1 ],
-    [ 1, 1 ],
-    [ 1, 1 ],
+    [ 1, 2 ],
+    [ 0, 1 ],
+    [ 0, 0 ],
+    [ 0, 0 ],
     [ 0, 0 ]
 ]
 blockLevels.reverse()
@@ -40,22 +42,24 @@ blockAngleMap = { 'F': 180, 'B': 0, 'R': 90, 'L': 270 }
 # support is an enum indicating foot support type
 #   0=heel-toe, 1=midfoot-toe, 2=heel-midfoot
 footstepData = [
-    [ 'left',  (0,0), (0.00, 0.00),  0 ],
-    [ 'right', (0,1), (0.00, 0.00),  0 ],
-    [ 'left',  (1,0), (0.00, 0.00),  0 ],
-    [ 'right', (1,1), (0.00, 0.00),  0 ],
-    [ 'left',  (2,0), (0.00, 0.00),  0 ],
-    [ 'right', (2,1), (0.00, 0.00),  0 ],
-    [ 'left',  (3,0), (0.00, 0.00),  0 ],
-    [ 'right', (3,1), (0.00, 0.00),  0 ],
-    [ 'left',  (4,0), (0.00, 0.00),  0 ],
-    [ 'right', (4,1), (0.00, 0.00),  0 ],
-    [ 'left',  (5,0), (0.00, 0.00),  0 ],
-    [ 'right', (5,1), (0.00, 0.00),  0 ]
+    [ 'right', (0,1), (-0.05, 0.05),  0 ],
+    [ 'left',  (0,0), (0.06, -0.12),  0 ],
+    [ 'right', (1,1), (-0.01, 0.11),  0 ],
+    [ 'left',  (1,0), (0.03, -0.06),  0 ],
+    [ 'right', (2,1), (-0.04, 0.11),  0 ],
+    [ 'left',  (2,0), (0.03, -0.08),  0 ],
+    [ 'right', (3,1), (-0.06, 0.12),  0 ],
+    [ 'left',  (4,0), (-0.05, -0.07),  0 ],
+    [ 'right', (4,1), (0.00, 0.08),  0 ],
+    [ 'left',  (4,0), (0.06, -0.07),  0 ],
+    [ 'right', (5,1), (-0.01, 0.12),  0 ],
+    [ 'left',  (5,0), (0.06, -0.07),  0 ],
+    [ 'right', (6,1), (-0.02, 0.12),  0 ],
+    [ 'left',  (6,0), (0.06, -0.07),  0 ],
 ]
 
 # where to stand, relative to front of first block
-startingPosition = np.array([-0.5, 0, 0])
+startingPosition = np.array([-0.39, 0, 0])
 startingYaw = 0   # degrees
 
 blockColor = [0.4, 0.6, 0.4]
