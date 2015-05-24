4fbb18ae8888bba7936b04580be8e65bc1fc0c34
Matthew Antone <antone@csail.mit.edu>
2015-05-24 16:37:46 -0400

    diabled pointwise association; allow forcing zero lateral footstep shift

--- software/config/terrain/stairs.py
+++ software/config/terrain/stairs.py
@@ -50,5 +50,8 @@ footstepData = [
 startingPosition = np.array([-0.34, 0, 0])
 startingYaw = 0   # degrees
 
+# whether to lock lateral footstep offset
+forceZeroLateralFootstepOffset = True
+
 blockColor = [0.4, 0.6, 0.4]
 blockColorMatched = [0.5, 0.8, 0.5]
