5cd19ffe4de75fb0cdedf396421dc27bb7974161
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-05-26 13:43:33 +0100

    Moved communication with EXOTica to exotica bridge node

--- catkin_ws/drc_translators/src/drc_translators/src/lcm2ros_exotica.cpp
+++ catkin_ws/drc_translators/src/drc_translators/src/lcm2ros_exotica.cpp
@@ -30,18 +30,41 @@ class LCM2ROS{
     ros::NodeHandle nh_;
     ros::NodeHandle* rosnode;
     ros::Publisher robot_plan_pub_;
+    ros::Publisher planner_request_pub_;
     ros::ServiceClient robot_plan_pause_service_;
+    ros::Publisher ik_request_pub_;
+    ros::Publisher poses_pub_;
+    ros::Publisher joint_names_pub_;
 
     void robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg);
+    void plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
+    void jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg);
     void robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::plan_control_t* msg);
+
+
 };
 
 LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(lcm_),nh_(nh_) {
   lcm_->subscribe("COMMITTED_ROBOT_PLAN",&LCM2ROS::robotPlanHandler, this);
   robot_plan_pub_ = nh_.advertise<trajectory_msgs::JointTrajectory>("/kuka/robot_plan",10);
 
+  lcm_->subscribe("PLANNER_REQUEST",&LCM2ROS::plannerRequestHandler, this);
+  planner_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_request",10);
+
   lcm_->subscribe("COMMITTED_PLAN_PAUSE",&LCM2ROS::robotPlanPauseHandler, this);
   robot_plan_pause_service_ = nh_.serviceClient<std_srvs::Empty>("stop_trajectory_execution");
+  
+  rosnode = new ros::NodeHandle();
+  lcm_->subscribe("IK_REQUEST",&LCM2ROS::ikRequestHandler, this);
+  ik_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/ik_request",10);
+
+  lcm_->subscribe("PLANNER_SETUP_POSES",&LCM2ROS::posesHandler, this);
+  poses_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_poses",10);
+
+  lcm_->subscribe("PLANNER_SETUP_JOINT_NAMES",&LCM2ROS::jointNamesHandler, this);
+  joint_names_pub_ = nh_.advertise<std_msgs::String>("/exotica/planner_joint_names",10);
 }
 
 void LCM2ROS::robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg) {
@@ -75,6 +98,43 @@ void LCM2ROS::robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::s
   } else {
     ROS_ERROR("Could not stop trajectory execution - DANGER");
   }
+}
+
+void LCM2ROS::plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+  ROS_ERROR("LCM2ROS got PLANNER_REQUEST");
+  ipab_msgs::PlannerRequest m;
+  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
+  m.poses = msg->poses;
+  m.constraints = msg->constraints;
+  planner_request_pub_.publish(m);
+}
+
+void LCM2ROS::ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+  ROS_ERROR("LCM2ROS got IK_REQUEST");
+  ipab_msgs::PlannerRequest m;
+  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
+  m.poses = msg->poses;
+  m.constraints = msg->constraints;
+  ik_request_pub_.publish(m);
+}
+
+void LCM2ROS::posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+  ROS_ERROR("LCM2ROS got PLANNER_SETUP_POSES");
+  ipab_msgs::PlannerRequest m;
+  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
+  m.poses = msg->poses;
+  m.constraints = msg->constraints;
+  poses_pub_.publish(m);
+}
+
+void LCM2ROS::jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg) {
+  ROS_ERROR("LCM2ROS got PLANNER_SETUP_JOINT_NAMES");
+  std_msgs::String m;
+  m.data="[";
+  bool first=true;
+  for(auto& s : msg->joint_name) {m.data += (first?"\"":", \"")+s+"\"";first=false;}
+  m.data += "]";
+  joint_names_pub_.publish(m);
 }
 
 int main(int argc,char** argv) {
--- catkin_ws/drc_translators/src/drc_translators/src/ros2lcm_kuka.cpp
+++ catkin_ws/drc_translators/src/drc_translators/src/ros2lcm_kuka.cpp
@@ -34,12 +34,6 @@ private:
   ros::Subscriber  joint_states_sub_;
   void joint_states_cb(const sensor_msgs::JointStateConstPtr& msg);
 
-  ros::Subscriber  traj_sub_;
-  void traj_cb(const trajectory_msgs::JointTrajectoryConstPtr& msg);
-
-  ros::Subscriber  ik_sub_;
-  void ik_cb(const trajectory_msgs::JointTrajectoryConstPtr& msg);
-
 };
 
 App::App(ros::NodeHandle node_) : node_(node_)
@@ -50,10 +44,6 @@ App::App(ros::NodeHandle node_) : node_(node_)
       ROS_ERROR_STREAM("lcm is not good()");
   }
   joint_states_sub_ = node_.subscribe(string("/joint_states"), 100, &App::joint_states_cb,this);
-
-  traj_sub_ = node_.subscribe(string("/exotica/robot_plan"), 100, &App::traj_cb,this);
-  ik_sub_ = node_.subscribe(string("/exotica/robot_ik"), 100, &App::ik_cb,this);
-
 }
 
 App::~App()  {
@@ -80,78 +70,6 @@ void App::joint_states_cb(const sensor_msgs::JointStateConstPtr& msg)
   }
 
   lcm_publish_.publish("KUKA_STATE", &msg_out);
-}
-
-void App::traj_cb(const trajectory_msgs::JointTrajectoryConstPtr& msg)
-{
-    drc::robot_plan_w_keyframes_t msg_out;
-    msg_out.utime = (int64_t) msg->header.stamp.toNSec()/1000; // from nsec to usec
-    msg_out.robot_name = std::string("lwr");
-    int T = msg->points.size();
-    msg_out.num_states = T;
-    msg_out.num_keyframes = 0;
-    msg_out.num_breakpoints = 0;
-    msg_out.is_keyframe.assign(T,false);
-    msg_out.is_breakpoint.assign(T,false);
-    msg_out.plan_info.assign(T,0);
-    msg_out.num_grasp_transitions = 0;
-    msg_out.num_bytes = 0;
-    msg_out.plan.resize(T);
-    for(int t=0;t<T;t++)
-    {
-        msg_out.plan[t].utime = msg->points[t].time_from_start.toNSec()/1000;
-        int n_joints = msg->joint_names.size();
-        msg_out.plan[t].joint_position.assign(n_joints , 0  );
-        msg_out.plan[t].joint_velocity.assign(n_joints , 0  );
-        msg_out.plan[t].joint_effort.assign(n_joints , 0  );
-        msg_out.plan[t].pose.rotation.w=1.0;
-        msg_out.plan[t].num_joints = n_joints;
-        msg_out.plan[t].joint_name= msg->joint_names;
-        for (int i = 0; i < n_joints; i++)
-        {
-          msg_out.plan[t].joint_position[ i ] = msg->points[t].positions[ i ];
-          // Ignoring velocity and effort for now
-        }
-    }
-
-
-    lcm_publish_.publish("CANDIDATE_MANIP_PLAN", &msg_out);
-}
-
-void App::ik_cb(const trajectory_msgs::JointTrajectoryConstPtr& msg)
-{
-    drc::robot_plan_w_keyframes_t msg_out;
-    msg_out.utime = (int64_t) msg->header.stamp.toNSec()/1000; // from nsec to usec
-    msg_out.robot_name = std::string("lwr");
-    int T = msg->points.size();
-    msg_out.num_states = T;
-    msg_out.num_keyframes = 0;
-    msg_out.num_breakpoints = 0;
-    msg_out.is_keyframe.assign(T,false);
-    msg_out.is_breakpoint.assign(T,false);
-    msg_out.plan_info.assign(T,0);
-    msg_out.num_grasp_transitions = 0;
-    msg_out.num_bytes = 0;
-    msg_out.plan.resize(T);
-    for(int t=0;t<T;t++)
-    {
-        msg_out.plan[t].utime = msg->points[t].time_from_start.toNSec()/1000;
-        int n_joints = msg->joint_names.size();
-        msg_out.plan[t].joint_position.assign(n_joints , 0  );
-        msg_out.plan[t].joint_velocity.assign(n_joints , 0  );
-        msg_out.plan[t].joint_effort.assign(n_joints , 0  );
-        msg_out.plan[t].pose.rotation.w=1.0;
-        msg_out.plan[t].num_joints = n_joints;
-        msg_out.plan[t].joint_name= msg->joint_names;
-        for (int i = 0; i < n_joints; i++)
-        {
-          msg_out.plan[t].joint_position[ i ] = msg->points[t].positions[ i ];
-          // Ignoring velocity and effort for now
-        }
-    }
-
-
-    lcm_publish_.publish("CANDIDATE_MANIP_IKPLAN", &msg_out);
 }
 
 int main(int argc, char **argv){
