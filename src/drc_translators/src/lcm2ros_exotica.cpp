51aa476edc2ade80dda8280ff2f122221b316cb2
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-05-27 14:22:09 +0100

    Collision affordances publishing to ROS

--- src/drc_translators/src/lcm2ros_exotica.cpp
+++ src/drc_translators/src/lcm2ros_exotica.cpp
@@ -12,6 +12,7 @@ http://docs.ros.org/indigo/api/sensor_msgs/html/msg/JointState.html
 #include "lcmtypes/drc/robot_plan_t.hpp"
 #include "lcmtypes/drc/plan_control_t.hpp"
 #include "lcmtypes/drc/planner_request_t.hpp"
+#include "lcmtypes/drc/affordance_collection_t.hpp"
 #include "lcmtypes/drc/robot_state_t.hpp"
 #include <trajectory_msgs/JointTrajectory.h>
 #include <ipab_msgs/PlannerRequest.h>
@@ -35,6 +36,7 @@ class LCM2ROS{
     ros::Publisher ik_request_pub_;
     ros::Publisher poses_pub_;
     ros::Publisher joint_names_pub_;
+    ros::Publisher affordance_pub_;
 
     void robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg);
     void plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
@@ -43,18 +45,15 @@ class LCM2ROS{
     void jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg);
     void robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::plan_control_t* msg);
 
+    void affordanceHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::affordance_collection_t* msg);
+
 
 };
 
-LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(lcm_),nh_(nh_) {
-  lcm_->subscribe("COMMITTED_ROBOT_PLAN",&LCM2ROS::robotPlanHandler, this);
-  robot_plan_pub_ = nh_.advertise<trajectory_msgs::JointTrajectory>("/kuka/robot_plan",10);
 
+LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(lcm_),nh_(nh_) {
   lcm_->subscribe("PLANNER_REQUEST",&LCM2ROS::plannerRequestHandler, this);
   planner_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_request",10);
-
-  lcm_->subscribe("COMMITTED_PLAN_PAUSE",&LCM2ROS::robotPlanPauseHandler, this);
-  robot_plan_pause_service_ = nh_.serviceClient<std_srvs::Empty>("stop_trajectory_execution");
   
   rosnode = new ros::NodeHandle();
   lcm_->subscribe("IK_REQUEST",&LCM2ROS::ikRequestHandler, this);
@@ -65,39 +64,17 @@ LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(
 
   lcm_->subscribe("PLANNER_SETUP_JOINT_NAMES",&LCM2ROS::jointNamesHandler, this);
   joint_names_pub_ = nh_.advertise<std_msgs::String>("/exotica/planner_joint_names",10);
+
+  lcm_->subscribe("PLANNER_SETUP_COLLISION_AFFORDANCES",&LCM2ROS::affordanceHandler, this);
+  affordance_pub_ = nh_.advertise<std_msgs::String>("/exotica/affordances",10);
 }
 
-void LCM2ROS::robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg) {
-  ROS_ERROR("LCM2ROS got plan");
-
-  trajectory_msgs::JointTrajectory m;
-  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
-  m.joint_names = msg->plan[0].joint_name;
-
-  for (int i=0; i < msg->num_states; i++){
-    drc::robot_state_t state = msg->plan[i];
-    trajectory_msgs::JointTrajectoryPoint point;
-
-    point.positions =     std::vector<double>(state.joint_position.begin(), state.joint_position.end());
-    point.velocities = std::vector<double>(state.joint_velocity.begin(), state.joint_velocity.end());
-    point.accelerations.assign ( state.joint_position.size()   ,0.0); // not provided, send zeros
-    point.effort = std::vector<double>(state.joint_effort.begin(), state.joint_effort.end());;
-    point.time_from_start = ros::Duration().fromSec(state.utime*1E-6);
-    m.points.push_back(point);
-  }
-
-  robot_plan_pub_.publish(m);
-}
-
-void LCM2ROS::robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::plan_control_t* msg) {
-  ROS_ERROR("LCM2ROS STOP button pressed");
-
-  std_srvs::Empty srv; // TODO: change to Trigger
-  if (robot_plan_pause_service_.call(srv)) {
-    ROS_ERROR("Successfully stopped trajectory execution");
-  } else {
-    ROS_ERROR("Could not stop trajectory execution - DANGER");
-  }
+void LCM2ROS::affordanceHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::affordance_collection_t* msg)
+{
+    ROS_ERROR("LCM2ROS got AFFORDANCE_COLLECTION_COMMAND");
+    std_msgs::String m;
+    m.data=msg->name;
+    affordance_pub_.publish(m);
 }
 
 void LCM2ROS::plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
