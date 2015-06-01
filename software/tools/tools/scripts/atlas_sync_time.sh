7f23a38a7f4d21265aa31799aab5f340441fba99
Toby Schneider <toby@gobysoft.org>
2015-06-01 08:10:33 -0700

    Switched back to ntpdate since ntpd -gq isn't reliable

--- software/tools/tools/scripts/atlas_sync_time.sh
+++ software/tools/tools/scripts/atlas_sync_time.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-service ntp restart
+service ntp stop
+
+if [ "atlas0" = $(hostname) || "paladin-12" = $(hostname) ]
+then
+    ntpdate 10.5.3.254
+else
+    ntpdate inter-atlas0
+fi
+
+service ntp start
 
 echo synchronized clock on `hostname`
