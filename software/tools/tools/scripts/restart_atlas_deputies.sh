4a7b05fab5745e5cb048bfa1f8c715cc795bf85b
Toby Schneider <toby@gobysoft.org>
2015-06-01 08:06:45 -0700

    Removed unnecessary sleep

--- software/tools/tools/scripts/restart_atlas_deputies.sh
+++ software/tools/tools/scripts/restart_atlas_deputies.sh
@@ -7,7 +7,6 @@ echo deputies complete
 
 # time sync (atlas0 syncs to server, atlas1+2 sync to atlas0)
 ssh atlas0 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
-sleep 5
 ssh atlas1 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
 ssh atlas2 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
 echo time sync complete
