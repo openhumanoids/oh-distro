2a3c9ea62a047f84a334e4096e5cba052b3e42c6
Toby Schneider <toby@gobysoft.org>
2015-06-01 19:52:42 -0400

    Update to scripts to sync ntp to DARPA server

--- software/tools/tools/scripts/atlas_sync_time.sh
+++ software/tools/tools/scripts/atlas_sync_time.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 service ntp stop
-
-if [ "atlas0" = $(hostname) ]
-then
-    ntpd -gq
-else
-    ntpdate inter-atlas0
-fi
-
+ntpd -gq
 service ntp start
 
 echo synchronized clock on `hostname`
--- software/tools/tools/scripts/restart_atlas_all_daemons.sh
+++ software/tools/tools/scripts/restart_atlas_all_daemons.sh
@@ -3,6 +3,9 @@
 # field scripts
 ssh field -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/restart_atlas_deputies.sh; \${DRC_BASE}/software/tools/tools/scripts/restart_atlas_field_networking.sh"'
 
+ssh field -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+
+
 # network shaper
 killall -qw drc-network-shaper
 screen -D -m -S shaper-link2 drc-network-shaper -r base -c drc_robot.cfg -i link2 &
