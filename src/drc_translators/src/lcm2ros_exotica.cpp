daeb54663387a73965e262bb3cc7785c37ca6f79
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-06-09 18:45:34 +0100

    Created a monolithic planner request message for exotica

--- src/drc_translators/src/lcm2ros_exotica.cpp
+++ src/drc_translators/src/lcm2ros_exotica.cpp
@@ -11,13 +11,14 @@ http://docs.ros.org/indigo/api/sensor_msgs/html/msg/JointState.html
 
 #include "lcmtypes/drc/robot_plan_t.hpp"
 #include "lcmtypes/drc/plan_control_t.hpp"
-#include "lcmtypes/drc/planner_request_t.hpp"
 #include "lcmtypes/drc/affordance_collection_t.hpp"
 #include "lcmtypes/drc/robot_state_t.hpp"
 #include <trajectory_msgs/JointTrajectory.h>
 #include <ipab_msgs/PlannerRequest.h>
 #include <std_srvs/Empty.h>
 #include <std_msgs/String.h>
+
+#include "lcmtypes/ipab/exotica_planner_request_t.hpp"
 
 using namespace std;
 
@@ -29,89 +30,50 @@ class LCM2ROS{
   private:
     boost::shared_ptr<lcm::LCM> lcm_;
     ros::NodeHandle nh_;
-    ros::NodeHandle* rosnode;
-    ros::Publisher robot_plan_pub_;
+
     ros::Publisher planner_request_pub_;
-    ros::ServiceClient robot_plan_pause_service_;
     ros::Publisher ik_request_pub_;
-    ros::Publisher poses_pub_;
-    ros::Publisher joint_names_pub_;
-    ros::Publisher affordance_pub_;
 
-    void robotPlanHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_plan_t* msg);
-    void plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
-    void ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
-    void posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg);
-    void jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg);
-    void robotPlanPauseHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::plan_control_t* msg);
-
-    void affordanceHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::affordance_collection_t* msg);
-
+    void plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const ipab::exotica_planner_request_t* msg);
+    void ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const ipab::exotica_planner_request_t* msg);
 
 };
 
 
-LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(lcm_),nh_(nh_) {
+LCM2ROS::LCM2ROS(boost::shared_ptr<lcm::LCM> &lcm_, ros::NodeHandle &nh_): lcm_(lcm_),nh_(nh_)
+{
   lcm_->subscribe("PLANNER_REQUEST",&LCM2ROS::plannerRequestHandler, this);
   planner_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_request",10);
   
-  rosnode = new ros::NodeHandle();
   lcm_->subscribe("IK_REQUEST",&LCM2ROS::ikRequestHandler, this);
   ik_request_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/ik_request",10);
-
-  lcm_->subscribe("PLANNER_SETUP_POSES",&LCM2ROS::posesHandler, this);
-  poses_pub_ = nh_.advertise<ipab_msgs::PlannerRequest>("/exotica/planner_poses",10);
-
-  lcm_->subscribe("PLANNER_SETUP_JOINT_NAMES",&LCM2ROS::jointNamesHandler, this);
-  joint_names_pub_ = nh_.advertise<std_msgs::String>("/exotica/planner_joint_names",10);
-
-  lcm_->subscribe("PLANNER_SETUP_COLLISION_AFFORDANCES",&LCM2ROS::affordanceHandler, this);
-  affordance_pub_ = nh_.advertise<std_msgs::String>("/exotica/affordances",10);
 }
 
-void LCM2ROS::affordanceHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::affordance_collection_t* msg)
+void translatePlannerRequest(const ipab::exotica_planner_request_t* msg, ipab_msgs::PlannerRequest& m)
 {
-    ROS_ERROR("LCM2ROS got AFFORDANCE_COLLECTION_COMMAND");
-    std_msgs::String m;
-    m.data=msg->name;
-    affordance_pub_.publish(m);
+    m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
+    m.poses = msg->poses;
+    m.constraints = msg->constraints;
+    m.affordances = msg->affordances;
+    m.seed_pose = msg->seed_pose;
+    m.nominal_pose = msg->nominal_pose;
+    m.end_pose = msg->end_pose;
+    m.joint_names = msg->joint_names;
+    m.options = msg->options;
 }
 
-void LCM2ROS::plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+void LCM2ROS::plannerRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const ipab::exotica_planner_request_t* msg) {
   ROS_ERROR("LCM2ROS got PLANNER_REQUEST");
   ipab_msgs::PlannerRequest m;
-  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
-  m.poses = msg->poses;
-  m.constraints = msg->constraints;
+  translatePlannerRequest(msg,m);
   planner_request_pub_.publish(m);
 }
 
-void LCM2ROS::ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
+void LCM2ROS::ikRequestHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const ipab::exotica_planner_request_t* msg) {
   ROS_ERROR("LCM2ROS got IK_REQUEST");
   ipab_msgs::PlannerRequest m;
-  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
-  m.poses = msg->poses;
-  m.constraints = msg->constraints;
+  translatePlannerRequest(msg,m);
   ik_request_pub_.publish(m);
-}
-
-void LCM2ROS::posesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::planner_request_t* msg) {
-  ROS_ERROR("LCM2ROS got PLANNER_SETUP_POSES");
-  ipab_msgs::PlannerRequest m;
-  m.header.stamp= ros::Time().fromSec(msg->utime*1E-6);
-  m.poses = msg->poses;
-  m.constraints = msg->constraints;
-  poses_pub_.publish(m);
-}
-
-void LCM2ROS::jointNamesHandler(const lcm::ReceiveBuffer* rbuf, const std::string &channel, const drc::robot_state_t* msg) {
-  ROS_ERROR("LCM2ROS got PLANNER_SETUP_JOINT_NAMES");
-  std_msgs::String m;
-  m.data="[";
-  bool first=true;
-  for(auto& s : msg->joint_name) {m.data += (first?"\"":", \"")+s+"\"";first=false;}
-  m.data += "]";
-  joint_names_pub_.publish(m);
 }
 
 int main(int argc,char** argv) {
