7efc307ff36f5d65202c18a5f2c04a53d9c68657
Marcocar <marcocaravagna@gmail.com>
2015-06-30 10:53:23 +0100

    capability map result, moved capability map scripts into a separate folder

--- software/ipab/control/matlab/CapabilityMap/drawCapabilityMap.m
+++ software/ipab/control/matlab/CapabilityMap/drawCapabilityMap.m
@@ -1,36 +1,31 @@
-function drawCapabilityMap(mapFile, center)
-  load(mapFile);
+function drawCapabilityMap(map, options)
   
-  if ~isfield(options, 'drawRobot'), options.drawRobot = true; end
+  options.floating = false;
+  urdf = fullfile(getDrakePath(),'..','models','valkyrie','V1_sim_shells_reduced_polygon_count_mit.urdf');
+  r = RigidBodyManipulator(urdf,options);
+  v = r.constructVisualizer();
+  shoulder = r.findLinkId('RightShoulderAdductor');
+  q = zeros(r.num_positions,1);
+  q(r.getBody(shoulder).position_num) = -pi/2;
+  v.draw(0,q);
+  kinsol = r.doKinematics(q);
+  center = r.forwardKin(kinsol, shoulder, [0;0;0], 0);
   
-  if options.drawRobot
-    options.floating = false;
-    urdf = fullfile(getDrakePath(),'..','models','valkyrie','V1_sim_shells_reduced_polygon_count_mit.urdf');
-    r = RigidBodyManipulator(urdf,options);
-    v = r.constructVisualizer();
-    shoulder = r.findLinkId('RightShoulderAdductor');
-    q = zeros(r.num_positions,1);
-    q(r.getBody(shoulder).position_num) = -pi/2;
-    v.draw(0,q);
-    kinsol = r.doKinematics(q);
-    center = r.forwardKin(kinsol, shoulder, [0;0;0], 0);
-  elseif nargin < 2
-    center = [0;0;0];
-  end
-  
-  
-  [map, reachabilityIndex, sphCenters] = pruneCapabilityMap(mapFile, 0, 0, 0.6, 0.9, 7.5);
-  sphCentersShoulder = sphCenters + repmat(center, 1, size(sphCenters, 2));
-  diameter = options.sphDiameter;
+  diameter = options.diameter;
+  nPointsPerSphere = options.nPointsPerSphere;
+  sphCenters = options.sphCenters + repmat(center, 1, size(options.sphCenters, 2));
   
   nSph = size(map, 1);
+  D = zeros(nSph, 1);
   lcmClient = LCMGLClient('CapabilityMap');
   for sph = 1:nSph
-    color = hsv2rgb([reachabilityIndex(sph) 1 1]);
-    lcmClient.glColor3f(color(1), color(2), color(3))
-    lcmClient.sphere(sphCentersShoulder(:,sph), diameter/2, 20, 20);   
+    nDir = nnz(map(sph,:));
+    D(sph) = nDir/nPointsPerSphere;
+    if D(sph) > 0 && sphCenters(1,sph) < 0
+      color = hsv2rgb([D(sph) 1 1]);
+      lcmClient.glColor3f(color(1), color(2), color(3))
+      lcmClient.sphere(sphCenters(:,sph), diameter/2, 20, 20);      
+    end
   end
   lcmClient.switchBuffers();
-  
-  capabilityMapControls(0, 0, 0.6, 0.9, 7.5, mapFile, center, nSph);
 end
