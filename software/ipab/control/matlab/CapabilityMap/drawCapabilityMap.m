a701eecfd410f0399300556c51055ab8532fe8da
marcoCar <marcocaravagna@gmail.com>
2015-07-02 15:24:55 +0100

    minor changes to capabiltyMap and drawCapabiltiyMap

--- software/ipab/control/matlab/CapabilityMap/drawCapabilityMap.m
+++ software/ipab/control/matlab/CapabilityMap/drawCapabilityMap.m
@@ -1,15 +1,21 @@
-function drawCapabilityMap(map, options)
+function drawCapabilityMap(map, options, center)
   
-  options.floating = false;
-  urdf = fullfile(getDrakePath(),'..','models','valkyrie','V1_sim_shells_reduced_polygon_count_mit.urdf');
-  r = RigidBodyManipulator(urdf,options);
-  v = r.constructVisualizer();
-  shoulder = r.findLinkId('RightShoulderAdductor');
-  q = zeros(r.num_positions,1);
-  q(r.getBody(shoulder).position_num) = -pi/2;
-  v.draw(0,q);
-  kinsol = r.doKinematics(q);
-  center = r.forwardKin(kinsol, shoulder, [0;0;0], 0);
+  if ~isfield(options, 'drawRobot'), options.drawRobot = true; end
+  
+  if options.drawRobot
+    options.floating = false;
+    urdf = fullfile(getDrakePath(),'..','models','valkyrie','V1_sim_shells_reduced_polygon_count_mit.urdf');
+    r = RigidBodyManipulator(urdf,options);
+    v = r.constructVisualizer();
+    shoulder = r.findLinkId('RightShoulderAdductor');
+    q = zeros(r.num_positions,1);
+    q(r.getBody(shoulder).position_num) = -pi/2;
+    v.draw(0,q);
+    kinsol = r.doKinematics(q);
+    center = r.forwardKin(kinsol, shoulder, [0;0;0], 0);
+  elseif nargin < 3
+    center = [0;0;0];
+  end
   
   diameter = options.diameter;
   nPointsPerSphere = options.nPointsPerSphere;
@@ -21,7 +27,7 @@ function drawCapabilityMap(map, options)
   for sph = 1:nSph
     nDir = nnz(map(sph,:));
     D(sph) = nDir/nPointsPerSphere;
-    if D(sph) > 0 && sphCenters(1,sph) < 0
+    if D(sph) > 0  && sphCenters(1,sph) > 0
       color = hsv2rgb([D(sph) 1 1]);
       lcmClient.glColor3f(color(1), color(2), color(3))
       lcmClient.sphere(sphCenters(:,sph), diameter/2, 20, 20);      
