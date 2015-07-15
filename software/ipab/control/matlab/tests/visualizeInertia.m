68520b7146961457dbf747590a99160fd7607f74
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-07-15 10:12:16 +0100

    mods for this model

--- software/ipab/control/matlab/tests/visualizeInertia.m
+++ software/ipab/control/matlab/tests/visualizeInertia.m
@@ -2,19 +2,24 @@ options.visualize = true;
 options.floating = true;
 options.terrain = RigidBodyFlatTerrain();
 
-val_version = 2
+val_version = 3
 if (val_version == 1)
   urdf = fullfile(getDrakePath(),'..','models','valkyrie','V1_sim_shells_reduced_polygon_count_mit.urdf');
   S = load([getDrakePath(), '/../control/matlab/data/valkyrie_fp.mat']);
-else
+elseif (val_version == 2)
   urdf = fullfile(getDrakePath(),'..','models','val_description','model.urdf');
   S = load([getDrakePath(), '/../control/matlab/data/valkyrie_fp_june2015.mat']);
+else
+  urdf = '/home/mfallon/otherprojects/val_description/robots/valkyrie_A.urdf';
+  S = load([getDrakePath(), '/../control/matlab/data/valkyrie_fp_june2015.mat']);
 end
-
+  
 r = RigidBodyManipulator(urdf,options);
 r = r.compile();
 v = r.constructVisualizer(); % robot arms spread, pelvise at zero
 v = v.enableLCMGLInertiaEllipsoids();
 nq = 38;
 q_nom = S.xstar(1:nq);
+
+%q_nom = S.xstar(1:65);
 v.draw(0,q_nom); % standing configuration with left arm out to side
