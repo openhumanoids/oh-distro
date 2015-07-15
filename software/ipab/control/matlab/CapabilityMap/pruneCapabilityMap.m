a967645eb371aac352fc0108ae64ef56f50150d7
marcoCar <marcocaravagna@gmail.com>
2015-07-15 16:26:30 +0100

    new function to prune the capability map

---
+++ software/ipab/control/matlab/CapabilityMap/pruneCapabilityMap.m
@@ -0,0 +1,23 @@
+function [map, reachabilityIndex, sphCenters]  = pruneCapabilityMap(mapFile, sagittalAngle,...
+    transverseAngle, sagittalWeight, transverseWeight, reachabilityWeight)
+  
+  load(mapFile)
+  
+  Dmax = max(reachabilityIndex);
+  nSph = length(map);
+  indices = [];
+  
+  for sph = 1:nSph
+    sa = atan2(sphCenters(3,sph), sphCenters(1,sph));
+    ta = atan2(sphCenters(2,sph), sphCenters(1,sph));
+    sagittalCost = sagittalWeight * abs(sa - tan(sagittalAngle));
+    transverseCost = transverseWeight * abs(ta - tan(transverseAngle));
+    reachabilityCost = reachabilityWeight * (Dmax - reachabilityIndex(sph));
+    if sqrt(sagittalCost^2 + transverseCost^2) + reachabilityCost < 1
+      indices(end + 1) = sph;
+    end
+  end  
+  reachabilityIndex = reachabilityIndex(indices);
+  map = map(indices, :);
+  sphCenters = sphCenters(:, indices);
+end
