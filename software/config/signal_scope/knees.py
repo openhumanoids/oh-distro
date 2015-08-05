349f72fd98b7bcdddfe661a0cba8d8db1724b4c8
Pat Marion <james.patrick.marion@gmail.com>
2015-08-05 13:22:31 -0400

    update knees.py to include ATLAS_COMMAND

--- software/config/signal_scope/knees.py
+++ software/config/signal_scope/knees.py
@@ -1,15 +1,28 @@
-findLCMTypes(os.path.expanduser('../lib/python2.7/dist-packages/*'))
-
+# joints to plot
 joints = ['l_leg_kny','r_leg_kny']
-#addSignals('ATLAS_STATE', msg.utime, msg.joint_position, joints, keyLookup=msg.joint_name)
-addSignals('EST_ROBOT_STATE', msg.utime, msg.joint_position, joints, keyLookup=msg.joint_name)
 
 
-addPlot()
-#addSignals('ATLAS_STATE', msg.utime, msg.joint_effort, joints, keyLookup=msg.joint_name)
-addSignals('EST_ROBOT_STATE', msg.utime, msg.joint_effort, joints, keyLookup=msg.joint_name)
+# string arrays for EST_ROBOT_STATE and ATLAS_COMMAND
+jn = msg.joint_name
+jns = msg.joint_names
 
-addPlot()
-#addSignals('ATLAS_STATE', msg.utime, msg.joint_velocity, joints, keyLookup=msg.joint_name)
-addSignals('EST_ROBOT_STATE', msg.utime, msg.joint_velocity, joints, keyLookup=msg.joint_name)
 
+# position plot
+addPlot(timeWindow=30, yLimits=[-2.5, 2.5])
+for joint in joints:
+    addSignal('EST_ROBOT_STATE', msg.utime, msg.joint_position[jn[joint]])
+    addSignal('ATLAS_COMMAND', msg.utime, msg.position[jns[joint]])
+
+
+# effort plot
+addPlot(timeWindow=30, yLimits=[-100, 100])
+for joint in joints:
+    addSignal('EST_ROBOT_STATE', msg.utime, msg.joint_effort[jn[joint]])
+    addSignal('ATLAS_COMMAND', msg.utime, msg.effort[jns[joint]])
+
+
+# velocity plot
+addPlot(timeWindow=30, yLimits=[-2, 2])
+for joint in joints:
+    addSignal('EST_ROBOT_STATE', msg.utime, msg.joint_velocity[jn[joint]])
+    addSignal('ATLAS_COMMAND', msg.utime, msg.velocity[jns[joint]])
