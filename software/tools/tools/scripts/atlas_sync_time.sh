b7a0a0e458b45b1efa5d036aa7ad2cfa2042afd4
Pat Marion <james.patrick.marion@gmail.com>
2015-08-13 14:52:43 -0400

    use internet for atlas computers time sync

--- software/tools/tools/scripts/atlas_sync_time.sh
+++ software/tools/tools/scripts/atlas_sync_time.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-service ntp stop
+#service ntp stop
 
-if [ "atlas0" = $(hostname) ]
-then
-    ntpdate 10.5.3.254
-elif [ "paladin-12" = $(hostname) ]
-then
-    ntpdate 10.5.3.254
-else
-    ntpdate inter-atlas0
-fi
+#if [ "atlas0" = $(hostname) ]
+#then
+#    ntpdate 10.5.3.254
+#elif [ "paladin-12" = $(hostname) ]
+#then
+#    ntpdate 10.5.3.254
+#else
+#    ntpdate inter-atlas0
+#fi
 
-service ntp start
+#service ntp start
 
+ntpdate -u 0.ubuntu.pool.ntp.org
 echo synchronized clock on `hostname`
