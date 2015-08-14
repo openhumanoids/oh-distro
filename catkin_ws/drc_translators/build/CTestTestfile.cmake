5c07c1bfc856ddf10446461ca44d20a9e6b8e97a
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-08-14 09:02:32 +0100

    cleaning up marcos branch

--- catkin_ws/drc_translators/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/drc/catkin_ws/drc_translators/src
-# Build directory: /home/marco/drc/catkin_ws/drc_translators/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(drc_translators)
--- catkin_ws/drc_translators/build/drc_translators/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "drc_translators"
-PROJECT_SPACE_DIR = "/home/marco/drc/catkin_ws/drc_translators/install"
-PROJECT_VERSION = "0.0.0"
