69d5996a3307a8a24c3806a132e0574380c77175
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-08-19 18:26:55 +0100

    SCS test runs its own ROS core

--- tests/systemtests/src/launchSCS/test.bash
+++ tests/systemtests/src/launchSCS/test.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Launch roscore
+roscore & # If core is already running, this will fail silently
+pidcore=$!
+sleep 1
 # Launch SCS.
 $DRC_BASE/software/ipab/config/runscs.bash &
 # Get the bash pid.
@@ -7,9 +11,10 @@ while true; do
 	# If SCS crashes and bash script stops, return FAILURE
 	if ! kill -0 $pidsh > /dev/null 2>&1; then
 		echo "FAILURE - SCS crashed or didn't start."
+	        kill $pidcore
 		exit 1
 	fi
-	if [ "$(rostopic info /ihmc_ros/valkyrie/api_command | grep Subscribers)" != "Subscribers: None" ] ; then
+	if [ "$(rostopic info /ihmc_ros/valkyrie/api_command | grep Subscribers)" == "Subscribers: " ] ; then
 		# SCS is running now, send commands.
 		echo "Shutting down SCS"
                 rostopic pub /ihmc_ros/valkyrie/api_command std_msgs/String stop -1 > /dev/null 2>&1
@@ -25,10 +30,12 @@ pid="$(ps --ppid $pidsh | grep java | awk '{print $1;}')"
 sleep 5
 if ! kill -0 $pid > /dev/null 2>&1; then
 	echo "SUCCESS"
+        kill $pidcore
 	exit 0
 else
 	kill $pid
 	wait $pidsh
+        kill $pidcore
 	echo "FAILURE - SCS did not shut down! Killing process now."
 	exit 1
 fi
