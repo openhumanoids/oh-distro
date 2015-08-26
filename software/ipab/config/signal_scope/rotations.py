f8d94eef560291c923954b7116dbe024beaa61a3
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-08-26 17:49:40 +0100

    publishes the plan back to the scope as a series of samples

---
+++ software/ipab/config/signal_scope/rotations.py
@@ -0,0 +1,33 @@
+import math
+
+def quat_to_euler(q) :
+  roll_a = 2.0 * (q[0]*q[1] + q[2]*q[3]);
+  roll_b = 1.0 - 2.0 * (q[1]*q[1] + q[2]*q[2]);
+  roll = math.atan2 (roll_a, roll_b);
+
+  pitch_sin = 2.0 * (q[0]*q[2] - q[3]*q[1]);
+  pitch = math.asin (pitch_sin);
+
+  yaw_a = 2.0 * (q[0]*q[3] + q[1]*q[2]);
+  yaw_b = 1.0 - 2.0 * (q[2]*q[2] + q[3]*q[3]);  
+  yaw = math.atan2 (yaw_a, yaw_b);
+  return [roll,pitch,yaw]
+
+
+# was (roll, pitch, yaw)
+def euler_to_quat(rpy):
+  roll =  rpy[0]
+  pitch = rpy[1]
+  yaw =   rpy[2]
+
+  sy = math.sin(yaw*0.5);
+  cy = math.cos(yaw*0.5);
+  sp = math.sin(pitch*0.5);
+  cp = math.cos(pitch*0.5);
+  sr = math.sin(roll*0.5);
+  cr = math.cos(roll*0.5);
+  w = cr*cp*cy + sr*sp*sy;
+  x = sr*cp*cy - cr*sp*sy;
+  y = cr*sp*cy + sr*cp*sy;
+  z = cr*cp*sy - sr*sp*cy;
+  return np.array([w,x,y,z])
