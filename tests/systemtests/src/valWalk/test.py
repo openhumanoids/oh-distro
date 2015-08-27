91a691695ba8bfc2980cbe6dd05deb5d6491bbb0
Vladimir Ivan <v.ivan@ed.ac.uk>
2015-08-27 17:16:42 +0100

    Added Walking system test

--- tests/systemtests/src/valWalk/test.py
+++ tests/systemtests/src/valWalk/test.py
@@ -1,24 +1,35 @@
 import ipab
 import time
+import threading
 
-if ikPlanner.pushToMatlab==False:
-  print "FAILURE - NOT pushing requests to matlab!"
+def Tester():
+  if ikPlanner.pushToMatlab==False:
+    print "FAILURE - NOT pushing requests to matlab!"
+    exit()
+  
+  ## Begin Test ##
+  
+  # Start the simulator
+  msg=ipab.scs_api_command_t()
+  msg.command="simulate"
+  lcmUtils.publish('SCS_API_CONTROL', msg)
+  time.sleep(90.0)
+  time.sleep(2.0)
+  
+  footstepsPanel.onNewWalkingGoal()
+  time.sleep(10.0)
+  footstepsPanel.onExecute()
+  time.sleep(30.0)
+  
+  ## End Test ##
+
+  # When everything goes all right, report success
+  with open(os.environ.get('SYSTEMTEST_RESULT_FILE'),'w+') as f:
+    f.write('1\n')
   exit()
 
-## Begin Test ##
+thr = threading.Thread(target=Tester)
+thr.setDaemon(True)
+thr.start()
 
-# Start the simulator
-msg=ipab.scs_api_command_t()
-msg.command="simulate"
-lcmUtils.publish('SCS_API_CONTROL', msg)
-time.sleep(2.0)
 
-footstepsPanel.onNewWalkingGoal()
-footstepsPanel.onExecute()
-
-## End Test ##
-
-# When everything goes all right, report success
-with open(os.environ.get('SYSTEMTEST_RESULT_FILE'),'w+') as f:
-  f.write('1\n')
-exit()
