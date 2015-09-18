3a382f98f7f771ddad50ea5cc2d1b739a418d680
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-09-18 19:23:44 -0400

    correctly publish controller joint efforts
    Conflicts:
    	software/control/src/threadedController.cpp

--- software/control/src/threadedController.cpp
+++ software/control/src/threadedController.cpp
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <sys/select.h>
 #include "drake/lcmt_qp_controller_input.hpp"
+#include "drake/lcmt_qp_controller_state.hpp"
 #include "drc/controller_status_t.hpp"
 #include "drc/recovery_trigger_t.hpp"
 #include "bot_core/robot_state_t.hpp"
@@ -27,103 +28,138 @@ struct ThreadedControllerOptions {
   int max_infocount; // If we see info < 0 more than max_infocount times, freeze Atlas. Set to -1 to disable freezing.
 };
 
-std::atomic<bool> done(false);
 
-std::atomic<bool> newInputAvailable(false);
-std::atomic<bool> newStateAvailable(false);
+namespace {
 
-std::mutex pointerMutex;
+  using namespace Eigen;
 
-std::shared_ptr<RobotStateDriver> state_driver;
-std::shared_ptr<AtlasCommandDriver> command_driver;
-std::shared_ptr<FootContactDriver> foot_contact_driver;
-Matrix<bool, Dynamic, 1> b_contact_force;
+  struct ThreadedControllerOptions {
+    std::string atlas_command_channel;
+    std::string atlas_behavior_channel;
+    int max_infocount; // If we see info < 0 more than max_infocount times, freeze Atlas. Set to -1 to disable freezing.
+  };
 
-class SolveArgs {
-public:
+  std::atomic<bool> done(false);
 
   InstantaneousQPController *pdata;
+  std::atomic<bool> newInputAvailable(false);
+  std::atomic<bool> newStateAvailable(false);
 
-  std::shared_ptr<DrakeRobotState> robot_state;
-  std::shared_ptr<drake::lcmt_qp_controller_input> qp_input;
-  std::shared_ptr<QPControllerOutput> qp_output;
+  std::mutex pointerMutex;
 
+  std::shared_ptr<RobotStateDriver> state_driver;
+  std::shared_ptr<AtlasCommandDriver> command_driver;
+  std::shared_ptr<FootContactDriver> foot_contact_driver;
   Matrix<bool, Dynamic, 1> b_contact_force;
-  std::map<Side, ForceTorqueMeasurement> foot_force_torque_measurements;
-  std::shared_ptr<QPControllerDebugData> debug;
+  VectorXd drake_input_to_robot_state;
 
-  int info;
-};
+  class SolveArgs {
+  public:
 
-SolveArgs solveArgs;
+    NewQPControllerData *pdata;
 
-drc::behavior_command_t robot_behavior_msg;
+    drc::behavior_command_t robot_behavior_msg;
+    std::shared_ptr<DrakeRobotState> robot_state;
+    std::shared_ptr<drake::lcmt_qp_controller_input> qp_input;
+    std::shared_ptr<QPControllerOutput> qp_output;
 
-int infocount = 0;
+    Matrix<bool, Dynamic, 1> b_contact_force;
+    std::map<Side, ForceTorqueMeasurement> foot_force_torque_measurements;
+    std::shared_ptr<QPControllerDebugData> debug;
 
-class LCMHandler {
+    int info;
+  };
 
-public:
+  SolveArgs solveArgs;
 
-  std::thread ThreadHandle;
-  std::shared_ptr<lcm::LCM> LCMHandle;
-  bool ShouldStop;
+  drc::atlas_behavior_command_t atlas_behavior_msg;
 
-  LCMHandler()
-  {
-    this->ShouldStop = false;
-    this->InitLCM();
-  }
+  int infocount = 0;
 
-  void InitLCM()
-  {
-    this->LCMHandle = std::shared_ptr<lcm::LCM>(new lcm::LCM);
-    if(!this->LCMHandle->good())
+//determines whether we should publish the controller state as an LCM message
+  bool publishControllerState = true;
+  std::string CONTROLLER_STATE_CHANNEL("CONTROLLER_STATE");
+
+
+  class LCMHandler {
+
+  public:
+
+    std::thread ThreadHandle;
+    std::shared_ptr<lcm::LCM> LCMHandle;
+    bool ShouldStop;
+
+    LCMHandler()
     {
-      std::cout << "ERROR: lcm is not good()" << std::endl;
+      this->ShouldStop = false;
+      this->InitLCM();
     }
-  }
 
-  bool WaitForLCM(double timeout)
-  {
-    int lcmFd = this->LCMHandle->getFileno();
-
-    timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = timeout * 1e6;
-
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(lcmFd, &fds);
-
-    int status = select(lcmFd + 1, &fds, 0, 0, &tv);
-    if (status == -1 && errno != EINTR)
+    void InitLCM()
     {
-      printf("select() returned error: %d\n", errno);
-    }
-    else if (status == -1 && errno == EINTR)
-    {
-      printf("select() interrupted\n");
-    }
-    return (status > 0 && FD_ISSET(lcmFd, &fds));
-  }
-
-  void ThreadLoopWithSelect()
-  {
-
-    std::cout << "ThreadLoopWithSelect " << std::this_thread::get_id() << std::endl;
-
-    while (!this->ShouldStop)
-    {
-      const double timeoutInSeconds = 0.3;
-      bool lcmReady = this->WaitForLCM(timeoutInSeconds);
-
-      if (this->ShouldStop)
+      this->LCMHandle = std::shared_ptr<lcm::LCM>(new lcm::LCM);
+      if(!this->LCMHandle->good())
       {
-        break;
+        std::cout << "ERROR: lcm is not good()" << std::endl;
+      }
+    }
+
+    bool WaitForLCM(double timeout)
+    {
+      int lcmFd = this->LCMHandle->getFileno();
+
+      timeval tv;
+      tv.tv_sec = 0;
+      tv.tv_usec = timeout * 1e6;
+
+      fd_set fds;
+      FD_ZERO(&fds);
+      FD_SET(lcmFd, &fds);
+
+      int status = select(lcmFd + 1, &fds, 0, 0, &tv);
+      if (status == -1 && errno != EINTR)
+      {
+        printf("select() returned error: %d\n", errno);
+      }
+      else if (status == -1 && errno == EINTR)
+      {
+        printf("select() interrupted\n");
+      }
+      return (status > 0 && FD_ISSET(lcmFd, &fds));
+    }
+
+    void ThreadLoopWithSelect()
+    {
+
+      std::cout << "ThreadLoopWithSelect " << std::this_thread::get_id() << std::endl;
+
+      while (!this->ShouldStop)
+      {
+        const double timeoutInSeconds = 0.3;
+        bool lcmReady = this->WaitForLCM(timeoutInSeconds);
+
+        if (this->ShouldStop)
+        {
+          break;
+        }
+
+        if (lcmReady)
+        {
+          if (this->LCMHandle->handle() != 0)
+          {
+            printf("lcm->handle() returned non-zero\n");
+            break;
+          }
+        }
       }
 
-      if (lcmReady)
+      std::cout << "ThreadLoopWithSelect ended " << std::this_thread::get_id() << std::endl;
+
+    }
+
+    void ThreadLoop()
+    {
+      while (!this->ShouldStop)
       {
         if (this->LCMHandle->handle() != 0)
         {
@@ -133,102 +169,87 @@ public:
       }
     }
 
-    std::cout << "ThreadLoopWithSelect ended " << std::this_thread::get_id() << std::endl;
-
-  }
-
-  void ThreadLoop()
-  {
-    while (!this->ShouldStop)
+    bool IsRunning()
     {
-      if (this->LCMHandle->handle() != 0)
+      return this->ThreadHandle.joinable();
+    }
+
+    void Start()
+    {
+      std::cout << "LCMHandler start... " << std::this_thread::get_id() << std::endl;
+      if (this->IsRunning())
       {
-        printf("lcm->handle() returned non-zero\n");
-        break;
+        std::cout << "already running lcm thread. " << std::this_thread::get_id() << std::endl;
+        return;
       }
+
+      this->ShouldStop = false;
+      this->ThreadHandle = std::thread(&LCMHandler::ThreadLoopWithSelect, this);
     }
-  }
 
-  bool IsRunning()
-  {
-    return this->ThreadHandle.joinable();
-  }
-
-  void Start()
-  {
-    std::cout << "LCMHandler start... " << std::this_thread::get_id() << std::endl;
-    if (this->IsRunning())
+    void Stop()
     {
-      std::cout << "already running lcm thread. " << std::this_thread::get_id() << std::endl;
-      return;
+      this->ShouldStop = true;
+      this->ThreadHandle.join();
     }
 
-    this->ShouldStop = false;
-    this->ThreadHandle = std::thread(&LCMHandler::ThreadLoopWithSelect, this);
-  }
-
-  void Stop()
-  {
-    this->ShouldStop = true;
-    this->ThreadHandle.join();
-  }
-
-};
+  };
 
 
-class LCMControlReceiver {
-public:
+  class LCMControlReceiver {
+  public:
 
-  LCMControlReceiver(LCMHandler* lh)
-  {
-    this->lcmHandler = lh;
-  }
+    LCMControlReceiver(LCMHandler* lh)
+    {
+      this->lcmHandler = lh;
+    }
 
-  void InitSubscriptions()
-  {
-    lcm::Subscription* sub;
+    void InitSubscriptions()
+    {
+      lcm::Subscription* sub;
 
-    sub = this->lcmHandler->LCMHandle->subscribe("QP_CONTROLLER_INPUT", &LCMControlReceiver::inputHandler, this);
-    sub->setQueueCapacity(1);
+      sub = this->lcmHandler->LCMHandle->subscribe("QP_CONTROLLER_INPUT", &LCMControlReceiver::inputHandler, this);
+      sub->setQueueCapacity(1);
 
-    sub = this->lcmHandler->LCMHandle->subscribe("EST_ROBOT_STATE", &LCMControlReceiver::onRobotState, this);
-    sub->setQueueCapacity(1);
+      sub = this->lcmHandler->LCMHandle->subscribe("EST_ROBOT_STATE", &LCMControlReceiver::onRobotState, this);
+      sub->setQueueCapacity(1);
 
-    sub = this->lcmHandler->LCMHandle->subscribe("FOOT_CONTACT_ESTIMATE", &LCMControlReceiver::onFootContact, this);
-    sub->setQueueCapacity(1);
-  }
+      sub = this->lcmHandler->LCMHandle->subscribe("FOOT_CONTACT_ESTIMATE", &LCMControlReceiver::onFootContact, this);
+      sub->setQueueCapacity(1);
+    }
 
-  void inputHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const drake::lcmt_qp_controller_input* msg)
-  {
-    //std::cout << "received qp_input on lcm thread " << std::this_thread::get_id() << std::endl;
+    void inputHandler(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const drake::lcmt_qp_controller_input* msg)
+    {
+      //std::cout << "received qp_input on lcm thread " << std::this_thread::get_id() << std::endl;
 
-    std::shared_ptr<drake::lcmt_qp_controller_input> msgCopy(new drake::lcmt_qp_controller_input);
-    *msgCopy = *msg;
+      std::shared_ptr<drake::lcmt_qp_controller_input> msgCopy(new drake::lcmt_qp_controller_input);
+      *msgCopy = *msg;
 
-    // std::cout << "got input msg with param set name: " << msgCopy->param_set_name << std::endl;
-    pointerMutex.lock();
-    solveArgs.qp_input = msgCopy;
-    pointerMutex.unlock();
-    newInputAvailable = true;
-  }
+      // std::cout << "got input msg with param set name: " << msgCopy->param_set_name << std::endl;
+      pointerMutex.lock();
+      solveArgs.qp_input = msgCopy;
+      pointerMutex.unlock();
+      newInputAvailable = true;
+    }
 
   void onRobotState(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const bot_core::robot_state_t* msg)
   {
     //std::cout << "received robotstate on lcm thread " << std::this_thread::get_id() << std::endl;
 
-    std::shared_ptr<DrakeRobotState> state(new DrakeRobotState);
+      std::shared_ptr<DrakeRobotState> state(new DrakeRobotState);
 
     int nq = solveArgs.pdata->getRobot().num_positions;
     int nv = solveArgs.pdata->getRobot().num_velocities;
     state->q = VectorXd::Zero(nq);
     state->qd = VectorXd::Zero(nv);
 
-    state_driver->decode(msg, state.get());
 
-    // std::cout << "decoded robot state " << state->t << std::endl;
+      state_driver->decode(msg, state.get());
 
-    pointerMutex.lock();
-    solveArgs.robot_state = state;
+      // std::cout << "decoded robot state " << state->t << std::endl;
+
+      pointerMutex.lock();
+      solveArgs.robot_state = state;
 
     const bot_core::force_torque_t& force_torque = msg->force_torque;
 
@@ -238,98 +259,116 @@ public:
     solveArgs.foot_force_torque_measurements[Side::RIGHT].frame_idx = solveArgs.pdata->getRPC().foot_ids.at(Side::RIGHT); // TODO: make sure that this is right
     solveArgs.foot_force_torque_measurements[Side::RIGHT].wrench << force_torque.r_foot_torque_x, force_torque.r_foot_torque_y, 0.0, 0.0, 0.0, force_torque.r_foot_force_z;
 
-    pointerMutex.unlock();
-    newStateAvailable = true;
-  }
+      pointerMutex.unlock();
+      newStateAvailable = true;
+    }
 
   void onFootContact(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const drc::foot_contact_estimate_t* msg)
   {
     Matrix<bool, Dynamic, 1> b_contact_force = Matrix<bool, Dynamic, 1>::Zero(solveArgs.pdata->getRobot().bodies.size());
 
-    foot_contact_driver->decode(msg, b_contact_force);
 
-    pointerMutex.lock();
-    solveArgs.b_contact_force = b_contact_force;
-    pointerMutex.unlock();
+      foot_contact_driver->decode(msg, b_contact_force);
+
+      pointerMutex.lock();
+      solveArgs.b_contact_force = b_contact_force;
+      pointerMutex.unlock();
+    }
+
+
+    LCMHandler* lcmHandler;
+  };
+
+
+  LCMHandler lcmHandler;
+  LCMControlReceiver controlReceiver(&lcmHandler);
+
+  bool isOutputSafe(QPControllerOutput &qp_output) {
+    for (int i=0; i < qp_output.q_ref.size(); i++) {
+      if (std::isnan(qp_output.q_ref(i))) {
+        std::cout << "Error: NaN in q_ref" << std::endl;
+        return false;
+      }
+      if (std::isnan(qp_output.qd_ref(i))) {
+        std::cout << "Error: NaN in qd_ref" << std::endl;
+        return false;
+      }
+      if (std::isnan(qp_output.qdd(i))) {
+        std::cout << "Error: NaN in qdd" << std::endl;
+        return false;
+      }
+      if (std::isinf(qp_output.q_ref(i))) {
+        std::cout << "Error: Inf in q_ref" << std::endl;
+        return false;
+      }
+      if (std::isinf(qp_output.qd_ref(i))) {
+        std::cout << "Error: Inf in qd_ref" << std::endl;
+        return false;
+      }
+      if (std::isinf(qp_output.qdd(i))) {
+        std::cout << "Error: Inf in qdd" << std::endl;
+        return false;
+      }
+    }
+    for (int i=0; i < qp_output.u.size(); i++) {
+      if (std::isnan(qp_output.u(i))) {
+        std::cout << "Error: NaN in u" << std::endl;
+        return false;
+      }
+      if (std::isinf(qp_output.u(i))) {
+        std::cout << "Error: Inf in u" << std::endl;
+        return false;
+      }
+    }
+    return true;
   }
 
+  drake::lcmt_qp_controller_state encodeControllerState(double t, int num_joints, const QPControllerOutput &qp_output){
 
-  LCMHandler* lcmHandler;
-};
+    drake::lcmt_qp_controller_state msg;
+    msg.timestamp = (long)(t*1000000);
+    msg.num_joints = num_joints;
+    msg.q_integrator_state.resize(num_joints);
+    msg.vref_integrator_state.resize(num_joints);
+    msg.q_ref.resize(num_joints);
+    msg.qd_ref.resize(num_joints);
+    msg.qdd.resize(num_joints);
+    msg.u.resize(num_joints);
+    msg.joint_name.resize(num_joints);
 
 
-LCMHandler lcmHandler;
-LCMControlReceiver controlReceiver(&lcmHandler);
+    // std::cout << "size of qp_output.u is " << qp_output.u.size() << std::endl;
+    // std::cout << "size of qp_output.qdd is " << qp_output.qdd.size() << std::endl;
+    // std::cout << "size of state coordinate names are" << solveArgs.pdata->state_coordinate_names.size() << std::endl;
 
-bool isOutputSafe(QPControllerOutput &qp_output) {
-  for (int i=0; i < qp_output.q_ref.size(); i++) {
-    if (std::isnan(qp_output.q_ref(i))) {
-      std::cout << "Error: NaN in q_ref" << std::endl;
-      return false;
+
+    for(int i=0; i<num_joints; i++){
+      msg.q_integrator_state[i] = solveArgs.pdata->state.q_integrator_state(i);
+      msg.vref_integrator_state[i] = qp_output.qd_ref(i);
+      msg.q_ref[i] = qp_output.q_ref(i);
+      msg.qd_ref[i] = qp_output.qd_ref(i);
+      msg.qdd[i] = qp_output.qdd(i);
+      msg.joint_name[i] = solveArgs.pdata->state_coordinate_names[i];
     }
-    if (std::isnan(qp_output.qd_ref(i))) {
-      std::cout << "Error: NaN in qd_ref" << std::endl;
-      return false;
+
+    //Inputs are in a different order, need to deal with that
+    int num_inputs = drake_input_to_robot_state.size();
+    int robot_state_idx;
+    for( int i=0; i<num_inputs; i++){
+      robot_state_idx = drake_input_to_robot_state[i];
+      msg.u[robot_state_idx] = qp_output.u(i);
     }
-    if (std::isnan(qp_output.qdd(i))) {
-      std::cout << "Error: NaN in qdd" << std::endl;
-      return false;
-    }
-    if (std::isinf(qp_output.q_ref(i))) {
-      std::cout << "Error: Inf in q_ref" << std::endl;
-      return false;
-    }
-    if (std::isinf(qp_output.qd_ref(i))) {
-      std::cout << "Error: Inf in qd_ref" << std::endl;
-      return false;
-    }
-    if (std::isinf(qp_output.qdd(i))) {
-      std::cout << "Error: Inf in qdd" << std::endl;
-      return false;
-    }
+
+    return msg;
   }
-  for (int i=0; i < qp_output.u.size(); i++) {
-    if (std::isnan(qp_output.u(i))) {
-      std::cout << "Error: NaN in u" << std::endl;
-      return false;
-    }
-    if (std::isinf(qp_output.u(i))) {
-      std::cout << "Error: Inf in u" << std::endl;
-      return false;
-    }
-  }
-  return true;
-}
 
-void threadLoop(std::shared_ptr<ThreadedControllerOptions> ctrl_opts)
-{
+  void threadLoop(std::shared_ptr<ThreadedControllerOptions> ctrl_opts)
+  {
 
-  QPControllerOutput qp_output;
-  done = false;
+    QPControllerOutput qp_output;
+    done = false;
 
-  while (!done) {
-
-    //std::cout << "waiting for new data... " << std::this_thread::get_id() << std::endl;
-
-    while (!newStateAvailable || !newInputAvailable) {
-      std::this_thread::yield();
-    }
-
-    // auto begin = std::chrono::high_resolution_clock::now();
-
-
-    // copy pointers
-    pointerMutex.lock();
-    std::shared_ptr<DrakeRobotState> robot_state = solveArgs.robot_state;
-    std::shared_ptr<drake::lcmt_qp_controller_input> qp_input = solveArgs.qp_input;
-    b_contact_force = solveArgs.b_contact_force;
-    std::map<Side, ForceTorqueMeasurement> foot_force_torque_measurements = solveArgs.foot_force_torque_measurements;
-    pointerMutex.unlock();
-
-    // newInputAvailable = false;
-    newStateAvailable = false;
-
-    // std::cout << "calling solve " << std::endl;
+    while (!done) {
 
     if (qp_input->be_silent) {
       // Act as a dummy controller, produce no ATLAS_COMMAND, and reset all integrator states
@@ -384,17 +423,39 @@ void threadLoop(std::shared_ptr<ThreadedControllerOptions> ctrl_opts)
 
       // publish ATLAS_COMMAND
       bot_core::atlas_command_t* command_msg = command_driver->encode(robot_state->t, &qp_output, params.hardware);
-      lcmHandler.LCMHandle->publish(ctrl_opts->atlas_command_channel, command_msg);
-    }
+      lcmHandler.LCMHandle->publish(ctrl_opts->atlas_command_channel, command_msg); // publishes the atlas_command msg
 
-    // typedef std::chrono::duration<float> float_seconds;
-    // auto end = std::chrono::high_resolution_clock::now();
-    // auto elapsed = std::chrono::duration_cast<float_seconds>(end - begin);
-    //std::cout << "solve speed: " << elapsed.count() << " ms.  " << 1.0 /elapsed.count() << " hz." << std::endl;
+      //publish CONTROLLER_STATE lcm message for debugging purposes
+        if (publishControllerState){
+          int num_joints = qp_output.q_ref.size();
+          drake::lcmt_qp_controller_state controller_state_msg = encodeControllerState(robot_state->t, num_joints, qp_output);
+          lcmHandler.LCMHandle->publish(CONTROLLER_STATE_CHANNEL, &controller_state_msg);
+        }
+    }
 
   }
 
-}
+  void getDrakeInputToRobotStateIndexMap(const std::vector<std::string> &input_joint_names, const std::vector<std::string> & state_coordinate_names, VectorXd & drake_input_to_robot_state){
+    int num_inputs = input_joint_names.size();
+    drake_input_to_robot_state.resize(num_inputs);
+
+    bool has_match;
+    for( int i=0; i<num_inputs; i++){
+      has_match = false;
+      for( int j=0; j<state_coordinate_names.size(); j++){
+        if (input_joint_names[i].compare(state_coordinate_names[j]) == 0){
+          has_match=true;
+          drake_input_to_robot_state(i) = j;
+        }
+      }
+      if (!has_match){
+        std::cout << "Could not match joint " << input_joint_names[i] << std::endl;
+        throw std::runtime_error("Could not find a match for drake input joint name");
+      }
+    }
+
+  }
+
 
 void controllerLoop(InstantaneousQPController *pdata, std::shared_ptr<ThreadedControllerOptions> ctrl_opts)
 {
@@ -412,13 +473,10 @@ void controllerLoop(InstantaneousQPController *pdata, std::shared_ptr<ThreadedCo
 
   // std::cout << "pdata num bodies: " << pdata->getRobot().bodies.size() << std::endl;
 
-  lcmHandler.Start();
-  controlReceiver.InitSubscriptions();
+    threadLoop(ctrl_opts);
+  }
 
-  std::cout << "starting control loop... " << std::endl;
 
-  threadLoop(ctrl_opts);
-}
 
 
 
