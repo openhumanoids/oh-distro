707d5ae601322f0ec1a0423d179af8f2d473ea70
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-09-21 19:04:09 -0400

    add to plotting for residual-detector

--- software/config/signal_scope/residual-detector.py
+++ software/config/signal_scope/residual-detector.py
@@ -3,12 +3,16 @@
 joints = ['base_x', 'base_y', 'base_z', 'r_leg_kny', 'l_leg_kny', 'r_leg_hpy', 'l_leg_hpy', 'back_bkx', 'back_bky', 'back_bkz','r_arm_shx','r_arm_shz']
 names = msg.joint_name
 
-addPlot(timeWindow=30, yLimits=[-2.5, 2.5])
+addPlot(timeWindow=30, yLimits=[-50, 50])
 addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.joint_position, joints, keyLookup=names)
 addSignals('RESIDUAL_OBSERVER_STATE_WITH_FORCES', msg.utime, msg.joint_position, joints, keyLookup=names)
 
 
-# # effort plot
+# gravity vs. torque, should be equal, something is funky if they are not
+addPlot(timeWindow=30, yLimits=[-50, 50])
+addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.joint_velocity, joints, keyLookup=names)
+addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.joint_effort, joints, keyLookup=names)
+
 # addPlot(timeWindow=30, yLimits=[-100, 100])
 # for joint in joints:
 #     addSignal('EST_ROBOT_STATE', msg.utime, msg.joint_effort[jn[joint]])
