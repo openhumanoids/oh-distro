dee504707d0394dda4b5ee9c72e204f50a87e921
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-10-08 16:54:50 -0400

    more plotting updates

--- software/config/signal_scope/residual-detector.py
+++ software/config/signal_scope/residual-detector.py
@@ -1,6 +1,6 @@
 # residual state plot
 
-joints = ['base_x', 'base_y', 'base_z', 'r_leg_kny', 'l_leg_kny', 'r_leg_hpy', 'l_leg_hpy', 'back_bkx', 'back_bky', 'back_bkz','r_arm_shx','r_arm_shz']
+joints = ['base_x', 'base_y', 'base_z', 'r_leg_kny', 'l_leg_kny', 'r_leg_hpy', 'l_leg_hpy', 'back_bkx', 'back_bky', 'back_bkz','l_arm_shx','l_arm_shz']
 names = msg.joint_name
 
 addPlot(timeWindow=30, yLimits=[-50, 50])
@@ -12,6 +12,8 @@ addSignals('RESIDUAL_OBSERVER_STATE_WITH_FORCES', msg.utime, msg.joint_position,
 addPlot(timeWindow=30, yLimits=[-50, 50])
 addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.joint_velocity, joints, keyLookup=names)
 addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.joint_effort, joints, keyLookup=names)
+# addSignals('RESIDUAL_OBSERVER_STATE_WITH_FORCES', msg.utime, msg.joint_velocity, joints, keyLookup=names)
+addSignals('RESIDUAL_OBSERVER_STATE_WITH_FORCES', msg.utime, msg.joint_effort, joints, keyLookup=names)
 
 # addPlot(timeWindow=30, yLimits=[-100, 100])
 # for joint in joints:
