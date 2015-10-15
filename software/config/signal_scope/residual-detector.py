3cf636f43cc5cb701eeb5c4b83c3fcb3275c17f3
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-10-14 20:08:44 -0400

    udpate plotting function to reflect new types

--- software/config/signal_scope/residual-detector.py
+++ software/config/signal_scope/residual-detector.py
@@ -1,24 +1,36 @@
 # residual state plot
 
-joints = ['base_x', 'base_y', 'base_z', 'r_leg_kny', 'l_leg_kny', 'r_leg_hpy', 'l_leg_hpy', 'back_bkx', 'back_bky', 'back_bkz','l_arm_shx','l_arm_shz']
+joints = ['base_x', 'base_y', 'base_z', 'r_leg_kny', 'l_leg_kny', 'l_leg_hpy', 'back_bkx', 'back_bky', 'back_bkz','l_arm_shx','l_arm_shz']
 
 # shortened set of joint names to keep it readable for now.
-joints = ['base_z', 'r_leg_kny', 'l_leg_kny', 'l_arm_shz']
+joints = ['base_x','base_y','base_z','base_roll', 'base_pitch', 'base_yaw', 'r_leg_aky', 'r_leg_kny', 'r_leg_hpy', 'r_leg_hpx', 'l_leg_kny', 'l_arm_shz']
+
+# joints = ['base_x','base_y','base_z','base_roll', 'base_pitch', 'base_yaw', 'r_leg_aky', 'r_leg_kny', 'r_leg_hpy', 'r_leg_hpx', 'l_leg_kny', 'l_arm_shz']
 names = msg.joint_name
+vel_names = msg.velocity_names
+
+joints_w_dot = [];
+for jointName in joints:
+    joints_w_dot.append(jointName + "dot")
+
+
 
 addPlot(timeWindow=30, yLimits=[-50, 50])
-addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.residual, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.residual, joints, keyLookup=names)
+# addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.residual, joints, keyLookup=names)
+# addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.residual, joints, keyLookup=names)
 addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FT', msg.utime, msg.residual, joints, keyLookup=names)
-
+addSignals('CONTACT_FILTER_POINT_ESTIMATE', msg.utime, msg.implied_residual, joints_w_dot, keyLookup=vel_names)
 
 # gravity vs. torque, should be equal, something is funky if they are not
-addPlot(timeWindow=30, yLimits=[-50, 50])
-addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.gravity, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.gravity, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.internal_torque, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.foot_contact_torque, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FT', msg.utime, msg.foot_contact_torque, joints, keyLookup=names)
+# addPlot(timeWindow=30, yLimits=[-50, 50])
+# # addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.gravity, joints, keyLookup=names)
+# # addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.gravity, joints, keyLookup=names)
+# # addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.internal_torque, joints, keyLookup=names)
+# # addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.foot_contact_torque, joints, keyLookup=names)
+
+# addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FT', msg.utime, msg.gravity, joints, keyLookup=names)
+# addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FT', msg.utime, msg.internal_torque, joints, keyLookup=names)
+# addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FT', msg.utime, msg.foot_contact_torque, joints, keyLookup=names)
 
 # addPlot(timeWindow=30, yLimits=[-100, 100])
 # for joint in joints:
