685c8dfc3855e46bc3a6a930828d21c617a48d87
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-10-20 12:52:05 +0100

    minor fix to joint names

--- software/ipab/config/signal_scope/l_arm.py
+++ software/ipab/config/signal_scope/l_arm.py
@@ -2,11 +2,12 @@ execfile(os.path.join(os.path.dirname(__file__), 'rotations.py'))
 
 # joints to plot
 #joints = ['l_arm_shz','l_arm_shx','l_arm_ely','l_arm_elx','l_arm_uwy','l_arm_mwx','l_arm_lwy']
-joints = ['LeftShoulderPitch','LeftShoulderRoll','LeftShoulderYaw','LeftElbowPitch','LeftForearmYaw','LeftWristRoll','LeftWristPitch']
+joints = ['leftShoulderPitch','leftShoulderRoll','leftShoulderYaw','leftElbowPitch','leftForearmYaw','leftWristRoll','leftWristPitch']
 
 # string arrays for EST_ROBOT_STATE and ATLAS_COMMAND
 jn = msg.joint_name
 jns = msg.joint_names
+
 
 import numpy
 
