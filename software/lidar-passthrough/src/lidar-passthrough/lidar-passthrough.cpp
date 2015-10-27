3ac4b06f85ab61ef3d139301a3ba27797e540305
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-10-27 19:06:29 +0000

    minor fix for lidar passthrough. broken during recent drake update

--- software/lidar-passthrough/src/lidar-passthrough/lidar-passthrough.cpp
+++ software/lidar-passthrough/src/lidar-passthrough/lidar-passthrough.cpp
@@ -292,9 +292,7 @@ void Pass::DoCollisionCheck(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& scan_c
   
   // 2. Extract the indices of the points in collision:
   VectorXd q(robotStateToDrakePosition(rstate, dofMap_, drake_model_.num_positions));
-  VectorXd v = VectorXd::Zero(0);
-  KinematicsCache<double> cache = drake_model_.doKinematics(q, v);
-  
+  KinematicsCache<double> cache = drake_model_.doKinematics(q, 0);
   vector<size_t> filtered_point_indices = drake_model_.collidingPoints(cache, points, collision_threshold_);
   
   // 3. Modify the outgoing ranges of the colliding points:
