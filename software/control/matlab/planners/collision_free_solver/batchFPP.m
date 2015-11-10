01cbaa7991a86161f5d7aa5ed9377d7c7bfd96fa
Marco <marcocaravagna@gmail.com>
2015-11-10 11:47:30 +0000

    save batch FPP output

--- software/control/matlab/planners/collision_free_solver/batchFPP.m
+++ software/control/matlab/planners/collision_free_solver/batchFPP.m
@@ -1,29 +1,49 @@
-function batchFPP(n, scenes, options)
+function output = batchFPP(n, scenes, options)
 
   if nargin < 3 || isempty(options), options = struct(); end
 
-  if ~isfield(options,'models'), models = {'val2'}; else models = options.models; end;
-  if ~isfield(options,'grasping_hands'), grasping_hands = {'right'}; else grasping_hands = options.grasping_hands; end;
+  if ~isfield(options,'models')
+    models = {'val2'};
+  elseif ischar(options.models)
+    models = {options.models};
+  else
+    models = options.models;
+  end;
+  if ~isfield(options,'grasping_hands')
+    grasping_hands = {'right'};
+  elseif ischar(options.grasping_hands)
+    grasping_hands = {options.grasping_hands};
+  else
+    grasping_hands = options.grasping_hands;
+  end;
   
-  for hand_idx = 1:numel(grasping_hands)
-    for model_idx = 1:numel(models)
-      for scene = scenes
-        
-        opt.floating = true;
-        opt.terrain = RigidBodyFlatTerrain();
-        
-        opt.scene = scene;
-        opt.model = models{model_idx};
+  output.details.models = models;
+  output.details.n_iterations = n;
+  output.details.scenes = scenes;
+  output.details.grasping_hands = grasping_hands;
+  output.details.time = sprintf('%d-%d-%d %d:%d:%.0f', clock);
+  
+  for model_idx = 1:numel(models)
+    for scene = scenes
+
+      opt.floating = true;
+      opt.terrain = RigidBodyFlatTerrain();
+
+      opt.scene = scene;
+      opt.model = models{model_idx};
+      opt.convex_hull = true;
+
+      opt.robot = Scenes.generateScene(opt);
+
+      for hand_idx = 1:numel(grasping_hands)
         opt.graspingHand = grasping_hands{hand_idx};
-        opt.convex_hull = true;
-        
-        opt.robot = Scenes.generateScene(opt);
-        
         parfor i = 1:n
-          fprintf('Computing iteration %d of scene %d with %s model and %s hand\n', ...
-                  i, scene, models{model_idx}, grasping_hands{hand_idx})
-          [info, debug_vars] = exploringFPP(opt);
+            fprintf('Computing iteration %d of scene %d with %s model and %s hand\n', ...
+                    i, scene, models{model_idx}, grasping_hands{hand_idx})
+            [~, debug_vars] = exploringFPP(opt);
+            debug_vars_array(i) = debug_vars;
         end
+        output.(models{model_idx}).(sprintf('scene%d', scene)).(grasping_hands{hand_idx}) = debug_vars_array;
       end
     end
   end
