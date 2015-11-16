75e64abbfa739e1ca37ec78185eb7d480fc0da45
Marco <marcocaravagna@gmail.com>
2015-11-16 11:51:07 +0000

    minor fixes to CandidateRobotPosePublisher

--- software/control/matlab/util/CandidateRobotPosePublisher.m
+++ software/control/matlab/util/CandidateRobotPosePublisher.m
@@ -20,39 +20,36 @@ classdef CandidateRobotPosePublisher
         end
 
         function publish(obj,varargin)
-            if nargin < 3
-                utime = get_timestamp_now();% equivalent to bot_timestamp_now();
-            end                  
             switch nargin
+                case 2
+                    utime = get_timestamp_now();% equivalent to bot_timestamp_now();
                 case 3
-                    X = varargin{1};
                     utime= varargin{2};
-                    msg = encodeRobotPose(obj,X,utime);
-                    obj.lc.publish(obj.channel,msg);
                 otherwise
                     error('Incorrect usage of publish in CandidateRobotPosePublisher. Undefined number of varargin.')
             end
+            X = varargin{1};
+            msg = encodeRobotPose(obj,X,utime);
+            obj.lc.publish(obj.channel,msg);
         end
 
 
         function msg = encodeRobotPose(obj,X,t)
-            if nargin < 4
+            if nargin < 3
                 t = get_timestamp_now();% equivalent to bot_timestamp_now();
             end
-            offset = 0;
-            num_dofs = (size(X,1)-offset)/2;
+            num_dofs = (size(X,1))/2;
             
             msg = drc.robot_state_t();
             msg.utime = t;
-                        i=1;
             msg.pose = drc.position_3d_t();
             msg.pose.translation = drc.vector_3d_t();
             msg.pose.rotation = drc.quaternion_t();
-            msg.pose.translation.x = X(1,i);
-            msg.pose.translation.y = X(2,i);
-            msg.pose.translation.z = X(3,i);
+            msg.pose.translation.x = X(1);
+            msg.pose.translation.y = X(2);
+            msg.pose.translation.z = X(3);
             
-            q = rpy2quat([X(4,i) X(5,i) X(6,i)]);                  
+            q = rpy2quat([X(4) X(5) X(6)]);
             msg.pose.rotation.w = q(1);
             msg.pose.rotation.x = q(2);
             msg.pose.rotation.y = q(3);
@@ -61,12 +58,12 @@ classdef CandidateRobotPosePublisher
             msg.twist = drc.twist_t();
             msg.twist.linear_velocity = drc.vector_3d_t();
             msg.twist.angular_velocity = drc.vector_3d_t();   
-            msg.twist.linear_velocity.x = X(offset+num_dofs+1,i);
-            msg.twist.linear_velocity.y = X(offset+num_dofs+2,i);
-            msg.twist.linear_velocity.z = X(offset+num_dofs+3,i);
-            msg.twist.angular_velocity.x = X(offset+num_dofs+4,i);
-            msg.twist.angular_velocity.y = X(offset+num_dofs+5,i);
-            msg.twist.angular_velocity.z = X(offset+num_dofs+6,i);     
+            msg.twist.linear_velocity.x = X(num_dofs+1);
+            msg.twist.linear_velocity.y = X(num_dofs+2);
+            msg.twist.linear_velocity.z = X(num_dofs+3);
+            msg.twist.angular_velocity.x = X(num_dofs+4);
+            msg.twist.angular_velocity.y = X(num_dofs+5);
+            msg.twist.angular_velocity.z = X(num_dofs+6);
             
 
             float_offset = 6;
@@ -77,8 +74,8 @@ classdef CandidateRobotPosePublisher
             msg.joint_effort=zeros(1,msg.num_joints);
             for j=float_offset+1:num_dofs,
                 msg.joint_name(j-float_offset) = java.lang.String(obj.joint_names{j});
-                msg.joint_position(j-float_offset) = X(j+offset,i);
-                msg.joint_velocity(j-float_offset) = X(j+offset+num_dofs,i);
+                msg.joint_position(j-float_offset) = X(j);
+                msg.joint_velocity(j-float_offset) = X(j+num_dofs);
             end
                    
             msg.force_torque = drc.force_torque_t();
