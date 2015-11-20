3d7c814b9e28f444c0721f94e573f60c6b70d73f
Pat Marion <james.patrick.marion@gmail.com>
2015-11-20 15:48:46 -0500

    rename tools/tools to utils/drc_utils

--- software/utils/drc_utils/scripts/restart_atlas_deputies.sh
+++ software/utils/drc_utils/scripts/restart_atlas_deputies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
-ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
-ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_deputy.sh"'
+ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_deputy.sh"'
+ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_deputy.sh"'
+ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_deputy.sh"'
 echo deputies complete
 
 # time sync (atlas0 syncs to server, atlas1+2 sync to atlas0)
-ssh atlas0 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas0 -t 'bash -ic "sudo \${DRC_BASE}/software/utils/drc_utils/scripts/atlas_sync_time.sh"'
 sleep 5
-ssh atlas1 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
-ssh atlas2 -t 'bash -ic "sudo \${DRC_BASE}/software/tools/tools/scripts/atlas_sync_time.sh"'
+ssh atlas1 -t 'bash -ic "sudo \${DRC_BASE}/software/utils/drc_utils/scripts/atlas_sync_time.sh"'
+ssh atlas2 -t 'bash -ic "sudo \${DRC_BASE}/software/utils/drc_utils/scripts/atlas_sync_time.sh"'
 echo time sync complete
--- software/utils/drc_utils/scripts/restart_atlas_field_networking.sh
+++ software/utils/drc_utils/scripts/restart_atlas_field_networking.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # lcm tunnel
-${DRC_BASE}/software/tools/tools/scripts/atlas_restart_tunnel.sh server
-ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_tunnel.sh"'
+${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_tunnel.sh server
+ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_tunnel.sh"'
 
 # lcm bridges
-ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_bridge.sh"'
-ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_bridge.sh"'
-ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/tools/tools/scripts/atlas_restart_bridge.sh"'
+ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
+ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
+ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
 
 # network shaper
 killall -qw drc-network-shaper
