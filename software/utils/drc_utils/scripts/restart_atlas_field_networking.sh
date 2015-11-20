72163440b5aa9cbfea54d00c1db171f0aa6c9574
Pat Marion <james.patrick.marion@gmail.com>
2015-11-20 15:49:06 -0500

    remove network-shaper from restart_atlas_field_networking.sh

--- software/utils/drc_utils/scripts/restart_atlas_field_networking.sh
+++ software/utils/drc_utils/scripts/restart_atlas_field_networking.sh
@@ -8,11 +8,3 @@ ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_res
 ssh atlas0 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
 ssh atlas1 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
 ssh atlas2 -t 'bash -ic "\${DRC_BASE}/software/utils/drc_utils/scripts/atlas_restart_bridge.sh"'
-
-# network shaper
-killall -qw drc-network-shaper
-screen -D -m -S shaper-link3 drc-network-shaper -r robot -c drc_robot.cfg  -i link3 -l -p /home/drc/logs/raw/shaper &
-screen -D -m -S shaper-link2 drc-network-shaper -r robot -c drc_robot.cfg  -i link2 &
-
-until pids=$(pidof drc-network-shaper); do sleep 0.1; done
-echo started network shaper on `hostname`
