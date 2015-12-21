260042a57e6ed8793ae236624cef91e025564d2c
Pat Marion <james.patrick.marion@gmail.com>
2015-12-20 23:05:06 -0500

    refactor externals cmake script to support private externals
    fixes #2260.

--- software/externals/cmake/externals.cmake
+++ software/externals/cmake/externals.cmake
@@ -1,10 +1,19 @@
-set(lcm_pod_url https://github.com/RobotLocomotion/lcm-pod.git)
-set(lcm_pod_revision c43ae7d)
-set(lcm_pod_depends)
+
+
+if(NOT USE_SYSTEM_LCM)
+  set(lcm_proj lcm)
+  set(lcm_url https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip)
+  set(lcm_download_hash 5d46a902fe04608809af3dc526909f9b)
+  set(lcm_depends)
+  set(lcm_external_args
+    CONFIGURE_COMMAND ${source_prefix}/lcm/configure --prefix=${CMAKE_INSTALL_PREFIX}
+    BUILD_IN_SOURCE 1
+    )
+endif()
 
 set(libbot_url https://github.com/openhumanoids/libbot.git)
-set(libbot_revision eb61f9bed5df654ffb0ce47ce44ca3d379a6e6e8)
-set(libbot_depends)
+set(libbot_revision 92cf3eaef17659990104da40866c5d466a453b17)
+set(libbot_depends ${lcm_proj})
 
 set(Eigen_pod_url https://github.com/RobotLocomotion/eigen-pod.git)
 set(Eigen_pod_revision 9061e9d5481fe0fdf55d5de322dfb1ed9d380a1c)
@@ -15,9 +24,8 @@ set(opencv_revision 2.4.12.3)
 set(opencv_depends Eigen_pod)
 set(opencv_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
+    ${python_args}
     -DWITH_CUDA:BOOL=OFF
   )
 
@@ -26,9 +34,7 @@ set(flann_revision 4969acc) # master from march 2015
 set(flann_depends)
 set(flann_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
     -DBUILD_MATLAB_BINDINGS:BOOL=OFF
     -DBUILD_PYTHON_BINDINGS:BOOL=OFF
     -DBUILD_TESTS:BOOL=OFF
@@ -36,13 +42,11 @@ set(flann_external_args
   )
 
 set(pcl_url http://github.com/pointcloudlibrary/pcl.git)
-set(pcl_revision pcl-1.7.1)
+set(pcl_revision pcl-1.7.2)
 set(pcl_depends flann Eigen_pod)
 set(pcl_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
   )
 
 set(octomap_url https://github.com/OctoMap/octomap.git)
@@ -50,9 +54,7 @@ set(octomap_revision 5ba840e58a35e2d14c40d0af807da879a1a2fd83)
 set(octomap_depends)
 set(octomap_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
   )
 
 set(occ-map_url https://github.com/openhumanoids/occ-map.git)
@@ -64,7 +66,7 @@ set(common_utils_revision 90df348c8acb7fde57920f4bd3edeb903ac42667)
 set(common_utils_depends Eigen_pod libbot occ-map octomap)
 
 set(frsm_url ssh://git@github.com/openhumanoids/frsm.git)
-set(frsm_revision bbf1d83023ae35ecf595c93e46b8601206be9e9b)
+set(frsm_revision 0fb51ac1580a7f6ce3b5f4d7afc302caa976246f)
 set(frsm_depends libbot)
 
 set(kinect_url https://github.com/openhumanoids/kinect.git)
@@ -80,8 +82,8 @@ set(bullet_revision 4319ffd7e9251066d93064f5a8dab12e33dbe5e2)
 set(bullet_depends)
 
 set(fovis_url ssh://git@github.com/fovis/fovis.git)
-set(fovis_revision ee2fe6593ed9e7e5ce2b2f6f1c64b627da119090)
-set(fovis_depends libbot kinect)
+set(fovis_revision 2862e080dee2519932585b46cc301a38d8cad1f4)
+set(fovis_depends libbot)
 
 set(estimate-pose_url https://github.com/openhumanoids/estimate-pose.git)
 set(estimate-pose_revision e24a46af00c46116b76f69ff0f20923e60daa519)
@@ -103,26 +105,37 @@ set(snopt_url ssh://git@github.com/openhumanoids/snopt.git)
 set(snopt_revision 95d908275156f2665ef3941f08cb89c767480a6e)
 set(snopt_depends)
 
+set(gurobi-private_url ssh://git@github.com/openhumanoids/gurobi-private.git)
+set(gurobi-private_revision cfeea24766ea1a11d5fc6eeff193ab520c3e58d2)
+set(gurobi-private_depends)
+set(gurobi-private_external_args ${download_only_args})
+
 set(gurobi_url ssh://git@github.com/RobotLocomotion/gurobi.git)
 set(gurobi_revision b95a186b4d988db00ada55bd8efb08c651a83fe7)
-set(gurobi_environment_args GUROBI_DISTRO=${CMAKE_CURRENT_SOURCE_DIR}/cmake/gurobi5.6.2_linux64.tar.gz)
-set(gurobi_depends)
+if(APPLE)
+  set(gurobi_distro_file ${source_prefix}/gurobi-private/gurobi5.6.2_mac64.pkg)
+else()
+  set(gurobi_distro_file ${source_prefix}/gurobi-private/gurobi5.6.2_linux64.tar.gz)
+endif()
+set(gurobi_environment_args GUROBI_DISTRO=${gurobi_distro_file})
+set(gurobi_depends gurobi-private)
+
+set(atlas-driver_url ssh://git@github.com/openhumanoids/atlas-drivers.git)
+set(atlas-driver_revision 59955966eda449b4ca8bd9f4fd09afcf3d9d20cf)
+set(atlas-driver_depends)
+set(atlas-driver_external_args
+  ${download_only_args}
+  SOURCE_DIR ${source_prefix}/../drivers/atlas
+  )
 
 set(iris_url ssh://git@github.com/openhumanoids/iris-distro.git)
 set(iris_revision b278a85ba4b3b3da3af41feb39c13e4150d12d98)
 set(iris_depends Eigen_pod)
 set(iris_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
     -DIRIS_WITH_EIGEN:BOOL=OFF
   )
-
-# not installed anymore because iris-distro installs mosek
-set(mosek_url ssh://git@github.com/RobotLocomotion/mosek.git)
-set(mosek_revision bb7000e)
-set(mosek_depends)
 
 set(pypolyhedron_url ssh://git@github.com/rdeits/pypolyhedron.git)
 set(pypolyhedron_revision 1f110addf89398f62644830bf69a69930db8c4d0)
@@ -137,9 +150,7 @@ set(libmultisense_hg_tag a57026c)
 set(libmultisense_depends opencv)
 set(libmultisense_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${default_cmake_args}
   )
 
 set(multisense-lcm_url http://github.com/openhumanoids/multisense-lcm.git)
@@ -151,9 +162,9 @@ set(libnabo_revision 7d6b111de63a5118e11f551e336606233018ee8d)
 set(libnabo_depends Eigen_pod)
 set(libnabo_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
+    ${default_cmake_args}
+    ${eigen_args}
+    ${python_args}
   )
 
 set(libpointmatcher_url https://github.com/ethz-asl/libpointmatcher.git)
@@ -161,130 +172,181 @@ set(libpointmatcher_revision 6c6acbb17199f9a51467dff86875bbc11add5bf2)
 set(libpointmatcher_depends Eigen_pod libnabo)
 set(libpointmatcher_external_args
   CMAKE_CACHE_ARGS
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
+    ${default_cmake_args}
+    ${eigen_args}
   )
 
 set(hokuyo_url ssh://git@github.com/openhumanoids/hokuyo.git)
 set(hokuyo_revision 6fc1b804d80838ae314d162929bb0a25a231ca35)
 set(hokuyo_depends libbot)
 
+set(cmake_scripts_url https://github.com/RobotLocomotion/cmake.git)
+set(cmake_scripts_revision 2c4ee11aa719ad548df7eaf3c7047a98c6e3e01c)
+set(cmake_scripts_external_args
+  ${download_only_args}
+  SOURCE_DIR ${source_prefix}/../drake/drake/cmake
+  )
+
+set(PythonQt_url https://github.com/commontk/PythonQt.git)
+set(PythonQt_revision 00e6c6b2)
+set(PythonQt_depends)
+set(PythonQt_external_args
+  CMAKE_CACHE_ARGS
+    ${default_cmake_args}
+    ${python_args}
+    ${qt_args}
+    -DPythonQt_Wrap_Qtcore:BOOL=ON
+    -DPythonQt_Wrap_Qtgui:BOOL=ON
+    -DPythonQt_Wrap_Qtuitools:BOOL=ON
+  )
+
+set(ctkPythonConsole_url https://github.com/patmarion/ctkPythonConsole.git)
+set(ctkPythonConsole_revision 15988c5)
+set(ctkPythonConsole_depends PythonQt)
+set(ctkPythonConsole_external_args
+  CMAKE_CACHE_ARGS
+    ${default_cmake_args}
+    ${python_args}
+    ${qt_args}
+  )
+
+set(QtPropertyBrowser_url https://github.com/patmarion/QtPropertyBrowser.git)
+set(QtPropertyBrowser_revision baf10af)
+set(QtPropertyBrowser_depends)
+set(QtPropertyBrowser_external_args
+  CMAKE_CACHE_ARGS
+    ${default_cmake_args}
+    ${qt_args}
+  )
+
+set(PointCloudLibraryPlugin_url https://github.com/patmarion/PointCloudLibraryPlugin.git)
+set(PointCloudLibraryPlugin_revision cb119b0)
+set(PointCloudLibraryPlugin_depends pcl)
+set(PointCloudLibraryPlugin_external_args
+  CMAKE_CACHE_ARGS
+    ${default_cmake_args}
+    ${vtk_args}
+    -DPCL_REQUIRED_VERSION:STRING=1.7
+  )
+
+
+
 set(externals
   Eigen_pod
+  ${lcm_proj}
   libbot
   opencv
   flann
   pcl
   octomap
-  libmultisense
-  multisense-lcm
   occ-map
-  common_utils
   frsm
-  kinect
-  microstrain
   fovis
   estimate-pose
-  vicon
   apriltags
   bullet
   spotless
-  snopt
-  gurobi
   iris
   pypolyhedron
-  kinematics-utils
   libnabo
   libpointmatcher
   hokuyo
+  cmake_scripts
+  PythonQt
+  ctkPythonConsole
+  QtPropertyBrowser
+  PointCloudLibraryPlugin
   )
 
+if(BUILD_PRIVATE_EXTERNALS)
+
+  list(APPEND externals
+    atlas-driver
+    gurobi-private
+    gurobi
+    snopt
+    )
+
+endif()
+
+
+if(NOT APPLE)
+
+  # These modules only compile on linux. Some have strict requirements on
+  # linux, others could be fixed up to support mac osx builds.
+  list(APPEND externals
+    common_utils
+    libmultisense
+    multisense-lcm
+    microstrain
+    vicon
+    kinematics-utils
+  )
+
+  list(FIND externals fovis fovis_index)
+  list(INSERT externals ${fovis_index} kinect)
+  list(APPEND fovis_depends kinect)
+
+endif()
 
 
 macro(add_external proj)
-  if (DEFINED ${proj}_hg_tag)
-    add_mercurial_external(${proj})
-  elseif (${${proj}_url} MATCHES "\\.git$")
-    add_git_external(${proj})
-  else()
-    add_svn_external(${proj})
-  endif()
-endmacro()
 
-macro(add_svn_external proj)
-  ExternalProject_Add(${proj}
+  # depending on which variables are defined, the external project
+  # might be mercurial, svn, git, or an archive download.
+  if(DEFINED ${proj}_hg_tag)
+    set(download_args
+      HG_REPOSITORY ${${proj}_url}
+      HG_TAG ${${proj}_hg_tag})
+  elseif(DEFINED ${proj}_svn_revision)
+    set(download_args
     SVN_REPOSITORY ${${proj}_url}
-    SVN_REVISION -r ${${proj}_revision}
-    DEPENDS ${${proj}_depends}
-    CONFIGURE_COMMAND ""
-    INSTALL_COMMAND ""
-    BUILD_COMMAND $(MAKE) BUILD_PREFIX=${CMAKE_INSTALL_PREFIX} BUILD_TYPE=${CMAKE_BUILD_TYPE}  ${${proj}_environment_args}
-    BUILD_IN_SOURCE 1
-    SOURCE_DIR ${DRCExternals_SOURCE_DIR}/${proj}
-    )
-endmacro()
-
-macro(add_git_external proj)
-
-  if (DEFINED ${proj}_external_args)
-    ExternalProject_Add(${proj}
-      GIT_REPOSITORY ${${proj}_url}
-      GIT_TAG ${${proj}_revision}
-      DEPENDS ${${proj}_depends}
-      SOURCE_DIR ${DRCExternals_SOURCE_DIR}/${proj}
-      ${${proj}_external_args}
-      )
+    SVN_REVISION -r ${${proj}_revision})
+  elseif(DEFINED ${proj}_download_hash)
+    set(download_args
+    URL ${${proj}_url}
+    URL_MD5 ${${proj}_download_hash})
   else()
-    ExternalProject_Add(${proj}
+    set(download_args
       GIT_REPOSITORY ${${proj}_url}
-      GIT_TAG ${${proj}_revision}
-      DEPENDS ${${proj}_depends}
-      CONFIGURE_COMMAND ""
-      INSTALL_COMMAND ""
+      GIT_TAG ${${proj}_revision})
+  endif()
+
+  # if this variable is not defined then this external will be treated as
+  # a standard pod so we'll define the required configure and build commands
+  if(NOT DEFINED ${proj}_external_args)
+    set(pod_build_args
+      CONFIGURE_COMMAND ${empty_command}
+      INSTALL_COMMAND ${empty_command}
       BUILD_COMMAND $(MAKE) BUILD_PREFIX=${CMAKE_INSTALL_PREFIX} BUILD_TYPE=${CMAKE_BUILD_TYPE} ${${proj}_environment_args}
       BUILD_IN_SOURCE 1
-      SOURCE_DIR ${DRCExternals_SOURCE_DIR}/${proj}
-      )
-    endif()
-endmacro()
+    )
+    set(${proj}_external_args ${pod_build_args})
+    set(${proj}_is_pod TRUE)
+  endif()
 
+  # this supports non-standard download locations
+  set(source_dir_arg)
+  list(FIND ${proj}_external_args SOURCE_DIR res)
+  if (res EQUAL -1)
+    set(source_dir_arg SOURCE_DIR ${source_prefix}/${proj})
+  endif()
 
-macro(add_mercurial_external proj)
-    ExternalProject_Add(${proj}
-      HG_REPOSITORY ${${proj}_url}
-      HG_TAG ${${proj}_hg_tag}
-      DEPENDS ${${proj}_depends}
-      SOURCE_DIR ${DRCExternals_SOURCE_DIR}/${proj}
-      ${${proj}_external_args}
-      )
+  # workaround a cmake issue, we need to support empty strings as list elements
+  # so replace the string NONE with empty string here right before arg conversion
+  # and then the variable will be quoted in the following call to ExternalProject_Add.
+  string(REGEX REPLACE "NONE" "" ${proj}_external_args "${${proj}_external_args}")
+
+  ExternalProject_Add(${proj}
+    DEPENDS ${${proj}_depends}
+    ${download_args}
+    ${source_dir_arg}
+    "${${proj}_external_args}"
+    )
+
 endmacro()
 
 
 foreach(external ${externals})
   add_external(${external})
 endforeach()
-
-
-# Eigen will install eigen3.pc to build/share instead of build/lib
-# unless this directory is created before Eigen configures.
-ExternalProject_Add_Step(Eigen_pod make_pkgconfig_dir
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig
-  DEPENDERS configure)
-
-
-# This can be removed after CRL updates their install rule for libmultisense
-ExternalProject_Add_Step(libmultisense copy_include_dir
-  COMMAND cp -r ${DRCExternals_SOURCE_DIR}/libmultisense/source/LibMultiSense/details ${CMAKE_INSTALL_PREFIX}/include/MultiSense/
-  DEPENDEES install)
-
-# Install Drake CMake scripts repository
-set(cmake_GIT_TAG 2c4ee11aa719ad548df7eaf3c7047a98c6e3e01c)
-ExternalProject_Add(drake-cmake
-  GIT_REPOSITORY https://github.com/RobotLocomotion/cmake.git
-  GIT_TAG ${cmake_GIT_TAG}
-  SOURCE_DIR ${DRCExternals_SOURCE_DIR}/../drake/drake/cmake
-	CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  )
