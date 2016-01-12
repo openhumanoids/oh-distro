c36b7618fa52fbcf2dec121079040a8d8bbfaecf
Wolfgang Merkt <w.merkt@gmail.com>
2016-01-12 00:07:27 +0000

    Cleanup

--- software/drivers/multisense/utils/src/multisense-glview/main.c
+++ software/drivers/multisense/utils/src/multisense-glview/main.c
@@ -74,44 +74,44 @@ void DrawGLScene()
   if (status > 0) {
     lcm_handle(lcm);
   }
-  
+
   pthread_mutex_lock( &mutex1 );
-  
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
-  
+
   glEnable(GL_TEXTURE_2D);
-  
+
   glBindTexture(GL_TEXTURE_2D, gl_leftpane_tex);
-  if ((left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) || 
-      (left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG )){ 
+  if ((left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) ||
+      (left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG )){
     glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, left_img_data);
   }else if (left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_GRAY){
-    glTexImage2D(GL_TEXTURE_2D, 0, 1, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, left_img_data);  
+    glTexImage2D(GL_TEXTURE_2D, 0, 1, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, left_img_data);
   }
-  
+
   glBegin(GL_TRIANGLE_FAN);
   glColor4f(255.0f, 255.0f, 255.0f, 255.0f);
   glTexCoord2f(0, 0); glVertex3f(0,0,0);
   glTexCoord2f(1, 0); glVertex3f(width,0,0);
   glTexCoord2f(1, 1); glVertex3f(width,height,0);
   glTexCoord2f(0, 1); glVertex3f(0,height,0);
-  glEnd();  
-  
+  glEnd();
+
   glBindTexture(GL_TEXTURE_2D, gl_rightpane_tex);
   if (rightpane_is_depth){
   glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, depth_img_data);
   } else{
 
-    if ((right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) || 
-        (right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG )){ 
+    if ((right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) ||
+        (right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_MJPEG )){
       glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, right_img_data);
     }else if (right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_GRAY){
-      glTexImage2D(GL_TEXTURE_2D, 0, 1, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, right_img_data);  
-    }    
-    
+      glTexImage2D(GL_TEXTURE_2D, 0, 1, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, right_img_data);
+    }
+
   }
-	
+
   glBegin(GL_TRIANGLE_FAN);
   glColor4f(255.0f, 255.0f, 255.0f, 255.0f);
   glTexCoord2f(0, 0); glVertex3f(width,0,0);
@@ -119,9 +119,9 @@ void DrawGLScene()
   glTexCoord2f(1, 1); glVertex3f(width*2,height,0);
   glTexCoord2f(0, 1); glVertex3f(width,height,0);
   glEnd();
-  
+
   glutSwapBuffers();
-  
+
   pthread_mutex_unlock( &mutex1 );
 }
 
@@ -180,7 +180,6 @@ on_frame(const lcm_recv_buf_t* lcm, const char* channel, const multisense_images
   height = msg->images[0].height;
 
   if(msg->images[0].size) {
-    //memcpy(left_img_data, msg->images[0].data, msg->images[0].size * sizeof(uint8_t));
     left_color_format =  msg->images[0].pixelformat;
     if(left_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) {
       memcpy(left_img_data, msg->images[0].data, width * height * 3);
@@ -196,7 +195,7 @@ on_frame(const lcm_recv_buf_t* lcm, const char* channel, const multisense_images
 
   if( msg->image_types[1] == MULTISENSE_IMAGES_T_RIGHT ){
     rightpane_is_depth = 0;
-    
+
     right_color_format =  msg->images[1].pixelformat;
     if(right_color_format == BOT_CORE_IMAGE_T_PIXEL_FORMAT_RGB) {
       memcpy(right_img_data, msg->images[1].data, width * height * 3);
@@ -207,12 +206,12 @@ on_frame(const lcm_recv_buf_t* lcm, const char* channel, const multisense_images
       memcpy(right_img_data, msg->images[1].data, width * height);
     }else{
       printf("Second Image Format Not Understood\n");
-    }    
-    
+    }
+
   }else if ( (msg->image_types[1] == MULTISENSE_IMAGES_T_DISPARITY || msg->image_types[1] == MULTISENSE_IMAGES_T_DISPARITY_ZIPPED) ||
              (msg->image_types[1] == MULTISENSE_IMAGES_T_DEPTH_MM  || msg->image_types[1] == MULTISENSE_IMAGES_T_DEPTH_MM_ZIPPED) ){
     rightpane_is_depth = 1;
-    
+
     int i;
     const uint16_t* depth = NULL;
     int scaling=10;
@@ -286,13 +285,12 @@ on_frame(const lcm_recv_buf_t* lcm, const char* channel, const multisense_images
         break;
       }
     }
-    
+
     row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
     for ( i = 0; i < height; i++ ) {
       row_pointers[i] = (png_bytep) malloc(3*width*2);
       memcpy(row_pointers[i] + 0*3*width, depth_img_data + i*3*width, width*3);
       memcpy(row_pointers[i] + 1*3*width, depth_img_data + i*3*width, width*3);
-      //row_pointers[i] = depth_img_data + (i*3*width);
     }
     if ( doOutput ) {
       char filename[1024];
@@ -306,7 +304,6 @@ on_frame(const lcm_recv_buf_t* lcm, const char* channel, const multisense_images
     free(row_pointers);
 
     pthread_mutex_unlock( &mutex1 );
-    //exit(-1);
   }else{
       printf("Second Image Format Not Understood [C]\n");
   }
@@ -357,7 +354,7 @@ void write_png_file(char* file_name, int width, int height, png_bytep* row_point
   png_infop info_ptr = png_create_info_struct(png_ptr);
   if (!info_ptr)
     abort_("[write_png_file] png_create_info_struct failed");
-  
+
   if (setjmp(png_jmpbuf(png_ptr)))
     abort_("[write_png_file] Error during init_io");
 
@@ -367,33 +364,33 @@ void write_png_file(char* file_name, int width, int height, png_bytep* row_point
   /* write header */
   if (setjmp(png_jmpbuf(png_ptr)))
     abort_("[write_png_file] Error during writing header");
-  
+
   png_set_IHDR(png_ptr, info_ptr, width, height,
 	       bit_depth, color_type, PNG_INTERLACE_NONE,
 	       PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-  
+
   png_write_info(png_ptr, info_ptr);
-  
-  
+
+
   /* write bytes */
   if (setjmp(png_jmpbuf(png_ptr)))
     abort_("[write_png_file] Error during writing bytes");
-  
+
   png_write_image(png_ptr, row_pointers);
-  
-  
+
+
   /* end write */
   if (setjmp(png_jmpbuf(png_ptr)))
     abort_("[write_png_file] Error during end of write");
-  
+
   png_write_end(png_ptr, NULL);
-  
+
   /*
   for (y=0; y<height; y++)
     free(row_pointers[y]);
   free(row_pointers);
 */
-  
+
   fclose(fp);
 }
 
@@ -433,32 +430,32 @@ int main(int argc, char **argv)
       usage(argv[0]);
     }
   }
-  lcm = lcm_create(lcm_url);  
-  
+  lcm = lcm_create(lcm_url);
+
   int npixels = width*height;
-  
+
   depth_img_data = malloc(npixels*3);
   depth_uncompress_buffer = malloc(npixels*sizeof(uint16_t));
   left_img_data = malloc(npixels*3);
   right_img_data = malloc(npixels*3);
   rightpane_is_depth = 0;
-  
+
   int i;
   for (i=0; i<DEPTH_VAL; i++) {
     float v = i/(float)DEPTH_VAL;
     v = powf(v, 3)* 6;
     t_gamma[i] = v*6*256;
   }
-  
+
   glutInit(&argc, argv);
-  
+
   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
   //glutInitWindowSize(width*2, height);
   glutInitWindowSize(width, height/2);
   glutInitWindowPosition(0, 0);
-  
+
   window = glutCreateWindow("Multisense LCM viewer");
-  
+
   glutDisplayFunc(&DrawGLScene);
   glutIdleFunc(&DrawGLScene);
   glutReshapeFunc(&ReSizeGLScene);
@@ -466,15 +463,15 @@ int main(int argc, char **argv)
 
   InitGL(width*2, height);
 
-  
-  multisense_images_t_subscribe(lcm, channelName, on_frame, NULL);
-  
-  glutMainLoop();
 
+  multisense_images_t_subscribe(lcm, channelName, on_frame, NULL);
+
+
+  glutMainLoop();
   free(depth_img_data);
   free(depth_uncompress_buffer);
   free(left_img_data);
   lcm_destroy(lcm);
-  
+
   return 0;
 }
