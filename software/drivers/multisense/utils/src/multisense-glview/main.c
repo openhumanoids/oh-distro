cf81f1f3c91484997db51348c75f2264078c8bad
Wolfgang Merkt <w.merkt@gmail.com>
2016-01-12 00:04:11 +0000

    Rotate img

--- software/drivers/multisense/utils/src/multisense-glview/main.c
+++ software/drivers/multisense/utils/src/multisense-glview/main.c
@@ -150,6 +150,14 @@ void InitGL(int Width, int Height)
 	glEnable(GL_BLEND);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glShadeModel(GL_SMOOTH);
+
+  if (rotateImg) {
+    glMatrixMode(GL_TEXTURE);
+    glTranslatef(1.0f, 0.0f, 0.0f);
+    glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
+    glMatrixMode(GL_MODELVIEW);
+  }
+
 	glGenTextures(1, &gl_rightpane_tex);
 	glBindTexture(GL_TEXTURE_2D, gl_rightpane_tex);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
