5f7dd6f918f89ffc05fd51c60f85649f4fd24561
simalpha <nobili.simona@gmail.com>
2016-02-03 16:16:45 +0000

    Fixed issues for running multisense and visualization in ui.

---
+++ software/utils/drc_utils/python/send_rotate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os,sys
+import time
+import datetime
+import subprocess
+
+home_dir =os.getenv("HOME")
+#print home_dir
+sys.path.append(home_dir + "/drc/software/build/lib/python2.7/site-packages")
+sys.path.append(home_dir + "/drc/software/build/lib/python2.7/dist-packages")
+
+import lcm
+import multisense as lcmmultisense
+
+print "drc-send-rotate: "
+
+print "Waiting 5 seconds..."
+lc = lcm.LCM()
+time.sleep(5)
+msg = lcmmultisense.command_t()
+msg.utime = time.time () * 1000000
+msg.fps = 15
+msg.gain = -1
+msg.exposure_us = 10000
+msg.agc = -1
+msg.rpm = 5
+msg.leds_flash = False
+msg.leds_duty_cycle = 0
+lc.publish("MULTISENSE_COMMAND", msg.encode())
+print "Publishing Multisense command to spin at 5rpm!"
+
