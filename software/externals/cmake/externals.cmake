72651e49892dc68e86446b9b8565e3ac47099c7d
Gregory Izatt <gizatt@mit.edu>
2016-03-09 15:47:32 -0500

    Switch kinematics utils to one not requiring eigen

--- software/externals/cmake/externals.cmake
+++ software/externals/cmake/externals.cmake
@@ -165,8 +165,8 @@ set(pypolyhedron_url https://github.com/rdeits/pypolyhedron.git)
 set(pypolyhedron_revision 1f110addf89398f62644830bf69a69930db8c4d0)
 set(pypolyhedron_depends)
 
-set(kinematics-utils_url https://github.com/ipab-slmc/kinematics-utils.git)
-set(kinematics-utils_revision 062e36b056eaa0b5ddc38ed7c738999b9bb5831b)
+set(kinematics-utils_url https://github.com/gizatt/kinematics-utils.git)
+set(kinematics-utils_revision 84328b5aecac406fe7022c4d097bd266dfd0dcb3)
 set(kinematics-utils_depends Eigen_pod)
 
 set(libmultisense_url https://bitbucket.org/crl/libmultisense)
