6c5af3a9c6a28122836e11e9df7f826b9d212476
Wolfgang Merkt <w.merkt@gmail.com>
2016-04-18 12:54:12 +0100

    valkyriedriver: park neck, preliminary hand integration

--- software/modules/ohdirector/valkyriedriver.py
+++ software/modules/ohdirector/valkyriedriver.py
@@ -16,7 +16,7 @@ from director.utime import getUtime
 import time
 
 import drc as lcmdrc
-import bot_core
+import bot_core as lcmbotcore
 
 
 class ValkyrieDriver(object):
@@ -24,15 +24,30 @@ class ValkyrieDriver(object):
     def __init__(self, ikPlanner):
         self.ikPlanner = ikPlanner
 
-    #    #self._setupSubscriptions()
-
-    #def _setupSubscriptions(self):
-
     def sendWholeBodyCommand(self, wholeBodyMode):
         msg = lcmdrc.int64_stamped_t()
         msg.utime = getUtime()
         msg.data = wholeBodyMode
         lcmUtils.publish('IHMC_CONTROL_MODE_COMMAND', msg)
+
+    def sendParkNeckCommand(self):
+        msg = lcmbotcore.joint_angles_t()
+        msg.utime = getUtime()
+        msg.joint_name = ["lowerNeckPitch", "neckYaw", "upperNeckPitch"]
+        msg.joint_position = [0] * len(msg.joint_name)
+        msg.num_joints = len(msg.joint_name)
+        lcmUtils.publish("DESIRED_NECK_ANGLES", msg)
+
+    def sendHandCommand(self, side="right"):
+        assert side in ["left", "right"]
+
+        msg = lcmbotcore.joint_angles_t()
+        msg.joint_name = [ side + "IndexFingerMotorPitch1", side + "MiddleFingerMotorPitch1", side + "PinkyMotorPitch1", side + "ThumbMotorPitch1", side + "ThumbMotorPitch2", side + "ThumbMotorRoll"]
+        msg.num_joints = len(msg.joint_name)
+        msg.joint_position = [0, 0, 0, 0, 0, 0] #1]
+
+        lcmUtils.publish("DESIRED_HAND_ANGLES", msg)
+
 
 def init(ikPlanner):
 
