012c796c12d17a595a8c0582280fe07388fca9f3
Wolfgang Merkt <w.merkt@gmail.com>
2016-04-20 20:12:25 +0100

    Relay wrist commands should relay left instead of right

--- software/modules/wrist-control/relay-wrist-commands.py
+++ software/modules/wrist-control/relay-wrist-commands.py
@@ -11,24 +11,24 @@ def onRobotPlan(channel, data):
     msg = lcmdrc.robot_plan_t.decode(data)
     plan = msg.plan[-1] # last pose
 
-    rightWristRollIndex = plan.joint_name.index("rightWristRoll")
-    rightWristPitchIndex = plan.joint_name.index("rightWristPitch")
-    rightForearmYawIndex = plan.joint_name.index("rightForearmYaw")
+    leftWristRollIndex = plan.joint_name.index("leftWristRoll")
+    leftWristPitchIndex = plan.joint_name.index("leftWristPitch")
+    leftForearmYawIndex = plan.joint_name.index("leftForearmYaw")
 
-    rightWristRollRad = plan.joint_position[rightWristRollIndex]
-    rightWristPitchRad = plan.joint_position[rightWristPitchIndex]
-    rightForearmYawRad = plan.joint_position[rightForearmYawIndex]
+    leftWristRollRad = plan.joint_position[leftWristRollIndex]
+    leftWristPitchRad = plan.joint_position[leftWristPitchIndex]
+    leftForearmYawRad = plan.joint_position[leftForearmYawIndex]
 
-    print "Commanding forearm yaw to " + str(rightForearmYawRad)
-    print "Commanding wrist roll to " + str(rightWristRollRad)
-    print "Commanding wrist pitch to " + str(rightWristPitchRad)
-    sendWristCommand(msg.utime, rightForearmYawRad, rightWristRollRad, rightWristPitchRad)
+    print "Commanding forearm yaw to " + str(leftForearmYawRad)
+    print "Commanding wrist roll to " + str(leftWristRollRad)
+    print "Commanding wrist pitch to " + str(leftWristPitchRad)
+    sendWristCommand(msg.utime, leftForearmYawRad, leftWristRollRad, leftWristPitchRad)
 
 
 def sendWristCommand(utime, forearmYaw, roll, pitch):
 	msg = lcmbotcore.joint_angles_t()
 	msg.utime = utime
-	msg.joint_name = [ "rightForearmYaw", "rightWristRoll", "rightWristPitch" ]
+	msg.joint_name = [ "leftForearmYaw", "leftWristRoll", "leftWristPitch" ]
 	msg.num_joints = len(msg.joint_name)
 	msg.joint_position = [ forearmYaw, roll, pitch ]
 
