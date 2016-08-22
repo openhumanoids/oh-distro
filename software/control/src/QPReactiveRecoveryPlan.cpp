1d3947bf2eb164a103d1c4337e151cfb94815b3d
Robin Deits <robin.deits@gmail.com>
2016-08-22 10:57:47 -0400

    replace deprecated IRIS C header with new C++ header

--- software/control/src/QPReactiveRecoveryPlan.cpp
+++ software/control/src/QPReactiveRecoveryPlan.cpp
@@ -6,9 +6,7 @@
 #include "drake/util/lcmUtil.h"
 #include "drake/core/Gradient.h"
 #include "lcmtypes/drc/reactive_recovery_debug_t.hpp"
-extern "C" {
-  #include "iris/solver.h"
-}
+#include "iris/cvxgen_ldp.h"
 
 #define CVXGEN_MAX_ROWS 3
 #define CVXGEN_MAX_PTS 8
@@ -17,17 +15,9 @@ extern "C" {
 
 using namespace Eigen;
 
-Vars vars;
-Params params;
-Workspace work;
-Settings settings;
-
 VectorXd QPReactiveRecoveryPlan::closestPointInConvexHull(const Ref<const VectorXd> &x, const Ref<const MatrixXd> &V) {
 
   int dim = x.size();
-
-  Matrix<double, CVXGEN_MAX_ROWS, 1> x_resized;
-  Matrix<double, CVXGEN_MAX_ROWS, CVXGEN_MAX_PTS> V_resized;
 
   if (dim > CVXGEN_MAX_ROWS) {
     fprintf(stderr, "x can not be more than %d elements\n", CVXGEN_MAX_ROWS);
@@ -42,43 +32,9 @@ VectorXd QPReactiveRecoveryPlan::closestPointInConvexHull(const Ref<const Vector
     exit(1);
   }
 
-  x_resized.head(dim) = x;
-  V_resized.block(0, 0, dim, V.cols()) = V;
-
-  if (dim < CVXGEN_MAX_ROWS) {
-    x_resized.tail(CVXGEN_MAX_ROWS - dim) = VectorXd::Zero(CVXGEN_MAX_ROWS-dim);
-    V_resized.block(dim, 0, CVXGEN_MAX_ROWS - dim, V_resized.cols()) = MatrixXd::Zero(CVXGEN_MAX_ROWS-dim, V_resized.cols());
-  }
-
-  for (int i=V.cols(); i < CVXGEN_MAX_PTS; i++) {
-    for (int j=0; j < CVXGEN_MAX_ROWS; j++) {
-      V_resized(j,i) = V_resized(j,i-1);
-    }
-  }
-
-  for (int j=0; j < CVXGEN_MAX_PTS; j++) {
-    V_resized.col(j) = V_resized.col(j) - x_resized;
-  }
-
-  set_defaults();
-  setup_indexing();
-  settings.verbose = 0;
-
-
-  double *src = V_resized.data();
-  double *dest = params.Y;
-  for (int i=0; i < CVXGEN_MAX_ROWS * CVXGEN_MAX_PTS; i++) {
-    *dest++ = *src++;
-  }
-
-  solve();
-
-
-  Map<VectorXd>y(vars.v, CVXGEN_MAX_ROWS);
-  Map<VectorXd>w(vars.w, CVXGEN_MAX_PTS);
-
-  y.head(dim) = y.head(dim) + x.head(dim);
-  return y.head(dim);
+  VectorXd result(dim);
+  iris_cvxgen::closest_point_in_convex_hull(V.colwise() - x, result);
+  return result;
 }
 
 Isometry3d QPReactiveRecoveryPlan::closestPoseInConvexHull(const Isometry3d &pose, const Ref<const MatrixXd> &V) {
